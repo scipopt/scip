c3884e4c039fda4544f92c354807ef5372318dbe
Tobias Achterberg <achterberg@zib.de>
2003-10-16 13:20:09 +0000

    *** empty log message ***

--- zippack.sh
+++ zippack.sh
@@ -1,1 +1,4 @@
 zip scip050 INSTALL Makefile doc/* lib lint/* lint/cpp/* lint/posix/* lint/posix/sys/* make/* scip.set src/* -x ".*" -x "*~" -x "*/CVS/*"
+echo "check version numbers in src/def.h, doc/xternal.c and zippack.sh:"
+grep "SCIP_VERSION" src/def.h
+grep "@version" doc/xternal.c
