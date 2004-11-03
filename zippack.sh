bd8cc1a795c11a0cb98bb5a7ef48398e444c671b
Tobias Achterberg <achterberg@zib.de>
2004-11-03 17:00:27 +0000

    - the file names in the archive file are now preceeded with a directory "scip-<version>/" (zippack.sh)

--- zippack.sh
+++ zippack.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-NAME=scip076a
+VERSION="0.76a"
+NAME="scip-$VERSION"
 cd ..
 ln -s scip $NAME
-zip scip/release/$NAME $NAME/COPYING $NAME/INSTALL $NAME/CHANGELOG $NAME/Makefile $NAME/doc/* $NAME/lib $NAME/lint/* $NAME/lint/cpp/* $NAME/lint/posix/* $NAME/lint/posix/sys/* $NAME/make/* $NAME/scip.set $NAME/src/* -x ".*" -x "*~" -x "*/CVS/*"
+zip scip/release/$NAME.zip $NAME/COPYING $NAME/INSTALL $NAME/CHANGELOG $NAME/Makefile $NAME/doc/* $NAME/lib $NAME/lint/* $NAME/lint/cpp/* $NAME/lint/posix/* $NAME/lint/posix/sys/* $NAME/make/* $NAME/scip.set $NAME/src/* -x ".*" -x "*~" -x "*/CVS/*"
 rm $NAME
 cd scip
-echo "check version numbers in src/def.h, doc/xternal.c and zippack.sh ($NAME):"
+echo "check version numbers in src/def.h, doc/xternal.c and zippack.sh ($VERSION):"
 grep "SCIP_VERSION" src/def.h
 grep "@version" doc/xternal.c
