0086a4f68b137ab038a507338ce43911a85b5455
Tobias Achterberg <achterberg@zib.de>
2005-05-03 08:57:39 +0000

    - switched to version 0.79 - fixed zippack.sh

--- zippack.sh
+++ zippack.sh
@@ -5,7 +5,7 @@ cd ..
 ln -s scip $NAME
 zip scip/release/$NAME.zip $NAME/COPYING $NAME/INSTALL $NAME/CHANGELOG $NAME/Makefile $NAME/doc/* $NAME/lib \
 $NAME/lint/* $NAME/lint/cpp/* $NAME/lint/posix/* $NAME/lint/posix/sys/* $NAME/make/* $NAME/scip.set \
-$NAME/src/* $NAME/src/scip/* $NAME/src/objscip/* $NAME/src/tclique/* \
+$NAME/src/* $NAME/src/scip/* $NAME/src/objscip/* \
 $NAME/examples/* \
 $NAME/examples/MIPSolver/* $NAME/examples/MIPSolver/doc/* $NAME/examples/MIPSolver/lib \
 $NAME/examples/MIPSolver/lint/* $NAME/examples/MIPSolver/lint/cpp/* $NAME/examples/MIPSolver/lint/posix/* \
@@ -16,6 +16,6 @@ $NAME/examples/TSP/lint/posix/sys/* $NAME/examples/TSP/make/* $NAME/examples/TSP
 -x ".*" -x "*~" -x "*/CVS/*"
 rm $NAME
 cd scip
-echo "check version numbers in src/def.h, doc/xternal.c and zippack.sh ($VERSION):"
-grep "SCIP_VERSION" src/def.h
+echo "check version numbers in src/scip/def.h, doc/xternal.c and zippack.sh ($VERSION):"
+grep "SCIP_VERSION" src/scip/def.h
 grep "@version" doc/xternal.c
