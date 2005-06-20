0390b0e1f6ac838d9839ed8832f0d9e8e69a3cd7
Tobias Achterberg <achterberg@zib.de>
2005-06-20 14:56:32 +0000

    *** empty log message ***

--- zippack.sh
+++ zippack.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 VERSION="0.79b"
 NAME="scip-$VERSION"
-cd ..
-ln -s scip $NAME
-zip scip/release/$NAME.zip $NAME/COPYING $NAME/INSTALL $NAME/CHANGELOG $NAME/Makefile $NAME/doc/* $NAME/lib \
+ln -s . $NAME
+zip release/$NAME.zip $NAME/COPYING $NAME/INSTALL $NAME/CHANGELOG $NAME/Makefile $NAME/doc/* $NAME/lib \
 $NAME/lint/* $NAME/lint/cpp/* $NAME/lint/posix/* $NAME/lint/posix/sys/* $NAME/make/* $NAME/scip.set \
 $NAME/src/* $NAME/src/scip/* $NAME/src/objscip/* \
 $NAME/examples/* \
@@ -15,7 +14,6 @@ $NAME/examples/TSP/lint/* $NAME/examples/TSP/lint/cpp/* $NAME/examples/TSP/lint/
 $NAME/examples/TSP/lint/posix/sys/* $NAME/examples/TSP/make/* $NAME/examples/TSP/src/* $NAME/examples/TSP/tspviewer/* \
 -x ".*" -x "*~" -x "*/CVS/*"
 rm $NAME
-cd scip
 echo "check version numbers in src/scip/def.h, doc/xternal.c and zippack.sh ($VERSION):"
 grep "VERSION" src/scip/def.h
 grep "@version" doc/xternal.c
