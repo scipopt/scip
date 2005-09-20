6934c8bb94f1c9e0640c05466982a46daeb7a929
Tobias Achterberg <achterberg@zib.de>
2005-09-20 13:46:03 +0000

    - switched to version 0.80

--- doc/xternal.c
+++ doc/xternal.c
@@ -23,7 +23,7 @@
 /*--+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
 
 /**@mainpage SCIP (Solving Constraint Integer Programs)
- * @version  0.79g
+ * @version  0.80
  * @author   Tobias Achterberg
  * @author   Timo Berthold
  * @author   Thorsten Koch
--- zippack.sh
+++ zippack.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-VERSION="0.79g"
+VERSION="0.80"
 NAME="scip-$VERSION"
 ln -s . $NAME
 zip release/$NAME.zip $NAME/COPYING $NAME/INSTALL $NAME/CHANGELOG $NAME/Makefile $NAME/doc/* $NAME/lib \
