d394e0f547dd6365d920758511fc89019bf52a6d
Tobias Achterberg <achterberg@zib.de>
2006-02-23 12:41:21 +0000

    interface: - new methods SCIPcolGetMinPrimsol() and SCIPcolGetMaxPrimsol() - new method SCIPgetColRedcost() - new method SCIPvarGetRootRedcost() - new methods SCIPgetLPRootObjval(), SCIPgetLPRootColumnObjval(), and SCIPgetLPRootLooseObjval() - new methods SCIPchgVarLbRoot() and SCIPchgVarUbRoot() - new method SCIPinRepropagation() - new methods SCIPgetAvgPseudocostScore(), SCIPgetAvgPseudocostScoreCurrentRun(), SCIPgetAvgConflictScore(),   SCIPgetAvgConflictScoreCurrentRun(), SCIPgetAvgInferenceScore(), SCIPgetAvgInferenceScoreCurrentRun(),   SCIPgetAvgCutoffScore(), and SCIPgetAvgCutoffScoreCurrentRun() - new methods SCIPaddDialogInputLine() and SCIPaddDialogHistoryLine()
    plugins:
    - new primal heuristic "shifting"
    - new separator "redcost" which replaces the internal reduced cost strengthening
    - new propagator "rootredcost" which applies reduced cost fixing at the root node whenever a best new primal solution
      was found
    
    parameters:
    - new parameter conflict/lpiterations
    - removed parameter propagating/redcostfreq, because reduced cost strengthening is now an external separator plugin
    - new parameters branching/relpscost/conflictweight, branching/relpscost/inferenceweight,
      branching/relpscost/cutoffweight, and branching/relpscost/pscostweight
    - new parameter conflict/settlelocal
    
    features:
    - slightly changed priorities of constraint handlers
    - command line syntax changed to support batch modus without piping stdin with "<" or "|" operators
    
    fixed bugs:
    - fixed bug in crossover heuristic with negative timelimit
    - fixed bugs resulting from inactive general integer variables being member of the variable bounds array of a variable

---
+++ src/scip/prop_rootredcost.c
@@ -0,0 +1,284 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the program and library             */
+/*         SCIP --- Solving Constraint Integer Programs                      */
+/*                                                                           */
+/*    Copyright (C) 2002-2006 Tobias Achterberg                              */
+/*                                                                           */
+/*                  2002-2006 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SCIP is distributed under the terms of the ZIB Academic License.         */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+#pragma ident "@(#) $Id: prop_rootredcost.c,v 1.1 2006/02/23 12:41:21 bzfpfend Exp $"
+
+/**@file   prop_rootredcost.c
+ * @brief  reduced cost strengthening at the root node
+ * @author Tobias Achterberg
+ */
+
+/*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
+
+#include <assert.h>
+
+#include "scip/prop_rootredcost.h"
+
+
+#define PROP_NAME              "rootredcost"
+#define PROP_DESC              "reduced cost strengthening at the root node"
+#define PROP_PRIORITY          +1000000
+#define PROP_FREQ                     1
+#define PROP_DELAY                FALSE /**< should propagation method be delayed, if other propagators found reductions? */
+
+
+
+
+/*
+ * Data structures
+ */
+
+/** propagator data */
+struct SCIP_PropData
+{
+   SCIP_Real             lastcutoffbound;    /**< cutoff bound for which the root reduced costs were already processed */
+};
+
+
+
+
+/*
+ * Callback methods of propagator
+ */
+
+/** destructor of propagator to free user data (called when SCIP is exiting) */
+static
+SCIP_DECL_PROPFREE(propFreeRootredcost)
+{  /*lint --e{715}*/
+   SCIP_PROPDATA* propdata;
+
+   /* free propagator data */
+   propdata = SCIPpropGetData(prop);
+   SCIPfreeMemory(scip, &propdata);
+   SCIPpropSetData(prop, NULL);
+
+   return SCIP_OKAY;
+}
+
+
+/** initialization method of propagator (called after problem was transformed) */
+#define propInitRootredcost NULL
+
+
+/** deinitialization method of propagator (called before transformed problem is freed) */
+#define propExitRootredcost NULL
+
+
+/** solving process initialization method of propagator (called when branch and bound process is about to begin) */
+static
+SCIP_DECL_PROPINITSOL(propInitsolRootredcost)
+{  /*lint --e{715}*/
+   SCIP_PROPDATA* propdata;
+
+   propdata = SCIPpropGetData(prop);
+   assert(propdata != NULL);
+
+   propdata->lastcutoffbound = SCIPinfinity(scip);
+
+   return SCIP_OKAY;
+}
+
+
+/** solving process deinitialization method of propagator (called before branch and bound process data is freed) */
+#define propExitsolRootredcost NULL
+
+
+/** execution method of propagator */
+static
+SCIP_DECL_PROPEXEC(propExecRootredcost)
+{  /*lint --e{715}*/
+   SCIP_PROPDATA* propdata;
+   SCIP_VAR** vars;
+   SCIP_Real cutoffbound;
+   SCIP_Real lpobjval;
+   SCIP_Bool root;
+   int nvars;
+   int v;
+
+   *result = SCIP_DIDNOTRUN;
+
+   /* propagator can only be applied during solving stage */
+   if( SCIPgetStage(scip) != SCIP_STAGE_SOLVING )
+      return SCIP_OKAY;
+
+   /* get propagator data */
+   propdata = SCIPpropGetData(prop);
+   assert(propdata != NULL);
+
+   /* get current cutoff bound */
+   cutoffbound = SCIPgetCutoffbound(scip);
+   assert(cutoffbound <= propdata->lastcutoffbound);
+   if( cutoffbound == propdata->lastcutoffbound ) /*lint !e777*/
+      return SCIP_OKAY;
+
+   propdata->lastcutoffbound = cutoffbound;
+
+   /* reduced cost strengthening can only be applied, if we have a finite upper bound on the LP value */
+   if( SCIPisInfinity(scip, cutoffbound) )
+      return SCIP_OKAY;
+
+   /* get root LP objective value */
+   lpobjval = SCIPgetLPRootObjval(scip);
+   if( lpobjval == SCIP_INVALID )
+      return SCIP_OKAY;
+
+   /* get variables */
+   vars = SCIPgetVars(scip);
+   nvars = SCIPgetNVars(scip);
+   if( nvars == 0 )
+      return SCIP_OKAY;
+
+   SCIPdebugMessage("searching for root node reduced cost fixings due to new cutoffbound %g\n", cutoffbound);
+
+   *result = SCIP_DIDNOTFIND;
+   root = (SCIPgetDepth(scip) == 0);
+
+   /* check reduced costs for variables that were columns of the root LP */
+   for( v = 0; v < nvars; ++v )
+   {
+      SCIP_VAR* var;
+      SCIP_Real redcost;
+
+      var = vars[v];
+      redcost = SCIPvarGetRootRedcost(var);
+      if( redcost == SCIP_INVALID )
+         continue;
+
+      if( SCIPisFeasPositive(scip, redcost) )
+      {
+         SCIP_Real rootsol;
+         SCIP_Real oldlb;
+         SCIP_Real oldub;
+         SCIP_Real newub;
+         SCIP_Bool strengthen;
+
+         rootsol = SCIPvarGetRootSol(var);
+         oldlb = SCIPvarGetLbGlobal(var);
+         oldub = SCIPvarGetUbGlobal(var);
+         assert(SCIPisLE(scip, rootsol, SCIPvarGetLbGlobal(var))); /* lb might have been increased in the meantime */
+
+         /* calculate reduced cost based bound */
+         newub = (cutoffbound - lpobjval) / redcost + rootsol;
+
+         /* check, if new bound is good enough:
+          *  - in the root node: strengthenings lead to a resolve
+          *     - integer variables: take all possible strengthenings
+          *     - continuous variables: strengthening must cut at least 20% of the current domain
+          *  - at other nodes: strengthening is applied only once, because the root LP is not resolved
+          *     - take all possible strengthenings
+          */
+         if( SCIPvarGetType(var) != SCIP_VARTYPE_CONTINUOUS )
+         {
+            newub = SCIPadjustedVarUb(scip, var, newub);
+            strengthen = (newub < oldub - 0.5);
+         }
+         else if( root )
+            strengthen = SCIPisUbBetter(scip, newub, oldub);
+         else
+            strengthen = (newub <= 0.2 * oldlb + 0.8 * oldub);
+
+         if( strengthen )
+         {
+            /* strengthen upper bound */
+            SCIPdebugMessage("root redcost strengthening upper bound: <%s> [%g,%g] -> [%g,%g] (ub=%g, lb=%g, redcost=%g)\n",
+               SCIPvarGetName(var), oldlb, oldub, oldlb, newub, cutoffbound, lpobjval, redcost);
+            SCIP_CALL( SCIPchgVarUbGlobal(scip, var, newub) );
+            *result = SCIP_REDUCEDDOM;
+         }
+      }
+      else if( SCIPisFeasNegative(scip, redcost) )
+      {
+         SCIP_Real rootsol;
+         SCIP_Real oldlb;
+         SCIP_Real oldub;
+         SCIP_Real newlb;
+         SCIP_Bool strengthen;
+
+         rootsol = SCIPvarGetRootSol(var);
+         oldlb = SCIPvarGetLbGlobal(var);
+         oldub = SCIPvarGetUbGlobal(var);
+         assert(SCIPisGE(scip, rootsol, SCIPvarGetUbGlobal(var))); /* ub might have been decreased in the meantime */
+
+         /* calculate reduced cost based bound */
+         newlb = (cutoffbound - lpobjval) / redcost + rootsol;
+
+         /* check, if new bound is good enough:
+          *  - in the root node: strengthenings lead to a resolve
+          *     - integer variables: take all possible strengthenings
+          *     - continuous variables: strengthening must cut at least 20% of the current domain
+          *  - at other nodes: strengthening is applied only once, because the root LP is not resolved
+          *     - take all possible strengthenings
+          */
+         if( SCIPvarGetType(var) != SCIP_VARTYPE_CONTINUOUS )
+         {
+            newlb = SCIPadjustedVarLb(scip, var, newlb);
+            strengthen = (newlb > oldlb + 0.5);
+         }
+         else if( root )
+            strengthen = SCIPisLbBetter(scip, newlb, oldlb);
+         else
+            strengthen = (newlb >= 0.8 * oldlb + 0.2 * oldub);
+
+         if( strengthen )
+         {
+            /* strengthen lower bound */
+            SCIPdebugMessage("root redcost strengthening lower bound: <%s> [%g,%g] -> [%g,%g] (ub=%g, lb=%g, redcost=%g)\n",
+               SCIPvarGetName(var), oldlb, oldub, newlb, oldub, cutoffbound, lpobjval, redcost);
+            SCIP_CALL( SCIPchgVarLbGlobal(scip, var, newlb) );
+            *result = SCIP_REDUCEDDOM;
+         }
+      }
+   }
+
+   return SCIP_OKAY;
+}
+
+
+/** propagation conflict resolving method of propagator */
+static
+SCIP_DECL_PROPRESPROP(propRespropRootredcost)
+{  /*lint --e{715}*/
+   SCIPerrorMessage("cannot resolve root node reduced cost fixings -- should not happen!\n");
+
+   return SCIP_INVALIDCALL;
+}
+
+
+
+
+/*
+ * propagator specific interface methods
+ */
+
+/** creates the root node reduced cost strengthening propagator and includes it in SCIP */
+SCIP_RETCODE SCIPincludePropRootredcost(
+   SCIP*                 scip                /**< SCIP data structure */
+   )
+{
+   SCIP_PROPDATA* propdata;
+
+   /* create rootredcost propagator data */
+   SCIP_CALL( SCIPallocMemory(scip, &propdata) );
+   propdata->lastcutoffbound = SCIPinfinity(scip);
+
+   /* include propagator */
+   SCIP_CALL( SCIPincludeProp(scip, PROP_NAME, PROP_DESC, PROP_PRIORITY, PROP_FREQ, PROP_DELAY,
+         propFreeRootredcost, propInitRootredcost, propExitRootredcost, 
+         propInitsolRootredcost, propExitsolRootredcost, propExecRootredcost, propRespropRootredcost,
+         propdata) );
+
+   return SCIP_OKAY;
+}
---
+++ src/scip/prop_rootredcost.h
@@ -0,0 +1,39 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the program and library             */
+/*         SCIP --- Solving Constraint Integer Programs                      */
+/*                                                                           */
+/*    Copyright (C) 2002-2006 Tobias Achterberg                              */
+/*                                                                           */
+/*                  2002-2006 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SCIP is distributed under the terms of the ZIB Academic License.         */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+#pragma ident "@(#) $Id: prop_rootredcost.h,v 1.1 2006/02/23 12:41:21 bzfpfend Exp $"
+
+/**@file   prop_rootredcost.h
+ * @brief  reduced cost strengthening at the root node
+ * @author Tobias Achterberg
+ */
+
+/*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
+
+#ifndef __SCIP_PROP_ROOTREDCOST_H__
+#define __SCIP_PROP_ROOTREDCOST_H__
+
+
+#include "scip/scip.h"
+
+
+/** creates the root node reduced cost strengthening propagator and includes it in SCIP */
+extern
+SCIP_RETCODE SCIPincludePropRootredcost(
+   SCIP*                 scip                /**< SCIP data structure */
+   );
+
+#endif
