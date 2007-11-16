17e088bb4489cc5d49180b0a4362e0dc6474a022
Marc Pfetsch <m.pfetsch@tu-bs.de>
2007-11-16 20:35:42 +0000

    *** empty log message ***

---
+++ examples/VRP/doc/xternal.c
@@ -0,0 +1,30 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the program and library             */
+/*         SCIP --- Solving Constraint Integer Programs                      */
+/*                                                                           */
+/*    Copyright (C) 2002-2006 Tobias Achterberg                              */
+/*                                                                           */
+/*                  2002-2006 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SCIP is distributed under the terms of the ZIB Academic License.         */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**@file   xternal.c
+ * @brief  main document page
+ * @author Tobias Achterberg
+ */
+
+/*--+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
+
+/**@mainpage SCIP VRP example
+ * @version  0.1
+ * @author   Andreas Bley
+ *
+*/
+
