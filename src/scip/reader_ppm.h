da257b628d9b74811f820947763f92891f298e71
Marc Pfetsch <m.pfetsch@tu-bs.de>
2008-02-04 12:32:20 +0000

    *** empty log message ***

---
+++ src/scip/reader_ppm.h
@@ -0,0 +1,59 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the program and library             */
+/*         SCIP --- Solving Constraint Integer Programs                      */
+/*                                                                           */
+/*    Copyright (C) 2002-2007 Tobias Achterberg                              */
+/*                                                                           */
+/*                  2002-2007 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SCIP is distributed under the terms of the ZIB Academic License.         */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+#pragma ident "@(#) $Id: reader_ppm.h,v 1.1 2008/02/04 12:32:20 bzfpfets Exp $"
+
+/**@file   reader_ppm.h
+ * @brief  PPM file reader
+ * @author Tobias Achterberg
+ * @author Michael Winkler
+ *
+ * Note: This reader can only write a file.
+ *
+ * It produces a picture of the constraint matrix.
+ */
+
+/*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
+
+#ifndef __SCIP_READER_PPM_H__
+#define __SCIP_READER_PPM_H__
+
+
+#include "scip/scip.h"
+
+
+/** includes the ppm file reader into SCIP */
+extern
+SCIP_RETCODE SCIPincludeReaderPpm(
+   SCIP*                 scip                /**< SCIP data structure */
+   );
+
+
+/* writes problem to file */
+extern
+SCIP_RETCODE SCIPwritePpm(
+   SCIP*              scip,               /**< SCIP data structure */
+   FILE*              file,               /**< output file, or NULL if standard output should be used */
+   const char*        name,               /**< problem name */
+   SCIP_Bool          transformed,        /**< TRUE iff problem is the transformed problem */
+   SCIP_VAR**         vars,               /**< array with active variables ordered binary, integer, implicit, continuous */
+   int                nvars,              /**< number of mutable variables in the problem */
+   SCIP_CONS**        conss,              /**< array with constraints of the problem */
+   int                nconss,             /**< number of constraints in the problem */
+   SCIP_RESULT*       result              /**< pointer to store the result of the file writing call */
+   );
+
+#endif
