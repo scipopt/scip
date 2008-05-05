7b7475f54f215d4a6aadaca246392d5d399063a5
Marc Pfetsch <m.pfetsch@tu-bs.de>
2008-05-05 09:35:25 +0000

    - removed inclusion of assert.h

--- src/scip/retcode.c
+++ src/scip/retcode.c
@@ -12,7 +12,7 @@
 /*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      */
 /*                                                                           */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-#pragma ident "@(#) $Id: retcode.c,v 1.25 2008/04/17 17:49:17 bzfpfets Exp $"
+#pragma ident "@(#) $Id: retcode.c,v 1.26 2008/05/05 09:35:25 bzfpfets Exp $"
 
 /**@file   retcode.c
  * @brief  methods for return codes for SCIP methods
@@ -21,7 +21,6 @@
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
 
-#include <assert.h>
 #include <stdio.h>
 
 #include "scip/retcode.h"
