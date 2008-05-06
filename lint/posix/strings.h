4777dc865a275721b65cd4637424507cd2a244b2
Marc Pfetsch <m.pfetsch@tu-bs.de>
2008-05-06 13:45:18 +0000

    minor adaptations to suppress some lint warnings

--- lint/posix/strings.h
+++ lint/posix/strings.h
@@ -12,7 +12,5 @@ void bzero (void *s, size_t n);
 int ffs (int i);
 char *index (const char *s, int c);
 char *rindex (const char *s, int c);
-int strcasecmp (const char *s1, const char *s2);
-int strncasecmp (const char *s1, const char *s2, size_t n);
 
 #endif
