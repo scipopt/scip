f2c98a2c65568d860f752f5147f9b2d4049be879
Stefan Heinz <heinz@zib.de>
2008-09-23 20:14:38 +0000

    - adjust project to interface changes of SCIP

--- examples/MIPSolver/src/cppmain.cpp
+++ examples/MIPSolver/src/cppmain.cpp
@@ -14,7 +14,7 @@
 /*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      */
 /*                                                                           */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-#pragma ident "@(#) $Id: cppmain.cpp,v 1.10 2007/08/22 13:22:36 bzfpfend Exp $"
+#pragma ident "@(#) $Id: cppmain.cpp,v 1.11 2008/09/23 20:14:38 bzfheinz Exp $"
 
 /**@file   cppmain.cpp
  * @brief  main file for C++ example project using SCIP as a callable library
@@ -66,7 +66,7 @@ SCIP_RETCODE fromCommandLine(
 
    std::cout << std::endl << "read problem <" << filename << ">" << std::endl;
    std::cout << "============" << std::endl << std::endl;
-   SCIP_CALL( SCIPreadProb(scip, filename) );
+   SCIP_CALL( SCIPreadProb(scip, filename, NULL) );
 
 
    /*******************
