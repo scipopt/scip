4f873c3dc69e1b95629c3a1f408b1f31ff8c7e6f
Stefan Heinz <heinz@zib.de>
2008-09-29 18:08:57 +0000

    - adjusted make/make.project and examples/*/Makefiles in such a way that is possible to compile   the SCIP examples/* on different architectures (the same as SCIP)

--- examples/makeall.sh
+++ examples/makeall.sh
@@ -6,7 +6,21 @@ do
     cd $i
     make OPT=dbg depend
     make OPT=opt depend
+    make OPT=dbg clean
     make OPT=dbg
+    make OPT=opt clean
     make OPT=opt
+    make OPT=dbg LPS=clp clean
+    make OPT=dbg LPS=clp
+    make OPT=opt LPS=clp clean
+    make OPT=opt LPS=clp
+    make OPT=dbg LPS=cpx clean
+    make OPT=dbg LPS=cpx
+    make OPT=opt LPS=cpx clean
+    make OPT=opt LPS=cpx
+    make OPT=dbg LPS=none clean
+    make OPT=dbg LPS=none
+    make OPT=opt LPS=none clean
+    make OPT=opt LPS=none
     cd -
 done
