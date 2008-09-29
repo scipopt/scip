7802050ddd12d85a5133b7a343fb6df782e6d74c
Gerald Gamrath <gamrath@zib.de>
2008-09-29 10:23:28 +0000

    *** empty log message ***

--- doc/xternal.c
+++ doc/xternal.c
@@ -22,6 +22,7 @@
  * @author Mathias Kinder
  * @author Marc Pfetsch
  * @author Kati Wolter
+ * @author Gerald Gamrath
  */
 
 /*--+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
