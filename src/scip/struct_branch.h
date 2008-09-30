35b659c9ce38e7b5b3e2fc03dc07f4ae2da08aa1
Stefan Heinz <heinz@zib.de>
2008-09-30 11:06:47 +0000

    - fixed Copyright

--- src/scip/struct_branch.h
+++ src/scip/struct_branch.h
@@ -3,6 +3,7 @@
 /*                  This file is part of the program and library             */
 /*         SCIP --- Solving Constraint Integer Programs                      */
 /*                                                                           */
+/*    Copyright (C) 2002-2008 Konrad-Zuse-Zentrum                            */
 /*                            fuer Informationstechnik Berlin                */
 /*                                                                           */
 /*  SCIP is distributed under the terms of the ZIB Academic License.         */
@@ -11,7 +12,7 @@
 /*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      */
 /*                                                                           */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-#pragma ident "@(#) $Id: struct_branch.h,v 1.21 2008/04/17 17:49:19 bzfpfets Exp $"
+#pragma ident "@(#) $Id: struct_branch.h,v 1.22 2008/09/30 11:06:47 bzfheinz Exp $"
 
 /**@file   struct_branch.h
  * @brief  datastructures for branching rules and branching candidate storage
--- src/scip/type_branch.h
+++ src/scip/type_branch.h
@@ -3,6 +3,7 @@
 /*                  This file is part of the program and library             */
 /*         SCIP --- Solving Constraint Integer Programs                      */
 /*                                                                           */
+/*    Copyright (C) 2002-2008 Konrad-Zuse-Zentrum                            */
 /*                            fuer Informationstechnik Berlin                */
 /*                                                                           */
 /*  SCIP is distributed under the terms of the ZIB Academic License.         */
@@ -11,7 +12,7 @@
 /*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      */
 /*                                                                           */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-#pragma ident "@(#) $Id: type_branch.h,v 1.18 2008/04/17 17:49:21 bzfpfets Exp $"
+#pragma ident "@(#) $Id: type_branch.h,v 1.19 2008/09/30 11:06:47 bzfheinz Exp $"
 
 /**@file   type_branch.h
  * @brief  type definitions for branching rules
