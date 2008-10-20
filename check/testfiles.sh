ea92bed838c79fa6bffe9a6b9166117da17ff59a
Marc Pfetsch <m.pfetsch@tu-bs.de>
2008-10-20 14:06:41 +0000

    *** empty log message ***

---
+++ check/testfiles.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+#*                                                                           *
+#*                  This file is part of the program and library             *
+#*         SCIP --- Solving Constraint Integer Programs                      *
+#*                                                                           *
+#*    Copyright (C) 2002-2008 Konrad-Zuse-Zentrum                            *
+#*                            fuer Informationstechnik Berlin                *
+#*                                                                           *
+#*  SCIP is distributed under the terms of the ZIB Academic License.         *
+#*                                                                           *
+#*  You should have received a copy of the ZIB Academic License              *
+#*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      *
+#*                                                                           *
+#* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+# $Id: testfiles.sh,v 1.1 2008/10/20 14:06:41 bzfpfets Exp $
+
+# Test the file names contained in all "*.test" files in the current directory.
+# Output names of files that do not exist.
+
+for j in *.test; do
+  for i in `cat $j`; do
+    if test ! -f $i; then
+	echo "Could not find: " $i
+    fi
+  done
+done
