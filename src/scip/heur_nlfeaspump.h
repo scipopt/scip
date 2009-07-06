2bff33fe525531f332761f06d78821b469c92c41
Pietro Belotti <belotti@zib.de>
2009-07-06 10:32:00 +0000

    added skeleton for nonlinear feasibility pump

---
+++ src/scip/heur_nlfeaspump.h
@@ -0,0 +1,37 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the program and library             */
+/*         SCIP --- Solving Constraint Integer Programs                      */
+/*                                                                           */
+/*    Copyright (C) 2002-2009 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SCIP is distributed under the terms of the ZIB Academic License.         */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+#pragma ident "@(#) $Id: heur_nlfeaspump.h,v 1.1 2009/07/06 10:32:00 bzfbelot Exp $"
+
+/**@file   heur_nlfeaspump.h
+ * @brief  MINLP feasibility pump primal heuristic
+ * @author Pietro Belotti, Timo Berthold
+ */
+
+/*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
+
+#ifndef __SCIP_HEUR_NLFEASPUMP_H__
+#define __SCIP_HEUR_NLFEASPUMP_H__
+
+
+#include "scip/scip.h"
+
+
+/** creates the MINLP feasibility pump primal heuristic and includes it in SCIP */
+extern
+SCIP_RETCODE SCIPincludeHeurNlFeasPump(
+   SCIP*                 scip                /**< SCIP data structure */
+   );
+
+#endif
