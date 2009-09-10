0c4487d46e4220d604d6716dee10a0ab789a2fe4
Marc Pfetsch <m.pfetsch@tu-bs.de>
2009-09-10 14:51:29 +0000

    - added echos to make output readable

--- examples/makeall.sh
+++ examples/makeall.sh
@@ -4,23 +4,58 @@ do
     echo ===== $i =====
     echo
     cd $i
+    echo make OPT=dbg depend
     make OPT=dbg depend
+    echo
+    echo make OPT=opt depend
     make OPT=opt depend
+    echo
+    echo make OPT=dbg clean
     make OPT=dbg clean
+    echo
+    echo make OPT=dbg
     make OPT=dbg
+    echo
+    echo make OPT=opt clean
     make OPT=opt clean
+    echo
+    echo make OPT=opt
     make OPT=opt
+    echo
+    echo make OPT=dbg LPS=clp clean
     make OPT=dbg LPS=clp clean
+    echo
+    echo make OPT=dbg LPS=clp
     make OPT=dbg LPS=clp
+    echo
+    echo make OPT=opt LPS=clp clean
     make OPT=opt LPS=clp clean
+    echo
+    echo make OPT=opt LPS=clp
     make OPT=opt LPS=clp
+    echo
+    echo make OPT=dbg LPS=cpx clean
     make OPT=dbg LPS=cpx clean
+    echo
+    echo make OPT=dbg LPS=cpx
     make OPT=dbg LPS=cpx
+    echo
+    echo make OPT=opt LPS=cpx clean
     make OPT=opt LPS=cpx clean
+    echo
+    echo make OPT=opt LPS=cpx
     make OPT=opt LPS=cpx
+    echo
+    echo make OPT=dbg LPS=none clean
     make OPT=dbg LPS=none clean
+    echo
+    echo make OPT=dbg LPS=none
     make OPT=dbg LPS=none
+    echo
+    echo make OPT=opt LPS=none clean
     make OPT=opt LPS=none clean
+    echo
+    echo make OPT=opt LPS=none
     make OPT=opt LPS=none
     cd -
 done
