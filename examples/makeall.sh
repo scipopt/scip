949fd9a7b5c6562355669b6dc99ef6a8ede7a9d8
Stefan Heinz <heinz@zib.de>
2010-07-21 21:36:52 +0000

    - adjusted makeall.sh in example directory

--- examples/makeall.sh
+++ examples/makeall.sh
@@ -1,63 +1,31 @@
 #
 # run with bash -e makeall.sh to stop on errors
-for i in Coloring LOP MIPSolver Queens SamplePricer SamplePricer_C TSP VRP
+#
+
+EXAMPLES=(Coloring Eventhdlr LOP MIPSolver Queens SamplePricer SamplePricer_C TSP VRP)
+LPSOLVERS=(clp cpx none spx)
+OPTS=(opt dbg)
+
+for EXAMPLE in ${EXAMPLES[@]}
 do
+    echo ===== $EXAMPLE =====
     echo
-    echo ===== $i =====
+    cd $EXAMPLE
     echo
-    cd $i
-    echo make OPT=dbg depend
-    make OPT=dbg depend
-    echo
-    echo make OPT=opt depend
-    make OPT=opt depend
-    echo
-    echo make OPT=dbg clean
-    make OPT=dbg clean
-    echo
-    echo make OPT=dbg
-    make OPT=dbg
-    echo
-    echo make OPT=opt clean
-    make OPT=opt clean
-    echo
-    echo make OPT=opt
-    make OPT=opt
-    echo
-    echo make OPT=dbg LPS=clp clean
-    make OPT=dbg LPS=clp clean
-    echo
-    echo make OPT=dbg LPS=clp
-    make OPT=dbg LPS=clp
-    echo
-    echo make OPT=opt LPS=clp clean
-    make OPT=opt LPS=clp clean
-    echo
-    echo make OPT=opt LPS=clp
-    make OPT=opt LPS=clp
-    echo
-    echo make OPT=dbg LPS=cpx clean
-    make OPT=dbg LPS=cpx clean
-    echo
-    echo make OPT=dbg LPS=cpx
-    make OPT=dbg LPS=cpx
-    echo
-    echo make OPT=opt LPS=cpx clean
-    make OPT=opt LPS=cpx clean
-    echo
-    echo make OPT=opt LPS=cpx
-    make OPT=opt LPS=cpx
-    echo
-    echo make OPT=dbg LPS=none clean
-    make OPT=dbg LPS=none clean
-    echo
-    echo make OPT=dbg LPS=none
-    make OPT=dbg LPS=none
-    echo
-    echo make OPT=opt LPS=none clean
-    make OPT=opt LPS=none clean
-    echo
-    echo make OPT=opt LPS=none
-    make OPT=opt LPS=none
+    for OPT in ${OPTS[@]}
+    do
+	echo make OPT=$OPT LPS=$LPS ZIMPL=false depend
+	make OPT=$OPT LPS=$LPS ZIMPL=false depend
+	echo
+	for LPS in ${LPSOLVERS[@]}
+	do
+	    echo make OPT=$OPT LPS=$LPS clean
+	    make OPT=$OPT LPS=$LPS clean
+	    echo
+	    echo make OPT=$OPT LPS=$LPS
+	    make OPT=$OPT LPS=$LPS
+	    echo
+	done
+    done
     cd -
 done
