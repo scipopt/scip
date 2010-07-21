1ee5906869b79aac28c1af2bcf462fc008b87e94
Stefan Heinz <heinz@zib.de>
2010-07-21 21:25:56 +0000

    - first version of an event handler example in C

---
+++ examples/Eventhdlr/src/cmain.c
@@ -0,0 +1,90 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the program and library             */
+/*         SCIP --- Solving Constraint Integer Programs                      */
+/*                                                                           */
+/*    Copyright (C) 2002-2010 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SCIP is distributed under the terms of the ZIB Academic License.         */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+#pragma ident "@(#) $Id: cmain.c,v 1.1 2010/07/21 21:25:57 bzfheinz Exp $"
+
+/**@file   cmain.c
+ * @brief  Main file for C compilation
+ * @author Stefan Heinz
+ */
+
+/*--+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
+
+
+#include "scip/scip.h"
+#include "scip/scipdefplugins.h"
+#include "scip/scipshell.h"
+
+#include "event_bestsol.h"
+
+/** runs the shell */
+static
+SCIP_RETCODE runShell(
+   int                        argc,               /**< number of shell parameters */
+   char**                     argv,               /**< array with shell parameters */
+   const char*                defaultsetname      /**< name of default settings file */
+   )
+{
+   SCIP* scip = NULL;
+
+   /*********
+    * Setup *
+    *********/
+
+   /* initialize SCIP */
+   SCIP_CALL( SCIPcreate(&scip) );
+
+   /* include default plugins */
+   SCIP_CALL( SCIPincludeDefaultPlugins(scip) );
+
+   /* create event handler for best solution found */
+   SCIPcreateEventHdlrBestsol(scip);
+   
+   /**********************************
+    * Process command line arguments *
+    **********************************/
+   
+   SCIP_CALL( SCIPprocessShellArguments(scip, argc, argv, defaultsetname) );
+
+
+   /********************
+    * Deinitialization *
+    ********************/
+
+   SCIP_CALL( SCIPfree(&scip) );
+
+   /* check block memory */
+   BMScheckEmptyMemory();
+   
+   return SCIP_OKAY;
+}
+
+/** main method */
+int main(
+   int                   argc,          /**< number of arguments */
+   char**                argv           /**< string array with arguments */
+   )
+{
+  SCIP_RETCODE retcode;
+
+  retcode = runShell(argc, argv, "scip.set");
+  
+  if( retcode != SCIP_OKAY )
+  {
+     SCIPprintError(retcode, stderr);
+     return -1;
+  }
+  
+  return 0;
+}
---
+++ examples/Eventhdlr/src/event_bestsol.h
@@ -0,0 +1,31 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the program and library             */
+/*         SCIP --- Solving Constraint Integer Programs                      */
+/*                                                                           */
+/*    Copyright (C) 2002-2010 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SCIP is distributed under the terms of the ZIB Academic License.         */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+#pragma ident "@(#) $Id: event_bestsol.h,v 1.1 2010/07/21 21:25:57 bzfheinz Exp $"
+
+/**@file   event_bestsol.h
+ * @brief  eventhdlr for best solution found
+ * @author Stefan Heinz
+ * @author Michael Winkler
+ */
+
+/*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
+
+#include "scip/scip.h"
+
+/** creates event handler for best solution found */
+extern
+SCIP_RETCODE SCIPcreateEventHdlrBestsol(
+   SCIP*                 scip                /**< SCIP data structure */
+   );
