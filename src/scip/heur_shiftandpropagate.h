48478fcd2609389cf1ed9b55d8e335e69872a7ba
Gregor Hendel <hendel@zib.de>
2010-09-03 10:23:51 +0000

    new starting heuristic heur_shiftandpropagate which tries to find a primarily feasible solution before root LP is solved. See documentation in src/scip/heur_shiftandprogate.c for details

---
+++ src/scip/heur_shiftandpropagate.h
@@ -0,0 +1,44 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the program and library             */
+/*         SCIP --- Solving Constraint Integer Programs                      */
+/*                                                                           */
+/*    Copyright (C) 2002-2009 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SCIP is distributed under the terms of the ZIB Academic License.         */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+#pragma ident "@(#) $Id: heur_shiftandpropagate.h,v 1.1 2010/09/03 10:23:51 bzfhende Exp $"
+
+/**@file   heur_shiftandpropagate.h
+ * @brief  shiftandpropagate primal heuristic
+ * @author Tobias Achterberg
+ */
+
+/*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
+
+#ifndef __SCIP_HEUR_SHIFTANDPROPAGATE_H__
+#define __SCIP_HEUR_SHIFTANDPROPAGATE_H__
+
+
+#include "scip/scip.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** creates the shiftandpropagate primal heuristic and includes it in SCIP */
+extern
+SCIP_RETCODE SCIPincludeHeurShiftandpropagate(
+   SCIP*                 scip                /**< SCIP data structure */
+   );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
