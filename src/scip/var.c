d2dcd748b3c0a43d02da203db4bdf10460616092
Michael Winkler <michael.winkler@zib.de>
2010-12-22 22:25:06 +0000

    - fixed bug in vars.c which handled arrays with parts of original variables during resolving to active variables (using SCIPvarsGetProbvarBinary) wrong; added forgotten CHANCHLOG entry

--- src/scip/var.c
+++ src/scip/var.c
@@ -12,7 +12,7 @@
 /*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      */
 /*                                                                           */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-#pragma ident "@(#) $Id: var.c,v 1.304.2.1 2010/10/07 10:03:12 bzfheinz Exp $"
+#pragma ident "@(#) $Id: var.c,v 1.304.2.2 2010/12/22 22:25:06 bzfwinkm Exp $"
 
 /**@file   var.c
  * @brief  methods for problem variables
@@ -3391,8 +3391,8 @@ SCIP_RETCODE SCIPvarGetActiveRepresentatives(
 
    assert( set != NULL );
    assert( nvars != NULL );
-   assert( vars != NULL || *nvars == 0);
-   assert( scalars != NULL );
+   assert( vars != NULL || *nvars == 0 );
+   assert( scalars != NULL || *nvars == 0 );
    assert( constant != NULL );
    assert( requiredsize != NULL );
    assert( *nvars <= varssize );
@@ -9272,8 +9272,9 @@ SCIP_RETCODE SCIPvarsGetProbvarBinary(
          {
          case SCIP_VARSTATUS_ORIGINAL:
             if( (*var)->data.original.transvar == NULL )
-               return SCIP_OKAY;
-            *var = (*var)->data.original.transvar;
+               resolved = TRUE;
+            else
+               *var = (*var)->data.original.transvar;
             break;
             
          case SCIP_VARSTATUS_LOOSE:
