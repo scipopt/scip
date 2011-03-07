eb0030c2200cf0c8f99bec355cd4503d81f14fbe
Stefan Heinz <heinz@zib.de>
2011-03-07 18:31:37 +0100

    - fixed (problem with windows) include call in nlpi/intervalarith.cpp

--- src/nlpi/intervalarith.cpp
+++ src/nlpi/intervalarith.cpp
@@ -21,7 +21,7 @@
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
 
-#include <scip/def.h>
-#include <nlpi/intervalarith.h>
+#include "scip/def.h"
+#include "nlpi/intervalarith.h"
 
 SCIP_Real SCIPInterval::infinity = SCIP_DEFAULT_INFINITY;
