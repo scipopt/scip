c287bb4a62a44cfc6a884f8dfd61f1468cbe1882
Stefan Heinz <heinz@zib.de>
2011-06-30 23:30:49 +0200

    adjusted updatedates.sh script
    - now we also check the COPYRIGHTS in INSTALL files

--- updatedates.sh
+++ updatedates.sh
@@ -17,49 +17,75 @@ LASTYEAR=`expr $NEWYEAR - 1`
 
 DIRECTORIES=(check doc src src/* examples examples/* examples/*/src examples/*/doc)
 EXTENSIONS=(sh awk h c hpp cpp)
-EXTRAFILE=(Makefile)
+EXTRAFILES=(Makefile INSTALL)
 
 echo ""
 echo "This script reports *all* files which have not a correct COPYRIGHT."
 echo "Only files which are included in the distribution need a COPYRIGHT (see makedist.sh)"
 echo ""
 
+# collect all files
+FILES=""
 for DIRECTORY in ${DIRECTORIES[@]}
 do
-  for EXTENSION in ${EXTENSIONS[@]}
-  do
-    for FILE in $DIRECTORY/*.$EXTENSION $DIRECTORY/$EXTRAFILE $EXTRAFILE
+    for EXTENSION in ${EXTENSIONS[@]}
     do
-      if test -f $FILE
-      then
-	  # check if the file has already the new copyright 
-	  COUNT1=`grep -c 2002-$NEWYEAR $FILE`
-	  COUNT2=`grep -c 1996-$NEWYEAR $FILE`
-
-	  if test "$COUNT1" != "$COUNT2"
-	  then
-	      continue
-	  fi
-	  
-	  # check if the file has a correct old copyright 
-	  COUNT1=`grep -c 2002-$LASTYEAR $FILE`
-	  COUNT2=`grep -c 1996-$LASTYEAR $FILE`
-
-	  if test "$COUNT1" == "$COUNT2"
-	  then
-	      # post those files which have a wrong old copyright
-	      echo "COPYRIGHT ERROR --------------------> $FILE"
-	      grep "2002-2" $FILE
-	      grep "1996-2" $FILE	  
-	  else
-	      echo $FILE
-  
-	      mv $FILE $FILE.olddate
-	      sed 's!2002-'$LASTYEAR'!2002-'$NEWYEAR'!g 
-                   s!1996-'$LASTYEAR'!1996-'$NEWYEAR'!g' $FILE.olddate > $FILE
-	  fi
-      fi
+	for FILE in $DIRECTORY/*.$EXTENSION 
+	do
+	    if test -f $FILE
+	    then
+		FILES="$FILES $FILE"
+	    fi
+	done
     done
-  done
+    for EXTRAFILE in ${EXTRAFILES[@]}
+    do 
+	if test -f $DIRECTORY/$EXTRAFILE
+	then
+	    FILES="$FILES $DIRECTORY/$EXTRAFILE"
+	fi
+    done
 done
 
+for EXTRAFILE in ${EXTRAFILES[@]}
+do 
+    if test -f $EXTRAFILE
+    then
+	FILES="$FILES $EXTRAFILE"
+    fi
+done
+
+for FILE in ${FILES[@]}
+do
+    if test -f $FILE
+    then
+	echo $FILE
+
+	# check if the file has already the new copyright 
+	COUNT1=`grep -c 2002-$NEWYEAR $FILE`
+	COUNT2=`grep -c 1996-$NEWYEAR $FILE`
+
+	if test "$COUNT1" != "$COUNT2"
+	then
+	    continue
+	fi
+	
+	# check if the file has a correct old copyright 
+	COUNT1=`grep -c 2002-$LASTYEAR $FILE`
+	COUNT2=`grep -c 1996-$LASTYEAR $FILE`
+
+	if test "$COUNT1" == "$COUNT2"
+	then
+	    # post those files which have a wrong old copyright
+	    echo "COPYRIGHT ERROR --------------------> $FILE"
+	    grep "2002-2" $FILE
+	    grep "1996-2" $FILE	  
+	else
+	    echo $FILE
+  
+	    mv $FILE $FILE.olddate
+	    sed 's!2002-'$LASTYEAR'!2002-'$NEWYEAR'!g 
+                   s!1996-'$LASTYEAR'!1996-'$NEWYEAR'!g' $FILE.olddate > $FILE
+	fi
+    fi
+done
