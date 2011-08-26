1f5867b72a3b8baa7aef8737fb8c49ce56d379f0
Stefan Vigerske <stefan@math.hu-berlin.de>
2011-08-26 19:16:28 +0200

    add zlib to external codes, if linked in

--- src/scip/scip.c
+++ src/scip/scip.c
@@ -38,6 +38,10 @@
 #include <time.h>
 #endif
 
+#ifdef WITH_ZLIB
+#include <zlib.h>
+#endif
+
 #include "scip/def.h"
 #include "scip/retcode.h"
 #include "scip/set.h"
@@ -560,6 +564,10 @@ SCIP_RETCODE SCIPcreate(
    {
       SCIP_CALL( SCIPsetIncludeExternalCode((*scip)->set, SCIPexprintGetName(), SCIPexprintGetDesc()) );
    }
+
+#ifdef WITH_ZLIB
+   SCIP_CALL( SCIPsetIncludeExternalCode((*scip)->set, "ZLIB " ZLIB_VERSION, "General purpose compression library by J. Gailly and M. Adler (zlib.net)") );
+#endif
 
    return SCIP_OKAY;
 }
