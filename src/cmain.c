ad9e304c7f2e311a5d8c91017b43a73ee2843a2a
Marc Pfetsch <m.pfetsch@tu-bs.de>
2011-09-11 14:34:52 +0200

    remove superfluous todos
    The todos in cmain.c have been moved to a global scope. The following
    todos have been deleted:
    
    - implement remaining events
      [this seems to be done]
    - avoid addition of identical rows
      [this is handled implicitly by sepastore.c]
    - avoid addition of identical constraints
      [for preprocessing this is handled in several constraint handlers
      by pairwise comparison]
    - it's a bit ugly, that user call backs may be called before the
      nodequeue was processed
      [it is unclear what this originally referred to]
    - use aging in all constraint handlers [this seems to be done]

--- src/cmain.c
+++ src/cmain.c
@@ -18,6 +18,16 @@
  * @author Tobias Achterberg
  */
 
+/* global todos: */
+
+/**@todo pricing for pseudo solutions */
+/**@todo unboundness detection in presolving -> convert problem into feasibility problem to decide
+ *       unboundness/infeasibility */
+/**@todo variable event PSSOLCHANGED, update pseudo activities in constraints to speed up checking of pseudo solutions */
+/**@todo branching rule acting as a filter by temporary changing the branching priority of variables and returning 
+ *       SCIP_DIDNOTFIND to let the next branching rule select the branching variable */
+/**@todo try to not use the first but the shortest constraint as reason for a deduction */
+
 /*--+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
 
 #include <stdio.h>
@@ -33,19 +43,6 @@ main(
    )
 {
    SCIP_RETCODE retcode;
-
-   /**@todo implement remaining events */
-   /**@todo avoid addition of identical rows */
-   /**@todo avoid addition of identical constraints */
-   /**@todo pricing for pseudo solutions */
-   /**@todo it's a bit ugly, that user call backs may be called before the nodequeue was processed */
-   /**@todo unboundness detection in presolving -> convert problem into feasibility problem to decide
-    *       unboundness/infeasibility */
-   /**@todo variable event PSSOLCHANGED, update pseudo activities in constraints to speed up checking of pseudo solutions */
-   /**@todo branching rule acting as a filter by temporary changing the branching priority of variables and returning 
-    *       SCIP_DIDNOTFIND to let the next branching rule select the branching variable */
-   /**@todo use aging in all constraint handlers */
-   /**@todo try to not use the first but the shortest constraint as reason for a deduction */
 
    retcode = SCIPrunShell(argc, argv, "scip.set");
    if( retcode != SCIP_OKAY )
