748c5906f234d807feb6b692cc000931796ed556
Michael Winkler <michael.winkler@zib.de>
2011-09-19 19:54:08 +0200

    removed old files and on old method

--- src/scip/prop_pseudoobj.c
+++ src/scip/prop_pseudoobj.c
@@ -961,18 +961,3 @@ SCIP_RETCODE SCIPincludePropPseudoobj(
 
    return SCIP_OKAY;
 }
-
-/** propagates the cutoff bound for the given variables */
-SCIP_RETCODE SCIPpropagateCutoffboundVar(
-   SCIP*                 scip,               /**< SCIP data structure */
-   SCIP_PROP*            prop,               /**< propagator, or NULL */
-   SCIP_VAR*             var,                /**< variables to propagate */
-   SCIP_Real             cutoffbound,        /**< cutoff bound to use */
-   SCIP_Real             pseudoobjval,       /**< pseudo objective value to use */
-   int*                  nchgbds             /**< pointer to store the number of changed bounds */
-   )
-{
-   SCIP_CALL( propagateCutoffboundVar(scip, prop, var, cutoffbound, pseudoobjval, nchgbds) );
-
-   return SCIP_OKAY;
-}
