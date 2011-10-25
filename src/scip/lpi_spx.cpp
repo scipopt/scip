653fe4443ec8459ae2c778213c4925a5f1089350
Matthias Miltenberger <miltenberger@zib.de>
2011-10-25 12:30:41 +0200

    improved printing of SoPlex githash
    - calls function getgithash instead of printing predefined hash

--- src/scip/lpi_spx.cpp
+++ src/scip/lpi_spx.cpp
@@ -88,7 +88,7 @@
 
 /* get githash of SoPlex */
 #if (SOPLEX_VERSION > 150)
-#include "githash.cpp"
+#include "spxgithash.h"
 #endif
 
 /* reset the SCIP_DEBUG define to its original SCIP value */
@@ -1305,6 +1305,7 @@ void invalidateSolution(SCIP_LPI* lpi)
  */
 
 static char spxname[100];
+static char spxdesc[200];
 
 /**@name Miscellaneous Methods */
 /**@{ */
@@ -1321,9 +1322,6 @@ const char* SCIPlpiGetSolverName(
 #else
    sprintf(spxname, "SoPlex %d.%d.%d", SOPLEX_VERSION/100, (SOPLEX_VERSION % 100)/10, SOPLEX_VERSION % 10);
 #endif
-#ifdef SPX_GITHASH
-   sprintf(spxname, "%s (#%s)", spxname, SPX_GITHASH);
-#endif
    return spxname;
 }
 
@@ -1332,11 +1330,14 @@ const char* SCIPlpiGetSolverDesc(
    void
    )
 {
-#ifdef WITH_LPSCHECK
-   return "Linear Programming Solver developed at Zuse Institute Berlin (soplex.zib.de) - including CPLEX double check";
-#else
-   return "Linear Programming Solver developed at Zuse Institute Berlin (soplex.zib.de)";
+   sprintf(spxdesc, "%s", "Linear Programming Solver developed at Zuse Institute Berlin (soplex.zib.de)");
+#if (SOPLEX_VERSION > 150)
+   sprintf(spxdesc, "%s [GitHash: %s]", spxdesc, getGitHash());
 #endif
+#ifdef WITH_LPSCHECK
+   sprintf(spxdesc, "%s %s", spxdesc, "- including CPLEX double check");
+#endif
+   return spxdesc;
 }
 
 /** gets pointer for LP solver - use only with great care */
