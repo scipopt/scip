5ae82b13fcc1f40353d20db795134088746e15ff
Ambros Gleixner <gleixner@zib.de>
2011-11-10 15:41:09 +0100

    Fixed bug in lpi_spx.cpp which deactivated LP presolving.

--- src/scip/lpi_spx.cpp
+++ src/scip/lpi_spx.cpp
@@ -746,8 +746,8 @@ public:
 
          if( simplifier != NULL || scaler != NULL )
          {
-            setObjLoLimit(soplex::infinity);
-            setObjUpLimit(-soplex::infinity);
+            setObjLoLimit(-soplex::infinity);
+            setObjUpLimit(soplex::infinity);
          }
 
          doSolve();
