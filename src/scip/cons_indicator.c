94d0d4fb343450ecfb4eb69f4c99ee1d140443cc
Marc Pfetsch <m.pfetsch@tu-bs.de>
2011-11-20 14:52:23 +0100

    add return value and exit if infeasible

--- src/scip/cons_indicator.c
+++ src/scip/cons_indicator.c
@@ -4166,6 +4166,14 @@ SCIP_DECL_CONSINITPRE(consInitpreIndicator)
    assert( scip != NULL );
    assert( conshdlr != NULL );
    assert( strcmp(SCIPconshdlrGetName(conshdlr), CONSHDLR_NAME) == 0 );
+   assert( result != NULL );
+
+   *result = SCIP_FEASIBLE;
+
+   if ( isinfeasible || isunbounded )
+      return SCIP_OKAY;
+
+   SCIPdebugMessage("Initpre method for indicator constraints.\n");
 
    /* check each constraint and get transformed linear constraint */
    for (c = 0; c < nconss; ++c)
@@ -4367,6 +4375,10 @@ SCIP_DECL_CONSEXITPRE(consExitpreIndicator)
    assert( result != NULL );
 
    *result = SCIP_FEASIBLE;
+
+   if ( isinfeasible || isunbounded )
+      return SCIP_OKAY;
+
    SCIPdebugMessage("Exitpre method for indicator constraints.\n");
 
    /* get constraint handler data */
