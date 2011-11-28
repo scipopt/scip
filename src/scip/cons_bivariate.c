7c37b8be081bb55844b492a44b428272965ed036
Gerald Gamrath <bzfgamra@zib.de>
2011-11-28 14:39:29 +0100

    added explicit cast for microsoft compiler

--- src/scip/cons_bivariate.c
+++ src/scip/cons_bivariate.c
@@ -697,7 +697,7 @@ SCIP_RETCODE removeFixedNonlinearVariables(
    i = 0;
    while( i < SCIPexprgraphGetNVars(conshdlrdata->exprgraph) )
    {
-      var = SCIPexprgraphGetVars(conshdlrdata->exprgraph)[i];
+      var = (SCIP_VAR*) SCIPexprgraphGetVars(conshdlrdata->exprgraph)[i];
       if( SCIPvarIsActive(var) )
       {
          ++i;
