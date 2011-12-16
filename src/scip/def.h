94e8c8002f61800985d7e8d47495727a52d72b8b
Stefan Heinz <heinz@zib.de>
2011-12-16 22:57:17 +0100

    changed version to 2.1.1

--- src/scip/def.h
+++ src/scip/def.h
@@ -49,7 +49,7 @@
 extern "C" {
 #endif
 
-#define SCIP_VERSION                210 /**< SCIP version number (multiplied by 100 to get integer number) */
+#define SCIP_VERSION                211 /**< SCIP version number (multiplied by 100 to get integer number) */
 #define SCIP_SUBVERSION               0 /**< SCIP sub version number */
 #define SCIP_COPYRIGHT   "Copyright (c) 2002-2011 Konrad-Zuse-Zentrum fuer Informationstechnik Berlin (ZIB)"
 
