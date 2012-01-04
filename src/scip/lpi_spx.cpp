74318e6702e98ca9dcb07e4fccb345886a176108
Matthias Miltenberger <miltenberger@zib.de>
2012-01-04 14:01:43 +0100

    time limit of SoPlex is updated after every solve
    This fixes the previous behaviour of not stopping the LP solver
    after the time limit of SCIP has been reached.

--- src/scip/lpi_spx.cpp
+++ src/scip/lpi_spx.cpp
@@ -496,6 +496,8 @@ public:
 
    void trySolve(bool printwarning = true)
    {
+      Real timespent;
+      Real timelimit;
       try
       {
 	 m_stat = SPxSolver::solve();
@@ -519,6 +521,21 @@ public:
       /* save iteration count */
       m_itused += SPxSolver::iterations();
       assert(m_itlim < 0 || m_itused <= m_itlim);
+
+      /* update time limit */
+      timespent = SPxSolver::time();
+      if( timespent > 0 )
+      {
+         /* get current time limit */
+         timelimit = SPxSolver::terminationTime();
+         if( timelimit > timespent )
+            timelimit -= timespent;
+         else
+            timelimit = 0;
+         /* set new time limit */
+         assert(timelimit >= 0);
+         SPxSolver::setTerminationTime(timelimit);
+      }
    }
 
    virtual Status doSolve(bool printwarning = true)
