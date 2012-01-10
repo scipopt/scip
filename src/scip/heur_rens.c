3be832907aad5af262335a2b40d45e02cef06410
Timo Berthold <berthold@zib.de>
2012-01-10 15:20:00 +0100

    - disabled define in RENS

--- src/scip/heur_rens.c
+++ src/scip/heur_rens.c
@@ -20,7 +20,7 @@
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
 
-#define STATISTIC_INFORMATION /* uncomment to get statistical output at the end of RENS run */
+/* #define STATISTIC_INFORMATION uncomment to get statistical output at the end of RENS run */
 
 #include <assert.h>
 #include <string.h>
