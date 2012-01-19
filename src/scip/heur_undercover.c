cb78b7072e15fb76531a0e70fbbaad42d7b1da95
Michael Winkler <michael.winkler@zib.de>
2012-01-19 16:30:05 +0100

    fixed bug in heur_undercover.c

--- src/scip/heur_undercover.c
+++ src/scip/heur_undercover.c
@@ -652,6 +652,7 @@ SCIP_RETCODE createCoveringProblem(
          SCIP_CONS* andcons;
          SCIP_CONS* coveringcons;
          SCIP_VAR** andvars;
+         SCIP_VAR* andres;
          SCIP_VAR** coveringconsvars;
          SCIP_Real* coveringconsvals;
          SCIP_Bool negated;
@@ -739,7 +740,9 @@ SCIP_RETCODE createCoveringProblem(
          }
 
          /* if constraints with inactive variables are present, we have to find the corresponding active variable */
-         probindex = SCIPvarGetProbindex(SCIPgetResultantAnd(scip, andcons));
+	 andres = SCIPgetResultantAnd(scip, andcons);
+	 assert(andres != NULL);
+         probindex = SCIPvarGetProbindex(andres);
          negated = FALSE;
 
          if( probindex == -1 )
@@ -780,6 +783,14 @@ SCIP_RETCODE createCoveringProblem(
                negated = FALSE;
             }
          }
+	 else if( SCIPvarGetLbGlobal(andres) > 0.5 || SCIPvarGetUbGlobal(andres) < 0.5 )
+	 {
+	    /* free memory for covering constraint */
+	    SCIPfreeBufferArray(coveringscip, &coveringconsvals);
+	    SCIPfreeBufferArray(coveringscip, &coveringconsvars);
+
+	    continue;
+	 }
          assert(probindex >= 0);
          assert(!termIsConstant(scip, (negated ? SCIPvarGetNegatedVar(vars[probindex]) : vars[probindex]), 1.0, globalbounds));
 
