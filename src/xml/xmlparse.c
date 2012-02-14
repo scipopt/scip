JasperNL/scip-unified

speed up freeing of xml data
We now free data from back to front. This significantly speeds up
freeing the xml data. Moreover, we remove a valgrind warning
by first reserving space and then copying the filename.

src/xml/xmlparse.c
@@ -1072,8 +1072,13 @@ XML_NODE* xmlProcess(
    XML_ATTR* attr;
    int       result = FALSE;
    char*     myfilename;
+   size_t    filenamelen;
 
-   ALLOC_FALSE( BMSduplicateMemoryArray(&myfilename, filename, strlen(filename) + 5) );
+   /* allocate space and copy filename (possibly modified below) in two steps in order to satisfy valgrind */
+   assert( filename != NULL );
+   filenamelen = strlen(filename);
+   ALLOC_FALSE( BMSallocMemoryArray(&myfilename, filenamelen + 5) );
+   BMScopyMemoryArray(myfilename, filename, filenamelen + 1);
 
 #ifdef WITH_ZLIB
    if ( access(filename, R_OK) != 0 )
@@ -1268,11 +1273,12 @@ void xmlFreeNode(
    if ( node == NULL )
       return;
 
-   n = node->firstchild;
+   /* free data from back to front (because free is faster this way) */
+   n = node->lastchild;
    while ( n != NULL )
    {
       XML_NODE* m;
-      m = n->nextsibl;
+      m = n->prevsibl;
       xmlFreeNode(n);
       n = m;
    }
@@ -1354,7 +1360,7 @@ const char* xmlGetAttrval(
 
 #if 0
    if (a == NULL)
-      infoMessage("Error: Attribut %s in TAG <%s> not found\n",
+      infoMessage("Error: Attribute %s in TAG <%s> not found\n",
          name, node->name);
 #endif
 

scipopt/scip

src/xml/xmlparse.c
@@ -1052,8 +1052,13 @@ XML_NODE* xml_process(
    XML_ATTR* attr;
    int       result = FALSE;
    char*     myfilename;
+   size_t    filenamelen;
 
-   ALLOC_FALSE( BMSduplicateMemoryArray(&myfilename, filename, strlen(filename) + 5) );
+   /* allocate space and copy filename (possibly modified below) in two steps in order to satisfy valgrind */
+   assert( filename != NULL );
+   filenamelen = strlen(filename);
+   ALLOC_FALSE( BMSallocMemoryArray(&myfilename, filenamelen + 5) );
+   BMScopyMemoryArray(myfilename, filename, filenamelen + 1);
 
 #ifdef WITH_ZLIB
    if (access(filename, R_OK) != 0)
@@ -1248,11 +1253,12 @@ void xml_free_node(
    if (node == NULL)
       return;
 
-   n = node->first_child;
-   while (n != NULL)
+   /* free data from back to front (because free is faster this way) */
+   n = node->last_child;
+   while ( n != NULL )
    {
       XML_NODE* m;
-      m = n->next_sibl;
+      m = n->prev_sibl;
       xml_free_node(n);
       n = m;
    }
@@ -1334,7 +1340,7 @@ const char* xml_get_attrval(
 
 #if 0
    if (a == NULL)
-      infoMessage("Error: Attribut %s in TAG <%s> not found\n",
+      infoMessage("Error: Attribute %s in TAG <%s> not found\n",
          name, node->name);
 #endif
 