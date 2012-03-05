c569f539766d5f288639edf9204c3510b07d7f0a
Stefan Vigerske <stefan@math.hu-berlin.de>
2012-03-05 17:36:54 +0100

    disable keeping orig sols in subscip of subnlp heuristic
    - keeping the solutions of one subscip for the next subscip is usually
      not useful, but leads to printing a message about violation of
      variable bounds, since (unfortunately) misc/printreason is TRUE by
      default
    - thus, disable keeping original solutions between subscip solves in
      subnlp heuristic

--- src/scip/heur_subnlp.c
+++ src/scip/heur_subnlp.c
@@ -261,6 +261,9 @@ SCIP_RETCODE createSubSCIP(
    /* do not abort subproblem on CTRL-C */
    SCIP_CALL( SCIPsetBoolParam(heurdata->subscip, "misc/catchctrlc", FALSE) );
 
+   /* disable keeping solutions from one subscip solve for next solve (with usually different fixings) */
+   SCIP_CALL( SCIPsetIntParam(heurdata->subscip, "limits/maxorigsol", 0) );
+
    /* disable output to console */
    SCIP_CALL( SCIPsetIntParam(heurdata->subscip, "display/verblevel", 0) );
 
