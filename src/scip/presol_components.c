dfe0d2352d6e73eeb14e0e864acf5f1840de698f
Gerald Gamrath <bzfgamra@zib.de>
2012-04-30 12:37:09 +0200

    enabled components and domcol presolvers

--- src/scip/presol_components.c
+++ src/scip/presol_components.c
@@ -31,7 +31,7 @@
 #define PRESOL_NAME            "components"
 #define PRESOL_DESC            "components presolver"
 #define PRESOL_PRIORITY        -9200000      /**< priority of the presolver (>= 0: before, < 0: after constraint handlers); combined with propagators */
-#define PRESOL_MAXROUNDS              0      /**< maximal number of presolving rounds the presolver participates in (-1: no limit) */
+#define PRESOL_MAXROUNDS             -1      /**< maximal number of presolving rounds the presolver participates in (-1: no limit) */
 #define PRESOL_DELAY               TRUE      /**< should presolver be delayed, if other presolvers found reductions? */
 
 #define DEFAULT_WRITEPROBLEMS     FALSE      /**< should the single components be written as an .lp-file? */
