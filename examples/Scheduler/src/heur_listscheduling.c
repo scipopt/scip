396061bfefaefd0539211741601dadb6ea53434c
Stefan Heinz <heinz@zib.de>
2012-05-22 18:29:36 +0200

    adjusted scheduler example

--- examples/Scheduler/src/heur_listscheduling.c
+++ examples/Scheduler/src/heur_listscheduling.c
@@ -33,7 +33,7 @@
 #define HEUR_FREQ             1
 #define HEUR_FREQOFS          0
 #define HEUR_MAXDEPTH         100000
-#define HEUR_TIMING           SCIP_HEURTIMING_BEFOREPRESOL
+#define HEUR_TIMING           SCIP_HEURTIMING_AFTERNODE | SCIP_HEURTIMING_BEFOREPRESOL
 #define HEUR_USESSUBSCIP      FALSE      /**< does the heuristic use a secondary SCIP instance? */
 
 /*
