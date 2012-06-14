5f35e3e03627e1401092a5a16177bc46e87d8ce3
Stefan Heinz <heinz@zib.de>
2012-06-14 14:47:22 +0200

    Revert "change opt-mode define for SCIPboundtypeOpposite"
    This reverts commit a10755f8796cdc979274715f3a8854d4b4450640.

--- src/scip/pub_lp.h
+++ src/scip/pub_lp.h
@@ -229,7 +229,8 @@ SCIP_BOUNDTYPE SCIPboundtypeOpposite(
 #define SCIPcolGetVals(col)             (col)->vals
 #define SCIPcolGetStrongbranchNode(col) (col)->sbnode
 #define SCIPcolGetNStrongbranchs(col)   (col)->nsbcalls
-#define SCIPboundtypeOpposite(boundtype) ((SCIP_BOUNDTYPE)((boundtype) ^ 1))
+#define SCIPboundtypeOpposite(boundtype) \
+   ((boundtype) == SCIP_BOUNDTYPE_LOWER ? SCIP_BOUNDTYPE_UPPER : SCIP_BOUNDTYPE_LOWER)
 
 #endif
 
