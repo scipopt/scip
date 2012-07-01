7fbe82085bee0be15c4bbc6c23d7b63cd8bc2202
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2012-07-01 12:04:08 +0200

    fixed lint warning

--- src/scip/scipshell.c
+++ src/scip/scipshell.c
@@ -83,10 +83,9 @@ SCIP_RETCODE fromCommandLine(
    case SCIP_READERROR:
       SCIPinfoMessage(scip, NULL, "error reading file <%s>\n", filename);
       return SCIP_OKAY;
-      /*lint -fallthrough*/
    default:
       SCIP_CALL( retcode );
-   }
+   } /*lint !e788*/
 
    /*******************
     * Problem Solving *
