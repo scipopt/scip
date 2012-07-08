515009c706eda9b289ef739913736937b4d63295
Stefan Heinz <heinz@zib.de>
2012-07-08 14:38:39 +0200

    adjusted reader in scheduler example

--- examples/Scheduler/src/reader_sm.c
+++ examples/Scheduler/src/reader_sm.c
@@ -326,6 +326,7 @@ SCIP_RETCODE getJobs(
    )
 {
    char jobname[SCIP_MAXSTRLEN];
+   int value;
    int jobid;
    int r;
 
@@ -339,18 +340,30 @@ SCIP_RETCODE getJobs(
       return SCIP_OKAY;
    }
 
-   jobid = strtol(linestr, &linestr, 10) - 1;
-   sprintf(jobname,"%d" , jobid) ;
+   /* parse job id */
+   SCIPstrToIntValue(linestr, &value, &linestr);
+   jobid = value - 1;
+
+   /* construct job name */
+   (void)SCIPsnprintf(jobname, SCIP_MAXSTRLEN, "%d" , jobid) ;
+
+   /* copy job name */
    SCIP_CALL( SCIPduplicateBufferArray(scip, &rcpspdata->jobnames[jobid], jobname, strlen(jobname) + 1) );
 
-   strtol(linestr, &linestr, 10);
-   rcpspdata->durations[jobid] = strtol(linestr, &linestr, 10);
+   /* skip next value */
+   SCIPstrToIntValue(linestr, &value, &linestr);
+
+   /* parse duration */
+   SCIPstrToIntValue(linestr, &value, &linestr);
+   rcpspdata->durations[jobid] = value;
 
    SCIP_CALL( SCIPallocBufferArray(scip, &rcpspdata->demands[jobid], rcpspdata->nresources) );
 
+   /* parse demands */
    for( r = 0; r < rcpspdata->nresources; ++r )
    {
-      rcpspdata->demands[jobid][r] = strtol(linestr, &linestr, 10);
+      SCIPstrToIntValue(linestr, &value, &linestr);
+      rcpspdata->demands[jobid][r] = value;
    }
 
    /* check if we paresed the last job */
@@ -370,8 +383,9 @@ SCIP_RETCODE getPrecedence(
    SCIP_RCPSPDATA*       rcpspdata           /**< pointer to resources constrained project scheduling data */
    )
 {
-   int pred;
    int nsuccessors;
+   int value;
+   int pred;
    int p;
 
    assert(s != NULL);
@@ -389,15 +403,23 @@ SCIP_RETCODE getPrecedence(
       SCIP_CALL( SCIPdigraphCreate(&rcpspdata->precedencegraph, rcpspdata->njobs) );
    }
 
-   pred = strtol(s,&s,10) -1;
-   strtol(s,&s,10);
-   nsuccessors = strtol(s,&s,10);
+   /* parse predecessor */
+   SCIPstrToIntValue(s, &value, &s);
+   pred = value - 1;
 
+   /* skip integer value */
+   SCIPstrToIntValue(s, &value, &s);
+
+   /* parse number of successors */
+   SCIPstrToIntValue(s, &nsuccessors, &s);
+
+   /* parse successors */
    for( p = 0; p < nsuccessors; ++p )
    {
       int succ;
 
-      succ = strtol(s,&s,10)-1;
+      SCIPstrToIntValue(s, &value, &s);
+      succ = value - 1;
 
       /* add precedence to digraph */
       SCIP_CALL( SCIPdigraphAddArc(rcpspdata->precedencegraph, pred, succ, (void*)(size_t)INT_MAX) );
