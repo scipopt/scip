1a9d67df70775666a045ae580a4db4900b30d0d1
Stefan Heinz <heinz@zib.de>
2012-07-29 21:11:07 +0200

    added reader to scheduler example

--- examples/Scheduler/src/reader_rcp.c
+++ examples/Scheduler/src/reader_rcp.c
@@ -13,8 +13,8 @@
 /*                                                                           */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-/**@file   reader_sch.c
- * @brief  scheduling problem file reader for RCPSP/max problem format
+/**@file   reader_rcp.c
+ * @brief  file reader for "pack" scheduling instances
  * @author Stefan Heinz
  */
 
@@ -25,46 +25,25 @@
 #include <errno.h>
 #include <ctype.h>
 
-#include "reader_sch.h"
+#include "reader_rcp.h"
 #include "reader_sm.h"
 
-
-#define READER_NAME             "schreader"
-#define READER_DESC             "scheduling file reader for sch files (RCPSP/max format)"
-#define READER_EXTENSION        "sch"
-
-
-#define SCH_MAX_LINELEN       1024     /**< size of the line buffer for reading or writing */
-
-/*
- * Local methods
+/**@name Reader properties
+ *
+ * @{
  */
 
-/** parse job id and check if only one job mode is present */
-static
-SCIP_RETCODE getJobId(
-   SCIP*                 scip,               /**< SCIP data structure */
-   const char*           str,                /**< string to search */
-   int*                  job,                /**< pointer to store the parsed job id */
-   char**                endptr              /**< pointer to store the final string position if successfully parsed */
-   )
-{
-   int mode;
+#define READER_NAME             "rcpreader"
+#define READER_DESC             "reader for \"pack\" scheduling instances"
+#define READER_EXTENSION        "rcp"
 
-   /* get job id */
-   SCIPstrToIntValue(str, job, endptr);
+/**@} */
 
-   /* get job mode */
-   SCIPstrToIntValue(*endptr, &mode, endptr);
 
-   if( mode != 1 )
-   {
-      SCIPwarningMessage(scip, "jobs with different modes are not supported\n");
-      return SCIP_READERROR;
-   }
-
-   return SCIP_OKAY;
-}
+/**@name Local methods
+ *
+ * @{
+ */
 
 /** parse job and capacities details */
 static
@@ -80,94 +59,9 @@ SCIP_RETCODE parseDetails(
    int                   nresources          /**< number of capacities to be parsed */
    )
 {
-   char buf[SCH_MAX_LINELEN];
+   char buf[SCIP_MAXSTRLEN];
    char* endptr;
    int j;
-
-   /* get data for each job including a dummy job at the beginning and a dummy job at the end */
-   for( j = 0; j < njobs; ++j )
-   {
-      if( NULL != SCIPfgets(buf, sizeof(buf), file) )
-      {
-         int* successors;
-         int distance;
-         int nsuccessors;
-         int job;
-         int s;
-
-         /* get job id and check if only one mode is present */
-         SCIP_CALL( getJobId(scip, buf, &job, &endptr) );
-
-         SCIPdebugMessage("job %d -> ", j);
-
-         /* get number of direct successors */
-         SCIPstrToIntValue(endptr, &nsuccessors, &endptr);
-
-         /* allocate buffer to temporarily collect the successors */
-         SCIP_CALL( SCIPallocBufferArray(scip, &successors, nsuccessors) );
-
-         /* parse successor job ids */
-         for( s = 0; s < nsuccessors; ++s )
-         {
-            if( !SCIPstrToIntValue(endptr, &successors[s], &endptr) )
-               return SCIP_READERROR;
-         }
-
-         /* parse distances between the job and its successor and add the arc with their data to the precedence graph */
-         for( s = 0; s < nsuccessors; ++s )
-         {
-            char token[SCIP_MAXSTRLEN];
-            char* tmpptr;
-
-            SCIPstrCopySection(endptr, '[', ']', token, SCIP_MAXSTRLEN, &endptr);
-
-            if( SCIPstrToIntValue(token, &distance, &tmpptr) )
-            {
-               SCIP_CALL( SCIPdigraphAddArc(precedencegraph, job, successors[s], (void*)(size_t)distance) );
-
-               SCIPdebugPrintf(" %d[%d] ", successors[s], distance);
-            }
-            else
-               return SCIP_READERROR;
-         }
-
-         SCIPdebugPrintf("\n");
-
-         /* free the buffers */
-         SCIPfreeBufferArray(scip, &successors);
-      }
-      else
-         return SCIP_READERROR;
-
-      (*lineno)++;
-   }
-
-   /* get data for each job including a dummy job at the beginning and a dummy job at the end */
-   for( j = 0; j < njobs; ++j )
-   {
-      if( NULL != SCIPfgets(buf, sizeof(buf), file) )
-      {
-         int job;
-         int r;
-
-         /* get job id and check if only one mode is present */
-         SCIP_CALL( getJobId(scip, buf, &job, &endptr) );
-
-         /* get processing time */
-         SCIPstrToIntValue(endptr, &durations[job], &endptr);
-
-         SCIPdebugMessage("job %d has a processing times: %d\n", job, durations[job]);
-
-         for( r = 0; r < nresources; ++r )
-         {
-            SCIPstrToIntValue(endptr, &demands[job][r], &endptr);
-         }
-      }
-      else
-         return SCIP_READERROR;
-
-      (*lineno)++;
-   }
 
    /* get resources capacities */
    if( nresources > 0 && NULL != SCIPfgets(buf, sizeof(buf), file) )
@@ -188,11 +82,61 @@ SCIP_RETCODE parseDetails(
       }
 
       SCIPdebugPrintf("\n");
+
+      (*lineno)++;
    }
    else
       return SCIP_READERROR;
 
-   (*lineno)++;
+   /* get job details */
+   for( j = 0; j < njobs; ++j )
+   {
+      if( NULL != SCIPfgets(buf, sizeof(buf), file) )
+      {
+         int nsuccessors;
+         int r;
+         int s;
+
+         /* get job duration */
+         SCIPstrToIntValue(buf, &durations[j], &endptr);
+
+         SCIPdebugMessage("job %d: duration %d, demands (", j, durations[j]);
+
+         /* parse resources demands */
+         for( r = 0; r < nresources; ++r )
+         {
+            if( !SCIPstrToIntValue(endptr, &demands[j][r], &endptr) )
+               return SCIP_READERROR;
+
+            SCIPdebugPrintf(" %d ", demands[j][r]);
+         }
+
+         /* get number of successors */
+         SCIPstrToIntValue(endptr, &nsuccessors, &endptr);
+
+         SCIPdebugPrintf("), successors %d:", nsuccessors);
+
+         /* parse successor job ids */
+         for( s = 0; s < nsuccessors; ++s )
+         {
+            int successor;
+
+            if( !SCIPstrToIntValue(endptr, &successor, &endptr) )
+               return SCIP_READERROR;
+
+            /* add precedence to precedence graph */
+            SCIP_CALL( SCIPdigraphAddArc(precedencegraph, j, successor-1, (void*)(size_t)INT_MAX) );
+
+            SCIPdebugPrintf(" %d ", successor);
+         }
+
+         SCIPdebugPrintf("\n");
+      }
+      else
+         return SCIP_READERROR;
+
+      (*lineno)++;
+   }
 
    return SCIP_OKAY;
 }
@@ -206,7 +150,7 @@ SCIP_RETCODE readFile(
    )
 {
    SCIP_RETCODE retcode;
-   char buf[SCH_MAX_LINELEN];
+   char buf[SCIP_MAXSTRLEN];
    SCIP_DIGRAPH* precedencegraph;
    int** demands;
    int* durations;
@@ -226,14 +170,11 @@ SCIP_RETCODE readFile(
    if( NULL != SCIPfgets(buf, sizeof(buf), file) )
    {
       char* endptr;
-      int value;
 
       lineno++;
 
-      SCIPstrToIntValue(buf, &value, &endptr);
-
-      /* note that this format includes two dummy jobs */
-      njobs = value + 2;
+      /* get number of jobs */
+      SCIPstrToIntValue(buf, &njobs, &endptr);
 
       /* get number of resources */
       SCIPstrToIntValue(endptr, &nresources, &endptr);
@@ -257,6 +198,7 @@ SCIP_RETCODE readFile(
 
    retcode = parseDetails(scip, file, &lineno, demands, precedencegraph, durations, capacities, njobs, nresources);
 
+   /* create problem */
    if( retcode == SCIP_OKAY )
    {
       SCIP_CALL( SCIPcreateSchedulingProblem(scip, filename, NULL, NULL, demands,
@@ -280,21 +222,23 @@ SCIP_RETCODE readFile(
    return SCIP_OKAY;
 }
 
+/**@} */
 
-/*
- * Callback methods of reader
+/**@name Callback methods of reader
+ *
+ * @{
  */
 
 /** copy method for reader plugins (called when SCIP copies plugins) */
 static
-SCIP_DECL_READERCOPY(readerCopySch)
+SCIP_DECL_READERCOPY(readerCopyRcp)
 {  /*lint --e{715}*/
    assert(scip != NULL);
    assert(reader != NULL);
    assert(strcmp(SCIPreaderGetName(reader), READER_NAME) == 0);
 
    /* call inclusion method of reader handler */
-   SCIP_CALL( SCIPincludeReaderSch(scip) );
+   SCIP_CALL( SCIPincludeReaderRcp(scip) );
 
    return SCIP_OKAY;
 }
@@ -305,7 +249,7 @@ SCIP_DECL_READERCOPY(readerCopySch)
 
 /** problem reading method of reader */
 static
-SCIP_DECL_READERREAD(readerReadSch)
+SCIP_DECL_READERREAD(readerReadRcp)
 {  /*lint --e{715}*/
    SCIP_FILE* file;
    SCIP_RETCODE retcode;
@@ -317,7 +261,7 @@ SCIP_DECL_READERREAD(readerReadSch)
       return SCIP_NOFILE;
    }
 
-   /* read file */
+   /* read file and create problem */
    retcode = readFile(scip, file, filename);
 
    /* close file */
@@ -336,32 +280,27 @@ SCIP_DECL_READERREAD(readerReadSch)
 #define readerWriteSch NULL
 
 
+/**@} */
+
+/**@name Interface methods
+ *
+ * @{
+ */
+
 /*
  * reader specific interface methods
  */
 
 /** includes the sch file reader in SCIP */
-SCIP_RETCODE SCIPincludeReaderSch(
+SCIP_RETCODE SCIPincludeReaderRcp(
    SCIP*                 scip                /**< SCIP data structure */
    )
 {
-   SCIP_READERDATA* readerdata;
-   SCIP_READER* reader;
-
-   /* create sch reader data */
-   readerdata = NULL;
-
    /* include sch reader */
-   SCIP_CALL( SCIPincludeReaderBasic(scip, &reader, READER_NAME, READER_DESC, READER_EXTENSION, readerdata) );
-   assert(reader != NULL);
-
-   SCIP_CALL( SCIPsetReaderCopy(scip, reader, readerCopySch) );
-   SCIP_CALL( SCIPsetReaderRead(scip, reader, readerReadSch) );
-
-   /* add reader parameters */
-   SCIP_CALL( SCIPaddBoolParam(scip,
-         "reading/"READER_NAME"/mipmodel", "create MIP model?",
-         NULL, FALSE, FALSE, NULL, NULL) );
+   SCIP_CALL( SCIPincludeReader(scip, READER_NAME, READER_DESC, READER_EXTENSION,
+         readerCopyRcp, readerFreeSch, readerReadRcp, readerWriteSch, NULL) );
 
    return SCIP_OKAY;
 }
+
+/**@} */
