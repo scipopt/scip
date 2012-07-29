6dca10a55df00bd6b786d99ff764cbad679637ee
Stefan Heinz <heinz@zib.de>
2012-07-29 22:25:10 +0200

    added scheduler example to release

--- makedist.sh
+++ makedist.sh
@@ -90,6 +90,12 @@ $NAME/examples/MIPSolver/src/*.cpp \
 $NAME/examples/Queens/* $NAME/examples/Queens/doc/scip_intro.tex \
 $NAME/examples/Queens/src/depend.* \
 $NAME/examples/Queens/src/*.cpp $NAME/examples/Queens/src/*.hpp \
+$NAME/examples/Scheduler/Makefile
+$NAME/examples/Scheduler/doc/*
+$NAME/examples/Scheduler/check/testset/short.test $NAME/examples/Scheduler/check/testset/short.solu \
+$NAME/examples/Scheduler/src/depend.* \
+$NAME/examples/Scheduler/src/*.c $NAME/examples/Scheduler/src/*.h \
+$NAME/examples/Scheduler/data/*.sm \
 $NAME/examples/TSP/Makefile $NAME/examples/TSP/INSTALL \
 $NAME/examples/TSP/runme.sh $NAME/examples/TSP/runviewer.sh \
 $NAME/examples/TSP/sciptsp.set \
