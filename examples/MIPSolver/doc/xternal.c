c9471331d52bd3da29b5f2f15e79b1cffe87ba2f
Timo Berthold <berthold@zib.de>
2012-07-30 18:51:20 +0200

    added documentation for MIPsolver example

--- examples/MIPSolver/doc/xternal.c
+++ examples/MIPSolver/doc/xternal.c
@@ -15,7 +15,7 @@
 
 /**@file   xternal.c
  * @brief  main document page
- * @author Gerald Gamrath
+ * @author Timo Berthold
  */
 
 /*--+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
@@ -24,6 +24,12 @@
  * @version  0.1
  * @author   Tobias Achterberg
  *
- * This example illustrates how to integrate SCIP into your source code.
+ * This very basic example illustrates how to integrate SCIP into your C++ source code.  The SCIP header files are
+ * included from the directory which is denoted by SCIPDIR in the Makefile. If your SCIP headers are installed somewhere
+ * else, just change this link. Since this example is written in C++, objscip/objscip.h is included at the beginning of
+ * cppmain.cpp. It includes scip/scip.h (which you would include when using SCIP as callable library in a C
+ * program). The main function shows how the SCIP_RETCODE can be caught and handled. In runSCIP(), you see how a SCIP
+ * instance is created and freed, plus a few more things. Here, the SCIP_RETCODEs are not checked explicitly but handled
+ * by the SCIP_CALL macro. Please also note the CallableLibrary example for nonlinear problems.
  */
 
