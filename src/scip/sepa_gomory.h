d0f2e4901329a02c45c1dbb6247f0db7c1bae501
Stefan Heinz <heinz@zib.de>
2012-08-07 12:12:05 +0200

    fixed Gomory separator w.r.t. releasing cuts and bound changes

--- src/scip/sepa_gomory.h
+++ src/scip/sepa_gomory.h
@@ -17,6 +17,8 @@
  * @ingroup SEPARATORS
  * @brief  Gomory MIR Cuts
  * @author Tobias Achterberg
+ * @author Stefan Heinz
+ * @author Domenico Salvagnin
  */
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
