3c08f91a1df651c631c3a8c4215a42f73ea7df74
Michael Winkler <michael.winkler@zib.de>
2012-08-15 21:13:57 +0200

    renewed slurm accounts and queues in check_cluster
    Conflicts:
    
    	check/check_cluster.sh

--- check/check_cluster.sh
+++ check/check_cluster.sh
@@ -182,18 +182,23 @@ echo > $EVALFILE
 #define clusterqueue, which might not be the QUEUE, cause this might be an alias for a bunch of QUEUEs
 CLUSTERQUEUE=$QUEUE
 
-ACCOUNT="mip"
 NICE=""
-if test $CLUSTERQUEUE = "opt"
+ACCOUNT="mip"
+
+if test $CLUSTERQUEUE = "dbg"
 then
-    CLUSTERQUEUE="opt"
+    CLUSTERQUEUE="mip-dbg,telekom-dbg"
+    ACCOUNT="mip-dbg"
+elif test $CLUSTERQUEUE = "telekom-dbg"
+then
+    ACCOUNT="mip-dbg"
+elif test $CLUSTERQUEUE = "mip-dbg"
+then
+    ACCOUNT="mip-dbg"
 elif test $CLUSTERQUEUE = "opt-low"
 then
     CLUSTERQUEUE="opt"
     NICE="--nice=10000"
-elif test $CLUSTERQUEUE = "mip-dbg"
-then
-    ACCOUNT="mip-dbg"
 fi
 
 
