a5c1a606c2b80467aa7675cfefe7f107ab0eb5c4
Michael Winkler <michael.winkler@zib.de>
2012-12-13 00:23:53 +0100

    added forgotten SCIP version increase to 3.0.0.2

--- makedist.sh
+++ makedist.sh
@@ -2,7 +2,7 @@
 
 # For release versions, only use VERSION="x.x.x".
 # For development versions, use VERSION="x.x.x.x" with subversion number.
-VERSION="3.0.0.1"
+VERSION="3.0.0.2"
 NAME="scip-$VERSION"
 rm -f $NAME
 ln -s . $NAME
