df9853ba4d49394ac54173783c4e62bfa4106651
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2013-03-31 18:13:35 +0200

    changed version to 3.0.1.2

--- src/scip/def.h
+++ src/scip/def.h
@@ -71,7 +71,7 @@ extern "C" {
 #endif
 
 #define SCIP_VERSION                301 /**< SCIP version number (multiplied by 100 to get integer number) */
-#define SCIP_SUBVERSION               1 /**< SCIP sub version number */
+#define SCIP_SUBVERSION               2 /**< SCIP sub version number */
 #define SCIP_COPYRIGHT   "Copyright (c) 2002-2013 Konrad-Zuse-Zentrum fuer Informationstechnik Berlin (ZIB)"
 
 
