ec8537883595b5883650695d16b190ea489a73a2
Stefan Heinz <heinz@zib.de>
2013-05-02 17:44:00 +0200

    fixed compiler waring in Scheduler example

--- examples/Scheduler/src/reader_sm.c
+++ examples/Scheduler/src/reader_sm.c
@@ -754,7 +754,7 @@ SCIP_RETCODE SCIPcreateSchedulingProblem(
 
    /* compute a feasible upper bound on the makespan */
    ubmakespan = computeUbmakespan(durations, njobs);
-   //ubmakespan *= 2;
+   ubmakespan *= 2;
 
    /* allocate buffer for jobs and precedence constraints */
    SCIP_CALL( SCIPallocBufferArray(scip, &jobs, njobs) );
