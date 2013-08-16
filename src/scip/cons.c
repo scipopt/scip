d7ba0094e1d19818a33bd9450e4e3abb0f380f60
Michael Winkler <michael.winkler@zib.de>
2013-08-16 21:50:17 +0200

    fixed issued added with f65a2e0 with a too small int in struct cons

--- src/scip/cons.c
+++ src/scip/cons.c
@@ -5657,7 +5657,7 @@ void SCIPconsCapture(
    )
 {
    assert(cons != NULL);
-   assert(cons->nuses < (1 << 16) - 1);
+   assert(cons->nuses >= 0);
 
    SCIPdebugMessage("capture constraint <%s> with nuses=%d, cons pointer %p\n", cons->name, cons->nuses, (void*)cons);
    cons->nuses++;
@@ -7778,7 +7778,7 @@ void SCIPconsAddUpgradeLocks(
 {
    assert(cons != NULL);
 
-   assert(cons->nupgradelocks < (1 << 16) - nlocks);
+   assert(cons->nupgradelocks < (1 << 29) - nlocks);
    cons->nupgradelocks += nlocks;
 }
 
