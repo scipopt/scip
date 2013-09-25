81dee809eb7e944ccdcdb53f31c83766835b4192
Gregor Hendel <hendel@zib.de>
2013-09-25 11:22:50 +0200

    Minor revision of UCT-code

--- src/scip/scip.c
+++ src/scip/scip.c
@@ -33149,22 +33149,22 @@ SCIP_NODE* SCIPgetBestboundNode(
    return SCIPtreeGetLowerboundNode(scip->tree, scip->set);
 }
 
-/** access to all data of open nodes including leaves, children, and siblings
+/** access to all data of open nodes (leaves, children, and siblings)
  *
  *  @pre This method can be called if @p scip is in one of the following stages:
  *       - \ref SCIP_STAGE_SOLVING
  */
-void SCIPgetOpenNodesData(
+SCIP_RETCODE SCIPgetOpenNodesData(
    SCIP*                 scip,               /**< SCIP data structure */
-   SCIP_NODE***          leaves,
-   SCIP_NODE***          children,
-   SCIP_NODE***          siblings,
-   int*                  nleaves,
-   int*                  nchildren,
-   int*                  nsiblings
+   SCIP_NODE***          leaves,             /**< pointer to store the leaves, or NULL if not needed */
+   SCIP_NODE***          children,           /**< pointer to store the children, or NULL if not needed */
+   SCIP_NODE***          siblings,           /**< pointer to store the siblings, or NULL if not needed */
+   int*                  nleaves,            /**< pointer to store the number of leaves, or NULL */
+   int*                  nchildren,          /**< pointer to store the number of children, or NULL */
+   int*                  nsiblings           /**< pointer to store the number of siblings, or NULL */
    )
 {
-   SCIP_CALL_ABORT( checkStage(scip, "SCIPgetBestNode", FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, FALSE) );
+   SCIP_CALL( checkStage(scip, "SCIPgetOpenNodesData", FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, FALSE) );
 
    if( leaves != NULL )
       *leaves = SCIPnodepqNodes(scip->tree->leaves);
@@ -33178,6 +33178,8 @@ void SCIPgetOpenNodesData(
       *nchildren = SCIPtreeGetNChildren(scip->tree);
    if( nsiblings != NULL )
       *nsiblings = SCIPtreeGetNSiblings(scip->tree);
+
+   return SCIP_OKAY;
 }
 
 /** cuts off node and whole sub tree from branch and bound tree
