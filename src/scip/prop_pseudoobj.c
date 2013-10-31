b61d189175e09b83d4bfbe74907a3b58244ef9fa
Michael Winkler <michael.winkler@zib.de>
2013-10-31 15:17:01 +0100

    fixed handling of infinite maximal pseudo activity value

--- src/scip/prop_pseudoobj.c
+++ src/scip/prop_pseudoobj.c
@@ -3058,6 +3058,7 @@ SCIP_RETCODE propagateLowerbound(
       return SCIP_OKAY;
 
    maxpseudoobjact = getMaxObjPseudoactivity(scip, propdata);
+   assert(!SCIPisInfinity(scip, maxpseudoobjact) || !SCIPisInfinity(scip, lowerbound));
 
 #ifndef NDEBUG
    /* check that the global indices are correct */
@@ -3075,7 +3076,7 @@ SCIP_RETCODE propagateLowerbound(
       int nobjintvars;
       int v;
 
-      if( propdata->maxpseudoobjactinf == 0 )
+      if( propdata->maxpseudoobjactinf == 0 && !SCIPisInfinity(scip, maxpseudoobjact) )
       {
          SCIP_VAR** maxactvars;
          int nmaxactvars;
