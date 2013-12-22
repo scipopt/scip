6b22ac747483be0a8ef7944e41d2596853e2590b
Ambros Gleixner <gleixner@zib.de>
2013-12-22 18:42:21 +0100

    removed wrong asserts in lp.c connected to NULL LPI state
    - removed wrong assert that we reload a non-NULL state in SCIPlpEndDive()
    - removed wrong assert that we set a NULL LPI state only when LP is empty
    
    note that, e.g., when we run Cplex with barrier without crossover, LPI
    states are always NULL

--- src/scip/lp.c
+++ src/scip/lp.c
@@ -11950,11 +11950,7 @@ SCIP_RETCODE SCIPlpSetState(
 
    /* set LPI state in the LP solver */
    if( lpistate == NULL )
-   {
-      /**@todo the assert might be to hard since in case of solving the LP with the barrier the lpistate is also NULL */
-      assert(lp->nlpicols == 0 && lp->nlpirows == 0);
       lp->solisbasic = FALSE;
-   }
    else
    {
       SCIP_CALL( SCIPlpiSetState(lp->lpi, blkmem, lpistate) );
@@ -11978,7 +11974,10 @@ SCIP_RETCODE SCIPlpFreeState(
 {
    assert(lp != NULL);
 
-   SCIP_CALL( SCIPlpiFreeState(lp->lpi, blkmem, lpistate) );
+   if( *lpistate != NULL )
+   {
+      SCIP_CALL( SCIPlpiFreeState(lp->lpi, blkmem, lpistate) );
+   }
 
    return SCIP_OKAY;
 }
@@ -17153,7 +17152,6 @@ SCIP_RETCODE SCIPlpEndDive(
 
    assert(lp != NULL);
    assert(lp->diving);
-   assert(lp->divelpistate != NULL);
    assert(blkmem != NULL);
    assert(nvars == 0 || vars != NULL);
 
@@ -17201,7 +17199,9 @@ SCIP_RETCODE SCIPlpEndDive(
    /* restore LPI iteration limit */
    SCIP_CALL( lpSetIterationLimit(lp, lp->divinglpiitlim) );
 
-   /* reload LPI state saved at start of diving, free LPI state afterwards */
+   /* reload LPI state saved at start of diving and free it afterwards; it may be NULL, in which case simply nothing
+    * happens
+    */
    SCIP_CALL( SCIPlpSetState(lp, blkmem, set, eventqueue, lp->divelpistate,
          lp->divelpwasprimfeas, lp->divelpwasdualfeas) );
    SCIP_CALL( SCIPlpFreeState(lp, blkmem, &lp->divelpistate) );
