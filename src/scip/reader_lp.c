6a31a15cd5803b82914be4a422c7237671d4928c
Stefan Vigerske <stefan@math.hu-berlin.de>
2014-02-19 10:51:50 +0100

    binary variables have default bound 1
    - if not stated otherwise, then binary variables have a default upper
      bound of 1.0 in .lp files
    - however, when creating the variable and setting its bounds, the .lp
      reader does not know yet whether a variable is of binary type and thus
      creates it with upper bound of infinity
    - when recognizing later that a variable is binary, we should not complain
      about the upper bound at infinity

--- src/scip/reader_lp.c
+++ src/scip/reader_lp.c
@@ -1978,7 +1978,7 @@ SCIP_RETCODE readBinaries(
       lb = SCIPvarGetLbGlobal(var);
       ub = SCIPvarGetUbGlobal(var);
 
-      if( !SCIPisFeasZero(scip, lb) || !SCIPisFeasEQ(scip, ub, 1.0) )
+      if( !SCIPisFeasZero(scip, lb) || (!SCIPisFeasEQ(scip, ub, 1.0) && !SCIPisInfinity(scip, ub)) )
       {
          SCIPwarningMessage(scip, "variable <%s> declared as binary has non-binary bounds[%g, %g] -> if feasible, bounds will be adjusted\n", SCIPvarGetName(var), lb, ub);
       }
