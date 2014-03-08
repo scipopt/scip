b2e0b639823fada36688d0f10544e45d79d0b859
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2014-03-08 20:17:51 +0100

    add scripts to evaluate permutations

---
+++ check/permcmpresall.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+#*                                                                           *
+#*                  This file is part of the program and library             *
+#*         SCIP --- Solving Constraint Integer Programs                      *
+#*                                                                           *
+#*    Copyright (C) 2002-2014 Konrad-Zuse-Zentrum                            *
+#*                            fuer Informationstechnik Berlin                *
+#*                                                                           *
+#*  SCIP is distributed under the terms of the ZIB Academic License.         *
+#*                                                                           *
+#*  You should have received a copy of the ZIB Academic License              *
+#*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      *
+#*                                                                           *
+#* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+AWKARGS=""
+FILES=""
+for i in $@
+do
+    if test ! -e $i
+    then
+	AWKARGS="$AWKARGS $i"
+    else
+	FILES="$FILES $i"
+    fi
+done
+
+export LC_NUMERIC=C
+
+awk -f permcmpresall.awk $AWKARGS $FILES
+
+
