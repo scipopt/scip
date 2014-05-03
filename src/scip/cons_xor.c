8ac9796cf378b5ec217f6372fa78d126937f1571
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2014-05-03 12:24:53 +0200

    fix bug 718 in cons_xor

--- src/scip/cons_xor.c
+++ src/scip/cons_xor.c
@@ -2058,6 +2058,11 @@ SCIP_RETCODE checkSystemGF2(
          assert( var != NULL );
          assert( SCIPvarGetType(var) == SCIP_VARTYPE_BINARY );
 
+         /* replace negated variables */
+         if ( SCIPvarIsNegated(var) )
+            var = SCIPvarGetNegatedVar(var);
+         assert( var != NULL );
+
          /* consider nonfixed variables */
          if ( SCIPcomputeVarLbLocal(scip, var) < 0.5 && SCIPcomputeVarUbLocal(scip, var) > 0.5 )
          {
@@ -4102,7 +4107,6 @@ SCIP_DECL_CONSPROP(consPropXor)
             SCIP_CALL( checkSystemGF2(scip, conss, nusefulconss, NULL, result) );
          }
       }
-
    }
 
    return SCIP_OKAY;
