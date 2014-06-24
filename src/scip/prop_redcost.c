d522714a647bd240435bca1b40618793aa674800
Michael Winkler <michael.winkler@zib.de>
2014-06-24 15:55:30 +0200

    fixed indentation

--- src/scip/prop_redcost.c
+++ src/scip/prop_redcost.c
@@ -320,7 +320,7 @@ SCIP_RETCODE propagateRedcostVar(
          assert(SCIPisEQ(scip, oldlb, SCIPcolGetLb(col)));
          assert(SCIPisEQ(scip, oldub, SCIPcolGetUb(col)));
 
-        if( SCIPisFeasLT(scip, oldlb, oldub) )
+         if( SCIPisFeasLT(scip, oldlb, oldub) )
          {
             SCIP_Real newub;
             SCIP_Bool strengthen;
