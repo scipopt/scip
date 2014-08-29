d20cd4a832908f2730b62d36489e18e346a3fa4b
Gregor Hendel <hendel@zib.de>
2014-08-29 15:01:47 +0200

    revision of MOMIP example, added some todos

--- examples/MultiObjective/doc/xternal.c
+++ examples/MultiObjective/doc/xternal.c
@@ -20,7 +20,7 @@
 
 /*--+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
 
-/**@mainpage multi objective  example
+/**@mainpage Multi Objective Mixed Integer Programming using a Lifted Weight Space Algorithm
  * @version  0.1
  * @author   Timo Strunk
  *
@@ -29,31 +29,40 @@
  * There are different concepts of optimality in multi criterial optimization and in general there is more than one optimal solution.
  *
  * Given a problem of the form
- *
+ * @TODO integrality restrictions
  * \f$ \min Cx \f$ 
  * \f$ Ax \leq b \f$
  * 
  * the algorithm computes all feasible solutions which minimize \f$ w^TCx \f$ for some \f$ w \geq 0 \f$.
- * From the example directory root you can call the program with the command
  * 
- * bin/multiopt <instance>.mop [-v] [-t <time in seconds>]
+ * @TODO
+ * - specify path to a SCIP installation
+ * - install the lemon library headers (link?)
+ * - specify lemon include path and library path in lib (lemoninc, lemonlib.a)
+ *
+ * After the sources were sucessfully built, you can call the program from the example directory root via
+ *
+ *
+ *     bin/multiopt <instance>.mop [<SCIP-settings-file>.set]
+ *
+ *
+ * The optional settings file argument can be used to pass customized settings for the solving process inside SCIP
+ * as the underlying MIP solver. Since solving times and other statistics are accumulated over during the sequence
+ * of MIP optimization processes, a time limit for SCIP will be used as a threshold for the overall solving process
+ * of the Lifted Weight Space algorithm.
+ *
  *
  * The problem file has to be in mps format, except that more than one row is marked "N" indicating a cost vector.
  * The program expects a minimization problem.  The file ending has to be ".mop".
- * 
+ *
+ *  @TODO exact specifications as list
+ *
  * Tip: If you want to use ZIMPL to generate your instance, model the extra objectives as equality constraints with a right hand side of 0.
  * Then use ZIMPL to translate your instance to MPS. Now open the output file with an editor, find the names of the extra objectives and
  * change the mark "E" to "N" manually.
  * 
- * Options:\\
- * -v	output SCIP messages as well as computation results\\
- * -t	set a time limit.
  * 
  * 
- * The example implements a version of the weight space decomposition algorithm specified in 
- * "An Exact Algorithm for Finding Extreme Supported Nondominated Points of Multiobjective Mixed Integer Programs"
- * by Özgür Özpeynirci and Murat Köksalan
- * Manage. Sci., 56(12):2302{2315, December 2010}
  * 
  * The program implements the Lifted Weightspace Algorithm.  Basically, in each iteration, the algorithm chooses a weight 
  * \f$ w \in W = \{ w \geq 0 | \sum w = 1 \} \f$ and uses
@@ -64,7 +73,19 @@
  * In each iteration, the algorithm chooses a vertex \f$ (a,w) \f$ of \f$ P \f$ such that \f$ w \f$ has not yet been used in weighted optimization.
  * Every time a new solution is found, \f$ P \f$ is updated.
  * 
- * To understand the algorithm, consider this: The problem of finding the minimal weighted objective value a for every weight \f$ w \f$
+ * To understand the algorithm, consider this: The problem of finding the minimal weighted objective value \f$ a_w \f$ for every weight \f$ w \f$
  * is equivalent to computing the set of all pairs \f$ (a,w) \f$ such that there exists a solution \f$ x \f$ with \f$ a \geq w^TCx\f$. \f$ P \f$ 
- * is exactely the complement of that set. 
+ * is exactely the complement of that set.
+ * @TODO add one or two pictures
+ *
+ * @TODO explanation of classes with reference reader_mop.h
+ *
+ * @TODO add eigene referenz
+ *
+ *
+ * Further information about multi objective mixed integer programs can be found in
+ *
+ * Özgür Özpeynirci and Murat Köksalan,
+ * An Exact Algorithm for Finding Extreme Supported Nondominated Points of Multiobjective Mixed Integer Programs.
+ * _Management Science_, 56(12):2302--2315, 2010.
  */
--- examples/MultiObjective/src/reader_mop.h
+++ examples/MultiObjective/src/reader_mop.h
@@ -17,6 +17,9 @@
  * @ingroup FILEREADERS
  * @brief  multi objective mps reader
  * @author Timo Strunk
+ *
+ * The mop-reader adapts the mps-reader of SCIP by the functionality to read multiple objectives.
+ *
  */
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
