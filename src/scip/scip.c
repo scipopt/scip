cbd79e9ea6c974820150c725695334a8c05f602c
Gerald Gamrath <gamrath@zib.de>
2014-09-04 14:23:54 +0200

    reset obj limit correctly in SCIPfreeTransform() for maximization

--- src/scip/scip.c
+++ src/scip/scip.c
@@ -13321,7 +13321,7 @@ SCIP_RETCODE freeTransform(
    scip->set->stage = SCIP_STAGE_PROBLEM;
 
    /* reset objective limit */
-   SCIP_CALL( SCIPsetObjlimit(scip, SCIPinfinity(scip)) );
+   SCIP_CALL( SCIPsetObjlimit(scip, SCIP_INVALID) );
 
    /* reset original variable's local and global bounds to their original values */
    SCIP_CALL( SCIPprobResetBounds(scip->origprob, scip->mem->probmem, scip->set, scip->stat) );
