65c0b05aa8165b640e7d994e22f390a75f87c233
Gerald Gamrath <gamrath@zib.de>
2014-12-10 16:09:09 +0100

    fixed bug (#757) in cons_setppc with dual presolving disabled

--- src/scip/cons_setppc.c
+++ src/scip/cons_setppc.c
@@ -255,7 +255,9 @@ int setppcCompare2(
       return 0;
    else
    {
-      assert(consdata1->setppctype > consdata2->setppctype || (consdata1->setppctype == SCIP_SETPPCTYPE_PARTITIONING && consdata1->setppctype == consdata2->setppctype && consdata1->nvars > consdata2->nvars) || (consdata1->setppctype == SCIP_SETPPCTYPE_PACKING && consdata1->setppctype == consdata2->setppctype && consdata1->nvars < consdata2->nvars)); /*lint !e641*/
+      assert(consdata1->setppctype > consdata2->setppctype || ((consdata1->setppctype == consdata2->setppctype) &&
+            ((consdata1->setppctype == SCIP_SETPPCTYPE_PARTITIONING && consdata1->nvars > consdata2->nvars)
+               || (consdata1->setppctype == SCIP_SETPPCTYPE_PACKING && consdata1->nvars < consdata2->nvars)))); /*lint !e641*/
       return +1;
    }
 }
@@ -5626,7 +5628,7 @@ SCIP_RETCODE removeDoubleAndSingletonsAndPerformDualpresolve(
          }
       }
 
-      /* perform dualpresolve on set-packing constraints with exaclty two variables */
+      /* perform dualpresolve on set-packing constraints with exactly two variables */
       if( !donotaggr && consdata->nvars == 2 && dualpresolvingenabled && (SCIP_SETPPCTYPE)consdata->setppctype == SCIP_SETPPCTYPE_PACKING )
       {
          SCIP_VAR* var;
@@ -5742,14 +5744,14 @@ SCIP_RETCODE removeDoubleAndSingletonsAndPerformDualpresolve(
          aggregated = FALSE;
          nuplocks = SCIPvarGetNLocksUp(var);
          ndownlocks = SCIPvarGetNLocksDown(var);
-         assert((SCIPvarGetStatus(var) == SCIP_VARSTATUS_NEGATED && nuplocks >= 0 && ndownlocks >= 1) || (nuplocks >= 1 && ndownlocks >= 0));
+         assert(nuplocks >= 1 && ndownlocks >= 0); /* we are only treating set partitioning and set packing constraints, so every variable in there should have an uplock */
 
-         if( dualpresolvingenabled && (SCIP_SETPPCTYPE)consdata->setppctype == SCIP_SETPPCTYPE_PACKING && (nuplocks == 1 || ndownlocks == 1) && nuplocks + ndownlocks <= 2 )
+         if( dualpresolvingenabled && (SCIP_SETPPCTYPE)consdata->setppctype == SCIP_SETPPCTYPE_PACKING && nuplocks <= 1 && nuplocks + ndownlocks <= 2 )
          {
-            assert((SCIPvarGetStatus(var) == SCIP_VARSTATUS_NEGATED && nuplocks <= 1 && ndownlocks == 1) || (nuplocks == 1 && ndownlocks <= 1));
+            assert(nuplocks == 1 && ndownlocks <= 1);
 
             /* we found a redundant variable in a set-partitioning constraint */
-            if( nuplocks == 0 || ndownlocks == 0 )
+            if( ndownlocks == 0 )
             {
                SCIP_Real objval;
 
@@ -5936,14 +5938,14 @@ SCIP_RETCODE removeDoubleAndSingletonsAndPerformDualpresolve(
             ++ndecs;
          }
          /* we might have found a redundant variable */
-         else if( nuplocks <= 2 && nuplocks <= 2 && SCIPvarGetNegatedVar(var) != NULL )
+         else if( ndownlocks <= 2 && nuplocks <= 2 && SCIPvarGetNegatedVar(var) != NULL )
          {
             SCIP_CONSDATA* aggrconsdata;
             int image;
             int consindex;
             int varindex;
 
-            /* if we have two times the same variable in a set-partitioning constraint, so we cannot aggregate this */
+            /* if we have two times the same variable in a set-partitioning constraint, we cannot aggregate this */
             if( SCIPhashmapExists(vartoindex, (void*) var) )
             {
                image = (int) (size_t) SCIPhashmapGetImage(vartoindex, (void*) var);
@@ -5983,7 +5985,9 @@ SCIP_RETCODE removeDoubleAndSingletonsAndPerformDualpresolve(
             {
                assert((SCIP_SETPPCTYPE)consdata->setppctype == SCIP_SETPPCTYPE_PACKING);
 
-               /* if we have two times the same variable in a set-partitioning constraint, so we cannot aggregate this */
+               /* the negated variable did not occur in a set partitioning constraint (those will be iterated over
+                * first), so we cannot aggregate this variable
+                */
                if( !SCIPhashmapExists(vartoindex, (void*) SCIPvarGetNegatedVar(var)) )
                   continue;
             }
@@ -6019,13 +6023,16 @@ SCIP_RETCODE removeDoubleAndSingletonsAndPerformDualpresolve(
 
             /* must not multi-aggregate variables that are locked more then twice by all setppc constraints */
             if( (SCIP_SETPPCTYPE)consdata->setppctype == SCIP_SETPPCTYPE_PACKING &&
-               (SCIP_SETPPCTYPE)aggrconsdata->setppctype == SCIP_SETPPCTYPE_PACKING && nuplocks + ndownlocks > 2 )
+               (SCIP_SETPPCTYPE)aggrconsdata->setppctype == SCIP_SETPPCTYPE_PACKING )
+            {
+               assert(!dualpresolvingenabled || nuplocks + ndownlocks > 2);
                continue;
+            }
 
             assert((SCIP_SETPPCTYPE)consdata->setppctype == SCIP_SETPPCTYPE_PARTITIONING ||
                (SCIP_SETPPCTYPE)aggrconsdata->setppctype == SCIP_SETPPCTYPE_PARTITIONING);
 
-            /* we already remove a variable before, so our positioning information might be wrong, so we need to walk
+            /* we already removed a variable before, so our positioning information might be wrong, so we need to walk
              * over all variables again
              */
             if( chgtype[consindex] )
@@ -6043,6 +6050,8 @@ SCIP_RETCODE removeDoubleAndSingletonsAndPerformDualpresolve(
                }
                assert(v2 >= 0);
 #endif
+               assert((SCIP_SETPPCTYPE)consdata->setppctype == SCIP_SETPPCTYPE_PARTITIONING);
+
                SCIPdebugMessage("multi-aggregating in one set-partitioning or one set-packing constraint\n");
 
                /* perform aggregation on variables resulting from a set-partitioning constraint */
@@ -6059,7 +6068,7 @@ SCIP_RETCODE removeDoubleAndSingletonsAndPerformDualpresolve(
             {
                /* @note it might have happened that we have a variable at hand which exists actually in a set-packing
                 *       constraint and due to some other aggregation we increased the number of locks and reached this
-                *       part of the code, where we would expect only set-partitioning constraint in generally, so in
+                *       part of the code, where we would expect only set-partitioning constraints in general, so in
                 *       such a strange case we cannot aggregate anything
                 */
                if( (SCIP_SETPPCTYPE)aggrconsdata->setppctype != SCIP_SETPPCTYPE_PARTITIONING )
@@ -6067,6 +6076,7 @@ SCIP_RETCODE removeDoubleAndSingletonsAndPerformDualpresolve(
 
                assert(0 <= varindex && varindex < aggrconsdata->nvars);
                assert(aggrconsdata->vars[varindex] == SCIPvarGetNegatedVar(var));
+               assert((SCIP_SETPPCTYPE)aggrconsdata->setppctype == SCIP_SETPPCTYPE_PARTITIONING);
 
                SCIPdebugMessage("multi-aggregating in two set-partitioning or one set-partitioning and -packing constraint\n");
 
@@ -6091,11 +6101,15 @@ SCIP_RETCODE removeDoubleAndSingletonsAndPerformDualpresolve(
 
             if( ((nuplocks == 1 && ndownlocks == 0) || (nuplocks == 0 && ndownlocks == 1)) && (SCIP_SETPPCTYPE)consdata->setppctype == SCIP_SETPPCTYPE_PACKING )
             {
+               assert(deleteconsindex < 0);
+
                SCIP_CALL( delCoefPos(scip, cons, v) );
                ++(*nchgcoefs);
             }
             else if( nuplocks == 1 && ndownlocks == 1 && (SCIP_SETPPCTYPE)consdata->setppctype == SCIP_SETPPCTYPE_PARTITIONING )
             {
+               assert(deleteconsindex < 0);
+
                SCIP_CALL( delCoefPos(scip, cons, v) );
                ++(*nchgcoefs);
 
