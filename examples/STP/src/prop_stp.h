8ec6a835787f392f6cbcf253f97999b609165860
Daniel Rehfeldt <rehfeldt@zib.de>
2015-05-27 15:45:31 +0200

    ...propagator

---
+++ examples/STP/src/prop_stp.h
@@ -0,0 +1,48 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the program and library             */
+/*         SCIP --- Solving Constraint Integer Programs                      */
+/*                                                                           */
+/*    Copyright (C) 2002-2014 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SCIP is distributed under the terms of the ZIB Academic License.         */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**@file   prop_stp.h
+ * @ingroup PROPAGATORS
+ * @brief  propagator for Steiner problems, that makes use of the LP reduced cost and cutoff bound
+ * @author Daniel Rehfeldt
+ *
+ * This propagator uses the reduced cost of an optimally solved LP-relaxation to propagate the variables by means of the
+ * cutoff bound.
+ */
+
+/*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
+
+#ifndef __SCIP_PROP_STP_H__
+#define __SCIP_PROP_STP_H__
+
+#include "scip/scip.h"
+#include "grph.h"
+#include "probdata_stp.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** creates the stp propagator and includes it in SCIP */
+EXTERN
+SCIP_RETCODE SCIPincludePropStp(
+   SCIP*                 scip                /**< SCIP data structure */
+   );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
