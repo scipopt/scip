f2c5f7e2ad7d1c2204c92769c105bef8f4766c61
Gerald Gamrath <gamrath@zib.de>
2015-06-17 20:29:13 +0200

    fixed 868: numerical issue in bound widening

--- src/scip/cons_varbound.c
+++ src/scip/cons_varbound.c
@@ -504,7 +504,11 @@ SCIP_RETCODE resolvePropagation(
       {
          SCIP_Real relaxedbd;
 
-         if( SCIPvarIsIntegral(var) )
+         /* For integer variables, we can reduce the inferbound by 1 - 2eps, because this will be adjusted
+          * to the bound we need; however, if inferbound has a large value, adding 2*eps might be lost
+          * due to fixed precision floating point arithmetics, so we explicitly check this here.
+          */
+         if( SCIPvarIsIntegral(var) && inferbd < SCIPgetHugeValue(scip) * SCIPfeastol(scip))
             relaxedbd = (consdata->lhs - (inferbd - 1.0 + 2*SCIPfeastol(scip))) / vbdcoef;
          else
             relaxedbd = (consdata->lhs - inferbd) / vbdcoef;
