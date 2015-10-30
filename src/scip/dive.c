27ffd55a92197902b01c0f6c895267befc822281
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2015-10-30 18:29:01 +0100

    fixed missing check of contrsints in dive.c in the presence of indicator constraints

--- src/scip/dive.c
+++ src/scip/dive.c
@@ -455,7 +455,7 @@ SCIP_RETCODE SCIPperformGenericDivingAlgorithm(
 
             success = FALSE;
             /* try to add solution to SCIP */
-            SCIP_CALL( SCIPtrySol(scip, worksol, FALSE, FALSE, FALSE, FALSE, &success) );
+            SCIP_CALL( SCIPtrySol(scip, worksol, FALSE, FALSE, FALSE, changed, &success) );
 
             /* check, if solution was feasible and good enough */
             if( success )
