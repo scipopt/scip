ba4508a5a75333c6a02f6be94a5b984c337f50e1
Tobias Fischer <fischer@gsc.tu-darmstadt.de>
2015-11-10 14:40:52 +0100

    added documentation

--- src/scip/sepa_disjunctive.c
+++ src/scip/sepa_disjunctive.c
@@ -23,6 +23,10 @@
  * "A complementarity-based partitioning and disjunctive cut algorithm for mathematical programming problems with
  * equilibrium constraints"@n
  * Júdice, J.J., Sherali, H.D., Ribeiro, I.M., Faustino, A.M., Journal of Global Optimization 36(1), 89–114 (2006)
+ *
+ * Cut coefficients belonging to integer variables can be strengthened by the 'monoidal cut strengthening' procedure, see@n
+ * "Strengthening cuts for mixed integer programs"@n
+ * Egon Balas, Robert G. Jeroslow, European Journal of Operational Research, Volume 4, Issue 4, 1980, Pages 224-234
  */
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
