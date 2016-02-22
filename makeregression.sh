5c2fe91c37674ce7442965830cbe619a2a21a2a7
Matthias Miltenberger <miltenberger@zib.de>
2016-02-22 22:07:33 +0100

    regression tests using LPSOPT=dbg

--- makeregression.sh
+++ makeregression.sh
@@ -66,6 +66,12 @@ do
 
     for OPT in ${OPTS[@]}
     do
+        if [ "$OPT" == "dbg" ];
+        then
+            LPSOPT=dbg
+        else
+            LPSOPT=opt
+        fi
 
         for TEST in ${TESTS[@]}
         do
@@ -73,6 +79,7 @@ do
             cd ../soplex
             git pull
             make
+            make OPT=dbg
 
             # pull new version from git repository
             cd ../scip
@@ -85,8 +92,8 @@ do
             GITHASH=`git describe --always --dirty  | sed -re 's/^.+-g//'`
 
             # compile SCIP in debug or opt mode
-            make OPT=$OPT VERSION=$GITHASH LPS=$LPS ZIMPL=true clean
-            make OPT=$OPT VERSION=$GITHASH LPS=$LPS ZIMPL=true
+            make OPT=$OPT LPSOPT=$LPSOPT VERSION=$GITHASH LPS=$LPS ZIMPL=true clean
+            make OPT=$OPT LPSOPT=$LPSOPT VERSION=$GITHASH LPS=$LPS ZIMPL=true
 
             # run test
             make OPT=$OPT VERSION=$GITHASH LPS=$LPS TIME=$TIME LOCK=$LOCK CONTINUE=$CONTINUE TEST=$TEST MEM=$MEM test
