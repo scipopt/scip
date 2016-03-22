be5185967b89a936b2d7891ffa4e3aad7dc683fb
Thomas <nagel@zib.de>
2016-03-22 11:49:47 +0100

    edited doc reference checker by Gregor's useful comments

--- scripts/check_doc.sh
+++ scripts/check_doc.sh
@@ -1,19 +1,29 @@
 #! /bin/bash
 
-# regex for all canonical files in given directory
-rEGEXFILE="$1/*.*"
+# filters doxygen documentation files for occurrences of unresolved references
+#
+# usage: check_doc.sh <DIRECTORYNAME>
+#
+# DIRECTORYNAME should be the root directory of doxygen-generated documentation, e.g., 'doc/html'
 
-# lists all files in directory which does not contain source in their name, but @ref in the file with their number of matches
-GREPRESULT=`ls $rEGEXFILE -a | grep "source" -v | xargs grep "@ref" -l | xargs grep "@ref" -c`
+# get file list after excluding source files
+FILELIST=`ls $1/*.* -a | grep "_source" -v`
+
+# lists all files in directory which do not contain source in their name,
+# but '@ref' in the file with their number of matches
+GREPRESULT=`grep "@ref" -l $FILELIST | xargs grep "@ref" -cH`
 
 # gives them out
 for RESULT in ${GREPRESULT[@]}
 do
+    # check length of RESULT variable: If grep command finds no match, GREPRESULT is '0'
     if [ "${#RESULT}" -gt "2" ]
     then
-        echo WARNING: unresolved references @ref in: $RESULT
+        echo === Warning: unresolved references @ref in: $RESULT
     fi
 done
 
-# lists all files in directory which does not contain source in their name, but @ref in the file and classifies the unresolved references by their matching
-echo classes of unresolved references:`ls $rEGEXFILE -a | grep "source" -v | xargs grep -hP "@ref [A-Z][^ .<>]*" -o | sort | uniq`
+# lists all files in directory which do not contain source in their name,
+# but '@ref' in the file and classifies the unresolved references by their matching
+echo === Classes of unresolved references:
+echo `grep -hP "@ref [A-Za-z][^ .<>]*" -o $FILELIST | sort | uniq`
