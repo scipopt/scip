60275faf7e9b1c7ed8cd271c768b42e3ba2375c2
Thomas <nagel@zib.de>
2016-03-22 11:17:14 +0100

    style increased

--- scripts/check_doc.sh
+++ scripts/check_doc.sh
@@ -1,8 +1,10 @@
+#! /bin/bash
+
 # regex for all canonical files in given directory
-CHECKDIR="$1*.*"
+rEGEXFILE="$1/*.*"
 
 # lists all files in directory which does not contain source in their name, but @ref in the file with their number of matches
-GREPRESULT=`ls $CHECKDIR -a | grep "source" -v | xargs grep "@ref" -l | xargs grep "@ref" -c`
+GREPRESULT=`ls $rEGEXFILE -a | grep "source" -v | xargs grep "@ref" -l | xargs grep "@ref" -c`
 
 # gives them out
 for RESULT in ${GREPRESULT[@]}
@@ -14,4 +16,4 @@ do
 done
 
 # lists all files in directory which does not contain source in their name, but @ref in the file and classifies the unresolved references by their matching
-echo classes of unresolved references:`ls $CHECKDIR -a | grep "source" -v | xargs grep -hP "@ref [A-Z][^ .<>]*" -o | sort | uniq | tee fails.txt`
+echo classes of unresolved references:`ls $rEGEXFILE -a | grep "source" -v | xargs grep -hP "@ref [A-Z][^ .<>]*" -o | sort | uniq`
