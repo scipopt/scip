331eaa5f736af3594e7ba5daab08bfba32e5ec4a
Felipe Serrano <serrano@zib.de>
2016-03-25 03:58:54 +0100

    add unittest

---
+++ unittests/src/unittest-consexpr/unittest-consexpr.c
@@ -0,0 +1,110 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the program and library             */
+/*         SCIP --- Solving Constraint Integer Programs                      */
+/*                                                                           */
+/*    Copyright (C) 2002-2016 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SCIP is distributed under the terms of the ZIB Academic License.         */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**@file   cmain.c
+ * @brief  unit test for checking cons_expr
+ *
+ */
+
+/*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
+
+#include <string.h>
+
+#include "scip/scip.h"
+#include "scip/cons_expr.h"
+
+/** macro to check the return of tests
+ *
+ *  @note assumes the existence of SCIP_RETCODE retcode
+ */
+#define CHECK_TEST(x)                            \
+   do                                            \
+   {                                             \
+      retcode = (x);                             \
+      if( retcode != SCIP_OKAY )                 \
+      {                                          \
+         printf("Unit test " #x " failed\n");    \
+         SCIPprintError(retcode);                \
+         return -1;                              \
+      }                                          \
+   }                                             \
+   while( FALSE )
+
+
+/*
+ * Local methods for tests
+ */
+
+
+/*
+ * TESTS
+ */
+
+/** test something */
+static
+SCIP_RETCODE testCons(void)
+{
+   SCIP* scip;
+   SCIP_CONSHDLR* conshdlr;
+
+   conshdlr = NULL;
+
+   SCIP_CALL( SCIPcreate(&scip) );
+
+   /* include cons_expr: this adds the operator handlers */
+   SCIP_CALL( SCIPincludeConshdlrExpr(scip) );
+
+   /* currently expr constraints cannot be created */
+   /* get expr conshdlr */
+   conshdlr = SCIPfindConshdlr(scip, "expr");
+   assert(conshdlr != NULL);
+
+   /* create problem */
+   SCIP_CALL( SCIPcreateProbBasic(scip, "test_problem") );
+
+   /* I don't have access to the operator hdlr, so basically I can't do anything */
+
+   /* test something? */
+
+   return SCIP_OKAY;
+}
+
+
+
+/** main function */
+int
+main(
+   int                        argc,
+   char**                     argv
+   )
+{
+   SCIP_RETCODE retcode;
+
+   /* output stuff for automatic unittest evaluation */
+   printf("@01 unittest-consexpr ===========\n");
+   printf("=opt=  unittest-consexpr 0\n\n");
+
+   CHECK_TEST( testCons() );
+
+   /* for automatic testing output the following */
+   printf("SCIP Status        : all tests passed\n");
+   printf("Ignore the following:\n");
+   printf("  solving          : 0.00\n");
+   printf("  nodes (total)    : 0\n");
+   printf("  Primal Bound     : 0.0\n");
+   printf("  Dual Bound       : 0.0\n");
+
+   return 0;
+}
