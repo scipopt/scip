9c24de36914c76a04a4e08d40451fe8b2d1b460d
Sebastian Schenker <schenker@math.tu-berlin.de>
2016-05-12 21:35:08 +0200

    new main()

---
+++ applications/PolySCIP/src/new_main.cpp
@@ -0,0 +1,31 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*        This file is part of the program PolySCIP                          */
+/*                                                                           */
+/*    Copyright (C) 2012-2016 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  PolySCIP is distributed under the terms of the ZIB Academic License.     */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with PolySCIP; see the file LICENCE.                               */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/** @brief  main file
+ *
+ * File containing main function
+ */
+
+#include <iostream>
+
+#include "polyscip.h"
+
+using polyscip::Polyscip;
+
+int main(int argc, char** argv) {
+    std::cout << "Starting PolySCIP...\n";
+    Polyscip p(argc, (const char *const *) argv);
+
+    return 0;
+}
