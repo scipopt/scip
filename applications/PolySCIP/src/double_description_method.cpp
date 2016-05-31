148fb6c80984188c006a3adffb111e0b8b6b4855
Sebastian Schenker <schenker@zib.de>
2016-05-31 18:54:16 +0200

    update VRepresentation

--- applications/PolySCIP/src/double_description_method.cpp
+++ applications/PolySCIP/src/double_description_method.cpp
@@ -16,6 +16,7 @@
 
 #include <algorithm>
 #include <iterator>
+#include <map>
 #include <utility>
 #include <vector>
 
@@ -24,6 +25,7 @@
 #include "polyscip_types.h"
 
 using std::pair;
+using std::size_t;
 using std::vector;
 
 namespace polyscip {
@@ -39,61 +41,139 @@ namespace polyscip {
 
     void VRepresentation::computeVRep() {
         assert (!bounded_.empty());
-        global::print(bounded_.front(), "initial bounded point");
         computeInitialRep(bounded_.front());
         auto current_v_rep = initial_v_rep_;
+        std::cout << "INITIAL VREP:\n";
+        printVRep(current_v_rep);
         for (auto bd = std::next(begin(bounded_)); bd != end(bounded_); ++bd) {
-            current_h_rep_.emplace_back(*bd, -1.);
-            current_v_rep = extendVRep(std::move(current_v_rep), current_h_rep_.back());
+            auto new_constraint = H_RepT(*bd,-1);
+            current_v_rep = extendVRep(std::move(current_v_rep), new_constraint);
+            current_h_rep_.push_back(new_constraint);
+            std::cout << "NEW VREP:\n";
+            printVRep(current_v_rep);
         }
         for (auto unbd = begin(unbounded_); unbd != end(unbounded_); ++unbd) {
-            current_h_rep_.emplace_back(*unbd, 0.);
-            current_v_rep = extendVRep(std::move(current_v_rep), current_h_rep_.back());
+            auto new_constraint = H_RepT(*unbd, 0.);
+            current_v_rep = extendVRep(std::move(current_v_rep), new_constraint);
+            current_h_rep_.push_back(new_constraint);
         }
         v_rep_ = current_v_rep;
         deleteZeroWeightRay();
         normalizeVRep();
     }
 
-    vector<VRepresentation::V_RepT> VRepresentation::extendVRep(vector<V_RepT> current_v_rep,
+    vector<VRepresentation::V_RepT> VRepresentation::extendVRep(vector<V_RepT> current_rep,
                                                                 const H_RepT &constraint) {
+
         auto extended_v_rep = vector<V_RepT> {};
         auto plus_inds = vector<std::size_t> {};
         auto minus_inds = vector<std::size_t> {};
+        auto zero_slack_map = SlackMap {};
+
         // partition current v-representation
-        for (std::size_t i = 0; i < current_v_rep.size(); ++i) {
-            auto v_rep = current_v_rep[i];
-            assert (v_rep.first.size() == constraint.first.size());
-            auto result = std::inner_product(begin(v_rep.first), end(v_rep.first),
-                                             begin(constraint.first), v_rep.second * constraint.second);
+        for (size_t i = 0; i < current_rep.size(); ++i) {
+            zero_slack_map.emplace(i, computeZeroSlackSet(current_rep[i]));
+            auto result = std::inner_product(begin(current_rep[i].first), end(current_rep[i].first),
+                                             begin(constraint.first), current_rep[i].second * constraint.second);
             if (SCIPisNegative(scip_, result)) {
                 minus_inds.push_back(i);
             }
             else if (SCIPisZero(scip_, result)) {
-                extended_v_rep.push_back(v_rep);
+                extended_v_rep.push_back(current_rep[i]);
             }
             else {
                 assert(SCIPisPositive(scip_, result));
                 plus_inds.push_back(i);
-                extended_v_rep.push_back(v_rep);
+                extended_v_rep.push_back(current_rep[i]);
             }
         }
-        // compute zero sets for rays of plus and minus partition
-        /*auto plus_inds_zero_set = vector<vector<std::size_t>> {};
-        for (const auto& index : plus_inds)
-            plus_inds_zero_set.push_back(computeZeroSet(current_v_rep[index]));
-        auto minus_inds_zero_set = vector<vector<std::size_t>> {};
-        for (const auto& index : minus_inds)
-            minus_inds_zero_set.push_back(computeZeroSet(current_v_rep[index]));*/
-        // compute new rays of adjacent rays of plus and minus partition
-        for (const auto &p_ind : plus_inds) {
-            for (const auto &m_ind : minus_inds) {
-                extended_v_rep.push_back(getNewVertex(current_v_rep[p_ind],
-                                                      current_v_rep[m_ind],
-                                                      constraint));
-            }
+
+        auto adj_pairs = computeAdjacentPairs(plus_inds, minus_inds, zero_slack_map, current_rep);
+        for (const auto& p : adj_pairs) {
+            extended_v_rep.push_back(computeNewRay(current_rep[p.first],
+                                                   current_rep[p.second],
+                                                   constraint));
         }
         return extended_v_rep;
+    }
+
+    vector<pair<size_t, size_t>> VRepresentation::computeAdjacentPairs(const vector<size_t>& plus_inds,
+                                                                       const vector<size_t>& minus_inds,
+                                                                       const SlackMap& zero_slacks,
+                                                                       const vector<V_RepT>& current_rep) const {
+        auto adj_pairs = vector<pair<size_t, size_t>> {};
+        for (const auto& plus_index : plus_inds) {
+            for (const auto& minus_index : minus_inds) {
+                if (rayPairIsAdjacent(plus_index, minus_index, zero_slacks, current_rep))
+                    adj_pairs.push_back({plus_index, minus_index});
+            }
+        }
+        return adj_pairs;
+    };
+
+    bool VRepresentation::rayPairIsAdjacent(size_t plus_ind,
+                                            size_t minus_ind,
+                                            const SlackMap& zero_slack,
+                                            const vector<V_RepT>& current_rep) const {
+        auto intersec = vector<size_t> {};
+        std::set_intersection(begin(zero_slack.at(plus_ind)),
+                              end(zero_slack.at(plus_ind)),
+                              begin(zero_slack.at(minus_ind)),
+                              end(zero_slack.at(minus_ind)),
+                              std::back_inserter(intersec));
+
+        for (const auto& kv : zero_slack) {
+            if (kv.first == plus_ind || kv.first == minus_ind || kv.second.size() <= intersec.size())
+                continue;
+            auto includes = std::includes(begin(kv.second),
+                                          end(kv.second),
+                                          begin(intersec),
+                                          end(intersec));
+
+            if (includes && !isMultiple(current_rep[kv.first], current_rep[plus_ind]) &&
+                !isMultiple(current_rep[kv.first], current_rep[minus_ind]))
+                return false;
+        }
+        return true;
+    }
+
+    bool VRepresentation::isMultiple(const V_RepT& ray, const V_RepT& ray2) const {
+        assert (ray.first.size() == ray2.first.size());
+        if (SCIPisZero(scip_, ray.second) && !SCIPisZero(scip_, ray2.second)) {
+            return false;
+        }
+        else if (!SCIPisZero(scip_, ray.second) && SCIPisZero(scip_,ray2.second)) {
+            return false;
+        }
+        else if (SCIPisEQ(scip_, ray.second, ray2.second)) {
+            for (size_t i=0; i<ray.first.size(); ++i) {
+                if (!SCIPisEQ(scip_, ray.first[i], ray2.first[i]))
+                    return false;
+            }
+            return true;
+        }
+        else {
+            // at this point: ray.second != ray2.second, ray.second != 0, ray2.second != 0
+            auto multiple = ray.second / ray2.second;
+            for (size_t i=0; i<ray.first.size(); ++i) {
+                if (!SCIPisEQ(scip_, ray.first[i], ray2.first[i]*multiple))
+                    return false;
+            }
+            return true;
+        }
+    }
+
+    vector<size_t> VRepresentation::computeZeroSlackSet(const VRepresentation::V_RepT& ray) const {
+        auto zeroSet = vector<size_t> {};
+        for (size_t i = 0; i < current_h_rep_.size(); ++i) {
+            auto weight_coeff = current_h_rep_[i].first;
+            auto a_coeff = current_h_rep_[i].second;
+            auto result = std::inner_product(begin(weight_coeff), end(weight_coeff),
+                                             begin(ray.first), a_coeff*ray.second);
+            if (SCIPisZero(scip_, result))
+                zeroSet.push_back(i);
+        }
+        return zeroSet;
     }
 
     void VRepresentation::normalizeVRep() {
@@ -106,48 +186,33 @@ namespace polyscip {
         }
     }
 
-    VRepresentation::V_RepT VRepresentation::getNewVertex(const V_RepT &ray_plus, const V_RepT &ray_minus,
-                                                          const H_RepT &ineq) const {
-        auto size = ray_plus.first.size();
-        assert (size == ray_minus.first.size());
+    VRepresentation::V_RepT VRepresentation::computeNewRay(const V_RepT &plus_ray, const V_RepT &minus_ray,
+                                                       const H_RepT &ineq) const {
+        auto size = plus_ray.first.size();
+        assert (size == minus_ray.first.size());
         assert (size == ineq.first.size());
         // m_coeff = ineq \cdot ray_plus
         auto m_coeff = std::inner_product(begin(ineq.first), end(ineq.first),
-                                          begin(ray_plus.first), ineq.second * ray_plus.second);
+                                          begin(plus_ray.first), ineq.second * plus_ray.second);
         // p_coeff = ineq \cdot ray_minus
         auto p_coeff = std::inner_product(begin(ineq.first), end(ineq.first),
-                                          begin(ray_minus.first), ineq.second * ray_minus.second);
+                                          begin(minus_ray.first), ineq.second * minus_ray.second);
         // return m_coeff * ray_minus - p_coeff * ray_plus
         auto new_weight = WeightType(size, 0.);
-        std::transform(begin(ray_minus.first), end(ray_minus.first),
-                       begin(ray_plus.first), begin(new_weight),
+        std::transform(begin(minus_ray.first), end(minus_ray.first),
+                       begin(plus_ray.first), begin(new_weight),
                        [m_coeff, p_coeff](ValueType m_val, ValueType p_val) {
                            return m_coeff * m_val - p_coeff * p_val;
                        });
-        return {new_weight, m_coeff * ray_minus.second - p_coeff * ray_plus.second};
+        return {new_weight, m_coeff * minus_ray.second - p_coeff * plus_ray.second};
     }
-
-    /*vector<std::size_t> VRepresentation::computeZeroSet(const VRepresentation::V_RepT& ray) {
-        auto zeroSet = vector < std::size_t > {};
-        for (std::size_t i = 0; i < current_h_rep_.size(); ++i) {
-            auto lhs = current_h_rep_[i].first;
-            auto rhs = current_h_rep_[i].second;
-            auto result = std::inner_product(begin(lhs), end(lhs),
-                                             begin(lhs), -ray.second * rhs);
-            if (SCIPisZero(scip_, result))
-                zeroSet.push_back(i);
-        }
-        assert(zeroSet.size() == current_h_rep_.size() - 1);
-        std::sort(begin(zeroSet), end(zeroSet));
-        return zeroSet;
-    }*/
 
 
     void VRepresentation::computeInitialRep(const OutcomeType &bd_outcome) {
         auto size = bd_outcome.size();
         initial_v_rep_.emplace_back(WeightType(size, 0), -1.);
         current_h_rep_.emplace_back(bd_outcome, -1.);
-        for (std::size_t i = 0; i < bd_outcome.size(); ++i) {
+        for (size_t i = 0; i < bd_outcome.size(); ++i) {
             auto ray = WeightType(size, 0.);
             ray[i] = 1.;
             initial_v_rep_.push_back({ray, bd_outcome[i]});
--- applications/PolySCIP/src/double_description_method.h
+++ applications/PolySCIP/src/double_description_method.h
@@ -20,9 +20,13 @@
 #ifndef POLYSCIP_SRC_DOUBLE_DESCRIPTION_METHOD_H_INCLUDED
 #define POLYSCIP_SRC_DOUBLE_DESCRIPTION_METHOD_H_INCLUDED
 
+#include <iostream>
+#include <map>
+#include <ostream>
 #include <utility>
 #include <vector>
 
+#include "global_functions.h"
 #include "objscip/objscip.h"
 #include "polyscip_types.h"
 
@@ -40,6 +44,8 @@ namespace polyscip {
         std::vector<V_RepT> getVRep() const {return v_rep_;};
 
     private:
+        using SlackMap = std::map<std::size_t, std::vector<std::size_t>>;
+
         /** Computes initial v-representation for the following h-representation:
          * 1) bounded \cdot (w_1,...,w_k) - a >= 0
          * 2) w_1 >= 0
@@ -48,14 +54,31 @@ namespace polyscip {
          */
         void computeInitialRep(const OutcomeType& bounded);
 
-        std::vector<std::size_t> computeZeroSet(const V_RepT& ray) = delete;
+        std::vector<std::size_t> computeZeroSlackSet(const V_RepT& ray) const;
 
-        std::vector<V_RepT> extendVRep(std::vector<V_RepT> current_v_rep, const H_RepT& ineq);
+        bool rayPairIsAdjacent(std::size_t plus_index,
+                               std::size_t minus_index,
+                               const SlackMap& zero_slacks,
+                               const std::vector<V_RepT>& current_v_rep) const;
 
-        V_RepT getNewVertex(const V_RepT& ray_plus, const V_RepT& ray_minus, const H_RepT& ineq) const;
+        /** Check whether first parameter is multiple of second or third parameter
+         */
+        bool isMultiple(const V_RepT& ray, const V_RepT& ray2) const;
+
+        std::vector<std::pair<std::size_t, std::size_t>> computeAdjacentPairs(const std::vector<std::size_t>& plus_indices,
+                                                                              const std::vector<std::size_t>& minus_indices,
+                                                                              const SlackMap& zero_slacks,
+                                                                              const std::vector<V_RepT>& current_v_rep) const;
+
+        std::vector<V_RepT> extendVRep(std::vector<V_RepT> current_v_rep, const H_RepT& new_constraint);
+
+        V_RepT computeNewRay(const V_RepT& plus_ray, const V_RepT& minus_ray, const H_RepT& new_constraint) const;
 
         void normalizeVRep();
         void deleteZeroWeightRay();
+
+        template<typename ContainerOfPairs>
+        void printVRep(const ContainerOfPairs& container, std::ostream &os = std::cout) const;
 
         SCIP* scip_;
         std::vector<OutcomeType> bounded_;
@@ -64,6 +87,15 @@ namespace polyscip {
         std::vector<V_RepT> initial_v_rep_;
         std::vector<V_RepT> v_rep_;
     };
+
+    template<typename ContainerOfPairs>
+    void VRepresentation::printVRep(const ContainerOfPairs& container, std::ostream& os) const {
+        for (const auto& elem : container) {
+            os << "A_coeff = " << elem.second;
+            global::print(elem.first, "Weight coeffs = ", os);
+            os << "\n";
+        }
+    }
 
 }
 #endif //POLYSCIP_SRC_DOUBLE_DESCRIPTION_METHOD_H_INCLUDED
