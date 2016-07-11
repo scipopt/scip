a01c3550b36b9a793fb07265d28367b3d82235d7
Gerald Gamrath <gamrath@zib.de>
2016-07-11 15:15:17 +0200

    corrected copying of constraint flags in cons_xor

--- src/scip/cons_xor.c
+++ src/scip/cons_xor.c
@@ -3641,10 +3641,8 @@ SCIP_RETCODE detectRedundantConstraints(
             goto TERMINATE;
          }
 
-         /* update flags of constraint which caused the redundancy s.t. nonredundant information doesn't get lost */
+         /* delete cons0 and update flags of cons1 s.t. nonredundant information doesn't get lost */
          SCIP_CALL( SCIPupdateConsFlags(scip, cons1, cons0) );
-
-         /* delete consdel */
          SCIP_CALL( SCIPdelCons(scip, cons0) );
          (*ndelconss)++;
 
@@ -3960,6 +3958,9 @@ SCIP_RETCODE preprocessConstraintPairs(
                SCIPconsGetName(cons0), SCIPconsGetName(cons1), SCIPconsGetName(cons1));
             SCIPdebugPrintCons(scip, cons0, NULL);
             SCIPdebugPrintCons(scip, cons1, NULL);
+
+            /* delete cons1 and update flags of cons0 s.t. nonredundant information doesn't get lost */
+            SCIP_CALL( SCIPupdateConsFlags(scip, cons0, cons1) );
             SCIP_CALL( SCIPdelCons(scip, cons1) );
             (*ndelconss)++;
 
@@ -4041,6 +4042,9 @@ SCIP_RETCODE preprocessConstraintPairs(
             *cutoff = *cutoff || infeasible;
             if ( fixed )
                (*nfixedvars)++;
+
+            /* delete cons1 and update flags of cons0 s.t. nonredundant information doesn't get lost */
+            SCIP_CALL( SCIPupdateConsFlags(scip, cons0, cons1) );
             SCIP_CALL( SCIPdelCons(scip, cons1) );
             (*ndelconss)++;
          }
@@ -4082,6 +4086,9 @@ SCIP_RETCODE preprocessConstraintPairs(
             assert(infeasible || fixed);
             *cutoff = *cutoff || infeasible;
             (*nfixedvars)++;
+
+            /* delete cons1 and update flags of cons0 s.t. nonredundant information doesn't get lost */
+            SCIP_CALL( SCIPupdateConsFlags(scip, cons0, cons1) );
             SCIP_CALL( SCIPdelCons(scip, cons1) );
             (*ndelconss)++;
          }
@@ -4138,6 +4145,8 @@ SCIP_RETCODE preprocessConstraintPairs(
 
          if( redundant )
          {
+            /* delete cons1 and update flags of cons0 s.t. nonredundant information doesn't get lost */
+            SCIP_CALL( SCIPupdateConsFlags(scip, cons0, cons1) );
             SCIP_CALL( SCIPdelCons(scip, cons1) );
             (*ndelconss)++;
 
