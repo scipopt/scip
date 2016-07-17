fb76655165dd00dbeccf2e10e9742c29492940f1
Stefan Vigerske <svigerske@gams.com>
2016-07-17 17:50:20 +0200

    disable ubsan on SCIPcalcFibHash and store intermediate in longer int
    - after casting to an unsigned int, there is nothing left to do for
      "% UINT_MAX", so cast the intermediate product to an unsigned long long
      instead
    - for large v, the cast of the product to an integer gives a runtime error
      from the undefined behavior sanitizer, which we can ignore here

--- src/scip/misc.c
+++ src/scip/misc.c
@@ -10182,15 +10182,14 @@ SCIP_Longint SCIPcalcBinomCoef(
 
 #ifndef NDEBUG
 /** calculates hash for floating-point number by using Fibonacci hashing */
+#if defined(__GNUC__) && __GNUC__ * 100 + __GNUC_MINOR__ * 10 >= 490
+__attribute__((no_sanitize_undefined))
+#endif
 unsigned int SCIPcalcFibHash(
    SCIP_Real             v                   /**< number to hash */
    )
 {
-   unsigned int res;
-
-   res = ((unsigned int)(v * 2654435769)) % UINT_MAX;
-
-   return res;
+   return ((unsigned long long)(v * 2654435769)) % UINT_MAX;
 }
 #endif
 
