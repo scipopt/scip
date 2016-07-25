8044602e5333da33d472867a5910582e433e3c0c
Benjamin Mueller <benjamin.mueller@zib.de>
2016-07-25 17:39:20 +0200

    add separation for absolute expressions

--- src/scip/cons_expr_abs.c
+++ src/scip/cons_expr_abs.c
@@ -162,6 +162,140 @@ SCIP_DECL_CONSEXPR_EXPRINTEVAL(intevalAbs)
    return SCIP_OKAY;
 }
 
+/** helper function to separate a given point; needed for proper unittest */
+static
+SCIP_RETCODE separatePointAbs(
+   SCIP*                 scip,               /**< SCIP data structure */
+   SCIP_CONSHDLR*        conshdlr,           /**< expression constraint handler */
+   SCIP_CONSEXPR_EXPR*   expr,               /**< sum expression */
+   SCIP_SOL*             sol,                /**< solution to be separated (NULL for the LP solution) */
+   SCIP_ROW**            cut                 /**< pointer to store the row */
+   )
+{
+   SCIP_CONSEXPR_EXPR* child;
+   SCIP_VAR* auxvar;
+   SCIP_VAR* childvar;
+   SCIP_Bool overestimate;
+   SCIP_Real violation;
+   SCIP_Real refpoint;
+   SCIP_Real lincoef;
+   SCIP_Real linconstant;
+   SCIP_Real lb;
+   SCIP_Real ub;
+   SCIP_Bool success;
+
+   assert(scip != NULL);
+   assert(conshdlr != NULL);
+   assert(strcmp(SCIPconshdlrGetName(conshdlr), "expr") == 0);
+   assert(expr != NULL);
+   assert(SCIPgetConsExprExprNChildren(expr) == 1);
+   assert(strcmp(SCIPgetConsExprExprHdlrName(SCIPgetConsExprExprHdlr(expr)), "abs") == 0);
+   assert(cut != NULL);
+
+   *cut = NULL;
+
+   /* get expression data */
+   auxvar = SCIPgetConsExprExprLinearizationVar(expr);
+   assert(auxvar != NULL);
+   child = SCIPgetConsExprExprChildren(expr)[0];
+   assert(child != NULL);
+   childvar = SCIPgetConsExprExprLinearizationVar(child);
+   assert(childvar != NULL);
+
+   /* compute the violation; this determines whether we need to over- or underestimate */
+   violation = REALABS(SCIPgetSolVal(scip, sol, childvar)) - SCIPgetSolVal(scip, sol, auxvar);
+
+   /* check if there is a violation */
+   if( SCIPisEQ(scip, violation, 0.0) )
+      return SCIP_OKAY;
+
+   /* determine whether we need to under- or overestimate */
+   overestimate = SCIPisLT(scip, violation, 0.0);
+
+   refpoint = SCIPgetSolVal(scip, sol, childvar);
+   lincoef = 0.0;
+   linconstant = 0.0;
+   success = FALSE;
+   lb = SCIPvarGetLbLocal(childvar);
+   ub = SCIPvarGetUbLocal(childvar);
+
+   if( overestimate )
+   {
+      printf("OVERESTIMATE\n");
+
+      if( !SCIPisInfinity(scip, REALABS(lb)) && !SCIPisInfinity(scip, REALABS(ub)) )
+      {
+         printf("lb = %e ub = %e\n", lb, ub);
+         success = TRUE;
+         lincoef = (REALABS(ub) - REALABS(lb)) / (ub - lb);
+         linconstant = REALABS(ub) - lincoef * ub;
+
+         printf("lincoef = %e linconst = %e\n", lincoef, linconstant);
+      }
+   }
+   else
+   {
+      printf("UNDERESTIMATE\n");
+
+      /* this case can be removed after implementing the INITLP callback of the expression handler */
+      success = TRUE;
+      lincoef = SCIPisLE(scip, refpoint, 0.0) ? -1.0 : 1.0;
+      linconstant = 0.0;
+   }
+
+   /* create cut */
+   if( success )
+   {
+      /* create cut */
+      SCIP_CALL( SCIPcreateRowCons(scip, cut, conshdlr, "abs_cut", 0, NULL, NULL,
+            overestimate ? -linconstant : -SCIPinfinity(scip),
+            overestimate ? SCIPinfinity(scip) : -linconstant,
+            FALSE, FALSE, FALSE) );
+      SCIP_CALL( SCIPaddVarToRow(scip, *cut, childvar, lincoef) );
+      SCIP_CALL( SCIPaddVarToRow(scip, *cut, auxvar, -1) );
+   }
+
+   return SCIP_OKAY;
+}
+
+/** expression separation callback */
+static
+SCIP_DECL_CONSEXPR_EXPRSEPA(sepaAbs)
+{
+   SCIP_ROW* cut;
+   SCIP_Real efficacy;
+
+   cut = NULL;
+   *ncuts = 0;
+   *result = SCIP_DIDNOTFIND;
+
+   SCIP_CALL( separatePointAbs(scip, conshdlr, expr, sol, &cut) );
+
+   /* failed to compute a cut */
+   if( cut == NULL )
+      return SCIP_OKAY;
+
+   efficacy = -SCIPgetRowSolFeasibility(scip, cut, sol);
+
+   /* add cut if it is violated */
+   if( SCIPisGE(scip, efficacy, minefficacy) )
+   {
+      SCIP_Bool infeasible;
+
+      SCIP_CALL( SCIPaddCut(scip, NULL, cut, FALSE, &infeasible) );
+      *result = infeasible ? SCIP_CUTOFF : SCIP_SEPARATED;
+
+#ifdef SCIP_DEBUG
+      SCIPdebugMessage("add cut with efficacy %e\n", efficacy);
+      SCIP_CALL( SCIPprintRow(scip, cut, NULL) );
+#endif
+   }
+
+   SCIP_CALL( SCIPreleaseRow(scip, &cut) );
+
+   return SCIP_OKAY;
+}
+
 /** expression reverse propagation callback */
 static
 SCIP_DECL_CONSEXPR_REVERSEPROP(reversepropAbs)
@@ -241,6 +375,7 @@ SCIP_RETCODE SCIPincludeConsExprExprHdlrAbs(
    SCIP_CALL( SCIPsetConsExprExprHdlrPrint(scip, consexprhdlr, exprhdlr, printAbs) );
    SCIP_CALL( SCIPsetConsExprExprHdlrParse(scip, consexprhdlr, exprhdlr, parseAbs) );
    SCIP_CALL( SCIPsetConsExprExprHdlrIntEval(scip, consexprhdlr, exprhdlr, intevalAbs) );
+   SCIP_CALL( SCIPsetConsExprExprHdlrSepa(scip, consexprhdlr, exprhdlr, sepaAbs) );
    SCIP_CALL( SCIPsetConsExprExprHdlrHash(scip, consexprhdlr, exprhdlr, hashAbs) );
    SCIP_CALL( SCIPsetConsExprExprHdlrReverseProp(scip, consexprhdlr, exprhdlr, reversepropAbs) );
 
--- tests/src/cons/expr/separation.c
+++ tests/src/cons/expr/separation.c
@@ -28,6 +28,7 @@
 #include "scip/cons_expr_sumprod.c"
 #include "scip/cons_expr_exp.c"
 #include "scip/cons_expr_log.c"
+#include "scip/cons_expr_abs.c"
 
 /* needed to add auxiliary variables */
 #include "scip/struct_cons_expr.h"
@@ -480,6 +481,88 @@ Test(separation, logarithmic, .init = setup, .fini = teardown,
 
       if( var == SCIPvarGetTransVar(z) )
          cr_assert_eq(coef, log(3) / 2.0);
+      else if( var == SCIPvarGetTransVar(auxvar) )
+         cr_assert_eq(coef, -1.0);
+      else
+         cr_assert(FALSE, "found an unknown variable");
+   }
+
+   SCIP_CALL( SCIPreleaseRow(scip, &cut) );
+
+   /* release expression */
+   SCIP_CALL( SCIPreleaseConsExprExpr(scip, &expr) );
+}
+
+Test(separation, absolute, .init = setup, .fini = teardown,
+   .description = "test separation for an absolute expression"
+   )
+{
+   SCIP_CONSEXPR_EXPR* expr;
+   SCIP_ROW* cut;
+   int i;
+
+   SCIP_CALL( SCIPcreateConsExprExprAbs(scip, conshdlr, &expr, xexpr) );
+
+   /* add the auxiliary variable to the expression; variable will be released in CONSEXITSOL */
+   SCIP_CALL( SCIPcaptureVar(scip, auxvar) );
+   SCIP_CALL( SCIPaddVarLocks(scip, auxvar, 1, 1) );
+   expr->auxvar = auxvar;
+
+   /* compute a cut for which we need an overestimation (secant) */
+   SCIP_CALL( SCIPsetSolVal(scip, sol, x, -0.5) );
+   SCIP_CALL( SCIPsetSolVal(scip, sol, auxvar, 3.0) );
+
+   cut = NULL;
+   SCIP_CALL( separatePointAbs(scip, conshdlr, expr, sol, &cut) );
+
+   printf("%e %e\n", SCIProwGetLhs(cut), SCIProwGetRhs(cut));
+
+   cr_assert(cut != NULL);
+   cr_assert_eq(SCIProwGetNNonz(cut), 2);
+   cr_assert(SCIPisEQ(scip, SCIProwGetLhs(cut), -5.0 / 3.0));
+   cr_assert_eq(SCIProwGetRhs(cut), SCIPinfinity(scip));
+
+
+   for( i = 0; i < SCIProwGetNNonz(cut); ++i )
+   {
+      SCIP_VAR* var;
+      SCIP_Real coef;
+
+      var = SCIPcolGetVar(SCIProwGetCols(cut)[i]);
+      coef = SCIProwGetVals(cut)[i];
+
+      if( var == SCIPvarGetTransVar(x) )
+         cr_assert(SCIPisEQ(scip, coef, 2.0 / 3.0));
+      else if( var == SCIPvarGetTransVar(auxvar) )
+         cr_assert_eq(coef, -1.0);
+      else
+         cr_assert(FALSE, "found an unknown variable");
+   }
+
+   SCIP_CALL( SCIPreleaseRow(scip, &cut) );
+
+   /* compute a cut for which we need an underestimation (linearization) */
+   SCIP_CALL( SCIPsetSolVal(scip, sol, z, 2.0) );
+   SCIP_CALL( SCIPsetSolVal(scip, sol, auxvar, -10.0) );
+
+   cut = NULL;
+   SCIP_CALL( separatePointAbs(scip, conshdlr, expr, sol, &cut) );
+
+   cr_assert(cut != NULL);
+   cr_assert_eq(SCIProwGetNNonz(cut), 2);
+   cr_assert_eq(SCIProwGetLhs(cut), -SCIPinfinity(scip));
+   cr_assert(SCIPisEQ(scip, SCIProwGetRhs(cut), 0.0));
+
+   for( i = 0; i < SCIProwGetNNonz(cut); ++i )
+   {
+      SCIP_VAR* var;
+      SCIP_Real coef;
+
+      var = SCIPcolGetVar(SCIProwGetCols(cut)[i]);
+      coef = SCIProwGetVals(cut)[i];
+
+      if( var == SCIPvarGetTransVar(x) )
+         cr_assert_eq(coef, -1.0);
       else if( var == SCIPvarGetTransVar(auxvar) )
          cr_assert_eq(coef, -1.0);
       else
