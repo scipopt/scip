849400dc6660b8c310671f510ea90835a5435487
Stefan Vigerske <svigerske@gams.com>
2016-07-27 16:57:47 +0200

    fix compiler warnings when OPT=opt

--- src/cppad/local/checkpoint.hpp
+++ src/cppad/local/checkpoint.hpp
@@ -451,7 +451,9 @@ private:
 		const sparsity_type&                    u  ,
 		      sparsity_type&                    v  )
 	{	size_t n = f_.Domain();
+# ifndef NDEBUG
 		size_t m = f_.Range();
+# endif
 		CPPAD_ASSERT_UNKNOWN( vx.size() == n );
 		CPPAD_ASSERT_UNKNOWN(  s.size() == m );
 		CPPAD_ASSERT_UNKNOWN(  t.size() == n );
@@ -690,8 +692,11 @@ public:
 		const vector<Base>&       ty ,
 		      vector<Base>&       px ,
 		const vector<Base>&       py )
-	{	size_t n = f_.Domain();
+	{
+# ifndef NDEBUG
+	   size_t n = f_.Domain();
 		size_t m = f_.Range();
+# endif
 		CPPAD_ASSERT_UNKNOWN( n == tx.size() / (q+1) );
 		CPPAD_ASSERT_UNKNOWN( m == ty.size() / (q+1) );
 		CPPAD_ASSERT_UNKNOWN( f_.size_var() > 0 );
@@ -912,7 +917,9 @@ public:
 		const vector< std::set<size_t> >&       u  ,
 		      vector< std::set<size_t> >&       v  )
 	{	size_t n = f_.Domain();
+# ifndef NDEBUG
 		size_t m = f_.Range();
+# endif
 		CPPAD_ASSERT_UNKNOWN( vx.size() == n );
 		CPPAD_ASSERT_UNKNOWN(  s.size() == m );
 		CPPAD_ASSERT_UNKNOWN(  t.size() == n );
