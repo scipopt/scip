597c4144380952b4f37b138ecd2473dc1d2de703
Stefan Vigerske <svigerske@gams.com>
2016-08-20 18:36:02 +0200

    replace tabs by spaces, remove trailing spaces

--- scripts/updatedates.sh
+++ scripts/updatedates.sh
@@ -3,10 +3,10 @@
 # This bash script updates all copyrights in the SCIP files and posted
 # those files which contain a copyright which do not have the right format
 #
-# You just have to run this script. There is nothing to adjust. 
-# The correct year is detected through the 'date' function 
+# You just have to run this script. There is nothing to adjust.
+# The correct year is detected through the 'date' function
 #
-# Note that not all files (usually scripts) contain a copyright. A copyright is only 
+# Note that not all files (usually scripts) contain a copyright. A copyright is only
 # needed for those files which are part of a SCIP distribution (see makedist.sh)
 #
 # This bash script also changes the copyrights of the SCIP examples
@@ -28,73 +28,73 @@ echo ""
 FILES=""
 for DIRECTORY in ${DIRECTORIES[@]}
 do
-    # exclude cppad subdirectory
-    if [[ "$DIRECTORY" =~ "src/cppad" ]]
-    then
-	continue
-    fi
-    for EXTENSION in ${EXTENSIONS[@]}
-    do
-	for FILE in $DIRECTORY/*.$EXTENSION
-	do
-	    if test -f $FILE
-	    then
-		FILES="$FILES $FILE"
-	    fi
-	done
-    done
-    for EXTRAFILE in ${EXTRAFILES[@]}
-    do
-	if test -f $DIRECTORY/$EXTRAFILE
-	then
-	    FILES="$FILES $DIRECTORY/$EXTRAFILE"
-	fi
-    done
+   # exclude cppad subdirectory
+   if [[ "$DIRECTORY" =~ "src/cppad" ]]
+   then
+      continue
+   fi
+   for EXTENSION in ${EXTENSIONS[@]}
+   do
+      for FILE in $DIRECTORY/*.$EXTENSION
+      do
+         if test -f $FILE
+         then
+            FILES="$FILES $FILE"
+         fi
+      done
+   done
+   for EXTRAFILE in ${EXTRAFILES[@]}
+   do
+      if test -f $DIRECTORY/$EXTRAFILE
+      then
+         FILES="$FILES $DIRECTORY/$EXTRAFILE"
+      fi
+   done
 done
 
 for EXTRAFILE in ${EXTRAFILES[@]}
-do 
-    if test -f $EXTRAFILE
-    then
-	FILES="$FILES $EXTRAFILE"
-    fi
+do
+   if test -f $EXTRAFILE
+   then
+      FILES="$FILES $EXTRAFILE"
+   fi
 done
 
 for FILE in ${FILES[@]}
 do
-    if test -f $FILE
-    then
-	echo $FILE
+   if test -f $FILE
+   then
+      echo $FILE
 
-	# check if the file has already the new copyright 
-	COUNT1=`grep -c 2002-$NEWYEAR $FILE`
-	COUNT2=`grep -c 1996-$NEWYEAR $FILE`
+      # check if the file has already the new copyright
+      COUNT1=`grep -c 2002-$NEWYEAR $FILE`
+      COUNT2=`grep -c 1996-$NEWYEAR $FILE`
 
-	if test "$COUNT1" != "$COUNT2"
-	then
-	    continue
-	fi
-	
-	# check if the file has a correct old copyright 
-	COUNT1=`grep -c 2002-$LASTYEAR $FILE`
-	COUNT2=`grep -c 1996-$LASTYEAR $FILE`
+      if test "$COUNT1" != "$COUNT2"
+      then
+         continue
+      fi
 
-	if test "$COUNT1" == "$COUNT2"
-	then
-	    # post those files which have a wrong old copyright
-	    echo "COPYRIGHT ERROR --------------------> $FILE"
-	    grep "2002-2" $FILE
-	    grep "1996-2" $FILE	  
-	else
-	    echo "updating date in $FILE"
-  
-	    mv $FILE $FILE.olddate
-	    sed 's!2002-'$LASTYEAR'!2002-'$NEWYEAR'!g 
-                   s!1996-'$LASTYEAR'!1996-'$NEWYEAR'!g' $FILE.olddate > $FILE
+      # check if the file has a correct old copyright
+      COUNT1=`grep -c 2002-$LASTYEAR $FILE`
+      COUNT2=`grep -c 1996-$LASTYEAR $FILE`
 
-            # change file permissions back, since piping might create the file with different file permissions
-            chmod --reference $FILE.olddate $FILE
-            rm $FILE.olddate
-	fi
-    fi
+      if test "$COUNT1" == "$COUNT2"
+      then
+         # post those files which have a wrong old copyright
+         echo "COPYRIGHT ERROR --------------------> $FILE"
+         grep "2002-2" $FILE
+         grep "1996-2" $FILE
+      else
+         echo "updating date in $FILE"
+
+         mv $FILE $FILE.olddate
+         sed 's!2002-'$LASTYEAR'!2002-'$NEWYEAR'!g
+              s!1996-'$LASTYEAR'!1996-'$NEWYEAR'!g' $FILE.olddate > $FILE
+
+         # change file permissions back, since piping might create the file with different file permissions
+         chmod --reference $FILE.olddate $FILE
+         rm $FILE.olddate
+      fi
+   fi
 done
