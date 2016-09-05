b887c363fc146ad2f90ef4f0048d3dfe332669d6
Cristina Munoz <munoz@zib.de>
2016-09-05 10:45:09 +0200

    beginning of automation scripts

---
+++ check/archive_results.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+#
+# Usage:
+# make testcluster | check/archive_results.sh
+#
+# This script reads stdout from make testcluster, parses the slurm job ids, and starts a
+# job after the previously queued slurm jobs finish. This job runs ./evalcheck_cluster.sh 
+# and rbcli to send the results to rubberband.
+#
+
+# read from stdin
+i=0
+while read line
+do
+  if [[ $line == Submitted[[:space:]]batch[[:space:]]job[[:space:]]* ]]
+  then
+    stringarray=($line)
+    field[$i]=${stringarray[-1]}
+    ((i++))
+  fi
+done < "${1:-/dev/stdin}"
+
+# build sbatch command
+jobids=$(printf ",%s" "${field[@]}")
+jobids=${jobids:1}
+sbatch --dependency=afterok:${jobids} --cpus-per-task=1 --mem=100 rbcliwrapper.sh
---
+++ check/rbcliwrapper.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+# This script will work only if there is one .eval file in results/
+# check/results should only contain results from one run
+
+./evalcheck_cluster.sh results/*.eval
+rbcli up results/*.{err,out,set}
