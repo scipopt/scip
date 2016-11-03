96730abd62b691e9e7811b8e7b663ff13090ffec
Felipe Serrano <serrano@zib.de>
2016-11-03 18:12:09 +0100

    fix some documentation issues

--- src/scip/sepa_convexproj.c
+++ src/scip/sepa_convexproj.c
@@ -3,7 +3,7 @@
 /*                  This file is part of the program and library             */
 /*         SCIP --- Solving Constraint Integer Programs                      */
 /*                                                                           */
-/*    Copyright (C) 2002-2015 Konrad-Zuse-Zentrum                            */
+/*    Copyright (C) 2002-2016 Konrad-Zuse-Zentrum                            */
 /*                            fuer Informationstechnik Berlin                */
 /*                                                                           */
 /*  SCIP is distributed under the terms of the ZIB Academic License.         */
@@ -16,44 +16,6 @@
 /**@file   sepa_convexproj.c
  * @brief  convexproj separator
  * @author Felipe Serrano
- *
- * This separator receives a point \f$ x_0 \f$ to separate, projects it onto a convex relaxation
- * of the current problem and then generates gradient cuts at the projection.
- *
- * In more detail, the separators builds and stores a convex relaxation of the problem
- * \f[
- *      C = \{ x \colon g_j(x) \le 0 \, \forall j=1,\ldots,m \}
- * \f]
- * where each \f$ g_j \f$ is a convex function and computes the projection by solving
- * \f[
- *      \min || x - x_0 ||^2 \\
- * \f]
- * \f[
- *      s.t. \; g_j(x) \le 0 \, \forall j=1,\ldots,m \\
- * \f]
- *
- * By default, the separator is only run if the convex relaxation has at least one nonlinear convex function
- *
- * The separator generates cuts for constraints which were violated by the solution to the separate and active
- * at the projection. If the projection problem is not solved to optimality, it still tries to add a cut at the
- * best solution found. In case that the projection problem is solved to optimality, it is guaranteed that a cut
- * separates the point. To see this, remember that \f$ z \f$ is the projection if and only if
- * \f[
- *      \langle x - z, z - x_0 \rangle \ge 0 \, \forall x \in C \\
- * \f]
- * This inequality is violated by for \f$ x = x_0 \f$. On the other hand, the optimality conditions of the projection
- * problem at the optimum look like
- * \f[
- *      2 (z - x_0) + \sum_j \lambda_j \nabla g_j(z) = 0
- * \f]
- * Now suppose that the no gradient cut at \f$ z \f$ separates \f$ x_0 \f$, ie
- * \f[
- *      g_j(z) + \langle \nabla g_j(z), x_0 - z \rangle \le 0
- * \f]
- * Multiplying each inequality with \f$ \lambda_j \ge \f$ and summing up, we get the following contradiction:
- * \f[
- *      \langle -2(z - x_0), x_0 - z \rangle \le 0
- * \f]
  */
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
 
