ade19ad629441cbb232e8ab911dbdc1bdaac5c3a
Benjamin Mueller <benjamin.mueller@zib.de>
2016-11-04 11:53:54 +0100

    fix indentation of two debug messages

--- src/scip/cons_quadratic.c
+++ src/scip/cons_quadratic.c
@@ -10051,7 +10051,7 @@ SCIP_RETCODE propagateBounds(
             }
             if( redundant )
             {
-            SCIPdebugMsg(scip, "deleting constraint <%s> locally\n", SCIPconsGetName(conss[c]));
+               SCIPdebugMsg(scip, "deleting constraint <%s> locally\n", SCIPconsGetName(conss[c]));
                SCIP_CALL( SCIPdelConsLocal(scip, conss[c]) );
             }
          }
@@ -11736,7 +11736,8 @@ SCIP_DECL_CONSPRESOL(consPresolQuadratic)
 
                if( propresult == SCIP_CUTOFF )
                {
-               SCIPdebugMsg(scip, "propagation on constraint <%s> says problem is infeasible in presolve\n", SCIPconsGetName(conss[c]));
+                  SCIPdebugMsg(scip, "propagation on constraint <%s> says problem is infeasible in presolve\n",
+                     SCIPconsGetName(conss[c]));
                   *result = SCIP_CUTOFF;
                   return SCIP_OKAY;
                }
