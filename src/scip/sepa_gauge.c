dcec92576d8830c19b41d7e65e57d1357b2fb8ca
Felipe Serrano <serrano@zib.de>
2016-11-09 17:41:45 +0100

    add some todos

--- src/scip/sepa_gauge.c
+++ src/scip/sepa_gauge.c
@@ -17,7 +17,9 @@
  * @brief  gauge separator
  * @author Felipe Serrano
  *
+ * @todo should separator only be run when SCIPallColsInLP is true?
  * @todo add SCIPisStopped(scip) to the condition of time consuming loops
+ * @todo check if it makes sense to implement the copy callback
  */
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
