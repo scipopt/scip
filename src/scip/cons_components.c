db60344f3cd4a40db32d36006912aa15fe29a70c
Jakob Witzig <witzig@zib.de>
2017-01-17 12:22:14 +0100

    use non-check constraints for components

--- src/scip/cons_components.c
+++ src/scip/cons_components.c
@@ -1869,11 +1869,8 @@ SCIP_RETCODE findComponents(
    (*nsortedconss) = 0;
    for( c = 0; c < ntmpconss; c++ )
    {
-      if( SCIPconsIsChecked(tmpconss[c]) )
-      {
-         sortedconss[(*nsortedconss)] = tmpconss[c];
-         (*nsortedconss)++;
-      }
+      sortedconss[(*nsortedconss)] = tmpconss[c];
+      (*nsortedconss)++;
    }
 
    if( nvars > 1 && *nsortedconss > 1 )
