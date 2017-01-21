d249813377af8356090a8fa8ce691c260bbcca71
Sebastian Schenker <schenker@zib.de>
2017-01-21 14:27:21 +0100

    PolySCIP version for SCIP-4.0 release

--- applications/PolySCIP/src/prob_data_objectives.cpp
+++ applications/PolySCIP/src/prob_data_objectives.cpp
@@ -2,7 +2,7 @@
 /*                                                                           */
 /*        This file is part of the program PolySCIP                          */
 /*                                                                           */
-/*    Copyright (C) 2012-2016 Konrad-Zuse-Zentrum                            */
+/*    Copyright (C) 2012-2017 Konrad-Zuse-Zentrum                            */
 /*                            fuer Informationstechnik Berlin                */
 /*                                                                           */
 /*  PolySCIP is distributed under the terms of the ZIB Academic License.     */
@@ -13,10 +13,10 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 /**
- * @brief  Problem data for objectives
+ * @file prob_data_objectives.cpp
+ * @brief  Implements class storing multiple objectives of given problem instance
  * @author Sebastian Schenker
  *
- * Implements problem data for objectives .
  */
 
 #include "prob_data_objectives.h"
@@ -39,6 +39,11 @@ using polyscip::OutcomeType;
 using polyscip::ValueType;
 using polyscip::WeightType;
 
+
+/**
+ * Add identifier of objective
+ * @param name Objective identifier
+ */
 void ProbDataObjectives::addObjName(const char* name)  {
     string obj_name(name);
     if (name_to_no_.count(obj_name) != 0)
@@ -49,6 +54,13 @@ void ProbDataObjectives::addObjName(const char* name)  {
     non_ignored_objs_.push_back(getNoObjs());
 }
 
+
+/**
+ * Add objective coefficient
+ * @param var Corresponding variable
+ * @param obj_name Corresponding objective identifier
+ * @param val Corresponding coefficient
+ */
 void ProbDataObjectives::addObjCoeff(SCIP_VAR *var, const char* obj_name, ValueType val) {
     if (val != 0) {
         if (var_to_coeffs_.count(var) == 0)
@@ -61,6 +73,13 @@ void ProbDataObjectives::addObjCoeff(SCIP_VAR *var, const char* obj_name, ValueT
         std::cout << "addObjCoeff with zero value\n";
 }
 
+
+/**
+ * Get objective coefficient
+ * @param var Corresponding variable
+ * @param obj_no Corresponding number of objective
+ * @todo Const qualification
+ */
 ValueType ProbDataObjectives::getObjCoeff(SCIP_VAR* var, size_t obj) {
     if (var_to_coeffs_.count(var))
         return (var_to_coeffs_[var]).at(non_ignored_objs_.at(obj));
@@ -68,11 +87,25 @@ ValueType ProbDataObjectives::getObjCoeff(SCIP_VAR* var, size_t obj) {
         return 0.;
 }
 
+
+/**
+ * Number of non-zero coefficients of objective
+ * @param obj_no Corresponding objective number
+ */
 size_t ProbDataObjectives::getNumberNonzeroCoeffs(size_t obj) const {
     auto obj_name = no_to_name_.at(non_ignored_objs_.at(obj));
     return name_to_nonzero_coeffs_.at(obj_name).size();
 }
 
+
+/**
+ * Scalar product of given weight and objectives w.r.t. given variable;
+ * if given variable is unknown, return 0.0 (since var can only have zero objective
+ * coefficients in given problem)
+ * @param var Corresponding variable
+ * @param weight Corresponding weight vector
+ * @todo Const qualification
+ */
 ValueType ProbDataObjectives::getWeightedObjVal(SCIP_VAR* var, const WeightType& weight) {
     assert (weight.size() == non_ignored_objs_.size());
     if (var_to_coeffs_.count(var)) {
@@ -83,38 +116,31 @@ ValueType ProbDataObjectives::getWeightedObjVal(SCIP_VAR* var, const WeightType&
                                   0.,
                                   std::plus<ValueType>(),
                                   [&coeffs](ValueType w, size_t obj){return w*coeffs[obj];});
-        /*return std::inner_product(begin(weight),
-                                  end(weight),
-                                  begin(var_to_coeffs_[var]),
-                                  0.);*/
     }
     else {
         return 0.;
     }
 }
 
-/*ValueType ProbDataObjectives::getWeightedObjVal(SCIP_VAR* var, const WeightType& weight,
-                                                const vector<size_t>& non_redundant_objs) {
-    assert (non_redundant_objs.size() == weight.size());
-    if (var_to_coeffs_.count(var)) {
-        auto coeffs = var_to_coeffs_[var];
-        return std::inner_product(begin(weight),
-                                  end(weight),
-                                  begin(non_redundant_objs),
-                                  0.0,
-                                  std::plus<ValueType>(),
-                                  [&coeffs](ValueType w, size_t index) { return w * coeffs.at(index); });
-    }
-    else {
-        return 0;
-    }
-}*/
 
+/**
+ * Variables corresponding to non-zero objective coefficients
+ * @param obj_no Corresponding objective number
+ */
 vector<SCIP_VAR*> ProbDataObjectives::getNonZeroCoeffVars(size_t obj) const {
     auto obj_name = no_to_name_.at(non_ignored_objs_.at(obj));
     return name_to_nonzero_coeffs_.at(obj_name);
 }
 
+
+/**
+ * Product of given solution value and objective coefficient w.r.t. given
+ * variable and objective number
+ * @param var Corresponding variable
+ * @param obj_no Corresponding objective number
+ * @param sol_val Corresponding solution value
+ * @todo Const qualification
+ */
 ValueType ProbDataObjectives::getObjVal(SCIP_VAR* var, size_t obj, ValueType sol_val) {
     if (var_to_coeffs_.count(var))
         return var_to_coeffs_[var].at(non_ignored_objs_.at(obj)) * sol_val;
@@ -122,6 +148,10 @@ ValueType ProbDataObjectives::getObjVal(SCIP_VAR* var, size_t obj, ValueType sol
         return 0.;
 }
 
+
+/**
+ * Negate all objective coefficients of all variables in all objectives
+ */
 void ProbDataObjectives::negateAllCoeffs() {
     for (auto it=begin(var_to_coeffs_); it!=end(var_to_coeffs_); ++it) {
         std::transform(begin(it->second), end(it->second),
@@ -129,6 +159,11 @@ void ProbDataObjectives::negateAllCoeffs() {
     }
 }
 
+/**
+ * Ignore two objectives
+ * @param obj_1 First objective to ignore
+ * @param obj_2 Second objective to ignore
+ */
 void ProbDataObjectives::ignoreObjectives(size_t obj_1, size_t obj_2) {
     auto obj_ind_1 = non_ignored_objs_.at(obj_1);
     auto obj_ind_2 = non_ignored_objs_.at(obj_2);
@@ -142,6 +177,9 @@ void ProbDataObjectives::ignoreObjectives(size_t obj_1, size_t obj_2) {
     ignored_obj_.push(obj_ind_2);
 }
 
+/**
+ * Unignore latest two objectives that were ignored previously
+ */
 void ProbDataObjectives::unignoreObjectives() {
     auto obj = ignored_obj_.top();
     ignored_obj_.pop();
