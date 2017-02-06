e3a9306b6c422efcfd47600143dbbab9d5f2d262
Gregor Hendel <hendel@zib.de>
2017-02-06 22:41:35 +0100

    some more new documentation files in distribution

--- makedist.sh
+++ makedist.sh
@@ -26,8 +26,10 @@ tar --no-recursion --ignore-failed-read -cvzhf release/$NAME.tgz \
 --exclude="*~" \
 --exclude=".*" \
 $NAME/COPYING $NAME/INSTALL $NAME/CHANGELOG $NAME/Makefile \
-$NAME/doc/scip* $NAME/doc/xternal.c \
-$NAME/doc/inc/authors.inc \
+$NAME/doc/scip* $NAME/doc/xternal.c $NAME/doc/builddoc.sh \
+$NAME/doc/inc/codestyle/* $NAME/doc/inc/shelltutorial $NAME/doc/inc/debugexamples
+$NAME/doc/inc/faq/faqtext.txt $NAME/doc/inc/faq/*.py $NAME/doc/inc/faq/localdoc.php \
+$NAME/doc/inc/makeexamples/* $NAME/doc/inc/shelltutorial/commands  $NAME/doc/inc/shelltutorial/insertsnippetstutorial.py \
 $NAME/doc/pictures/miniscippy.png $NAME/doc/pictures/scippy.png \
 $NAME/make/make.* \
 $NAME/check/check.sh $NAME/check/evalcheck.sh $NAME/check/check.awk \
