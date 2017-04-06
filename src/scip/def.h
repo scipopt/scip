04a78569db707fe1578df1ba9ef1dadfec72e7b3
Gerald Gamrath <gamrath@zib.de>
2017-04-06 11:59:52 +0200

    updated API version

--- src/scip/def.h
+++ src/scip/def.h
@@ -96,7 +96,7 @@ extern "C" {
 
 #define SCIP_VERSION                400 /**< SCIP version number (multiplied by 100 to get integer number) */
 #define SCIP_SUBVERSION               1 /**< SCIP sub version number */
-#define SCIP_APIVERSION               3 /**< SCIP API version number */
+#define SCIP_APIVERSION               4 /**< SCIP API version number */
 #define SCIP_COPYRIGHT   "Copyright (C) 2002-2017 Konrad-Zuse-Zentrum fuer Informationstechnik Berlin (ZIB)"
 
 
