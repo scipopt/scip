b63295c34fe91408b596980e838c4a62e67ffcfe
Robert Lion Gottwald <robert.gottwald@zib.de>
2017-06-26 18:35:27 +0200

    WIP: some changes in sepa_cmir

--- src/scip/sepa_cmir.h
+++ src/scip/sepa_cmir.h
@@ -18,6 +18,20 @@
  * @brief  complemented mixed integer rounding cuts separator (Marchand's version)
  * @author Kati Wolter
  * @author Tobias Achterberg
+ *
+ * TODO: LONGER DESCRIPTION
+ * - Overview of the algorithm
+ *    - Eagle view: we aggregate and try different methods
+ * - Details
+ *    - The aggregation is done the following way blablabl
+ * - Definitions: good variable
+ * - In general, continuous variables are less prefered than integer variables, since their cut
+ *   coefficient is worse.
+ * - We seek for aggregations that project out continuous variables that are far away from there bound,
+ *   since if it is at its bound then it doesn't contribute to the violation
+ * - These aggregations are also useful for the flowcover separation, so after building an aggregation
+ *   we try to generate a MIR cut and a flowcover cut.
+ * - We only keep the best cut
  */
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
