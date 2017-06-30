64aa1cd1f4087f394f6429159358960378626c76
Gerald Gamrath <gamrath@zib.de>
2017-06-30 16:17:25 +0200

    allow to switch conflict analysis in strong branching with propagation

--- src/scip/scip.c
+++ src/scip/scip.c
@@ -20503,6 +20503,7 @@ SCIP_RETCODE SCIPgetVarStrongbranchWithPropagation(
    SCIP_Bool downvalidlocal;
    SCIP_Bool upvalidlocal;
    SCIP_Bool allcolsinlp;
+   SCIP_Bool enabledconflict;
    int oldnconflicts;
    int nvars;
 
@@ -20647,6 +20648,10 @@ SCIP_RETCODE SCIPgetVarStrongbranchWithPropagation(
    firstchild = TRUE;
    cutoff = FALSE;
 
+   /* switch conflict analysis according to usesb parameter */
+   enabledconflict = scip->set->conf_enable;
+   scip->set->conf_enable = scip->set->conf_usesb;
+
    /* @todo: decide the branch to look at first based on the cutoffs in previous calls? */
    switch( scip->set->branch_firstsbchild )
    {
@@ -20665,6 +20670,7 @@ SCIP_RETCODE SCIPgetVarStrongbranchWithPropagation(
       default:
          SCIPerrorMessage("Error: Unknown parameter value <%c> for branching/firstsbchild parameter:\n",scip->set->branch_firstsbchild);
          SCIPABORT();
+         scip->set->conf_enable = enabledconflict;
          return SCIP_PARAMETERWRONGVAL; /*lint !e527*/
    }
 
@@ -20767,6 +20773,8 @@ SCIP_RETCODE SCIPgetVarStrongbranchWithPropagation(
       *downvalid = downvalidlocal;
    if( upvalid != NULL )
       *upvalid = upvalidlocal;
+
+   scip->set->conf_enable = enabledconflict;
 
    return SCIP_OKAY;
 }
