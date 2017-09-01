e8a0dc24aa1985375d4bcdba52b9fae9cbb14ed9
Stephen J. Maher <s.maher3@lancaster.ac.uk>
2017-09-01 11:27:44 +0100

    small changes to rins for testing

--- src/scip/heur_rins.c
+++ src/scip/heur_rins.c
@@ -35,8 +35,8 @@
 #define HEUR_FREQ             25
 #define HEUR_FREQOFS          0
 #define HEUR_MAXDEPTH         -1
-#define HEUR_TIMING           SCIP_HEURTIMING_AFTERLPNODE
-//#define HEUR_TIMING           SCIP_HEURTIMING_DURINGLPLOOP
+//#define HEUR_TIMING           SCIP_HEURTIMING_AFTERLPNODE
+#define HEUR_TIMING           SCIP_HEURTIMING_DURINGLPLOOP
 #define HEUR_USESSUBSCIP      TRUE      /**< does the heuristic use a secondary SCIP instance? */
 
 #define DEFAULT_NODESOFS      500       /* number of nodes added to the contingent of the total nodes          */
