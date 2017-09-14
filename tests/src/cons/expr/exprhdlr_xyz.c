77d621713e3ab34a323f858768d4a55cd32d9bdf
Benjamin Mueller <benjamin.mueller@zib.de>
2017-09-14 18:53:28 +0200

    add exprhdlr_xyz.c template

---
+++ tests/src/cons/expr/exprhdlr_xyz.c
@@ -0,0 +1,116 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the program and library             */
+/*         SCIP --- Solving Constraint Integer Programs                      */
+/*                                                                           */
+/*    Copyright (C) 2002-2013 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SCIP is distributed under the terms of the ZIB Academic License.         */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**@file   exprhdlr_xyz.c
+ * @brief  tests expression handler functions of xzy an expression
+ */
+
+/*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
+
+#include "scip/scip.h"
+#include "scip/cons_expr.h"
+
+#include "include/scip_test.h"
+
+static SCIP* scip;
+static SCIP_CONSHDLR* conshdlr;
+static SCIP_SOL* sol;
+static SCIP_VAR* x;
+static SCIP_VAR* y;
+
+/* creates scip, problem, includes expression constraint handler, creates  and adds variables */
+static
+void setup(void)
+{
+   SCIP_CALL( SCIPcreate(&scip) );
+
+   /* include cons_expr: this adds the operator handlers */
+   SCIP_CALL( SCIPincludeConshdlrExpr(scip) );
+
+   /* get expr conshdlr */
+   conshdlr = SCIPfindConshdlr(scip, "expr");
+   cr_assert(conshdlr != NULL);
+
+   /* create problem */
+   SCIP_CALL( SCIPcreateProbBasic(scip, "test_problem") );
+
+   SCIP_CALL( SCIPcreateVarBasic(scip, &x, "x", -1.0, 1.0, 0.0, SCIP_VARTYPE_CONTINUOUS) );
+   SCIP_CALL( SCIPcreateVarBasic(scip, &y, "y", -1.0, 1.0, 0.0, SCIP_VARTYPE_CONTINUOUS) );
+   SCIP_CALL( SCIPaddVar(scip, x) );
+   SCIP_CALL( SCIPaddVar(scip, y) );
+
+   /* create solution */
+   SCIP_CALL( SCIPcreateSol(scip, &sol, NULL) );
+}
+
+/* releases variables, frees scip */
+static
+void teardown(void)
+{
+   /* free allocated memory */
+   SCIP_CALL( SCIPfreeSol(scip, &sol) );
+   SCIP_CALL( SCIPreleaseVar(scip, &x) );
+   SCIP_CALL( SCIPreleaseVar(scip, &y) );
+   SCIP_CALL( SCIPfree(&scip) );
+
+   cr_assert_eq(BMSgetMemoryUsed(), 0, "There is are memory leak!!");
+}
+
+/* test suite */
+TestSuite(xyz, .init = setup, .fini = teardown);
+
+/*
+ * TESTS
+ */
+
+Test(xyz, creation, .description = "Tests the expression creation.")
+{
+   /* TODO */
+}
+
+Test(xyz, print, .description = "Tests the expression printing function.")
+{
+   /* TODO */
+}
+
+Test(xyz, parse, .description = "Tests the expression parsing.")
+{
+   /* TODO */
+}
+
+Test(xyz, eval, .description = "Tests the expression evaluation.")
+{
+   /* TODO */
+}
+
+Test(xyz, inteval, .description = "Tests the expression interval evaluation.")
+{
+   /* TODO */
+}
+
+Test(xyz, derivative, .description = "Tests the expression derivation.")
+{
+   /* TODO */
+}
+
+Test(xyz, hash, .description = "Tests the expression hash.")
+{
+   /* TODO */
+}
+
+Test(xyz, simplify, .description = "Tests the expression simplification.")
+{
+   /* TODO */
+}
