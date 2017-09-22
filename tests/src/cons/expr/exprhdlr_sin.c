0bd4f860dc77f49dfbc3cdfd7e036c1d31c109e7
Fabian Wegscheider <wegscheider@zib.de>
2017-09-22 16:45:04 +0200

    reorganized simple tests

--- tests/src/cons/expr/exprhdlr_sin.c
+++ tests/src/cons/expr/exprhdlr_sin.c
@@ -34,6 +34,8 @@ static SCIP_VAR* y;
 static SCIP_CONSEXPR_EXPR* sinexpr;
 static SCIP_CONSEXPR_EXPR* xexpr;
 static SCIP_CONSEXPR_EXPR* yexpr;
+static SCIP_RANDNUMGEN* rndgen;
+
 
 /* creates scip, problem, includes expression constraint handler, creates  and adds variables */
 static
@@ -63,12 +65,18 @@ void setup(void)
 
    /* create solution */
    SCIP_CALL( SCIPcreateSol(scip, &sol, NULL) );
+
+   /* create random number generator */
+   SCIP_CALL( SCIPcreateRandom(scip, &rndgen, 1) );
 }
 
 /* releases variables, frees scip */
 static
 void teardown(void)
 {
+   /* free random number generator */
+   SCIPfreeRandom(scip, &rndgen);
+
    /* free allocated memory */
    SCIP_CALL( SCIPfreeSol(scip, &sol) );
 
@@ -114,7 +122,7 @@ Test(sin, parse, .description = "Tests the expression parsing.")
    SCIP_CONSEXPR_EXPR* expr;
    const char* input = "sin(<x>[C])";
 
-   /* create expression for product of -5, sin(x), and sin(y) */
+   /* create sine expression */
    SCIP_CALL( SCIPparseConsExprExpr(scip, conshdlr, (char*)input, NULL, &expr) );
 
    cr_assert(expr != NULL);
@@ -127,67 +135,122 @@ Test(sin, parse, .description = "Tests the expression parsing.")
 
 Test(sin, eval, .description = "Tests the expression evaluation.")
 {
-   /* get random real number between -10 and 10 */
-   SCIP_RANDNUMGEN* rndgen;
-   SCIP_CALL( SCIPcreateRandom(scip, &rndgen, 1) );
-   SCIP_Real randnum = SCIPrandomGetReal(rndgen, -10.0, 10.0);
+   int i;
+   SCIP_Real randnum;
+   SCIP_Real testvalues[5] = {0.0, M_PI, 2.5*M_PI, -0.5*M_PI, -2.0*M_PI};
+   SCIP_Real results[5] = {0.0, 0.0, 1.0, -1.0, 0.0};
 
-   /* create 5 different testvalues and corresponding expected results */
-   SCIP_Real testvalues[5] = {0.0, M_PI, 2.5*M_PI, -0.5*M_PI, randnum};
-   SCIP_Real results[5] = {0.0, 0.0, 1.0, -1.0, SIN(randnum)};
-
-   for(int i = 0; i < 5; i++)
+   /* deterministic part */
+   for( i = 0; i < 5; ++i )
    {
       SCIP_CALL( SCIPsetSolVal(scip, sol, x, testvalues[i]) );
       SCIP_CALL( SCIPevalConsExprExpr(scip, sinexpr, sol, 0) );
 
-      cr_expect(SCIPisFeasEQ(scip, SCIPgetConsExprExprValue(sinexpr), results[i]));
+      cr_expect(SCIPisEQ(scip, SCIPgetConsExprExprValue(sinexpr), results[i]));
    }
 
-   SCIPfreeRandom(scip, &rndgen);
+   /* random part */
+   for( i = 0; i < 100; ++i )
+   {
+      randnum = SCIPrandomGetReal(rndgen, -10.0, 10.0);
+      SCIP_CALL( SCIPsetSolVal(scip, sol, x, randnum) );
+      SCIP_CALL( SCIPevalConsExprExpr(scip, sinexpr, sol, 0) );
+
+      cr_expect(SCIPisEQ(scip, SCIPgetConsExprExprValue(sinexpr), SIN(randnum)));
+   }
 }
 
 Test(sin, inteval, .description = "Tests the expression interval evaluation.")
 {
-   /* create 5 sets of bounds to cover different cases */
-   SCIP_Real testlowerbounds[5] = {0.0, -2.7, M_PI, 0.5*M_PI, 1.5};
-   SCIP_Real testupperbounds[5] = {2*M_PI, 1.9, 6.0, 1.5*M_PI, 4.0};
-   SCIP_Real reslowerbounds[5] = {-1.0, -1.0, -1.0, -1.0, SIN(4.0)};
-   SCIP_Real resupperbounds[5] = {1.0, 1.0, 0.0, 1.0, 1.0};
+   int i;
+   SCIP_INTERVAL interval;
+   SCIP_Real rndlb[5];
+   SCIP_Real rndub[5];
+   SCIP_Real rndreslb[5];
+   SCIP_Real rndresub[5];
 
-   for(int i = 0; i < 5; i++)
+   /* pick 5 special cases with well known results */
+   SCIP_Real detlb[5] = {0.0, -0.5*M_PI, -M_PI, 2.0*M_PI, 0.5*M_PI};
+   SCIP_Real detub[5] = {2.0*M_PI, 0.5*M_PI, -0.5*M_PI, 3.0*M_PI, 1.5*M_PI};
+   SCIP_Real detreslb[5] = {-1.0, -1.0, -1.0, 0.0, -1.0};
+   SCIP_Real detresub[5] = {1.0, 1.0, 0.0, 1.0, 1.0};
+
+   /* create 5 random cases within specific bounds that have non-trivial resulsts */
+   rndlb[0]    = SCIPrandomGetReal(rndgen, 0.0, 0.5*M_PI);
+   rndub[0]    = SCIPrandomGetReal(rndgen, 0.5*M_PI, M_PI);
+   rndreslb[0] = MIN(SIN(rndlb[0]), SIN(rndub[0]));
+   rndresub[0] = 1.0;
+
+   rndlb[1]    = SCIPrandomGetReal(rndgen, 0.0, 0.5*M_PI);
+   rndub[1]    = SCIPrandomGetReal(rndgen, M_PI, 1.5*M_PI);
+   rndreslb[1] = SIN(rndub[1]);
+   rndresub[1] = 1.0;
+
+   rndlb[2]    = SCIPrandomGetReal(rndgen, 0.5*M_PI, M_PI);
+   rndub[2]    = SCIPrandomGetReal(rndgen, M_PI, 1.5*M_PI);
+   rndreslb[2] = SIN(rndub[2]);
+   rndresub[2] = SIN(rndlb[2]);
+
+   rndlb[3]    = SCIPrandomGetReal(rndgen, M_PI, 1.5*M_PI);
+   rndub[3]    = SCIPrandomGetReal(rndgen, 1.5*M_PI, 2.0*M_PI);
+   rndreslb[3] = -1.0;
+   rndresub[3] = MAX(SIN(rndlb[3]), SIN(rndub[3]));
+
+   rndlb[4]    = SCIPrandomGetReal(rndgen, 0.0, 0.5*M_PI);
+   rndub[4]    = SCIPrandomGetReal(rndgen, 1.5*M_PI, 2.0*M_PI);
+   rndreslb[4] = -1.0;
+   rndresub[4] = 1.0;
+
+   /* deterministic part */
+   for( i = 0; i < 5; ++i )
    {
-      SCIP_CALL( SCIPchgVarLb(scip, x, testlowerbounds[i]) );
-      SCIP_CALL( SCIPchgVarUb(scip, x, testupperbounds[i]) );
+      SCIP_CALL( SCIPchgVarLb(scip, x, detlb[i]) );
+      SCIP_CALL( SCIPchgVarUb(scip, x, detub[i]) );
       SCIP_CALL( SCIPevalConsExprExpr(scip, sinexpr, sol, 0) );
       SCIP_CALL( SCIPevalConsExprExprInterval(scip, sinexpr, FALSE, 0, SCIPepsilon(scip)) );
 
-      SCIP_INTERVAL interval = SCIPgetConsExprExprInterval(sinexpr);
-      cr_expect(SCIPisFeasEQ(scip, SCIPintervalGetInf(interval), reslowerbounds[i]));
-      cr_expect(SCIPisFeasEQ(scip, SCIPintervalGetSup(interval), resupperbounds[i]));
+      interval = SCIPgetConsExprExprInterval(sinexpr);
+      cr_expect(SCIPisFeasEQ(scip, SCIPintervalGetInf(interval), detreslb[i]));
+      cr_expect(SCIPisFeasEQ(scip, SCIPintervalGetSup(interval), detresub[i]));
    }
 
+   /* random part */
+   for ( i = 0; i < 1; ++i )
+   {
+      SCIP_CALL( SCIPchgVarLb(scip, x, rndlb[i]) );
+      SCIP_CALL( SCIPchgVarUb(scip, x, rndub[i]) );
+      SCIP_CALL( SCIPevalConsExprExpr(scip, sinexpr, sol, 0) );
+      SCIP_CALL( SCIPevalConsExprExprInterval(scip, sinexpr, FALSE, 0, SCIPepsilon(scip)) );
+
+      interval = SCIPgetConsExprExprInterval(sinexpr);
+      cr_expect(SCIPisFeasEQ(scip, SCIPintervalGetInf(interval), rndreslb[i]));
+      cr_expect(SCIPisFeasEQ(scip, SCIPintervalGetSup(interval), rndresub[i]));
+   }
 }
 
 Test(sin, derivative, .description = "Tests the expression derivation.")
 {
-   /* get random real number between -10 and 10 */
-   SCIP_RANDNUMGEN* rndgen;
-   SCIP_CALL( SCIPcreateRandom(scip, &rndgen, 1) );
-   SCIP_Real randnum = SCIPrandomGetReal(rndgen, -10.0, 10.0);
+   int i;
+   SCIP_Real randnum;
+   SCIP_Real testvalues[5] = {0.0, M_PI, 2.0*M_PI, 2.5*M_PI, -0.5*M_PI};
+   SCIP_Real results[5] = {1.0, -1.0, 1.0, 0.0, 0.0};
 
-   /* create 5 different testvalues and corresponding expected results */
-   SCIP_Real testvalues[5] = {0.0, M_PI, 2.5*M_PI, -0.5*M_PI, randnum};
-   SCIP_Real results[5] = {1.0, -1.0, 0.0, 0.0, COS(randnum)};
-
-   for(int i = 0; i < 5; i++)
+   /* deterministic part */
+   for( i = 0; i < 5; ++i )
    {
       SCIP_CALL( SCIPsetSolVal(scip, sol, x, testvalues[i]) );
       SCIP_CALL( SCIPcomputeConsExprExprGradient(scip, conshdlr, sinexpr, sol, 0) );
-      cr_expect(SCIPisFeasEQ(scip, SCIPgetConsExprExprPartialDiff(scip, conshdlr, sinexpr, x), results[i]));
+      cr_expect(SCIPisEQ(scip, SCIPgetConsExprExprPartialDiff(scip, conshdlr, sinexpr, x), results[i]));
    }
 
-   SCIPfreeRandom(scip, &rndgen);
+   /* random part */
+   for( i = 0; i < 100; ++i)
+   {
+      randnum = SCIPrandomGetReal(rndgen, -10.0, 10.0);
+      SCIP_CALL( SCIPsetSolVal(scip, sol, x, randnum) );
+      SCIP_CALL( SCIPcomputeConsExprExprGradient(scip, conshdlr, sinexpr, sol, 0) );
+      cr_expect(SCIPisEQ(scip, SCIPgetConsExprExprPartialDiff(scip, conshdlr, sinexpr, x), COS(randnum)));
+   }
 }
 
 Test(sin, hash, .description = "Tests the expression hash.")
