ef6be69ea914929bbd718a66a62721c2b1488788
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2017-09-30 17:48:19 +0200

    add test for fixed variables

--- tests/src/cons/symmetry/compute.c
+++ tests/src/cons/symmetry/compute.c
@@ -145,8 +145,106 @@ Test(test_compute_symmetry, basic1, .description = "compute symmetry for a simpl
    SCIP_CALL( SCIPreleaseVar(scip, &var4) );
 }
 
+
 /* TEST 2 */
-Test(test_compute_symmetry, basic2, .description = "compute symmetry for a simple example with 4 variables and four linear constraints")
+Test(test_compute_symmetry, basic2, .description = "compute symmetry for a simple example with 4 variables and 2 linear constraints with fixed types")
+{
+   SCIP_VAR* var1;
+   SCIP_VAR* var2;
+   SCIP_VAR* var3;
+   SCIP_VAR* var4;
+   SCIP_CONS* cons;
+   SCIP_VAR* vars[2];
+   SCIP_Real vals[2];
+   SCIP_CONSHDLR* conshdlr;
+   int npermvars;
+   SCIP_VAR** permvars;
+   int nperms;
+   int** perms;
+   int j;
+
+   /* skip test if no symmetry can be computed */
+   if ( ! SYMcanComputeSymmetry() )
+      return;
+
+   /* setup problem:
+    *
+    * x1 + x2          = 1
+    *          x3 + x4 = 1
+    * x1, x2 binary; x3, x4 continuous
+    */
+   SCIP_CALL( SCIPcreateProbBasic(scip, "basic2"));
+
+   SCIP_CALL( SCIPcreateVarBasic(scip, &var1, "x1", 0.0, 1.0, 1.0, SCIP_VARTYPE_BINARY) );
+   SCIP_CALL( SCIPaddVar(scip, var1) );
+   SCIP_CALL( SCIPcreateVarBasic(scip, &var2, "x2", 0.0, 1.0, 1.0, SCIP_VARTYPE_BINARY) );
+   SCIP_CALL( SCIPaddVar(scip, var2) );
+   SCIP_CALL( SCIPcreateVarBasic(scip, &var3, "x3", 0.0, 1.0, 1.0, SCIP_VARTYPE_CONTINUOUS) );
+   SCIP_CALL( SCIPaddVar(scip, var3) );
+   SCIP_CALL( SCIPcreateVarBasic(scip, &var4, "x4", 0.0, 1.0, 1.0, SCIP_VARTYPE_CONTINUOUS) );
+   SCIP_CALL( SCIPaddVar(scip, var4) );
+
+   vars[0] = var1;
+   vars[1] = var2;
+   vals[0] = 1.0;
+   vals[1] = 1.0;
+   SCIP_CALL( SCIPcreateConsBasicLinear(scip, &cons, "e1", 2, vars, vals, 1.0, 1.0) );
+   SCIP_CALL( SCIPaddCons(scip, cons) );
+   SCIP_CALL( SCIPreleaseCons(scip, &cons) );
+
+   vars[0] = var3;
+   vars[1] = var4;
+   vals[0] = 1.0;
+   vals[1] = 1.0;
+   SCIP_CALL( SCIPcreateConsBasicLinear(scip, &cons, "e2", 2, vars, vals, 1.0, 1.0) );
+   SCIP_CALL( SCIPaddCons(scip, cons) );
+   SCIP_CALL( SCIPreleaseCons(scip, &cons) );
+
+   /* setup symmetry constraint */
+   SCIP_CALL( SCIPcreateConsSymmetry(scip, &cons, "sym") );
+   SCIP_CALL( SCIPaddCons(scip, cons) );
+   SCIP_CALL( SCIPreleaseCons(scip, &cons) );
+
+   /* get constraint handler */
+   conshdlr = SCIPfindConshdlr(scip, "symmetry");
+   cr_assert( conshdlr != NULL );
+
+   /* turn off presolving in order to avoid having trivial problem afterwards */
+   SCIP_CALL( SCIPsetIntParam(scip, "presolving/maxrounds", 0) );
+
+   /* mark that we want to have symmetry */
+   SYMsetSpecRequirement(conshdlr, SYM_SPEC_BINARY | SYM_SPEC_REAL);
+
+   /* mark that we want to fix all binary variables */
+   SYMsetSpecRequirementFixed(conshdlr, SYM_SPEC_BINARY);
+
+   /* presolve problem (symmetry will be available afterwards) */
+   SCIP_CALL( SCIPpresolve(scip) );
+
+   /* get symmetry */
+   SCIP_CALL( SCIPgetSymmetryGenerators(scip, conshdlr, &npermvars, &permvars, &nperms, &perms) );
+   cr_assert( nperms == 1 );
+
+   SCIPinfoMessage(scip, NULL, "Permutation: (");
+   for (j = 0; j < npermvars; ++j)
+   {
+      if ( j == 0 )
+         SCIPinfoMessage(scip, NULL, "%d", perms[0][j]);
+      else
+         SCIPinfoMessage(scip, NULL, " %d", perms[0][j]);
+   }
+   SCIPinfoMessage(scip, NULL, ")\n");
+
+
+   SCIP_CALL( SCIPreleaseVar(scip, &var1) );
+   SCIP_CALL( SCIPreleaseVar(scip, &var2) );
+   SCIP_CALL( SCIPreleaseVar(scip, &var3) );
+   SCIP_CALL( SCIPreleaseVar(scip, &var4) );
+}
+
+
+/* TEST 3 */
+Test(test_compute_symmetry, basic3, .description = "compute symmetry for a simple example with 4 variables and four linear constraints")
 {
    SCIP_VAR* var1;
    SCIP_VAR* var2;
@@ -175,7 +273,7 @@ Test(test_compute_symmetry, basic2, .description = "compute symmetry for a simpl
     *     2x2 + x3     <= 2
     * x1, ..., x4 binary
     */
-   SCIP_CALL( SCIPcreateProbBasic(scip, "basic1"));
+   SCIP_CALL( SCIPcreateProbBasic(scip, "basic3"));
 
    SCIP_CALL( SCIPcreateVarBasic(scip, &var1, "x1", 0.0, 1.0, 1.0, SCIP_VARTYPE_BINARY) );
    SCIP_CALL( SCIPaddVar(scip, var1) );
