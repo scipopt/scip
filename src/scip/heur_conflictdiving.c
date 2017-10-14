56ba48b9771be94225289a7f1d6ab0e18dd1f932
Jakob Witzig <witzig@zib.de>
2017-10-14 19:46:26 +0200

    cleanup code

--- src/scip/heur_conflictdiving.c
+++ src/scip/heur_conflictdiving.c
@@ -58,7 +58,7 @@
                                          *   more general constraint handler diving variable selection? */
 
 #define DEFAULT_ADDSOLUTION        TRUE /**< should the solution be added to the solution pool */
-#define DEFAULT_EXPREGION           'l' /**< which region of the search space should be explore? ('l'ocked, 'u'nlocked)*/
+#define DEFAULT_MAXVIOL            TRUE /**< prefere rounding direction with most violation */
 
 #define DEFAULT_MINNUMSOFTLOCKS      0
 
@@ -66,13 +66,10 @@
 struct SCIP_HeurData
 {
    SCIP_SOL*             sol;                /**< working solution */
-   int*                  inds;
-   int*                  nsoftlocks;
-   int                   ninds;
 
    SCIP_Bool             addsol;
+   SCIP_Bool             maxviol;
    int                   minnumsoftlocks;
-   char                  expregion;
 };
 
 /*
@@ -134,10 +131,6 @@ SCIP_DECL_HEURINIT(heurInitConflictdiving) /*lint --e{715}*/
    /* create working solution */
    SCIP_CALL( SCIPcreateSol(scip, &heurdata->sol, heur) );
 
-   heurdata->inds = NULL;
-   heurdata->nsoftlocks = NULL;
-   heurdata->ninds = -1;
-
    return SCIP_OKAY;
 }
 
@@ -158,37 +151,6 @@ SCIP_DECL_HEUREXIT(heurExitConflictdiving) /*lint --e{715}*/
    /* free working solution */
    SCIP_CALL( SCIPfreeSol(scip, &heurdata->sol) );
 
-   if( heurdata->ninds > -1 )
-   {
-      SCIPfreeBlockMemoryArray(scip, &heurdata->inds, SCIPgetNVars(scip));
-      SCIPfreeBlockMemoryArray(scip, &heurdata->nsoftlocks, SCIPgetNVars(scip));
-   }
-
-   return SCIP_OKAY;
-}
-
-/** solving process deinitialization method of primal heuristic (called before branch and bound process data is freed) */
-static
-SCIP_DECL_HEUREXITSOL(heurExitsolConflictdiving) /*lint --e{715}*/
-{  /*lint --e{715}*/
-   SCIP_HEURDATA* heurdata;
-
-   assert(heur != NULL);
-   assert(strcmp(SCIPheurGetName(heur), HEUR_NAME) == 0);
-
-   /* get heuristic data */
-   heurdata = SCIPheurGetData(heur);
-   assert(heurdata != NULL);
-
-   if( heurdata->ninds > -1 )
-   {
-      SCIPfreeBlockMemoryArray(scip, &heurdata->inds, SCIPgetNVars(scip));
-      SCIPfreeBlockMemoryArray(scip, &heurdata->nsoftlocks, SCIPgetNVars(scip));
-      heurdata->inds = NULL;
-      heurdata->nsoftlocks = NULL;
-      heurdata->ninds = -1;
-   }
-
    return SCIP_OKAY;
 }
 
@@ -196,11 +158,8 @@ SCIP_DECL_HEUREXITSOL(heurExitsolConflictdiving) /*lint --e{715}*/
 static
 SCIP_DECL_HEUREXEC(heurExecConflictdiving) /*lint --e{715}*/
 {  /*lint --e{715}*/
-   SCIP_VAR** vars;
    SCIP_HEURDATA* heurdata;
    SCIP_DIVESET* diveset;
-   int nvars;
-   int i;
 
    heurdata = SCIPheurGetData(heur);
    assert(heurdata != NULL);
@@ -218,46 +177,6 @@ SCIP_DECL_HEUREXEC(heurExecConflictdiving) /*lint --e{715}*/
 
    if( heurtiming == SCIP_HEURTIMING_DURINGLPLOOP && SCIPgetDepth(scip) != 0 )
       return SCIP_OKAY;
-
-   vars = SCIPgetVars(scip);
-   assert(vars != NULL);
-
-   nvars = SCIPgetNVars(scip);
-
-   if( heurdata->ninds == -1 )
-   {
-      SCIP_CALL( SCIPallocBlockMemoryArray(scip, &heurdata->inds, nvars) );
-      SCIP_CALL( SCIPallocBlockMemoryArray(scip, &heurdata->nsoftlocks, nvars) );
-
-      heurdata->ninds = 0;
-
-      for( i = 0; i < nvars; i++ )
-      {
-         heurdata->nsoftlocks[i] = SCIPvarGetNLocksSoftUp(vars[i]) + SCIPvarGetNLocksSoftDown(vars[i]);
-
-         if( heurdata->nsoftlocks[i] < heurdata->minnumsoftlocks )
-            heurdata->inds[heurdata->ninds++] = i;
-      }
-   }
-   else
-   {
-      /* check number of softlocks */
-      for( i = 0; i < heurdata->ninds; )
-      {
-         int v = heurdata->inds[i];
-
-         heurdata->nsoftlocks[v] = SCIPvarGetNLocksSoftUp(vars[v]) + SCIPvarGetNLocksSoftDown(vars[v]);
-
-         if( heurdata->nsoftlocks[v] >= heurdata->minnumsoftlocks )
-         {
-            --heurdata->ninds;
-            heurdata->inds[i] = heurdata->inds[heurdata->ninds];
-         }
-         else
-            i++;
-      }
-   }
-   assert(heurdata->ninds >= 0);
 
    SCIP_CALL( SCIPperformGenericDivingAlgorithm(scip, diveset, heurdata->sol, heur, result, nodeinfeasible) );
 
@@ -300,9 +219,22 @@ SCIP_DECL_DIVESETGETSCORE(divesetGetScoreConflictdiving)
    {
       *roundup = mayroundup;
    }
+   /* variable can be rounded in both directions, look at the "normal" locks */
+   else if( mayroundup )
+   {
+      assert(mayrounddown);
+
+      if( heurdata->maxviol )
+         *roundup = (SCIPvarGetNLocksUp(cand) >= SCIPvarGetNLocksDown(cand));
+      else
+         *roundup = (SCIPvarGetNLocksUp(cand) <= SCIPvarGetNLocksDown(cand));
+   }
    else
    {
-      *roundup = (SCIPvarGetNLocksSoftUp(cand) >= SCIPvarGetNLocksSoftDown(cand));
+      if( heurdata->maxviol )
+         *roundup = (SCIPvarGetNLocksSoftUp(cand) >= SCIPvarGetNLocksSoftDown(cand));
+      else
+         *roundup = (SCIPvarGetNLocksSoftUp(cand) <= SCIPvarGetNLocksSoftDown(cand));
    }
 
    if( *roundup )
@@ -322,8 +254,7 @@ SCIP_DECL_DIVESETGETSCORE(divesetGetScoreConflictdiving)
             return SCIP_INVALIDDATA; /*lint !e527*/
       } /*lint !e788*/
 
-
-      if( heurdata->expregion == 'l' )
+      if( heurdata->maxviol )
          *score = SCIPvarGetNLocksSoftUp(cand)/MAX(1.0, softlocksum)
                + (LOCKFRAC + SCIPrandomGetReal(rng, MIN_RAND, MAX_RAND)) * SCIPvarGetNLocksUp(cand)/MAX(1.0, locksum);
       else
@@ -335,7 +266,7 @@ SCIP_DECL_DIVESETGETSCORE(divesetGetScoreConflictdiving)
       if ( divetype == SCIP_DIVETYPE_SOS1VARIABLE && SCIPisFeasNegative(scip, candsol) )
          candsfrac = 1.0 - candsfrac;
 
-      if( heurdata->expregion == 'l' )
+      if( heurdata->maxviol )
          *score = SCIPvarGetNLocksSoftDown(cand)/MAX(1.0, softlocksum)
                + (LOCKFRAC + SCIPrandomGetReal(rng, MIN_RAND, MAX_RAND)) * SCIPvarGetNLocksDown(cand)/MAX(1.0, locksum);
       else
@@ -344,7 +275,7 @@ SCIP_DECL_DIVESETGETSCORE(divesetGetScoreConflictdiving)
    }
 
    /* penalize too less softlocks */
-   if( heurdata->nsoftlocks[SCIPvarGetProbindex(cand)] < heurdata->minnumsoftlocks )
+   if( softlocksum < heurdata->minnumsoftlocks )
       (*score) *= 0.1;
 
    /* penalize too small fractions */
@@ -391,7 +322,6 @@ SCIP_RETCODE SCIPincludeHeurConflictdiving(
    SCIP_CALL( SCIPsetHeurFree(scip, heur, heurFreeConflictdiving) );
    SCIP_CALL( SCIPsetHeurInit(scip, heur, heurInitConflictdiving) );
    SCIP_CALL( SCIPsetHeurExit(scip, heur, heurExitConflictdiving) );
-   SCIP_CALL( SCIPsetHeurExitsol(scip, heur, heurExitsolConflictdiving) );
 
    /* create a diveset (this will automatically install some additional parameters for the heuristic)*/
    SCIP_CALL( SCIPcreateDiveset(scip, NULL, heur, HEUR_NAME, DEFAULT_MINRELDEPTH, DEFAULT_MAXRELDEPTH, DEFAULT_MAXLPITERQUOT,
@@ -404,8 +334,8 @@ SCIP_RETCODE SCIPincludeHeurConflictdiving(
    SCIP_CALL( SCIPaddIntParam(scip, "heuristics/" HEUR_NAME "/minnumsoftlocks", "minimal number of softlocks per variable",
          &heurdata->minnumsoftlocks, TRUE, DEFAULT_MINNUMSOFTLOCKS, 0, INT_MAX, NULL, NULL) );
 
-   SCIP_CALL( SCIPaddCharParam(scip, "heuristics/" HEUR_NAME "/expregion", "which region should be explored",
-         &heurdata->expregion, TRUE, DEFAULT_EXPREGION, "lu", NULL, NULL) );
+   SCIP_CALL( SCIPaddBoolParam(scip, "heuristics/" HEUR_NAME "/maxviol", "try to maximize the violation",
+         &heurdata->maxviol, TRUE, DEFAULT_MAXVIOL, NULL, NULL) );
 
    return SCIP_OKAY;
 }
