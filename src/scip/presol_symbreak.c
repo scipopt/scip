ed60969b10ea5050b9e4a3bf32ef532069bf41ff
Christopher Hojny <hojny@mathematik.tu-darmstadt.de>
2017-10-17 14:38:18 +0200

    corrected data update in orbitope detection

--- src/scip/presol_symbreak.c
+++ src/scip/presol_symbreak.c
@@ -772,14 +772,15 @@ SCIP_RETCODE detectOrbitopes(
 
          SCIP_CALL( getPermProperties(perms[components[i][j]], permvars, npermvars, &iscompoftwocycles, &ntwocyclesperm, &allvarsbinary) );
 
+         if ( ntwocyclescomp == - 1 )
+            ntwocyclescomp = ntwocyclesperm;
+
          /* no or different number of 2-cycles or not all vars binary: permutations cannot generate orbitope */
          if ( ntwocyclescomp == 0 || ntwocyclescomp != ntwocyclesperm || ! allvarsbinary )
          {
             isorbitope = FALSE;
             break;
          }
-
-         ntwocyclescomp = ntwocyclesperm;
       }
 
       if ( ! isorbitope )
