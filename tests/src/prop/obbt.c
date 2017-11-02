90dec8e43ce73d7577ae4a2120192833e1876779
Benjamin Mueller <benjamin.mueller@zib.de>
2017-11-02 12:13:42 +0100

    move unittest to obbt.c

--- tests/src/prop/obbt.c
+++ tests/src/prop/obbt.c
@@ -13,14 +13,14 @@
 /*                                                                           */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-/**@file   gauge.c
- * @brief  unit test for cons_quadratic methods
+/**@file   obbt.c
+ * @brief  unit test for prop_obbt methods
  */
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
 
 #include "scip/scip.h"
-#include "scip/cons_quadratic.c"
+#include "scip/prop_obbt.c"
 
 #include "include/scip_test.h"
 
