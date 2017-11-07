3439930e692b30a73f45251205657aa7d7a74165
Gerald Gamrath <gamrath@zib.de>
2017-11-07 17:20:25 +0100

    fixed check for column status of variable

--- src/scip/heur_locks.c
+++ src/scip/heur_locks.c
@@ -524,10 +524,11 @@ SCIP_DECL_HEUREXEC(heurExecLocks)
 
       if( updatelocks )
       {
-         col = SCIPvarGetCol(var);
-
-         if( col == NULL )
+         if( SCIPvarGetStatus(var) != SCIP_VARSTATUS_COLUMN )
             continue;
+
+         col = SCIPvarGetCol(var);
+         assert(col != NULL);
 
          colrows = SCIPcolGetRows(col);
          colvals = SCIPcolGetVals(col);
