scipopt/scip

commit 0946b73f7fc0384120fb920c28beadce063a66a6

merge monomials only once when all children have been checked
- when simplifying a polynomial, a child that is itself a polynomial was
  moved into the parent polynomial
- after each such modification, the monomials where sorted and merged
  again
- in #1482, instances were reported where this takes very long
- with the change from this commit, we sort and merge the monomials only
  one time after all children of the polynomial have been processed

src/nlpi/expr.c
@@ -11622,10 +11622,10 @@ SCIP_RETCODE exprgraphNodeSimplify(
          SCIP_CALL( exprgraphNodeRemoveParent(exprgraph, &node->children[i], node) );
          node->children[i] = NULL;
       }
-
-      /* simplify current polynomial again */
-      polynomialdataMergeMonomials(blkmem, polynomialdata, eps, TRUE);
    }
+
+   /* simplify current polynomial again */
+   polynomialdataMergeMonomials(blkmem, polynomialdata, eps, TRUE);
 
    BMSfreeBlockMemoryArrayNull(blkmem, &childmap, childmapsize);
 

commit 4a01b4dd868f16ed67e9508a2233383f9cfecd93

ensure factors in monomial are merged before replacing child in simplify
- fixup for issue introduced by 0946b73f
- fixes #1848

src/nlpi/expr.c
@@ -11604,6 +11604,10 @@ SCIP_RETCODE exprgraphNodeSimplify(
          /* if monomial is not sorted, then polynomial should not be sorted either, or have only one monomial */
          assert(monomial->sorted || !polynomialdata->sorted || polynomialdata->nmonomials <= 1);
 
+         /* make sure factors are merged, should only be potentially necessary if not sorted, see also #1848 */
+         if( !monomial->sorted )
+            SCIPexprMergeMonomialFactors(monomial, eps);
+
          if( !SCIPexprFindMonomialFactor(monomial, i, &factorpos) )
          {
             ++j;