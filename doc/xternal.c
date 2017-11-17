3e6854cf5ec8e4ce55e96b8006b4312b605341f5
Gerald Gamrath <gamrath@zib.de>
2017-11-17 09:48:48 +0100

    updated version to 5.0.0.1

--- doc/xternal.c
+++ doc/xternal.c
@@ -131,7 +131,7 @@
  * @subsection AUTHORS SCIP Authors
  * - <a class="el" href="http://scip.zib.de/#developers">Developers</a>
  *
- * @version  5.0.0
+ * @version  5.0.0.1
  *
  * \image html scippy.png
  *
