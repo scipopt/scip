52d8ecfb7f1c9a3d8c2523a27ae79243fb9fcc62
Benjamin Mueller <benjamin.mueller@zib.de>
2017-11-27 14:43:26 +0100

    add Fabian as author of cons_expr_entropy.{c,h}

--- src/scip/cons_expr_entropy.h
+++ src/scip/cons_expr_entropy.h
@@ -16,6 +16,7 @@
 /**@file   cons_expr_entropy.h
  * @brief  handler for -x*log(x) expressions
  * @author Benjamin Mueller
+ * @author Fabian Wegscheider
  */
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
