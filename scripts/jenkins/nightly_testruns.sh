61931e77662afdcc4f0ed5b868db57ab7264ce33
Felipe Serrano <serrano@zib.de>
2017-11-30 00:13:04 +0100

    fix jenkins/nightly_testruns.sh: set flags correctly

--- scripts/jenkins/nightly_testruns.sh
+++ scripts/jenkins/nightly_testruns.sh
@@ -1,5 +1,5 @@
-#!/bin/bash -x
-# the -x is for writing each command to standard error (preceded by a ‘+ ’) before it is executed.
+#!/bin/bash -ex
+# the -x is for writing each command to standard error (preceded by a '+') before it is executed.
 # other flags for debugging: -Canvu, please also consult 'man sh'.
 
 ###################
@@ -36,7 +36,7 @@ ln -s /OPTI/adm_cple/ipopt lib/static/ipopt.linux.x86_64.gnu.opt
 ln -s /optimi/usr/sw/zimpl/src lib/include/zimplinc/zimpl
 ln -s /optimi/usr/sw/zimpl/lib/libzimpl.linux.x86_64.gnu.opt.a lib/static/libzimpl.linux.x86_64.gnu.opt.a
 
-SCIP_FLAGS="ZIMPL=true COMP=gnu OPT=dbg IPOPT=true SYM=bliss "
+SCIP_FLAGS="ZIMPL=true COMP=gnu OPT=dbg IPOPT=true SYM=bliss"
 
 # deal with different LPSolvers
 if [ "${LPS}" == "cpx" ];
@@ -44,18 +44,18 @@ then
     # cplex is globally installed
     ln -s /optimi/usr/sw/cplex/include/ilcplex lib/include/cpxinc
     ln -s /optimi/usr/sw/cplex/lib/x86-64_linux/static_pic/libcplex.a lib/static/libcplex.linux.x86_64.gnu.a
-    SCIP_FLAG+="LPS=cpx"
+    SCIP_FLAGS="$SCIP_FLAGS LPS=cpx"
 else
     # soplex is in adm_timos jenkins workspace
     ln -s /nfs/OPTI/jenkins/workspace/SOPLEX_COMP=gnu_OPT=dbg_nightly/src lib/include/spxinc
     ln -s /nfs/OPTI/jenkins/workspace/SOPLEX_COMP=gnu_OPT=dbg_nightly/lib/libsoplex.linux.x86_64.gnu.dbg.a lib/static/libsoplex.linux.x86_64.gnu.dbg.a
-    SCIP_FLAGS+="LPS=spx LPSOPT=dbg"
+    SCIP_FLAGS="$SCIP_FLAGS LPS=spx LPSOPT=dbg"
 fi
 
 ###################
 ### Compilation ###
 ###################
-yes "" | make $SCIP_FLAGS -j4 USRFLAGS=-Werror
+yes "" | make ${SCIP_FLAGS} -j4 USRFLAGS=-Werror
 
 ######################
 ### Testrun: setup ###
@@ -67,21 +67,21 @@ ln -s /optimi/kombadon/MINLP check/
 ### Testrun: execution ###
 ##########################
 # MIP
-# ./bin/scip -c "set rand rand 20171124 set diffsave settings/default_20171124.set q"
-# ./bin/scip -c "set heur emph aggr set rand rand 20171124 set diffsave settings/heuraggr_20171124.set q"
-# ./bin/scip -c "set sepa emph aggr set presol emph aggr set heur emph off set rand rand 20171124 set diffsave settings/presolaggr_sepaaggr_heuroff_20171124.set q"
-# for testset in mipdebug; do
-#   for setting in default_20171124 heuraggr_20171124 presolaggr_sepaaggr_heuroff_20171124; do
-#     make testcluster $SCIP_FLAGS TEST=$testset MEM=6000 TIME=60 SETTINGS=$setting QUEUE=opt-low | check/jenkins_check_results.sh $testset $setting
-#   done
-# done
+./bin/scip -c "set rand rand 20171124 set diffsave settings/default_20171124.set q"
+./bin/scip -c "set heur emph aggr set rand rand 20171124 set diffsave settings/heuraggr_20171124.set q"
+./bin/scip -c "set sepa emph aggr set presol emph aggr set heur emph off set rand rand 20171124 set diffsave settings/presolaggr_sepaaggr_heuroff_20171124.set q"
+for testset in mipdebug; do
+  for setting in default_20171124 heuraggr_20171124 presolaggr_sepaaggr_heuroff_20171124; do
+    make testcluster $SCIP_FLAGS TEST=$testset MEM=6000 TIME=60 SETTINGS=$setting QUEUE=opt-low | check/jenkins_check_results.sh $testset $setting
+  done
+done
 
 # MINLP
 ./bin/scip -c "set numerics checkfeastolfac 1000.0 set rand rand 20171124 set diffsave settings/minlp_default_20171124.set q"
 ./bin/scip -c "set heur emph aggr set numerics checkfeastolfac 1000.0 set rand rand 20171124 set diffsave settings/minlp_heuraggr_20171124.set q"
 ./bin/scip -c "set sepa emph aggr set presol emph aggr set heur emph off set numerics checkfeastolfac 1000.0 set rand rand 20171124 set diffsave settings/minlp_presolaggr_sepaaggr_heuroff_20171124.set q"
 for testset in MINLP; do
-  for setting in minlp_default_20171124; do #minlp_heuraggr_20171124 minlp_presolaggr_sepaaggr_heuroff_20171124; do
+  for setting in minlp_default_20171124 minlp_heuraggr_20171124 minlp_presolaggr_sepaaggr_heuroff_20171124; do
     make testcluster $SCIP_FLAGS TEST=$testset MEM=6000 TIME=60 SETTINGS=$setting QUEUE=opt-low | check/jenkins_check_results.sh $testset $setting
   done
 done
