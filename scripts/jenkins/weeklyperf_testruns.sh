a7b72f94e281726e12828fb9414274db1b94a778
Felipe Serrano <serrano@zib.de>
2017-12-01 16:21:21 +0100

    use soplex in minlp performance runs

--- scripts/jenkins/weeklyperf_testruns.sh
+++ scripts/jenkins/weeklyperf_testruns.sh
@@ -6,7 +6,7 @@
 ### Description ###
 ###################
 # This script is used by cijenkins.zib.de.
-# It builds SCIP with Make and an LPSolver and runs mipdebug and MINLP on different settings.
+# It builds optimized SCIP/SoPlex with make and runs mipdev-solvable, sapdev-solvable and minlpdev-solvable
 
 # Usage: from scip root execute
 #        ./weeklyperf_testruns.sh
@@ -20,19 +20,17 @@ mkdir -p settings
 ln -s ../../../SOPLEX_COMP=gnu_OPT=opt_weekly/src lib/include/spxinc
 ln -s ../../../SOPLEX_COMP=gnu_OPT=opt_weekly/lib/libsoplex.linux.x86_64.gnu.opt.a lib/static/libsoplex.linux.x86_64.gnu.opt.a
 
-ln -s /OPTI/adm_cple/cplex/include/ilcplex lib/include/cpxinc
-ln -s /OPTI/adm_cple/cplex/lib/x86-64_linux/static_pic/libcplex.a lib/static/libcplex.linux.x86_64.gnu.a
-
 ln -s /OPTI/adm_cple/ipopt lib/static/ipopt.linux.x86_64.gnu.opt
 
 ln -s /optimi/usr/sw/bliss lib/include/bliss
 ln -s /optimi/usr/sw/bliss/libbliss.a lib/static/libbliss.linux.x86_64.gnu.a
 
+SCIP_FLAGS="IPOPT=true SYM=bliss ZIMPL=false COMP=gnu LPS=spx OPT=opt"
+
 ###################
 ### Compilation ###
 ###################
-yes "" | make LPS=spx LPSOPT=opt VERSION=perfspx IPOPT=true SYM=bliss ZIMPL=false COMP=gnu OPT=opt -j4 USRFLAGS=-Werror
-yes "" | make LPS=cpx LPSOPT=opt VERSION=perfcpx IPOPT=true SYM=bliss ZIMPL=false COMP=gnu OPT=opt -j4 USRFLAGS=-Werror
+yes "" | make $SCIP_FLAGS -j4 USRFLAGS=-Werror
 
 ######################
 ### Testrun: setup ###
@@ -43,27 +41,23 @@ ln -s /optimi/kombadon/MINLP check/
 ##########################
 ### Testrun: execution ###
 ##########################
-# soplex
 for testset in mipdev-solvable; do
     for setting in default; do
-        make testcluster TEST=$testset SETTINGS=$setting LPS=spx OPT=opt VERSION=perfspx IPOPT=true SYM=bliss EXCLUSIVE=true TIME=7200 MEM=35000 QUEUE=M620v3-low | PERF=performance VERSION=perfspx check/jenkins_check_results.sh $testset $setting
+        make testcluster $SCIP_FLAGS TEST=$testset SETTINGS=$setting EXCLUSIVE=true TIME=7200 MEM=35000 QUEUE=M620v3-low | PERF=performance check/jenkins_check_results.sh $testset $setting
     done
 done
 
-# copy sap settings
 cp ~/sap-400-pure.set settings/.
-
 for testset in sapdev-solvable; do
     for setting in sap-400-pure; do
-        make testcluster TEST=$testset SETTINGS=$setting LPS=spx OPT=opt VERSION=perfspx IPOPT=true SYM=bliss EXCLUSIVE=true TIME=7200 MEM=35000 QUEUE=M630v2 | PERF=performance VERSION=perfspx check/jenkins_check_results.sh $testset $setting
+        make testcluster $SCIP_FLAGS TEST=$testset SETTINGS=$setting EXCLUSIVE=true TIME=7200 MEM=35000 QUEUE=M630v2 | PERF=performance check/jenkins_check_results.sh $testset $setting
     done
 done
 
-# cplex
 echo "limits/gap = 0.0001" > settings/minlp.set
 echo "numerics/checkfeastolfac = 10.0" >> settings/minlp.set
 for testset in minlpdev-solvable; do
     for setting in minlp; do
-        make testcluster TEST=$testset SETTINGS=$setting LPS=cpx OPT=opt VERSION=perfcpx IPOPT=true SYM=bliss EXCLUSIVE=true TIME=3600 MEM=35000 QUEUE=M630-low | PERF=performance VERSION=perfcpx check/jenkins_check_results.sh $testset $setting
+        make testcluster $SCIP_FLAGS TEST=$testset SETTINGS=$setting EXCLUSIVE=true TIME=3600 MEM=35000 QUEUE=M630-low | PERF=performance check/jenkins_check_results.sh $testset $setting
     done
 done
