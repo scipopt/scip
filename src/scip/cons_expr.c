60ec3dda45bb8ee3fffe5cfa9f597499f7511f4c
Stefan Vigerske <svigerske@gams.com>
2017-12-11 16:12:45 +0100

    fixup nlhdlr detect

--- src/scip/cons_expr.c
+++ src/scip/cons_expr.c
@@ -3606,7 +3606,6 @@ SCIP_DECL_CONSEXPREXPRWALK_VISIT(detectNlhdlrsEnterExpr)
    SCIP_CONSHDLR* conshdlr;
    NLHDLR_DETECT_DATA* detectdata;
    SCIP_CONSEXPR_EXPRENFO_METHOD desiredenfo;
-   SCIP_CONSEXPR_EXPRENFO_METHOD providedenfo;
    int nsuccess;
    int e, h;
 
@@ -3662,10 +3661,11 @@ SCIP_DECL_CONSEXPREXPRWALK_VISIT(detectNlhdlrsEnterExpr)
       assert(nlhdlr->detect != NULL); /* detect callback is mandatory */
 
       /* call detect routine of nlhdlr */
+      detectdata->nlhdlrssuccessprovided[nsuccess] = SCIP_CONSEXPR_EXPRENFO_NONE;
       detectdata->nlhdlrssuccessexprdata[nsuccess] = NULL;
       SCIP_CALL( (*nlhdlr->detect)(scip, conshdlr, nlhdlr, expr, desiredenfo, &detectdata->nlhdlrssuccessprovided[nsuccess], &detectdata->nlhdlrssuccessexprdata[nsuccess]) );
 
-      if( providedenfo == SCIP_CONSEXPR_EXPRENFO_NONE )
+      if( detectdata->nlhdlrssuccessprovided[nsuccess] == SCIP_CONSEXPR_EXPRENFO_NONE )
       {
          /* nlhdlrexprdata can only be non-NULL if it provided some functionality */
          assert(detectdata->nlhdlrssuccessexprdata[nsuccess] == NULL);
@@ -3675,11 +3675,13 @@ SCIP_DECL_CONSEXPREXPRWALK_VISIT(detectNlhdlrsEnterExpr)
 
       /* remember also nlhdlr */
       detectdata->nlhdlrssuccess[nsuccess] = nlhdlr;
-      ++nsuccess;
 
       /* update which enforcement methods we still require = desired before - provided now
-       * (it is possible that a handler provides a method that was not desired (anymore), which is allowed behavior) */
-      desiredenfo &= ~providedenfo;
+       * (it is possible that a handler provides a method that was not desired (anymore), which is allowed behavior)
+       */
+      desiredenfo &= ~detectdata->nlhdlrssuccessprovided[nsuccess];
+
+      ++nsuccess;
    }
 
    /* stop if some desired enforcement method was not provided
@@ -3733,6 +3735,7 @@ SCIP_RETCODE detectNlhdlrs(
    /* allocate some buffer for temporary storage of nlhdlr detect result */
    SCIP_CALL( SCIPallocBufferArray(scip, &nlhdlrdetect.nlhdlrssuccess, conshdlrdata->nnlhdlrs) );
    SCIP_CALL( SCIPallocBufferArray(scip, &nlhdlrdetect.nlhdlrssuccessexprdata, conshdlrdata->nnlhdlrs) );
+   SCIP_CALL( SCIPallocBufferArray(scip, &nlhdlrdetect.nlhdlrssuccessprovided, conshdlrdata->nnlhdlrs) );
 
    for( i = 0; i < nconss; ++i )
    {
@@ -3788,6 +3791,7 @@ SCIP_RETCODE detectNlhdlrs(
 
    SCIPfreeBufferArray(scip, &nlhdlrdetect.nlhdlrssuccess);
    SCIPfreeBufferArray(scip, &nlhdlrdetect.nlhdlrssuccessexprdata);
+   SCIPfreeBufferArray(scip, &nlhdlrdetect.nlhdlrssuccessprovided);
 
    return SCIP_OKAY;
 }
