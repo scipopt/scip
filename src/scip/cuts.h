a7e2801149ee63d8a279f24a0c96a07d1c91a2a2
Robert Lion Gottwald <robert.gottwald@zib.de>
2017-12-13 16:59:33 +0100

    some documentation improvements

--- src/scip/cuts.h
+++ src/scip/cuts.h
@@ -408,7 +408,7 @@ SCIP_RETCODE SCIPcalcStrongCG(
    SCIP_Real             minfrac,            /**< minimal fractionality of rhs to produce strong CG cut for */
    SCIP_Real             maxfrac,            /**< maximal fractionality of rhs to produce strong CG cut for */
    SCIP_Real             scale,              /**< additional scaling factor multiplied to all rows */
-   SCIP_AGGRROW*         aggrrow,            /**< the aggregation row to compute a flow cover cut for */
+   SCIP_AGGRROW*         aggrrow,            /**< the aggregation row to compute a strong CG cut for */
    SCIP_Real*            cutcoefs,           /**< array to store the non-zero coefficients in the cut */
    SCIP_Real*            cutrhs,             /**< pointer to store the right hand side of the cut */
    int*                  cutinds,            /**< array to store the problem indices of variables with a non-zero coefficient in the cut */
