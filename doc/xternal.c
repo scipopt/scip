f8d1d318d6e610910acafaccbac39273737b4cc6
Matthias Miltenberger <miltenberger@zib.de>
2017-12-22 17:47:55 +0100

    increase version to 5.0.1

--- doc/xternal.c
+++ doc/xternal.c
@@ -133,7 +133,7 @@
  * @subsection AUTHORS SCIP Authors
  * - <a class="el" href="http://scip.zib.de/#developers">Developers</a>
  *
- * @version  5.0.0
+ * @version  5.0.1
  *
  * \image html scippy.png
  *
