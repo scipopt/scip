7e4a4e731c98b9ce20617667ee586aeb605cd6d9
Benjamin Mueller <benjamin.mueller@zib.de>
2018-01-05 14:21:25 +0100

    fix typo

--- src/scip/cons_expr_nlhdlr_convex.h
+++ src/scip/cons_expr_nlhdlr_convex.h
@@ -13,7 +13,7 @@
 /*                                                                           */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-/**@file   cons_expr_nlhdlr_convex.c
+/**@file   cons_expr_nlhdlr_convex.h
  * @brief  nonlinear handler for convex expressions
  * @author Benjamin Mueller
  */
