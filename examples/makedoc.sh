fd05a340ec04e6af10f2e8b152ee142041ab6af8
Stephen J. Maher <s.maher3@lancaster.ac.uk>
2018-01-30 13:51:22 +0000

    updates to the make files for examples

--- examples/makedoc.sh
+++ examples/makedoc.sh
@@ -3,7 +3,7 @@
 # run with bash -e makeall.sh to stop on errors
 #
 
-EXAMPLES=(Binpacking CallableLibrary Eventhdlr GMI LOP MIPSolver Queens TSP VRP)
+EXAMPLES=(Binpacking CallableLibrary CAP Eventhdlr GMI LOP MIPSolver Queens TSP VRP)
 
 for EXAMPLE in ${EXAMPLES[@]}
 do
