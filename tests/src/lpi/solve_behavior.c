2779ea7d58b237f5110e73eee7693553df0ece7d
Franziska Schloesser <schloesser@zib.de>
2018-02-16 12:28:33 +0100

    simplify unittest-lpi-solve_behaviour

--- tests/src/lpi/solve_behavior.c
+++ tests/src/lpi/solve_behavior.c
@@ -17,8 +17,8 @@
  * @brief  unit tests for testing the behaviour of the LP solver when calling solving methods and
  *    getting the solving state afterwards.  *
  *    Methods tested are:
- *       SCIPlpiSolveDual(), SCIPlpiWasSolved()
- *       SCIPlpiHas{Primal,Dual,Barrier}Solve()
+ *       SCIPlpiSolveBarier(), SCIPlpiWasSolved()
+ *       SCIPlpiHas{Simplex,Barrier}Solve()
  *
  * @author Franziska Schloesser
  */
@@ -38,13 +38,8 @@ static SCIP_LPI* lpi = NULL;
 
 /** write ncols, nrows and objsen into variables to check later */
 static
-SCIP_Bool initProb(int pos, int* ncols, int* nrows, int* nnonz, SCIP_OBJSEN* objsen)
+SCIP_Bool initProb(int* ncols, int* nrows, int* nnonz, SCIP_OBJSEN* objsen)
 {
-   /* this is necessary because the theories don't setup and teardown after each call, but once before and after */
-   if ( lpi != NULL )
-   {
-      SCIP_CALL( SCIPlpiFree(&lpi) );
-   }
    SCIP_CALL( SCIPlpiCreate(&lpi, NULL, "prob", SCIP_OBJSEN_MAXIMIZE) );
 
    SCIP_Real obj[100] = { 1.0, 1.0 };
@@ -56,123 +51,25 @@ SCIP_Bool initProb(int pos, int* ncols, int* nrows, int* nnonz, SCIP_OBJSEN* obj
    int beg[100] = { 0, 1 };
    int ind[100] = { 0, 1 };
 
-   switch ( pos )
-   {
-
-   case 0:
-      /* maximization problems, ncols is 2, *nrows is 2 */
-      /* unbounded - infeasible
-       * (P):  max x+y
-       * -x    <= 1 (constr)
-       *    -y <= 1 (constr)
-       *
-       *  0 <= x (bound)
-       *  0 <= y (bound)
-       *
-       * (D):  min x+y
-       * 1 <= -x   (constr)
-       * 1 <=   -y (constr)
-       *
-       * 0 <= x (bound)
-       * 0 <= y (bound)
-       * */
-      *ncols = 2;
-      *nrows = 2;
-      *nnonz = 2;
-      *objsen = SCIP_OBJSEN_MAXIMIZE;
-      val[0] = -1.0;
-      val[1] = -1.0;
-      break;
-
-   case 1:
-      /* optimal - optimal
-       * (P):  max x+y
-       * x     <= 1 (constr)
-       *     y <= 1 (constr)
-       *
-       *  0 <= x (bound)
-       *  0 <= y (bound)
-       *
-       * (D):  min x+y
-       * 1 <= x    (constr)
-       * 1 <=    y (constr)
-       *
-       * 0 <= x (bound)
-       * 0 <= y (bound)
-       j* */
-      *ncols = 2;
-      *nrows = 2;
-      *nnonz = 2;
-      *objsen = SCIP_OBJSEN_MAXIMIZE;
-      break;
-
-   case 2:
-      /* infeasible - infeasible
-       * (P):  max x+y
-       * -x    <= -1 (constr)
-       *     y <= -1 (constr)
-       *
-       *  0 <= x (bound)
-       *  0 <= y (bound)
-       *
-       * (D):  min -x-y
-       * 1 <= -x    (constr)
-       * 1 <=     y (constr)
-       *
-       * 0 <= x (bound)
-       * 0 <= y (bound)
-       */
-      *ncols = 2;
-      *nrows = 2;
-      *nnonz = 2;
-      *objsen = SCIP_OBJSEN_MAXIMIZE;
-      rhs[0] = -1.0;
-      rhs[1] = -1.0;
-      val[0] = -1.0;
-      break;
-
-   case 3:
-      /* minimization problems (duals of the above) */
-      *ncols = 2;
-      *nrows = 2;
-      *nnonz = 2;
-      *objsen = SCIP_OBJSEN_MINIMIZE;
-      rhs[0] = SCIPlpiInfinity(lpi);
-      rhs[1] = SCIPlpiInfinity(lpi);
-      lhs[0] = 1.0;
-      lhs[1] = 1.0;
-      val[0] = -1.0;
-      val[1] = -1.0;
-      break;
-
-   case 4:
-      *ncols = 2;
-      *nrows = 2;
-      *nnonz = 2;
-      *objsen = SCIP_OBJSEN_MINIMIZE;
-      rhs[0] = SCIPlpiInfinity(lpi);
-      rhs[1] = SCIPlpiInfinity(lpi);
-      lhs[0] = 1.0;
-      lhs[1] = 1.0;
-      break;
-
-   case 5:
-      *ncols = 2;
-      *nrows = 2;
-      *nnonz = 2;
-      *objsen = SCIP_OBJSEN_MINIMIZE;
-      rhs[0] = SCIPlpiInfinity(lpi);
-      rhs[1] = SCIPlpiInfinity(lpi);
-      lhs[0] = 1.0;
-      lhs[1] = 1.0;
-      obj[0] = -1.0;
-      obj[1] = -1.0;
-      val[0] = -1.0;
-      break;
-
-   default:
-      return FALSE;
-   }
+   /* optimal - optimal
+    * (P):  max x+y
+    * x     <= 1 (constr)
+    *     y <= 1 (constr)
+    *
+    *  0 <= x (bound)
+    *  0 <= y (bound)
+    *
+    * (D):  min x+y
+    * 1 <= x    (constr)
+    * 1 <=    y (constr)
+    *
+    * 0 <= x (bound)
+    * 0 <= y (bound)
+    j* */
+   *ncols = 2;
+   *nrows = 2;
+   *nnonz = 2;
+   *objsen = SCIP_OBJSEN_MAXIMIZE;
 
    SCIP_CALL( SCIPlpiChgObjsen(lpi, *objsen) );
    SCIP_CALL( SCIPlpiAddCols(lpi, *ncols, obj, lb, ub, NULL, 0, NULL, NULL, NULL) );
@@ -203,35 +100,31 @@ void teardown(void)
 
 TestSuite(solve_behavior, .init = setup, .fini = teardown);
 
-/** Test */
+/** Tests */
 
-/** Test adding zero coeffs in rows, expecting an assert in debug mode */
-/* This test should fail with an assert from the LPI, which causes SIGABRT to be issued. Thus, this test should pass. */
-Test(solve_behavior, testbarriersolve, .signal = SIGABRT)
+/** SCIPlpiSolveBarrier should always solve the problem
+ *  if the lp solver does not have a barrier method it should default to dualsimplex. */
+Test(solve_behavior, testbarriersolve)
 {
    int nrows, ncols, nnonz;
    SCIP_OBJSEN sense;
-   cr_assume( initProb(4, &ncols, &nrows, &nnonz, &sense) );
-   cr_assume_eq( 3, nrows );
-   cr_assume_eq( 3, ncols );
+   initProb(&ncols, &nrows, &nnonz, &sense);
 
    SCIP_CALL( SCIPlpiSolveBarrier(lpi, true) );
-   SCIP_CALL( SCIPlpiSolveBarrier(lpi, false) );
 
    /* Problem is optimal - optimal, should be solved */
    cr_assert( SCIPlpiWasSolved(lpi) );
 
-   /* Does it make sense not to have a primal solve as an lp solver? */
-   cr_assert( SCIPlpiHasPrimalSolve() );
-   /* try calling all methods at least once */
-   SCIPlpiHasDualSolve();
+   SCIP_CALL( SCIPlpiSolveBarrier(lpi, false) );
 
-   if( !SCIPlpiHasBarrierSolve() )
-   {
-      /* make sure problem was solved with dual */
-      if ( !SCIPlpiHasDualSolve() )
-      {
-         /* make sure problem was solved with primal */
-      }
-   }
+   /* Problem is optimal - optimal, should be solved */
+   cr_assert( SCIPlpiWasSolved(lpi) );
+}
+
+/** Test if the two method giving information about availability of solve methods do not crash. */
+Test(solve_behavior, testhassolve)
+{
+   /* try calling all methods at least once */
+   SCIPlpiHasSimplexSolve(lpi);
+   SCIPlpiHasBarrierSolve(lpi);
 }
