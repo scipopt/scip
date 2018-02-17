d9b563356b9b19da061931f26672131829501d76
Benjamin Mueller <benjamin.mueller@zib.de>
2018-02-17 17:45:09 +0100

    add unittests to ringpacking example

---
+++ examples/Ringpacking/tests/src/ringpacking/enum.c
@@ -0,0 +1,34 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the program and library             */
+/*         SCIP --- Solving Constraint Integer Programs                      */
+/*                                                                           */
+/*    Copyright (C) 2002-2017 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SCIP is distributed under the terms of the ZIB Academic License.         */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**@file   enum.c
+ * @brief  unit test for testing circular pattern enumeration
+ * @author Benjamin Mueller
+ */
+
+/*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
+
+#include "scip/scip.h"
+#include "probdata_rpa.h"
+#include "include/scip_test.h"
+
+/* test suite */
+TestSuite(enumerate);
+
+/** run unittest */
+Test(enumerate, easy1)
+{
+
+}
