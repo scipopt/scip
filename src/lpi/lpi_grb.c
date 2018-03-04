3db1faec5675aa0cab267065ba2887482673ff2f
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2018-03-04 12:23:45 +0100

    fix scaling parameter in gurobi lpi

--- src/lpi/lpi_grb.c
+++ src/lpi/lpi_grb.c
@@ -5326,8 +5326,11 @@ SCIP_RETCODE SCIPlpiGetIntpar(
       return SCIP_PARAMETERUNKNOWN;
    case SCIP_LPPAR_SCALING:
       SCIP_CALL( getIntParam(lpi, GRB_INT_PAR_SCALEFLAG, &temp) );
-      assert(temp >= 0 && temp <= 2);
-      *ival = temp;
+      assert(temp >= -1 && temp <= 3);
+      if ( temp == 0 )
+         *ival = 0;
+      else
+         *ival = 1;
       break;
    case SCIP_LPPAR_PRESOLVING:
       SCIP_CALL( getIntParam(lpi, GRB_INT_PAR_PRESOLVE, &temp) );
@@ -5379,8 +5382,11 @@ SCIP_RETCODE SCIPlpiSetIntpar(
       assert(ival == TRUE || ival == FALSE);
       return SCIP_PARAMETERUNKNOWN;
    case SCIP_LPPAR_SCALING:
-      assert(ival >= 0 && ival <= 2);
-      SCIP_CALL( setIntParam(lpi, GRB_INT_PAR_SCALEFLAG, ival) );
+      assert(ival >= 0 && ival <= 1);
+      if ( ival == 0 )
+         SCIP_CALL( setIntParam(lpi, GRB_INT_PAR_SCALEFLAG, 0) );
+      else
+         SCIP_CALL( setIntParam(lpi, GRB_INT_PAR_SCALEFLAG, -1) );
       break;
    case SCIP_LPPAR_PRESOLVING:
       assert(ival == TRUE || ival == FALSE);
