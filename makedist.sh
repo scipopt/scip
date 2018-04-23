31ca39bbf4e871da4a68ed43e9727dbcc83b3682
Fabian Wegscheider <wegscheider@zib.de>
2018-04-23 18:05:59 +0200

    adjust scip/makedist.sh

--- makedist.sh
+++ makedist.sh
@@ -109,6 +109,11 @@ $NAME/applications/PolySCIP/README $NAME/applications/PolySCIP/scipmip.set \
 $NAME/applications/PolySCIP/src/CMakeLists.txt \
 $NAME/applications/PolySCIP/CMakeLists.txt     \
 $NAME/applications/PolySCIP/cmake/PolySCIPConfig.h.in \
+$NAME/applications/Ringpacking/* $NAME/applications/Ringpacking/doc/* $NAME/applications/Ringpacking/doc/pics/* $NAME/applications/Ringpacking/data/* \
+$NAME/applications/Ringpacking/check/CMakeLists.txt \
+$NAME/applications/Ringpacking/check/testset/short.test $NAME/applications/Ringpacking/check/testset/short.solu \
+$NAME/applications/Ringpacking/src/*.c $NAME/applications/Ringpacking/src/*.h \
+$NAME/applications/Ringpacking/tests/src/ringpacking/*.c \
 $NAME/applications/STP/doc/* \
 $NAME/applications/STP/src/depend.* \
 $NAME/applications/STP/src/*.c $NAME/applications/STP/src/*.h \
