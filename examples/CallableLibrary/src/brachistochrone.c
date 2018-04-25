d2af7ea7d6a8a47ef63a55f2ed2ec58cae505e6a
Stefan Vigerske <svigerske@gams.com>
2018-04-25 14:57:21 +0200

    minor fixes and changes
    - changes in whitespace for readability or coding style (sometimes both)
    - always declare variables at begin of block (for older C)
    - change loop index to unsigned int for warning-free comparisons with
      unsigned int n
    - use SCIPsnprintf instead of sprintf (not that it is necessary here, but
      just for convention)
    - add braces around SCIP_CALL (not really necessary here, but also for
      convention regarding use of macros)

--- examples/CallableLibrary/src/brachistochrone.c
+++ examples/CallableLibrary/src/brachistochrone.c
@@ -30,7 +30,7 @@
  * The optimisation problem is \f$ \min\limits_{x_0,\dots x_N, y_0,\dots y_N } \sum_{i=0}^{N-1} t_i \f$, such that:
  * \f$t_i = \frac{\sqrt{(y_{i+1} - y_i)^2 + (x_{i+1} - x_i)^2}}{\sqrt{1-y_{i+1}} + \sqrt{1 - y_i}}\f$
  * 
- * A more detailed description of the model can be found in the brachistochrone folder in:
+ * A more detailed description of the model can be found in the brachistochrone directory in:
  *
  * http://scip.zib.de/workshop2018/pyscipopt-exercises.tgz
  * 
@@ -47,25 +47,25 @@
 /** default number of points for the discretization of trajectory */
 #define DEFAULT_NPOINTS 4
 
-/* default starting and ending points */
-#define Y_START 1.0
-#define Y_END 0.0
-#define X_START 0.0
-#define X_END 10.0
+/* default start and end points */
+#define Y_START  1.0
+#define Y_END    0.0
+#define X_START  0.0
+#define X_END   10.0
 
 /** sets up problem */
 static
 SCIP_RETCODE setupProblem(
    SCIP*                 scip,               /**< SCIP data structure */
-   unsigned int n,                           /**< number of points for discretization */
-   SCIP_Real* coord                          /**< array containing [y(0), y(N), x(0), x(N)] */
+   unsigned int          n,                  /**< number of points for discretization */
+   SCIP_Real*            coord               /**< array containing [y(0), y(N), x(0), x(N)] */
    )
 {
    /* variables: 
-   * t[i] i=0..N-1, such that: value function=sum t[i] 
-   * y[i] i=0..N, projection of trajectory on y-axis
-   * x[i] i=0..N, projection of trajectory on x-axis 
-   */
+    * t[i] i=0..N-1, such that: value function=sum t[i]
+    * y[i] i=0..N, projection of trajectory on y-axis
+    * x[i] i=0..N, projection of trajectory on x-axis
+    */
    SCIP_VAR** t;
    SCIP_VAR** y;
    SCIP_VAR** x;
@@ -79,6 +79,8 @@ SCIP_RETCODE setupProblem(
    SCIP_Real xlb;
    SCIP_Real xub;
 
+   unsigned int i;
+
    /* create empty problem */
    SCIP_CALL( SCIPcreateProbBasic(scip, "brachistochrone") );
 
@@ -87,18 +89,19 @@ SCIP_RETCODE setupProblem(
    SCIP_CALL( SCIPallocBufferArray(scip, &y, (size_t) n + 1) );
    SCIP_CALL( SCIPallocBufferArray(scip, &x, (size_t) n + 1) );
 
-   /* create and add variables to the problem and set the initial and final point constraints through upper and lower 
-   *  bounds 
-   */
-   for( int i = 0; i< n+1; ++i ){
+   /* create and add variables to the problem and set the initial and final point constraints through upper and lower
+    * bounds
+    */
+   for( i = 0; i < n+1; ++i )
+   {
       /* setting up the names of the variables */
       namet[0] = '\0';
       namey[0] = '\0';
       namex[0] = '\0';
       if( i < n )
-         sprintf(namet, "t(%d)", i);
-      sprintf(namey, "y(%d)", i);
-      sprintf(namex, "x(%d)", i);
+         SCIPsnprintf(namet, SCIP_MAXSTRLEN, "t(%d)", i);
+      SCIPsnprintf(namey, SCIP_MAXSTRLEN, "y(%d)", i);
+      SCIPsnprintf(namex, SCIP_MAXSTRLEN, "x(%d)", i);
 
       /* fixing y(0), y(N), x(0), x(N) through lower and upper bounds */
       if( i == 0 )
@@ -125,32 +128,36 @@ SCIP_RETCODE setupProblem(
       }
 
       if( i < n )
+      {
          SCIP_CALL( SCIPcreateVarBasic(scip, &t[i], namet, 0.0, SCIPinfinity(scip), 1.0, SCIP_VARTYPE_CONTINUOUS) );
+      }
       SCIP_CALL( SCIPcreateVarBasic(scip, &y[i], namey, ylb, yub, 0.0, SCIP_VARTYPE_CONTINUOUS) );
       SCIP_CALL( SCIPcreateVarBasic(scip, &x[i], namex, xlb, xub, 0.0, SCIP_VARTYPE_CONTINUOUS) );
 
       if( i < n )
+      {
          SCIP_CALL( SCIPaddVar(scip, t[i]) );
+      }
       SCIP_CALL( SCIPaddVar(scip, y[i]) );
       SCIP_CALL( SCIPaddVar(scip, x[i]) );
    }
 
    /* add constraint related to the linear formulation of the value function 
-   * f = sum{ t[i] }
-   * t[i] =  sqrt( (y(i+1) - y(i))^2 +  (x(i+1) - x(i))^2 ) / ( sqrt(1 - y(i+1)) + sqrt(1 - y(i))  ) 
-   * in the loop, create the i-th constraint 
-   */
+    * f = sum{ t[i] }
+    * t[i] =  sqrt( (y(i+1) - y(i))^2 +  (x(i+1) - x(i))^2 ) / ( sqrt(1 - y(i+1)) + sqrt(1 - y(i))  )
+    * in the loop, create the i-th constraint
+    */
    {
       SCIP_CONS* conslinfor;
 
       /* child expressions: 
-      * yplusexpr: expression for y[i+1]
-      * yplusexprsec: second expression for y[i+1] (it appears twice in the constraint)
-      * yexpr: expression for y[i]
-      * yexprsec: second expression for y[i]
-      * xplusexpr: expression for x[i+1]
-      * xexpr: expression for x[i]
-      */
+       * yplusexpr: expression for y[i+1]
+       * yplusexprsec: second expression for y[i+1] (it appears twice in the constraint)
+       * yexpr: expression for y[i]
+       * yexprsec: second expression for y[i]
+       * xplusexpr: expression for x[i+1]
+       * xexpr: expression for x[i]
+       */
       SCIP_EXPR* yplusexpr;
       SCIP_EXPR* yplusexprsec;
       SCIP_EXPR* yexpr;
@@ -179,15 +186,15 @@ SCIP_RETCODE setupProblem(
       SCIP_Real coef2 = -1.0;
       SCIP_Real coef3 = 1.0;
 
-      /* At each iteration create an expression for the non-linear part of the i-th constraint and add it the problem */
-      for( int i = 0; i < n; ++i )
+      /* at each iteration create an expression for the non-linear part of the i-th constraint and add it the problem */
+      for( i = 0; i < n; ++i )
       {
          /* vars to be added to the exprtree in this step of the loop */
-         SCIP_VAR* varstoadd[6] = {y[i+1], y[i+1], y[i], y[i], x[i+1], x[i]};
+         SCIP_VAR* varstoadd[6] = { y[i+1], y[i+1], y[i], y[i], x[i+1], x[i] };
 
          /* create expressions meant to be child expressions in the tree. give different indexes to the expressions to 
-         *  assign the correct variables to them later 
-         */
+          * assign the correct variables to them later
+          */
          SCIP_CALL( SCIPexprCreate(SCIPblkmem(scip), &yplusexpr, SCIP_EXPR_VARIDX, 0) );
          SCIP_CALL( SCIPexprCreate(SCIPblkmem(scip), &yplusexprsec, SCIP_EXPR_VARIDX,  1) );
          SCIP_CALL( SCIPexprCreate(SCIPblkmem(scip), &yexpr, SCIP_EXPR_VARIDX, 2) );
@@ -196,15 +203,15 @@ SCIP_RETCODE setupProblem(
          SCIP_CALL( SCIPexprCreate(SCIPblkmem(scip), &xexpr, SCIP_EXPR_VARIDX, 5) );
 
          {
-            /*set up the i-th constraint
-            * expr1: y[i+1] - y[i]
-            * expr2: x[i+1] - x[i]
-            * expr6: sqrt( (y[i+1] - y[i])^2 + (x[i+1] - x[i])^2) [1]
-            * expr7: 1 - y[i+1]
-            * expr8: 1 - y[i]
-            * expr11: sqrt(1 - y[i+1]) + sqrt(1 - y[i]) [2]
-            * expr: [1] / [2]
-            */
+            /* set up the i-th constraint
+             * expr1: y[i+1] - y[i]
+             * expr2: x[i+1] - x[i]
+             * expr6: sqrt( (y[i+1] - y[i])^2 + (x[i+1] - x[i])^2) [1]
+             * expr7: 1 - y[i+1]
+             * expr8: 1 - y[i]
+             * expr11: sqrt(1 - y[i+1]) + sqrt(1 - y[i]) [2]
+             * expr: [1] / [2]
+             */
             SCIP_EXPR* children1[2] = {yplusexpr, yexpr};
             SCIP_EXPR* children2[2] = {xplusexpr, xexpr};
             SCIP_EXPR* children3 = yplusexprsec;
@@ -237,14 +244,15 @@ SCIP_RETCODE setupProblem(
          SCIP_CALL( SCIPexprtreeFree(&exprtree) );
          SCIP_CALL( SCIPreleaseCons(scip, &conslinfor) );
       }
-
    }
    
    /* release problem variables */
-   for( int i = 0; i < n + 1; ++i )
+   for( i = 0; i < n+1; ++i )
    {
       if( i < n )
+      {
          SCIP_CALL( SCIPreleaseVar(scip, &t[i]) );
+      }
       SCIP_CALL( SCIPreleaseVar(scip, &y[i]) );
       SCIP_CALL( SCIPreleaseVar(scip, &x[i]) );
    }
@@ -259,7 +267,10 @@ SCIP_RETCODE setupProblem(
 
 /** runs the brachistochrone example*/
 static
-SCIP_RETCODE runBrachistochrone(unsigned int n, SCIP_Real* coord)
+SCIP_RETCODE runBrachistochrone(
+   unsigned int          n,                  /**< number of points for discretization */
+   SCIP_Real*            coord               /**< array containing [y(0), y(N), x(0), x(N)] */
+)
 {
    SCIP* scip;
 
@@ -273,12 +284,10 @@ SCIP_RETCODE runBrachistochrone(unsigned int n, SCIP_Real* coord)
    SCIPinfoMessage(scip, NULL, "\n");
 
    /* set gap at which SCIP will stop */
-   SCIP_Real gap = 0.05;
-   SCIP_CALL( SCIPsetRealParam(scip, "limits/gap", gap) );
+   SCIP_CALL( SCIPsetRealParam(scip, "limits/gap", 0.05) );
 
    /* Set larger constraint violation tolerence to speed up the solving process */
-   SCIP_Real tol = 1e-4;
-   SCIP_CALL( SCIPsetRealParam(scip, "numerics/feastol", tol) );
+   SCIP_CALL( SCIPsetRealParam(scip, "numerics/feastol", 1e-4) );
 
    SCIP_CALL( setupProblem(scip, n, coord) );
 
@@ -306,15 +315,15 @@ SCIP_RETCODE runBrachistochrone(unsigned int n, SCIP_Real* coord)
 
 /** main method starting SCIP */
 int main(
-   int                        argc,          /**< number of arguments from the shell */
-   char**                     argv           /**< arguments: number of points and coordinates y(0), y(N), x(0), x(N)*/
+   int                   argc,               /**< number of arguments from the shell */
+   char**                argv                /**< arguments: number of points and coordinates y(0), y(N), x(0), x(N)*/
    )
 {
    SCIP_RETCODE retcode;
 
    /* setting up default problem parameters */
    unsigned int n = DEFAULT_NPOINTS;
-   SCIP_Real coord[4] = {Y_START, Y_END, X_START, X_END};
+   SCIP_Real coord[4] = { Y_START, Y_END, X_START, X_END };
 
    /* change the parameters if given as arguments */
    if( argc >= 2 )
