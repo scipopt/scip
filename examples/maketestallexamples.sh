06d49cca7cdf76ba4303df8e0baf0da9a802c5d5
Stefan Vigerske <svigerske@gams.com>
2018-04-27 12:29:27 +0200

    cleanup maketestallexamples
    - stop on any error
    - remove redundancies (double echo, explicit error checks)
    - handle case that there is no resfile

--- examples/maketestallexamples.sh
+++ examples/maketestallexamples.sh
@@ -3,7 +3,9 @@
 # Run all tests of examples, arguments are passed to the make test command.
 # Parameter "-q" turns on the quiet mode, i.e., does not output the logging of the programs.
 #
-#
+
+# stop on error
+set -e
 
 EXAMPLES=$(for f in *;do if [[ -d $f  ]]; then echo $f;fi; done)
 
@@ -54,8 +56,24 @@ OSTYPE=`uname -s | tr '[:upper:]' '[:lower:]' | \
     -e 's/windows.*/windows/' \
     -e 's/mingw.*/mingw/'`
 
+EXAMPLELOG=${PWD}/exampletestsummary.log
+
 # prepare log file
-echo "" > exampletestsummary.log
+rm -f $EXAMPLELOG
+touch $EXAMPLELOG
+
+# if something is failing, then write Failed to log
+trap "echo Last command failed >> $EXAMPLELOG" ERR
+
+# print summary on exit and remove log
+trap "
+echo
+echo
+echo ===== Summary =====
+
+cat $EXAMPLELOG
+rm -f $EXAMPLELOG
+" EXIT
 
 # pretest
 for OPT in ${OPTS[@]}
@@ -65,15 +83,13 @@ do
       LPILIB=../lib/$LIBTYPE/liblpi$LPS.$OSTYPE.$ARCH.gnu.$OPT.$LIBEXT
       if test ! -e $LPILIB
       then
-         echo "Error: "$LPILIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"." >> ../applicationtestsummary.log
-         echo "Error: "$LPILIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"."
+         echo "Error: "$LPILIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"." | tee -a ../applicationtestsummary.log
          exit 1
       fi
       SCIPLIB=../lib/$LIBTYPE/libscip.$OSTYPE.$ARCH.gnu.$OPT.$LIBEXT
       if test ! -e $SCIPLIB
       then
-         echo "Error: "$SCIPLIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"." >> ../applicationtestsummary.log
-         echo "Error: "$SCIPLIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"."
+         echo "Error: "$SCIPLIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"." | tee -a ../applicationtestsummary.log
          exit 1
       fi
    done
@@ -84,63 +100,36 @@ for EXAMPLE in $EXAMPLES
 do
    echo
    echo
-   echo ===== $EXAMPLE =====
-   echo ===== $EXAMPLE ===== >> exampletestsummary.log
+   echo ===== $EXAMPLE ===== | tee -a $EXAMPLELOG
    echo
-   cd $EXAMPLE
+   pushd $EXAMPLE > /dev/null
    for OPT in ${OPTS[@]}
    do
       for LPS in ${LPSOLVERS[@]}
       do
-         echo make OPT=$OPT LPS=$LPS $MAKEARGS
-         if (! make OPT=$OPT LPS=$LPS $MAKEARGS )
-         then
-            echo "Making "$EXAMPLE" failed." >> ../exampletestsummary.log
-            exit 1
-         else
-            echo "Making "$EXAMPLE" successful." >> ../exampletestsummary.log
-         fi
+         echo make OPT=$OPT LPS=$LPS $MAKEARGS | tee -a $EXAMPLELOG
+         make OPT=$OPT LPS=$LPS $MAKEARGS
+
          echo
+         echo make OPT=$OPT LPS=$LPS $MAKEARGS test | tee -a $EXAMPLELOG
          if test $QUIET = 1
          then
-            echo make OPT=$OPT LPS=$LPS $MAKEARGS test
-            if ( ! make OPT=$OPT LPS=$LPS $MAKEARGS test > /dev/null )
-            then
-               echo "Testing "$EXAMPLE" failed."
-               echo "Testing "$EXAMPLE" failed." >> ../exampletestsummary.log
-               exit 1
-            fi
+            make OPT=$OPT LPS=$LPS $MAKEARGS test > /dev/null
          else
-            echo make OPT=$OPT LPS=$LPS $MAKEARGS test
-            if ( ! make OPT=$OPT LPS=$LPS $MAKEARGS test )
-            then
-               echo "Testing "$EXAMPLE" failed."
-               echo "Testing "$EXAMPLE" failed." >> ../exampletestsummary.log
-               exit 1
-            fi
+            make OPT=$OPT LPS=$LPS $MAKEARGS test
          fi
-         echo "Testing "$EXAMPLE" successful."
-         echo "Testing "$EXAMPLE" successful." >> ../exampletestsummary.log
 
          # find most recently changed result file and display it
          if test -d check/results
          then
             RESFILE=`find check/results/*.res -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" "`
-            if test -e $RESFILE
+            if [ -n "$RESFILE" ] && [ -e "$RESFILE" ]
             then
-               cat $RESFILE >> ../exampletestsummary.log
+               cat $RESFILE >> $EXAMPLELOG
             fi
          fi
-         echo
-         echo >> ../exampletestsummary.log
+         echo | tee -a $EXAMPLELOG
       done
    done
-   cd - > /dev/null
+   popd > /dev/null
 done
-
-echo
-echo
-echo ===== Summary =====
-
-cat exampletestsummary.log
-rm -f exampletestsummary.log
