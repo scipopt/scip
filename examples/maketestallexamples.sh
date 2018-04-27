43d4cfd7ae7e9620d373a7456ef674dcdfa83e0c
Stefan Vigerske <svigerske@gams.com>
2018-04-27 12:05:06 +0200

    fix indentation in maketestallexamples.sh
    - tabs to spaces
    - three space indent

--- examples/maketestallexamples.sh
+++ examples/maketestallexamples.sh
@@ -19,19 +19,19 @@ LIBTYPE="static"
 LIBEXT="a"
 for i in $@
 do
-    if test "$i" = "-q"
-    then
-	echo "Quiet mode."
-	QUIET=1
-    else
-	# test whether we want to build shared libraries (need bash "testing" w.r.t. regexp)
-	if [[ "$i" =~ SHARED[\s]*=true ]]
-	then
-	    LIBTYPE="shared"
-	    LIBEXT="so"
-	fi
-	MAKEARGS="$MAKEARGS $i"
-    fi
+   if test "$i" = "-q"
+   then
+      echo "Quiet mode."
+      QUIET=1
+   else
+      # test whether we want to build shared libraries (need bash "testing" w.r.t. regexp)
+      if [[ "$i" =~ SHARED[\s]*=true ]]
+      then
+         LIBTYPE="shared"
+         LIBEXT="so"
+      fi
+      MAKEARGS="$MAKEARGS $i"
+   fi
 done
 
 
@@ -60,82 +60,82 @@ echo "" > exampletestsummary.log
 # pretest
 for OPT in ${OPTS[@]}
 do
-    for LPS in ${LPSOLVERS[@]}
-    do
-	LPILIB=../lib/$LIBTYPE/liblpi$LPS.$OSTYPE.$ARCH.gnu.$OPT.$LIBEXT
-	if test ! -e $LPILIB
-	then
-	    echo "Error: "$LPILIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"." >> ../applicationtestsummary.log
-	    echo "Error: "$LPILIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"."
-	    exit 1
-	fi
-	SCIPLIB=../lib/$LIBTYPE/libscip.$OSTYPE.$ARCH.gnu.$OPT.$LIBEXT
-	if test ! -e $SCIPLIB
-	then
-	    echo "Error: "$SCIPLIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"." >> ../applicationtestsummary.log
-	    echo "Error: "$SCIPLIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"."
-	    exit 1
-	fi
-    done
+   for LPS in ${LPSOLVERS[@]}
+   do
+      LPILIB=../lib/$LIBTYPE/liblpi$LPS.$OSTYPE.$ARCH.gnu.$OPT.$LIBEXT
+      if test ! -e $LPILIB
+      then
+         echo "Error: "$LPILIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"." >> ../applicationtestsummary.log
+         echo "Error: "$LPILIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"."
+         exit 1
+      fi
+      SCIPLIB=../lib/$LIBTYPE/libscip.$OSTYPE.$ARCH.gnu.$OPT.$LIBEXT
+      if test ! -e $SCIPLIB
+      then
+         echo "Error: "$SCIPLIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"." >> ../applicationtestsummary.log
+         echo "Error: "$SCIPLIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"."
+         exit 1
+      fi
+   done
 done
 
 # run tests
 for EXAMPLE in $EXAMPLES
 do
-    echo
-    echo
-    echo ===== $EXAMPLE =====
-    echo ===== $EXAMPLE ===== >> exampletestsummary.log
-    echo
-    cd $EXAMPLE
-    for OPT in ${OPTS[@]}
-    do
-	for LPS in ${LPSOLVERS[@]}
-	do
-	    echo make OPT=$OPT LPS=$LPS $MAKEARGS
-	    if (! make OPT=$OPT LPS=$LPS $MAKEARGS )
-	    then
-		echo "Making "$EXAMPLE" failed." >> ../exampletestsummary.log
-		exit 1
-	    else
-		echo "Making "$EXAMPLE" successful." >> ../exampletestsummary.log
-	    fi
-	    echo
-	    if test $QUIET = 1
-	    then
-		echo make OPT=$OPT LPS=$LPS $MAKEARGS test
-		if ( ! make OPT=$OPT LPS=$LPS $MAKEARGS test > /dev/null )
-		then
-		    echo "Testing "$EXAMPLE" failed."
-		    echo "Testing "$EXAMPLE" failed." >> ../exampletestsummary.log
-		    exit 1
-		fi
-	    else
-		echo make OPT=$OPT LPS=$LPS $MAKEARGS test
-		if ( ! make OPT=$OPT LPS=$LPS $MAKEARGS test )
-		then
-		    echo "Testing "$EXAMPLE" failed."
-		    echo "Testing "$EXAMPLE" failed." >> ../exampletestsummary.log
-		    exit 1
-		fi
-	    fi
-	    echo "Testing "$EXAMPLE" successful."
-	    echo "Testing "$EXAMPLE" successful." >> ../exampletestsummary.log
+   echo
+   echo
+   echo ===== $EXAMPLE =====
+   echo ===== $EXAMPLE ===== >> exampletestsummary.log
+   echo
+   cd $EXAMPLE
+   for OPT in ${OPTS[@]}
+   do
+      for LPS in ${LPSOLVERS[@]}
+      do
+         echo make OPT=$OPT LPS=$LPS $MAKEARGS
+         if (! make OPT=$OPT LPS=$LPS $MAKEARGS )
+         then
+            echo "Making "$EXAMPLE" failed." >> ../exampletestsummary.log
+            exit 1
+         else
+            echo "Making "$EXAMPLE" successful." >> ../exampletestsummary.log
+         fi
+         echo
+         if test $QUIET = 1
+         then
+            echo make OPT=$OPT LPS=$LPS $MAKEARGS test
+            if ( ! make OPT=$OPT LPS=$LPS $MAKEARGS test > /dev/null )
+            then
+               echo "Testing "$EXAMPLE" failed."
+               echo "Testing "$EXAMPLE" failed." >> ../exampletestsummary.log
+               exit 1
+            fi
+         else
+            echo make OPT=$OPT LPS=$LPS $MAKEARGS test
+            if ( ! make OPT=$OPT LPS=$LPS $MAKEARGS test )
+            then
+               echo "Testing "$EXAMPLE" failed."
+               echo "Testing "$EXAMPLE" failed." >> ../exampletestsummary.log
+               exit 1
+            fi
+         fi
+         echo "Testing "$EXAMPLE" successful."
+         echo "Testing "$EXAMPLE" successful." >> ../exampletestsummary.log
 
-	    # find most recently changed result file and display it
-	    if test -d check/results
-	    then
-		RESFILE=`find check/results/*.res -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" "`
-		if test -e $RESFILE
-		then
-		    cat $RESFILE >> ../exampletestsummary.log
-		fi
-	    fi
-	    echo
-	    echo >> ../exampletestsummary.log
-	done
-    done
-    cd - > /dev/null
+         # find most recently changed result file and display it
+         if test -d check/results
+         then
+            RESFILE=`find check/results/*.res -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" "`
+            if test -e $RESFILE
+            then
+               cat $RESFILE >> ../exampletestsummary.log
+            fi
+         fi
+         echo
+         echo >> ../exampletestsummary.log
+      done
+   done
+   cd - > /dev/null
 done
 
 echo
