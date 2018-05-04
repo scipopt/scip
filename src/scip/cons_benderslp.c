3799688f097abb16d495755db0df4ad10b82b569
Stephen J. Maher <s.maher3@lancaster.ac.uk>
2018-05-04 08:27:33 +0100

    adds description to the constraint handlers

--- src/scip/cons_benderslp.c
+++ src/scip/cons_benderslp.c
@@ -16,6 +16,23 @@
 /**@file   cons_benderslp.c
  * @brief  constraint handler for benderslp decomposition
  * @author Stephen J. Maher
+ *
+ * Two constriant handlers are implemented for the generation of Benders' decomposition
+ * cuts. When included in a problem, the Benders' decomposition constraint handlers
+ * generate cuts during the enforcement of LP and relaxation solutions. Additionally,
+ * Benders' decomposition cuts can be generated when checking the feasibility of
+ * solutions with respect to the subproblem constraints.
+ *
+ * This constraint handler has an enforcement priority that is greater than the integer
+ * constraint handler. This means that all LP solutions will be first checked for
+ * feasibility with respect to the Benders' decomposition second stage constraints before
+ * performing an integrality check. This is part of a multi-phase approach for solving
+ * mixed integer programs by Benders' decomposition.
+ *
+ * A parameter is available to control the depth at which the non-integer LP solution are
+ * enforced by solving the Benders' decomposition subproblems. This parameter is set to 0
+ * by default, indiciating that non-integer LP solutions are enforced only at the root
+ * node.
  */
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
