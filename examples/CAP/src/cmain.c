39594928c32ebd520a305ff5e2617754d5d3711e
Stephen J. Maher <s.maher3@lancaster.ac.uk>
2018-05-11 17:16:59 +0100

    changes for merge request

--- examples/CAP/src/cmain.c
+++ examples/CAP/src/cmain.c
@@ -3,7 +3,7 @@
 /*                  This file is part of the program and library             */
 /*         SCIP --- Solving Constraint Integer Programs                      */
 /*                                                                           */
-/*    Copyright (C) 2002-2017 Konrad-Zuse-Zentrum                            */
+/*    Copyright (C) 2002-2018 Konrad-Zuse-Zentrum                            */
 /*                            fuer Informationstechnik Berlin                */
 /*                                                                           */
 /*  SCIP is distributed under the terms of the ZIB Academic License.         */
@@ -57,7 +57,7 @@ SCIP_RETCODE runShell(
    /* include default SCIP plugins */
    SCIP_CALL( SCIPincludeDefaultPlugins(scip) );
 
-   /* for column generation instances, disable restarts */
+   /* for Benders' decomposition instances restarts must be disabled */
    SCIP_CALL( SCIPsetIntParam(scip,"presolving/maxrestarts",0) );
 
    /* turn off all separation algorithms */
