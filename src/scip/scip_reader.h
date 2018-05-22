d7fc940daf4258cf86e79841b0e196d0800f301c
Gregor Hendel <hendel@zib.de>
2018-05-22 16:12:46 +0200

    extract scip_reader.h from scip.h

---
+++ src/scip/scip_reader.h
@@ -0,0 +1,242 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the program and library             */
+/*         SCIP --- Solving Constraint Integer Programs                      */
+/*                                                                           */
+/*    Copyright (C) 2002-2018 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SCIP is distributed under the terms of the ZIB Academic License.         */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SCIP; see the file COPYING. If not email to scip@zib.de.      */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**@file   scip_reader.h
+ * @ingroup PUBLICCOREAPI
+ * @brief  public methods for reader plugins
+ * @author Tobias Achterberg
+ * @author Timo Berthold
+ * @author Thorsten Koch
+ * @author Alexander Martin
+ * @author Marc Pfetsch
+ * @author Kati Wolter
+ * @author Gregor Hendel
+ * @author Robert Lion Gottwald
+ */
+
+/*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
+
+#ifndef __SCIP_SCIP_READER_H__
+#define __SCIP_SCIP_READER_H__
+
+
+#include <stdio.h>
+
+#include "scip/def.h"
+#include "blockmemshell/memory.h"
+#include "scip/type_retcode.h"
+#include "scip/type_result.h"
+#include "scip/type_clock.h"
+#include "scip/type_misc.h"
+#include "scip/type_timing.h"
+#include "scip/type_paramset.h"
+#include "scip/type_event.h"
+#include "scip/type_lp.h"
+#include "scip/type_nlp.h"
+#include "scip/type_var.h"
+#include "scip/type_prob.h"
+#include "scip/type_tree.h"
+#include "scip/type_scip.h"
+
+#include "scip/type_bandit.h"
+#include "scip/type_branch.h"
+#include "scip/type_conflict.h"
+#include "scip/type_cons.h"
+#include "scip/type_dialog.h"
+#include "scip/type_disp.h"
+#include "scip/type_heur.h"
+#include "scip/type_compr.h"
+#include "scip/type_history.h"
+#include "scip/type_nodesel.h"
+#include "scip/type_presol.h"
+#include "scip/type_pricer.h"
+#include "scip/type_reader.h"
+#include "scip/type_relax.h"
+#include "scip/type_sepa.h"
+#include "scip/type_table.h"
+#include "scip/type_prop.h"
+#include "nlpi/type_nlpi.h"
+#include "scip/type_concsolver.h"
+#include "scip/type_syncstore.h"
+#include "scip/type_benders.h"
+#include "scip/type_benderscut.h"
+
+/* In debug mode, we include the SCIP's structure in scip.c, such that no one can access
+ * this structure except the interface methods in scip.c.
+ * In optimized mode, the structure is included in scip.h, because some of the methods
+ * are implemented as defines for performance reasons (e.g. the numerical comparisons).
+ * Additionally, the internal "set.h" is included, such that the defines in set.h are
+ * available in optimized mode.
+ */
+#ifdef NDEBUG
+#include "scip/struct_scip.h"
+#include "scip/struct_stat.h"
+#include "scip/set.h"
+#include "scip/tree.h"
+#include "scip/misc.h"
+#include "scip/var.h"
+#include "scip/cons.h"
+#include "scip/solve.h"
+#include "scip/debug.h"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**@addtogroup PublicReaderMethods
+ *
+ * @{
+ */
+
+/** creates a reader and includes it in SCIP
+ *
+ *  @return \ref SCIP_OKAY is returned if everything worked. Otherwise a suitable error code is passed. See \ref
+ *          SCIP_Retcode "SCIP_RETCODE" for a complete list of error codes.
+ *
+ *  @pre This method can be called if SCIP is in one of the following stages:
+ *       - \ref SCIP_STAGE_INIT
+ *       - \ref SCIP_STAGE_PROBLEM
+ *
+ *  @note method has all reader callbacks as arguments and is thus changed every time a new callback is added
+ *        in future releases; consider using SCIPincludeReaderBasic() and setter functions
+ *        if you seek for a method which is less likely to change in future releases
+ */
+EXTERN
+SCIP_RETCODE SCIPincludeReader(
+   SCIP*                 scip,               /**< SCIP data structure */
+   const char*           name,               /**< name of reader */
+   const char*           desc,               /**< description of reader */
+   const char*           extension,          /**< file extension that reader processes */
+   SCIP_DECL_READERCOPY  ((*readercopy)),    /**< copy method of reader or NULL if you don't want to copy your plugin into sub-SCIPs */
+   SCIP_DECL_READERFREE  ((*readerfree)),    /**< destructor of reader */
+   SCIP_DECL_READERREAD  ((*readerread)),    /**< read method */
+   SCIP_DECL_READERWRITE ((*readerwrite)),   /**< write method */
+   SCIP_READERDATA*      readerdata          /**< reader data */
+   );
+
+/** creates a reader and includes it in SCIP. All non-fundamental (or optional) callbacks will be set to NULL.
+ *  Optional callbacks can be set via specific setter functions, see
+ *  SCIPsetReaderCopy(), SCIPsetReaderFree(), SCIPsetReaderRead(), SCIPsetReaderWrite().
+ *
+ *  @return \ref SCIP_OKAY is returned if everything worked. Otherwise a suitable error code is passed. See \ref
+ *          SCIP_Retcode "SCIP_RETCODE" for a complete list of error codes.
+ *
+ *  @pre This method can be called if SCIP is in one of the following stages:
+ *       - \ref SCIP_STAGE_INIT
+ *       - \ref SCIP_STAGE_PROBLEM
+ *
+ *  @note if you want to set all callbacks with a single method call, consider using SCIPincludeReader() instead
+ */
+EXTERN
+SCIP_RETCODE SCIPincludeReaderBasic(
+   SCIP*                 scip,               /**< SCIP data structure */
+   SCIP_READER**         readerptr,          /**< reference to reader pointer, or NULL */
+   const char*           name,               /**< name of reader */
+   const char*           desc,               /**< description of reader */
+   const char*           extension,          /**< file extension that reader processes */
+   SCIP_READERDATA*      readerdata          /**< reader data */
+   );
+
+/** set copy method of reader
+ *
+ *  @return \ref SCIP_OKAY is returned if everything worked. Otherwise a suitable error code is passed. See \ref
+ *          SCIP_Retcode "SCIP_RETCODE" for a complete list of error codes.
+ *
+ *  @pre This method can be called if SCIP is in one of the following stages:
+ *       - \ref SCIP_STAGE_INIT
+ *       - \ref SCIP_STAGE_PROBLEM
+ */
+EXTERN
+SCIP_RETCODE SCIPsetReaderCopy(
+   SCIP*                 scip,               /**< SCIP data structure */
+   SCIP_READER*          reader,             /**< reader */
+   SCIP_DECL_READERCOPY  ((*readercopy))     /**< copy method of reader or NULL if you don't want to copy your plugin into sub-SCIPs */
+   );
+
+/** set deinitialization method of reader
+ *
+ *  @return \ref SCIP_OKAY is returned if everything worked. Otherwise a suitable error code is passed. See \ref
+ *          SCIP_Retcode "SCIP_RETCODE" for a complete list of error codes.
+ *
+ *  @pre This method can be called if SCIP is in one of the following stages:
+ *       - \ref SCIP_STAGE_INIT
+ *       - \ref SCIP_STAGE_PROBLEM
+ */
+EXTERN
+SCIP_RETCODE SCIPsetReaderFree(
+   SCIP*                 scip,               /**< SCIP data structure */
+   SCIP_READER*          reader,             /**< reader */
+   SCIP_DECL_READERFREE  ((*readerfree))     /**< destructor of reader */
+   );
+
+/** set read method of reader
+ *
+ *  @return \ref SCIP_OKAY is returned if everything worked. Otherwise a suitable error code is passed. See \ref
+ *          SCIP_Retcode "SCIP_RETCODE" for a complete list of error codes.
+ *
+ *  @pre This method can be called if SCIP is in one of the following stages:
+ *       - \ref SCIP_STAGE_INIT
+ *       - \ref SCIP_STAGE_PROBLEM
+ */
+EXTERN
+SCIP_RETCODE SCIPsetReaderRead(
+   SCIP*                 scip,               /**< SCIP data structure */
+   SCIP_READER*          reader,             /**< reader */
+   SCIP_DECL_READERREAD  ((*readerread))     /**< read method of reader */
+   );
+
+/** set write method of reader
+ *
+ *  @return \ref SCIP_OKAY is returned if everything worked. Otherwise a suitable error code is passed. See \ref
+ *          SCIP_Retcode "SCIP_RETCODE" for a complete list of error codes.
+ *
+ *  @pre This method can be called if SCIP is in one of the following stages:
+ *       - \ref SCIP_STAGE_INIT
+ *       - \ref SCIP_STAGE_PROBLEM
+ */
+EXTERN
+SCIP_RETCODE SCIPsetReaderWrite(
+   SCIP*                 scip,               /**< SCIP data structure */
+   SCIP_READER*          reader,             /**< reader */
+   SCIP_DECL_READERWRITE ((*readerwrite))    /**< write method of reader */
+   );
+
+/** returns the reader of the given name, or NULL if not existing */
+EXTERN
+SCIP_READER* SCIPfindReader(
+   SCIP*                 scip,               /**< SCIP data structure */
+   const char*           name                /**< name of reader */
+   );
+
+/** returns the array of currently available readers */
+EXTERN
+SCIP_READER** SCIPgetReaders(
+   SCIP*                 scip                /**< SCIP data structure */
+   );
+
+/** returns the number of currently available readers */
+EXTERN
+int SCIPgetNReaders(
+   SCIP*                 scip                /**< SCIP data structure */
+   );
+
+/* @} */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
