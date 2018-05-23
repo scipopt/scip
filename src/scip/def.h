111dbdbd00d52bb5f2d08ece4357891fec2f4c2a
Robert Lion Gottwald <robert.gottwald@zib.de>
2018-05-23 16:52:32 +0200

    increase version to 6.0.0

--- src/scip/def.h
+++ src/scip/def.h
@@ -108,8 +108,8 @@ extern "C" {
 #endif
 
 
-#define SCIP_VERSION                501 /**< SCIP version number (multiplied by 100 to get integer number) */
-#define SCIP_SUBVERSION               4 /**< SCIP sub version number */
+#define SCIP_VERSION                600 /**< SCIP version number (multiplied by 100 to get integer number) */
+#define SCIP_SUBVERSION               0 /**< SCIP sub version number */
 #define SCIP_APIVERSION              33 /**< SCIP API version number */
 #define SCIP_COPYRIGHT   "Copyright (C) 2002-2018 Konrad-Zuse-Zentrum fuer Informationstechnik Berlin (ZIB)"
 
