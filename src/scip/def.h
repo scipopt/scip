e89c2394b79cda6d42d22c7d00d4e0e1f419bdfe
Matthias Miltenberger <miltenberger@zib.de>
2018-07-03 10:20:18 +0200

    increase version to 6.0.0.2

--- src/scip/def.h
+++ src/scip/def.h
@@ -109,7 +109,7 @@ extern "C" {
 
 
 #define SCIP_VERSION                600 /**< SCIP version number (multiplied by 100 to get integer number) */
-#define SCIP_SUBVERSION               1 /**< SCIP sub version number */
+#define SCIP_SUBVERSION               2 /**< SCIP sub version number */
 #define SCIP_APIVERSION              33 /**< SCIP API version number */
 #define SCIP_COPYRIGHT   "Copyright (C) 2002-2018 Konrad-Zuse-Zentrum fuer Informationstechnik Berlin (ZIB)"
 
