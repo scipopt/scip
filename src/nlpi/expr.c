scipopt/scip

commit e7b7e5044f8d3d605072849e5e4eb78d1f112382

replace a strcpy by an equivalently unsafe strncpy
- maybe gets rid of a coverity warnings
- there is already a correct check for the buffer size in place

src/nlpi/expr.c
@@ -4987,7 +4987,7 @@ SCIP_RETCODE exprparseReadVariable(
 
       /* store index of variable and variable name in varnames buffer */
       **varnames = varidx;
-      strcpy((char*)(*varnames + 1), varname);
+      strncpy((char*)(*varnames + 1), varname, strlen(varname));
 
       /* insert variable into hashtable */
       SCIP_CALL( SCIPhashtableInsert(vartable, (void*)*varnames) );

commit a2bf414a07e8b6624bab5e7cabe93af1acf37be9

copy terminating \0 of string as well
- fix bug introduced by e7b7e504
- fixes #2424

src/nlpi/expr.c
@@ -4987,7 +4987,7 @@ SCIP_RETCODE exprparseReadVariable(
 
       /* store index of variable and variable name in varnames buffer */
       **varnames = varidx;
-      strncpy((char*)(*varnames + 1), varname, strlen(varname));
+      strncpy((char*)(*varnames + 1), varname, strlen(varname)+1);
 
       /* insert variable into hashtable */
       SCIP_CALL( SCIPhashtableInsert(vartable, (void*)*varnames) );