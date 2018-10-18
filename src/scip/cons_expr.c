07e02561e4cbb0aeb23a27cc75ff582f7832c9b2
Benjamin Mueller <benjamin.mueller@zib.de>
2018-10-18 08:27:23 +0200

    implement CONSGETNVARS and CONSGETVARS

--- src/scip/cons_expr.c
+++ src/scip/cons_expr.c
@@ -6773,32 +6773,49 @@ SCIP_DECL_CONSPARSE(consParseExpr)
 
 
 /** constraint method of constraint handler which returns the variables (if possible) */
-#if 0 /* TODO */
 static
 SCIP_DECL_CONSGETVARS(consGetVarsExpr)
 {  /*lint --e{715}*/
-   SCIPerrorMessage("method of expr constraint handler not implemented yet\n");
-   SCIPABORT(); /*lint --e{527}*/
+   SCIP_CONSDATA* consdata;
+   int i;
+
+   consdata = SCIPconsGetData(cons);
+   assert(consdata != NULL);
+
+   /* check whether array is too small in order to store all variables */
+   if( varssize < consdata->nvarexprs )
+   {
+      *success = FALSE;
+      return SCIP_OKAY;
+   }
+
+   for( i = 0; i < consdata->nvarexprs; ++i )
+   {
+      vars[i] = SCIPgetConsExprExprVarVar(consdata->varexprs[i]);
+      assert(vars[i] != NULL);
+   }
+
+   *success = TRUE;
 
    return SCIP_OKAY;
 }
-#else
-#define consGetVarsExpr NULL
-#endif
+
 
 /** constraint method of constraint handler which returns the number of variables (if possible) */
-#if 0 /* TODO */
 static
 SCIP_DECL_CONSGETNVARS(consGetNVarsExpr)
 {  /*lint --e{715}*/
-   SCIPerrorMessage("method of expr constraint handler not implemented yet\n");
-   SCIPABORT(); /*lint --e{527}*/
+   SCIP_CONSDATA* consdata;
+
+   consdata = SCIPconsGetData(cons);
+   assert(consdata != NULL);
+
+   *nvars = consdata->nvarexprs;
+   *success = TRUE;
 
    return SCIP_OKAY;
 }
-#else
-#define consGetNVarsExpr NULL
-#endif
+
 
 /** constraint handler method to suggest dive bound changes during the generic diving algorithm */
 #if 0 /* TODO? */
@@ -6813,6 +6830,7 @@ SCIP_DECL_CONSGETDIVEBDCHGS(consGetDiveBdChgsExpr)
 #else
 #define consGetDiveBdChgsExpr NULL
 #endif
+
 
 /** output method of statistics table to output file stream 'file' */
 static
