52c192870591bc5a4eaf1443dfe2e8d4793aeb39
Benjamin Mueller <benjamin.mueller@zib.de>
2018-10-25 12:25:23 +0200

    increase API version

--- src/scip/def.h
+++ src/scip/def.h
@@ -117,7 +117,7 @@ extern "C" {
 
 #define SCIP_VERSION                601 /**< SCIP version number (multiplied by 100 to get integer number) */
 #define SCIP_SUBVERSION               0 /**< SCIP sub version number */
-#define SCIP_APIVERSION              33 /**< SCIP API version number */
+#define SCIP_APIVERSION              34 /**< SCIP API version number */
 #define SCIP_COPYRIGHT   "Copyright (C) 2002-2018 Konrad-Zuse-Zentrum fuer Informationstechnik Berlin (ZIB)"
 
 
