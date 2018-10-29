ac01929f4bd2511ad2bff0fb139b23ef51ff4891
Stefan Vigerske <svigerske@gams.com>
2018-10-29 18:45:02 +0100

    add an initial unittest based on tests from separation_prod

---
+++ tests/src/cons/expr/separation_vertexpolyhedral.c
@@ -0,0 +1,243 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the program and library             */
+/*         SCIP --- Solving Constraint Integer Programs                      */
+/*                                                                           */
+/*    Copyright (C) 2002-2018 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SCIP is distributed under the terms of the ZIB Academic License.         */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SCIP; see the file COPYING. If not visit scip.zib.de.         */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**@file   separation_vertexpolyhedral.c
+ * @brief  tests estimation of vertexpolyhedral functions
+ */
+
+/*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
+
+#include "scip/cons_expr.c"
+#include "separation.h"
+
+/*
+ * the following test builds the matrices used to compute facets of convex/concave envelope from size 0 to 8 (otherwise
+ * we get a overlength string warning) and checks it is correctly built by printing it to std output and comparing with
+ * the matrices in matrices.sol
+ */
+
+#include "matrices.sol"
+
+/* specify parameters of parameterized test */
+ParameterizedTestParameters(separation, multilinearLP)
+{
+   static const int sizes[] = {1,2,3,4,5,6,7};
+
+   /* type of the parameter; the parameter; number of parameters */
+   return cr_make_param_array(const int, sizes, sizeof(sizes)/sizeof(int));
+}
+
+static
+void printMatrix(int size)
+{
+   int i, j, nrows, ncols;
+   SCIP_LPI* lp;
+
+   SCIP_CALL( SCIPcreate(&scip) );
+
+   cr_redirect_stdout();
+   SCIP_CALL( buildVertexPolyhedralSeparationLP(scip, size, &lp) );
+
+   SCIP_CALL( SCIPlpiGetNRows(lp, &nrows) );
+   SCIP_CALL( SCIPlpiGetNCols(lp, &ncols) );
+
+   for( i = 0; i < nrows; ++i )
+   {
+      for( j = 0; j < ncols; ++j )
+      {
+         SCIP_Real val;
+         SCIP_CALL( SCIPlpiGetCoef(lp, i, j, &val) );
+         printf("%g", val);
+      }
+      printf("\n");
+   }
+   fflush(stdout);
+   cr_assert_stdout_eq_str(matrices[size]);
+
+   SCIP_CALL( SCIPlpiFree(&lp) );
+   SCIP_CALL( SCIPfree(&scip) );
+   cr_assert_eq(BMSgetMemoryUsed(), 0, "Memory is leaking!!");
+}
+
+static
+SCIP_DECL_VERTEXPOLYFUN(prodfunction)
+{
+   /* params is a point to the double holding the coefficient */
+   SCIP_Real ret = *(SCIP_Real*)params;
+   int i;
+
+   for( i = 0; i < npoint; ++i )
+      ret *= point[i];
+
+   return ret;
+}
+
+
+/* generates matrix of size *size and prints it; checks it is the expected matrix */
+ParameterizedTest(const int* size, separation, multilinearLP)
+{
+   printMatrix(*size);
+}
+
+/*
+ * The following test checks that separation of multilinear terms is performed correctly.
+ * It also test the re-use of the separation LP
+ */
+Test(separation, bilinear_with_LP, .init = setup, .fini = teardown,
+   .description = "test separation for a bilinear expression, but using the code for general vertex-polyhedral functions"
+   )
+{
+   SCIP_Real prodcoef;
+   SCIP_Real xstar[2];
+   SCIP_Real box[4];
+   SCIP_Real facetcoefs[2];
+   SCIP_Real facetconstant;
+   SCIP_Bool success;
+
+   /*
+    * compute a facet of the concave envelope of 1.5*x*y with x* = 0, y* = -4
+    * together with the bounds (in separation.h) this should result in an estimator of the form
+    *   -4.5x - 1.5y - 4.5, so the facet = -4.5, -1.5, -4.5.
+    */
+   prodcoef = 1.5;
+
+   box[0] = SCIPvarGetLbLocal(x);
+   box[1] = SCIPvarGetUbLocal(x);
+   box[2] = SCIPvarGetLbLocal(y);
+   box[3] = SCIPvarGetUbLocal(y);
+
+   xstar[0] = 0.0;
+   xstar[1] = -4.0;
+
+   SCIP_CALL( SCIPcomputeFacetVertexPolyhedral(scip, conshdlr, TRUE /* overestimate */, prodfunction, &prodcoef, xstar, box, 2, SCIPinfinity(scip), &success, facetcoefs, &facetconstant) );
+
+   cr_assert(success);
+   cr_expect_float_eq(facetcoefs[0], -4.5, SCIPepsilon(scip));
+   cr_expect_float_eq(facetcoefs[1], -1.5, SCIPepsilon(scip));
+   cr_expect_float_eq(facetconstant, -4.5, SCIPepsilon(scip));
+
+   /*
+    * compute a facet of the convex envelope for 1.5*x*y with x* = 0, y* = -4, re-using the separation lp
+    * together with the bounds this should result in an estimator of the form
+    *    -9x - 1.5y - 9
+    *
+    * TODO reuse a previous LP
+    */
+
+   SCIP_CALL( SCIPcomputeFacetVertexPolyhedral(scip, conshdlr, FALSE /* underestimate */, prodfunction, &prodcoef, xstar, box, 2, -SCIPinfinity(scip), &success, facetcoefs, &facetconstant) );
+
+   cr_assert(success);
+   cr_expect_float_eq(facetcoefs[0], -9.0, SCIPepsilon(scip));
+   cr_expect_float_eq(facetcoefs[1], -1.5, SCIPepsilon(scip));
+   cr_expect_float_eq(facetconstant, -9.0, SCIPepsilon(scip));
+}
+
+
+/* This next test separates a 4-linear function and then a 3-linear function:
+ * The exact facets were computed using CDD and the following (ugly) Julia code:
+ *
+ * using Iterators, Polyhedra, CDDLib
+ * #Compute (vertex, f(vertex)) where the domain is [-0.2, 0.7], [-10, 8], [1, 1.3], [0.09, 2.1] and f is -0.7 x*y*w*z
+ * # choose only one of the following
+ * domz = [1//1,1//1]; zstar = 1; tstar =-3.4;
+ * domz = [9//100,21//10]; zstar = 0.18; tstar = 12.12;
+ *
+ * # get vertices in matrix notation for CDD
+ * vertices = map(t->vcat(t[2:end]..., prod(t)), collect(product(-7//10,[-2//10, 7//10], [-10, 8], [1, 13//10], domz)));
+ * vert = vcat([vertices[i]' for i in 1:length(vertices)]...);
+ *
+ * # compute polyhedron and get its H-representation
+ * vrepre = SimpleVRepresentation(vert);
+ * poly = polyhedron(vrepre, CDDLibrary(:exact));
+ * hrepre = SimpleHRepresentation(gethrep(poly));
+ *
+ * #find the most violated facet
+ * m = size(hrepre.A)[1] # number of facets
+ * values = hrepre.A * [0.2, -4.0, 1.1, zstar, tstar]  -  hrepre.b
+ * separates = values  .> 0 # this are the inequalities that separate
+ * maxel = 0.0; argmax = -1;
+ * for i in 1:m
+ *     if separates[i]
+ *         value = dot(hrepre.A[i,:], [0.2, -4.0, 1.1, zstar, tstar])  -  hrepre.b[i]
+ *         value /= abs(dot(hrepre.A[i,:], [0,0,0,0, 1])) # we have to normalize
+ *         @show dot(hrepre.A[i,:], [0,0,0,0, 1])
+ *         if maxel <= value
+ *             maxel = value
+ *             argmax = i
+ *         end
+ *     end
+ * end
+ * println(maxel, " index: ", argmax)
+ * violation = (hrepre.A[argmax,:]' * [0.2, -4.0, 1.1, zstar, tstar] - hrepre.b[argmax])/ hrepre.A[argmax,end]
+ * */
+
+/* this test needs to create its own data, it doesn't use the fixtures! */
+static
+Test(separation, multilinearseparation)
+{
+   /* char const* names[] = {"x", "y", "w", "z"}; */
+   SCIP_Real prodcoef = -0.7;
+   SCIP_Real box[] = {-0.2, 0.7, -10.0, 8.0, 1.0, 1.3, 0.09, 2.1};
+   SCIP_Real solval[] = { 0.2, -4.0, 1.1, 0.18};
+   SCIP_Bool success;
+   SCIP_Real facetcoefs[4];
+   SCIP_Real facetconstant;
+   int i;
+
+   SCIP_CALL( SCIPcreate(&scip) );
+
+   /* include cons_expr: this adds the operator handlers */
+   SCIP_CALL( SCIPincludeConshdlrExpr(scip) );
+
+   /* get expr conshdlr */
+   conshdlr = SCIPfindConshdlr(scip, "expr");
+   assert(conshdlr != NULL);
+
+   /* compute an overestimator for -0.7*x*y*w*z with x* = 0.2, y* = -4, w* = 1.1, z* = 0.18
+    * together with the bounds x,y,w,z \in [-0.2, 0.7], [-10, 8], [1, 1.3], [0.09, 2.1]
+    * -> 63/5000 * (50x + y + 35w + 4550/9 z - 141/2)
+    */
+   SCIP_CALL( SCIPcomputeFacetVertexPolyhedral(scip, conshdlr, TRUE /* overestimate */, prodfunction, &prodcoef, solval, box, 4, SCIPinfinity(scip), &success, facetcoefs, &facetconstant) );
+
+   cr_assert(success);
+
+   SCIP_Real exact_facet1[] = {63.0/100, 63.0/5000, 441.0/1000, 637.0/100, -8883.0/10000};
+   for( i = 0; i < 4; ++i ) /* index 4 is the constant */
+   {
+      cr_expect_float_eq(facetcoefs[i], exact_facet1[i], SCIPfeastol(scip), "coef %d: received %g instead of %g\n", i, facetcoefs[i], exact_facet1[i]);
+   }
+   cr_expect_float_eq(facetconstant, exact_facet1[4], SCIPfeastol(scip), "constant: received %g instead of %g\n", facetconstant, exact_facet1[i]);
+
+   /* compute an underestimator for the same function as before, but now z is fixed to 1 and we underestimate
+    *   -0.7*x*y*w with x* = 0.2, y* = -4, w* = 1.1
+    * together with the bounds x,y,w \in [-0.2, 0.7], [-10, 8], [1, 1.3]
+    * -> -49/100 * (-100/7 x + y + 8w + 2)
+    */
+   SCIP_CALL( SCIPcomputeFacetVertexPolyhedral(scip, conshdlr, FALSE /* underestimate */, prodfunction, &prodcoef, solval, box, 3, -SCIPinfinity(scip), &success, facetcoefs, &facetconstant) );
+
+   cr_assert(success);
+
+   SCIP_Real exact_facet2[] = {7.0, -49.0/100, -98.0/25, -49.0/50};
+   for( i = 0; i < 3; ++i ) /* index 3 is the constant */
+   {
+      cr_expect_float_eq(facetcoefs[i], exact_facet2[i], SCIPfeastol(scip), "coef %d: received %g instead of %g\n", i, facetcoefs[i], exact_facet2[i]);
+   }
+   cr_expect_float_eq(facetconstant, exact_facet2[3], SCIPfeastol(scip), "constant: received %g instead of %g\n", facetconstant, exact_facet2[i]);
+
+   /* free SCIP */
+   SCIP_CALL( SCIPfree(&scip) );
+
+   cr_assert_eq(BMSgetMemoryUsed(), 0, "Memory is leaking!!");
+}
