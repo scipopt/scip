4177d804dae8aded763dfbdcce04249214800eb4
Jakob Witzig <witzig@zib.de>
2018-11-19 19:52:30 +0100

    modified exec script

--- exec_runs_local_rl.sh
+++ exec_runs_local_rl.sh
@@ -20,14 +20,18 @@ TIME=3600
 
 
 #### MMM-IP
-# SETTINGS=rapidlearning-freq-5-exp4-degeneracy,rapidlearning-freq-5-exp4-dualbound,rapidlearning-freq-5-exp4-leaves,rapidlearning-freq-5-exp4-localobj,rapidlearning-freq-5-exp4-sblps,rapidlearning-freq-5-exp4-nsols
-# make VERSION=$VERSION2 GLBSEEDSHIFT=1 SEEDS=3 CONTINUE=true TIME=$TIME MEM=$MEM QUEUE=$QUEUE EXCLUSIVE=true SETTINGS=$SETTINGS TEST=MMM-IP OUTPUTDIR=$OUTPUTDIR testcluster
-#
-# SETTINGS=rapidlearning-freq-5-exp4-degeneracy-leaves,rapidlearning-freq-5-exp4-degeneracy-leaves-localobj,rapidlearning-freq-5-exp4-leaves-obj
-# make VERSION=$VERSION2 GLBSEEDSHIFT=1 SEEDS=3 CONTINUE=true TIME=$TIME MEM=$MEM QUEUE=$QUEUE EXCLUSIVE=true SETTINGS=$SETTINGS TEST=MMM-IP OUTPUTDIR=$OUTPUTDIR testcluster
-#
-# SETTINGS=rapidlearning-glb-degeneracy,rapidlearning-glb-localobj,rapidlearning-glb-nsols
-# make VERSION=$VERSION2 GLBSEEDSHIFT=1 SEEDS=3 CONTINUE=true TIME=$TIME MEM=$MEM QUEUE=$QUEUE EXCLUSIVE=true SETTINGS=$SETTINGS TEST=MMM-IP OUTPUTDIR=$OUTPUTDIR testcluster
+
+SETTINGS=rapidlearning-freq-5-exp4-degeneracy-applybdchgsF,rapidlearning-freq-5-exp4-degeneracy-applyinfervalsF,rapidlearning-freq-5-exp4-degeneracy-applyconflictsF,rapidlearning-freq-5-exp4-degeneracy-applyprimalsolF
+make VERSION=$VERSION2 CONTINUE=true TIME=$TIME MEM=$MEM QUEUE=$QUEUE EXCLUSIVE=true SETTINGS=$SETTINGS TEST=MMM-IP OUTPUTDIR=$OUTPUTDIR testcluster
+
+SETTINGS=rapidlearning-freq-5-exp4-degeneracy,rapidlearning-freq-5-exp4-dualbound,rapidlearning-freq-5-exp4-leaves,rapidlearning-freq-5-exp4-localobj,rapidlearning-freq-5-exp4-sblps,rapidlearning-freq-5-exp4-nsols
+make VERSION=$VERSION2 GLBSEEDSHIFT=1 SEEDS=3 CONTINUE=true TIME=$TIME MEM=$MEM QUEUE=$QUEUE EXCLUSIVE=true SETTINGS=$SETTINGS TEST=MMM-IP OUTPUTDIR=$OUTPUTDIR testcluster
+
+SETTINGS=rapidlearning-freq-5-exp4-degeneracy-leaves,rapidlearning-freq-5-exp4-degeneracy-leaves-localobj,rapidlearning-freq-5-exp4-leaves-obj
+make VERSION=$VERSION2 GLBSEEDSHIFT=1 SEEDS=3 CONTINUE=true TIME=$TIME MEM=$MEM QUEUE=$QUEUE EXCLUSIVE=true SETTINGS=$SETTINGS TEST=MMM-IP OUTPUTDIR=$OUTPUTDIR testcluster
+
+SETTINGS=rapidlearning-glb-degeneracy,rapidlearning-glb-localobj,rapidlearning-glb-nsols
+make VERSION=$VERSION2 GLBSEEDSHIFT=1 SEEDS=3 CONTINUE=true TIME=$TIME MEM=$MEM QUEUE=$QUEUE EXCLUSIVE=true SETTINGS=$SETTINGS TEST=MMM-IP OUTPUTDIR=$OUTPUTDIR testcluster
 
 SETTINGS=rapidlearning-freq-5-exp4-degeneracy-leaves-localobj-applybdchgsF,rapidlearning-freq-5-exp4-degeneracy-leaves-localobj-applyinfervalsF,rapidlearning-freq-5-exp4-degeneracy-leaves-localobj-applyconflictsF,rapidlearning-freq-5-exp4-degeneracy-leaves-localobj-applyprimalsolF
 make VERSION=$VERSION2 CONTINUE=true TIME=$TIME MEM=$MEM QUEUE=$QUEUE EXCLUSIVE=true SETTINGS=$SETTINGS TEST=MMM-IP OUTPUTDIR=$OUTPUTDIR testcluster
