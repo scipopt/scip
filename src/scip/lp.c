7d3c0f296c80b32a382cd91a3fb270b882347330
Benjamin Mueller <benjamin.mueller@zib.de>
2018-12-05 17:54:09 +0100

    fix lint warnings

--- src/scip/lp.c
+++ src/scip/lp.c
@@ -5218,7 +5218,7 @@ SCIP_RETCODE SCIProwFree(
    assert((*row)->eventfilter != NULL);
 
    /* release constraint that has been used for creating the row */
-   if( (*row)->origintype == SCIP_ROWORIGINTYPE_CONS )
+   if( (SCIP_ROWORIGINTYPE) (*row)->origintype == SCIP_ROWORIGINTYPE_CONS )
    {
       SCIP_CONS* cons = (SCIP_CONS*) (*row)->origin;
       assert(cons != NULL);
@@ -17183,7 +17183,7 @@ SCIP_CONS* SCIProwGetCons(
 {
    assert(row != NULL);
 
-   if( row->origintype == SCIP_ROWORIGINTYPE_CONS )
+   if( (SCIP_ROWORIGINTYPE) row->origintype == SCIP_ROWORIGINTYPE_CONS )
       return (SCIP_CONS*)row->origin;
    return NULL;
 }
