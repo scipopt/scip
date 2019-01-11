62440c0ccd5fbefe7d306f3696e6d42ae8c11e2f
Leon Eifler <eifler@zib.de>
2019-01-11 12:11:54 +0100

    add exact linear handlr to defplugins

--- src/scip/scipdefplugins.h
+++ src/scip/scipdefplugins.h
@@ -59,6 +59,7 @@
 #include "scip/cons_integral.h"
 #include "scip/cons_knapsack.h"
 #include "scip/cons_linear.h"
+#include "scip/cons_exactlp.h"
 #include "scip/cons_linking.h"
 #include "scip/cons_logicor.h"
 #include "scip/cons_nonlinear.h"
