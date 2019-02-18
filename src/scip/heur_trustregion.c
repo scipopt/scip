f87c8e01cd2ee3c332a98f3cbd62385cb69b8014
Gregor Hendel <hendel@zib.de>
2019-02-18 14:49:00 +0100

    use LNS display character

--- src/scip/heur_trustregion.c
+++ src/scip/heur_trustregion.c
@@ -78,7 +78,7 @@
 
 #define HEUR_NAME             "trustregion"
 #define HEUR_DESC             "LNS heuristic for Benders' decomposition based on trust region methods"
-#define HEUR_DISPCHAR         '?'
+#define HEUR_DISPCHAR         SCIP_HEURDISPCHAR_LNS
 #define HEUR_PRIORITY         -1102000
 #define HEUR_FREQ             -1
 #define HEUR_FREQOFS          0
