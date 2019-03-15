94267d70a9a7b39b79e4a0ae218ae49b4dff3680
Matthias Miltenberger <miltenberger@zib.de>
2019-03-15 08:35:39 +0100

    add basic asserts in ignoreInstability

--- src/scip/lp.c
+++ src/scip/lp.c
@@ -11380,6 +11380,9 @@ SCIP_RETCODE ignoreInstability(
    SCIP_Bool*            success             /**< was instability successfully ignored */
    )
 {
+   assert(lp != NULL);
+   assert(set != NULL);
+
    SCIP_CALL( SCIPlpiIgnoreInstability(lp->lpi, success) );
 
    if( *success )
