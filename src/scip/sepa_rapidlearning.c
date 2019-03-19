f8bcb29bdbb16d5eb1ff32bdf306f470d865211f
Jakob Witzig <witzig@zib.de>
2019-03-19 16:24:17 +0100

    enable local rapidlearning

--- src/scip/sepa_rapidlearning.c
+++ src/scip/sepa_rapidlearning.c
@@ -34,7 +34,7 @@
 #define SEPA_NAME              "rapidlearning"
 #define SEPA_DESC               "rapid learning heuristic and separator"
 #define SEPA_PRIORITY          -1200000
-#define SEPA_FREQ                    -1
+#define SEPA_FREQ                     5
 #define SEPA_MAXBOUNDDIST           1.0
 #define SEPA_USESSUBSCIP           TRUE /**< does the separator use a secondary SCIP instance? */
 #define SEPA_DELAY                FALSE /**< should separation method be delayed, if other separators found cuts? */
