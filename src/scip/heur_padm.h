f20d3eef663fef43ca411972ee548da7b3d4be2d
weninger <dieter.weninger@fau.de>
2019-04-11 15:31:09 +0200

    add comment

--- src/scip/heur_padm.h
+++ src/scip/heur_padm.h
@@ -19,6 +19,7 @@
  *         "A Decomposition Heuristic for Mixed-Integer Supply Chain Problems"
  *         by Martin Schmidt, Lars Schewe, and Dieter Weninger
  * @author Dieter Weninger
+ * @author Katrin Halbig
  */
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
