4eefc4c9ffd21fa7f78fb5f8eb98262dd33c9b50
Ksenia Bestuzheva <bestuzheva@zib.de>
2019-05-15 10:09:05 +0200

    Changed default maxunknownterms back to 0

--- src/scip/sepa_rlt.c
+++ src/scip/sepa_rlt.c
@@ -45,7 +45,7 @@
 #define SEPA_USESSUBSCIP          FALSE /**< does the separator use a secondary SCIP instance? */
 #define SEPA_DELAY                FALSE /**< should separation method be delayed, if other separators found cuts? */
 
-#define DEFAULT_MAXUNKNOWNTERMS       5 /**< default value for parameter maxunknownterms */
+#define DEFAULT_MAXUNKNOWNTERMS       0 /**< default value for parameter maxunknownterms */
 #define DEFAULT_MAXUSEDVARS         100 /**< default value for parameter maxusedvars */
 #define DEFAULT_MAXNONZEROPROP      0.0 /**< default value for parameter maxnonzeroprop */
 #define DEFAULT_MAXNCUTS             -1 /**< default value for parameter maxncuts */
