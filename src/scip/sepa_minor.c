ae9e8c9c21341a095cbddbe6f814abd053a2c5a4
Benjamin Mueller <benjamin.mueller@zib.de>
2019-05-24 20:19:06 +0200

    collect bilinear and quadratic terms

--- src/scip/sepa_minor.c
+++ src/scip/sepa_minor.c
@@ -25,7 +25,11 @@
 #include <string.h>
 
 #include "scip/sepa_minor.h"
-
+#include "scip/cons_expr.h"
+#include "scip/cons_expr_var.h"
+#include "scip/cons_expr_pow.h"
+#include "scip/cons_expr_product.h"
+#include "scip/cons_expr_iterator.h"
 
 #define SEPA_NAME              "minor"
 #define SEPA_DESC              "separator template"
@@ -66,6 +70,8 @@ SCIP_RETCODE sepadataClear(
 
    assert(sepadata != NULL);
 
+   SCIPdebugMsg(scip, "clear separation data\n");
+
    /* release captured variables */
    for( i = 0; i < 3 * sepadata->nminors; ++i )
    {
@@ -91,6 +97,14 @@ SCIP_RETCODE detectMinors(
    )
 {
    SCIP_SEPADATA* sepadata;
+   SCIP_CONSHDLR* conshdlr;
+   SCIP_CONSEXPR_ITERATOR* it;
+   SCIP_HASHMAP* exprmap;
+   SCIP_VAR** xs;
+   SCIP_VAR** ys;
+   SCIP_VAR** auxvars;
+   int nstored = 0;
+   int c;
 
    assert(sepa != NULL);
 
@@ -104,9 +118,105 @@ SCIP_RETCODE detectMinors(
    assert(sepadata->minors == NULL);
    assert(sepadata->nminors == 0);
 
-   /* TODO check whether auxiliary variables have been already generated */
-
+   /* we assume that the auxiliary variables in the expression constraint handler have been already generated */
    sepadata->detectedminors = TRUE;
+
+   /* check whether there are expression constraints available */
+   conshdlr = SCIPfindConshdlr(scip, "expr");
+   if( conshdlr == NULL || SCIPconshdlrGetNConss(conshdlr) == 0 )
+      return SCIP_OKAY;
+
+   SCIPdebugMsg(scip, "call detectMinors()\n");
+
+   /* allocate memory */
+   SCIP_CALL( SCIPexpriteratorCreate(&it, conshdlr, SCIPblkmem(scip)) );
+   SCIP_CALL( SCIPhashmapCreate(&exprmap, SCIPblkmem(scip), SCIPgetNVars(scip)) );
+   SCIP_CALL( SCIPallocBufferArray(scip, &xs, SCIPgetNVars(scip)) );
+   SCIP_CALL( SCIPallocBufferArray(scip, &ys, SCIPgetNVars(scip)) );
+   SCIP_CALL( SCIPallocBufferArray(scip, &auxvars, SCIPgetNVars(scip)) );
+
+   for( c = 0; c < SCIPconshdlrGetNConss(conshdlr); ++c )
+   {
+      SCIP_CONS* cons;
+      SCIP_CONSEXPR_EXPR* expr;
+
+      cons = SCIPconshdlrGetConss(conshdlr)[c];
+      assert(cons != NULL);
+      assert(SCIPgetExprConsExpr(scip, cons));
+
+      SCIP_CALL( SCIPexpriteratorInit(it, SCIPgetExprConsExpr(scip, cons), SCIP_CONSEXPRITERATOR_DFS, FALSE) );
+      SCIPexpriteratorSetStagesDFS(it, SCIP_CONSEXPRITERATOR_ENTEREXPR);
+
+      for( expr = SCIPexpriteratorGetCurrent(it); !SCIPexpriteratorIsEnd(it); expr = SCIPexpriteratorGetNext(it) ) /*lint !e441*/
+      {
+         SCIP_CONSEXPR_EXPRHDLR* exprhdlr;
+         SCIP_CONSEXPR_EXPR** children;
+         SCIP_VAR* auxvar;
+         int nchildren;
+
+         SCIPdebugMsg(scip, "visit expression %p in constraint %s\n", (void*)expr, SCIPconsGetName(cons));
+
+         /* check whether the expression as an auxiliary variable */
+         auxvar = SCIPgetConsExprExprAuxVar(expr);
+         if( auxvar == NULL )
+            continue;
+
+         /* check whether the expression has been considered in another constraint */
+         if( SCIPhashmapExists(exprmap, (void*)exprmap) )
+            continue;
+
+         exprhdlr = SCIPgetConsExprExprHdlr(expr);
+         assert(exprhdlr != NULL);
+         children = SCIPgetConsExprExprChildren(expr);
+         nchildren = SCIPgetConsExprExprNChildren(expr);
+
+         /* check for expr = (x)^2 */
+         if( nchildren == 1 && exprhdlr == SCIPgetConsExprExprHdlrPower(conshdlr)
+            && SCIPgetConsExprExprPowExponent(expr) == 2.0
+            && SCIPisConsExprExprVar(children[0]) )
+         {
+            assert(children[0] != NULL);
+
+            xs[nstored] = SCIPgetConsExprExprVarVar(children[0]);
+            ys[nstored] = xs[nstored];
+            auxvars[nstored] = auxvar;
+            SCIPdebugMsg(scip, "found %s = (%s)^2\n", SCIPvarGetName(auxvar), SCIPvarGetName(xs[nstored]));
+            ++nstored;
+
+            /* add expression to the map to not reconsider it */
+            SCIP_CALL( SCIPhashmapInsert(exprmap, (void*)expr, NULL) );
+         }
+         /* check for expr = x * y */
+         else if( nchildren == 2 && exprhdlr == SCIPgetConsExprExprHdlrProduct(conshdlr)
+            && SCIPisConsExprExprVar(children[0]) && SCIPisConsExprExprVar(children[1]) )
+         {
+            assert(children[0] != NULL);
+            assert(children[1] != NULL);
+
+            xs[nstored] = SCIPgetConsExprExprVarVar(children[0]);
+            ys[nstored] = SCIPgetConsExprExprVarVar(children[1]);
+            auxvars[nstored] = auxvar;
+            SCIPdebugMsg(scip, "found %s = %s * %s\n", SCIPvarGetName(auxvar), SCIPvarGetName(xs[nstored]), SCIPvarGetName(ys[nstored]));
+            ++nstored;
+
+            /* add expression to the map to not reconsider it */
+            SCIP_CALL( SCIPhashmapInsert(exprmap, (void*)expr, NULL) );
+         }
+      }
+   }
+   SCIPdebugMsg(scip, "stored %d terms in total\n", nstored);
+
+   /* it cannot happen that we have stored more terms than SCIPgetNVars() because every stored term has an auxiliary variable */
+   assert(nstored < SCIPgetNVars(scip));
+
+   /* TODO use arrays xs, ys, and auxvars to find all important 2x2 minors */
+
+   /* free memory */
+   SCIPfreeBufferArray(scip, &auxvars);
+   SCIPfreeBufferArray(scip, &ys);
+   SCIPfreeBufferArray(scip, &xs);
+   SCIPhashmapFree(&exprmap);
+   SCIPexpriteratorFree(&it);
 
    return SCIP_OKAY;
 }
@@ -136,6 +246,8 @@ SCIP_RETCODE separatePoint(
       return SCIP_OKAY;
 
    *result = SCIP_DIDNOTFIND;
+
+   /* TODO generate a/several cut/cuts */
 
    return SCIP_OKAY;
 }
