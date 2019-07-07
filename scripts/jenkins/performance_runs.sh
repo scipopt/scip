376704f088099aa2eaf3fdd65f384608ce7a5881
Franziska Schloesser <schloesser@zib.de>
2019-07-07 20:10:48 +0200

    update performance script

--- scripts/jenkins/performance_runs.sh
+++ scripts/jenkins/performance_runs.sh
@@ -100,7 +100,7 @@ if [ "${DAY_OF_WEEK}" == "6" ]; then
   cd soplex
   git checkout ${SOPLEXBRANCHNAME}
   git pull
-  git checkout -B performance-${GITBRANCH}
+  git checkout -f performance-${GITBRANCH}
   git merge ${SOPLEXBRANCHNAME} --ff-only
   git push
   cd ..
