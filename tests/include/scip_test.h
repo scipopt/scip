2090c517182bbc7860882971ea6bcf29a9da2b2e
Franziska Schloesser <schloesser@zib.de>
2019-07-22 15:31:04 +0200

    update criterion warnings

--- tests/include/scip_test.h
+++ tests/include/scip_test.h
@@ -14,6 +14,8 @@ SCIP_RETCODE TESTscipSetStage(SCIP* scip, SCIP_STAGE stage, SCIP_Bool enableNLP)
 
 #ifdef __GNUC__
 #pragma GCC diagnostic ignored "-Wredundant-decls"
+#pragma GCC diagnostic ignored "-Wstrict-prototypes"
+#pragma GCC diagnostic ignored "-Wdeclaration-after-statement"
 #endif
 
 #include <criterion/criterion.h>
@@ -23,6 +25,7 @@ SCIP_RETCODE TESTscipSetStage(SCIP* scip, SCIP_STAGE stage, SCIP_Bool enableNLP)
 
 #ifdef __GNUC__
 #pragma GCC diagnostic warning "-Wredundant-decls"
+#pragma GCC diagnostic warning "-Wstrict-prototypes"
 #endif
 
 #undef SCIP_CALL
