17a0616ae6eb60ba448bd156cff35c1cd75f44dd
Gregor Hendel <hendel@zib.de>
2019-07-31 15:10:56 +0200

    two more tests

--- tests/src/cons/or/disable_upgrades.c
+++ tests/src/cons/or/disable_upgrades.c
@@ -41,13 +41,19 @@ void setup(void)
    SCIP_CALL( SCIPcreate(&scip) );
 
    SCIP_CALL( SCIPincludeDefaultPlugins(scip) );
+
+   SCIP_CALL( SCIPsetIntParam(scip, "constraints/or/maxprerounds", 0) );
+   SCIP_CALL( SCIPsetIntParam(scip, "presolving/maxrestarts", 0) );
+   SCIP_CALL( SCIPsetIntParam(scip, "branching/inference/priority", 1000000) );
+
+   SCIP_CALL( SCIPsetHeuristics(scip, SCIP_PARAMSETTING_OFF, FALSE) );
 }
 
 /** free all allocated memory */
 static
 void teardown(void)
 {
-   SCIPfree(&scip);
+   SCIP_CALL( SCIPfree(&scip) );
 
 /*
     remove the cip-file for this test
@@ -104,4 +110,44 @@ Test(disable_upgrades, write_problem, .description="test that CIP write method w
 
 }
 
+Test(disable_upgrades, copy_problem, .description="test copying of or constraints")
+{
+   SCIP* targetscip;
+   SCIP_Bool valid;
+   SCIP_CALL( SCIPreadProb(scip, "../check/instances/Or/or_constraint.cip", "cip") );
 
+   SCIP_CALL( SCIPcreate(&targetscip) );
+
+   SCIP_CALL( SCIPcopy(scip, targetscip, NULL, NULL,  "copy_of_prob", TRUE, TRUE, FALSE, &valid) );
+
+   cr_assert(valid);
+
+   SCIP_CALL( SCIPsolve(targetscip) );
+
+   SCIP_CALL( SCIPfree(&targetscip) );
+}
+
+Test(disable_upgrades, test_basic_creation, .description="test SCIPcreateConsBasicOr")
+{
+   SCIP* targetscip;
+   SCIP_CONS* newcons;
+   SCIP_VAR** origvars;
+   SCIP_Bool valid;
+   SCIP_CALL( SCIPreadProb(scip, "../check/instances/Or/or_constraint.cip", "cip") );
+
+   cr_assert(SCIPgetNVars(scip) == 24);
+   cr_assert(SCIPgetNBinVars(scip) == 24);
+
+   origvars = SCIPgetOrigVars(scip);
+
+   SCIP_CALL( SCIPcreateConsBasicOr(scip, &newcons, "new_or_constraint", origvars[3], 6, &origvars[4]) );
+
+   SCIP_CALL( SCIPaddCons(scip, newcons) );
+
+   SCIP_CALL( SCIPreleaseCons(scip, &newcons) );
+
+   SCIP_CALL( SCIPsolve(scip) );
+
+   SCIP_CALL( SCIPprintBestSol(scip, NULL, TRUE) );
+
+}
