88c592f3d2ddfe772c0b2eaac06190acd136f98e
Daniel Rehfeldt <rehfeldt@zib.de>
2019-08-14 11:59:43 +0200

    use ext for pcmw

--- applications/STP/src/reduce_bnd.c
+++ applications/STP/src/reduce_bnd.c
@@ -2147,8 +2147,8 @@ SCIP_RETCODE reduce_da(
                  reduce_extendedEdge2(scip, graph, vnoi, cost, pathdist, (apsol ? result : NULL), minpathcost, daroot, FALSE, marked, &extfixed);
                  nfixed += extfixed;
 
-               //  graph_printInfo(graph);
-             //    printf("newly fixed %d \n", extfixed);
+           //      graph_printInfo(graph);
+            //     printf("newly fixed %d \n", extfixed);
 //                 assert(0);
 
               }
@@ -2194,7 +2194,13 @@ SCIP_RETCODE reduce_da(
          if( extended || 1 )
          {
             int todo;
+            if( rpc )
+               graph_pc_2org(graph);
+
             SCIP_CALL(reduce_deleteConflictEdges(scip, graph));
+
+            if( rpc )
+               graph_pc_2trans(graph);
          }
       }
 
