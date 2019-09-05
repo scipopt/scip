e61bbdfb35ccfc3d004826a115f61f7e8833f696
Jakob Witzig <witzig@zib.de>
2019-09-05 12:50:47 +0200

    tighten sides before upgrading linear cons

--- src/scip/cons_linear.c
+++ src/scip/cons_linear.c
@@ -8864,6 +8864,7 @@ SCIP_RETCODE tightenSides(
    assert(scip != NULL);
    assert(cons != NULL);
    assert(nchgsides != NULL);
+   assert(infeasible != NULL)
 
    consdata = SCIPconsGetData(cons);
    assert(consdata != NULL);
@@ -18462,6 +18463,7 @@ SCIP_RETCODE SCIPupgradeConsLinear(
    SCIP_Real negcoeffsum;
    SCIP_Bool infeasible;
    SCIP_Bool integral;
+   int nchgsides;
    int nposbin;
    int nnegbin;
    int nposint;
@@ -18537,6 +18539,12 @@ SCIP_RETCODE SCIPupgradeConsLinear(
     * TODO: this needs to be fixed on master by changing the API and passing a pointer to whether the constraint is
     *       proven to be infeasible.
     */
+   if( infeasible )
+      return SCIP_OKAY;
+
+   /* tighten sides */
+   SCIP_CALL( tightenSides(scip, cons, &nchgsides, &infeasible) );
+
    if( infeasible )
       return SCIP_OKAY;
 
