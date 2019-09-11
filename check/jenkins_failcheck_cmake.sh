976947df328aba74e6c2f9f94f65c8be5597cc58
Franziska Schloesser <schloesser@zib.de>
2019-09-11 11:24:28 +0200

    update check/jenkins_failcheck_cmake.sh

--- check/jenkins_failcheck_cmake.sh
+++ check/jenkins_failcheck_cmake.sh
@@ -192,7 +192,7 @@ SEEDSBND=$(expr ${SEEDS} + ${GLBSEEDSHIFT})
 PERMUTEBND=$(expr ${PERMUTE} + ${STARTPERM})
 
 SEED=${GLBSEEDSHIFT}
-while [ ${SEED} -le ${SEEDSBND} ]; do
+while [ "${SEED}" -le "${SEEDSBND}" ]; do
   # get ending given by seed
   if [ "${SEED}" == "0" ]; then
     SEED_ENDING=""
@@ -200,7 +200,7 @@ while [ ${SEED} -le ${SEEDSBND} ]; do
     SEED_ENDING="-s${SEED}"
   fi
   PERM=${STARTPERM}
-  while [ ${PERM} -le ${PERMUTEBND} ]; do
+  while [ "${PERM}" -le "${PERMUTEBND}" ]; do
     # get ending given by permutation
     if [ "${PERM}" == "0" ]; then
       PERM_ENDING=""
@@ -449,9 +449,9 @@ elif [ "${PERFORMANCE}" == "mergerequest" ]; then
   COMPAREIDS=""
 
   COUNT_S=${GLBSEEDSHIFT}
-  while [ ${COUNT_S} -le ${SEEDSBND} ]; do
+  while [ "${COUNT_S}" -le "${SEEDSBND}" ]; do
     COUNT_P=${STARTPERM}
-    while [ ${COUNT_P} -le ${PERMUTEBND} ]; do
+    while [ "${COUNT_P}" -le "${PERMUTEBND}" ]; do
       RBDB_STRS=$(grep -e "\(${COMPAREHASH}\|${FULLGITHASH}\|${NEWTIMESTAMP}\)" ${RBDB} ${MAINRBDB} | grep -P "p=${COUNT_P} s=${COUNT_S}")
       if [ "${RBDB_STRS}" != "" ]; then
         if [ 2 -le $(echo "${RBDB_STRS}" |wc -l) ]; then
