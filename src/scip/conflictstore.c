39f2e71746cd63b974b6741dd326b81e0968cddd
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2019-09-28 12:22:00 +0200

    avoid calling function with scip == NULL

--- src/scip/conflictstore.c
+++ src/scip/conflictstore.c
@@ -1274,7 +1274,7 @@ SCIP_RETCODE SCIPconflictstoreCleanNewIncumbent(
 
          if( !SCIPsetIsInfinity(set, rhs) )
          {
-            assert(SCIPsetIsInfinity(set, -SCIPgetLhsLinear(NULL, dualproof)));
+            assert(SCIPsetIsInfinity(set, -SCIPgetLhsLinear(set->scip, dualproof)));
             assert(SCIPsetIsPositive(set, conflictstore->scalefactors[i]));
 
             /* get unscaled rhs */
@@ -1289,7 +1289,9 @@ SCIP_RETCODE SCIPconflictstoreCleanNewIncumbent(
          }
          else
          {
-            SCIP_Real lhs = SCIPgetLhsLinear(NULL, dualproof);
+            SCIP_Real lhs;
+
+            lhs = SCIPgetLhsLinear(set->scip, dualproof);
             assert(!SCIPsetIsInfinity(set, -lhs));
             assert(SCIPsetIsNegative(set, conflictstore->scalefactors[i]));
 
