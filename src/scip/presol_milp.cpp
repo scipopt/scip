d97f8c9641ca8587c77ae2d2998d4e4a53f73ebf
Leona Gottwald <gottwald@zib.de>
2019-10-22 12:45:15 +0200

    fix author

--- src/scip/presol_milp.cpp
+++ src/scip/presol_milp.cpp
@@ -15,7 +15,7 @@
 
 /**@file   presol_milp.cpp
  * @brief  MILP presolver
- * @author Robert Lion Gottwald
+ * @author Leona Gottwald
  */
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
