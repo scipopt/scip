63ee541adc4686956acbc7623a0e66f78e12b669
Leona Gottwald <gottwald@zib.de>
2019-10-23 11:43:02 +0200

    fix call to SCIPmatrixCreate()

--- src/scip/presol_dualsparsify.c
+++ src/scip/presol_dualsparsify.c
@@ -1265,7 +1265,7 @@ SCIP_DECL_PRESOLEXEC(presolExecDualsparsify)
    *result = SCIP_DIDNOTFIND;
 
    matrix = NULL;
-   SCIP_CALL( SCIPmatrixCreate(scip, &matrix, &initialized, &complete) );
+   SCIP_CALL( SCIPmatrixCreate(scip, &matrix, TRUE, &initialized, &complete) );
 
    if( initialized && complete )
    {
