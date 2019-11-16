22c8ead6e5c8c7bb0580518ad1267ec933c1d807
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2019-11-16 19:55:46 +0100

    extend documentation

--- src/scip/sepa_cgmip.c
+++ src/scip/sepa_cgmip.c
@@ -2409,7 +2409,12 @@ SCIP_RETCODE solveSubscip(
 
 /** Computes cut from the given multipliers
  *
- *  Note that the cut computed here in general will not be the same as the one computed with the
+ *  When computing the cut, we take the fractional part of the multipliers. This is known to produce stronger cuts in
+ *  the pure integer case, since the cut is the sum of the one using fractional parts and integer multiples of the
+ *  original constraints. However, if there are continuous variables, the resulting cut might not be valid. This is
+ *  checked and returned.
+ *
+ *  Moreover, the cut computed here in general will not be the same as the one computed with the
  *  sub-MIP, because of numerical differences. Here, we only combine rows whose corresponding
  *  multiplier is positive w.r.t. the feasibility tolerance. In the sub-MIP, however, the rows are
  *  combined in any case. This makes a difference, if the coefficients in the matrix are large and
