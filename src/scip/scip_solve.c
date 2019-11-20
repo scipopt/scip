cfbc0f12ead75ca8a0c43f3cd384725ceb185c7d
Gerald Gamrath <gamrath@zib.de>
2019-11-20 19:17:00 +0100

    don't increase locks if no dual reductions are allowed

--- src/scip/scip_solve.c
+++ src/scip/scip_solve.c
@@ -2019,21 +2019,6 @@ SCIP_RETCODE freeTransform(
       SCIP_CALL( SCIPreoptReset(scip->reopt, scip->set, scip->mem->probmem) );
    }
 
-   /* @todo if a variable was removed from the problem during solving, its locks were not reduced;
-    *       we might want to remove locks also in that case
-    */
-   /* remove var locks set to avoid dual reductions */
-   if( scip->set->reopt_enable || !scip->set->misc_allowstrongdualreds )
-   {
-      int v;
-
-      /* unlock all variables */
-      for(v = 0; v < scip->transprob->nvars; v++)
-      {
-         SCIP_CALL( SCIPaddVarLocksType(scip, scip->transprob->vars[v], SCIP_LOCKTYPE_MODEL, -1, -1) );
-      }
-   }
-
    if( !reducedfree )
    {
       /* clear the conflict store
