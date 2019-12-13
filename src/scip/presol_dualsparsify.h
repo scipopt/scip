437e5ca46aad3942e1692352f6a4c37361b254ba
Gerald Gamrath <gamrath@zib.de>
2019-12-13 09:49:29 +0100

    Apply suggestion to src/scip/presol_dualsparsify.h

--- src/scip/presol_dualsparsify.h
+++ src/scip/presol_dualsparsify.h
@@ -29,7 +29,7 @@
  *
  * where eta is an nonnegative integer. Then we introduce a new variable y := s*x_j + x_k
  * and aggregate the variable x_k = y - s*x_j. After aggregation, the column of the variable
- * x_j is A_{j.} + s*A_{j.} which is sparser than A_{j.}. In the case that x_k is nonimplied
+ * x_j is A_{j.} + s*A_{j.} which is sparser than A_{j.}. In the case that x_k is no implied
  * free variable, we need to add a new constraint l_k <= y - weight*x_j <= u_k into the problem
  * to keep the bounds constraints of variable x_k.
  *
