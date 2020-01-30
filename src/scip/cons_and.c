0fb6eb78e7a846d67e454d53e970436facda202c
Benjamin Mueller <benjamin.mueller@zib.de>
2020-01-30 12:41:34 +0100

    fix typo

--- src/scip/cons_and.c
+++ src/scip/cons_and.c
@@ -4788,7 +4788,7 @@ SCIP_DECL_CONSPARSE(consParseAnd)
 
       if( endptr > startptr )
       {
-         /* copy string for parsing; note that isspace() in SCIPparseVarsList() requires that strcopy end with '\0' */
+         /* copy string for parsing; note that isspace() in SCIPparseVarsList() requires that strcopy ends with '\0' */
          SCIP_CALL( SCIPduplicateBufferArray(scip, &strcopy, startptr, (int)(endptr-startptr+1)) );
          strcopy[endptr-startptr] = '\0';
          varssize = 100;
--- src/scip/cons_logicor.c
+++ src/scip/cons_logicor.c
@@ -4866,7 +4866,7 @@ SCIP_DECL_CONSPARSE(consParseLogicor)
 
    if( endptr > startptr )
    {
-      /* copy string for parsing; note that isspace() in SCIPparseVarsList() requires that strcopy end with '\0' */
+      /* copy string for parsing; note that isspace() in SCIPparseVarsList() requires that strcopy ends with '\0' */
       SCIP_CALL( SCIPduplicateBufferArray(scip, &strcopy, startptr, (int)(endptr-startptr+1)) );
       strcopy[endptr-startptr] = '\0';
       varssize = 100;
