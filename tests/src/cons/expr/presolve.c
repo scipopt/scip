71fb0e3f0abc8401e2d7e464af4a77d242b5345b
Benjamin Mueller <benjamin.mueller@zib.de>
2020-03-25 17:00:20 +0100

    add tests for presolSingleLockedVars()

--- tests/src/cons/expr/presolve.c
+++ tests/src/cons/expr/presolve.c
@@ -21,6 +21,7 @@
 
 #include "scip/scip.h"
 #include "scip/cons_expr.c"
+#include "scip/cons_bounddisjunction.h"
 
 #include "include/scip_test.h"
 
@@ -45,9 +46,9 @@ void setup(void)
    /* create problem */
    SCIP_CALL( SCIPcreateProbBasic(scip, "test") );
 
-   SCIP_CALL( SCIPcreateVarBasic(scip, &x, "x", 0.0, 1.0, 0.0, SCIP_VARTYPE_BINARY) );
-   SCIP_CALL( SCIPcreateVarBasic(scip, &y, "y", -10.0, 10.0, 0.0, SCIP_VARTYPE_INTEGER) );
-   SCIP_CALL( SCIPcreateVarBasic(scip, &z, "z", -SCIPinfinity(scip), SCIPinfinity(scip), 0.0, SCIP_VARTYPE_CONTINUOUS) );
+   SCIP_CALL( SCIPcreateVarBasic(scip, &x, "x", 0.0, 1.0, 0.0, SCIP_VARTYPE_CONTINUOUS) );
+   SCIP_CALL( SCIPcreateVarBasic(scip, &y, "y", -10.0, 10.0, 0.0, SCIP_VARTYPE_CONTINUOUS) );
+   SCIP_CALL( SCIPcreateVarBasic(scip, &z, "z", 0.0, 1.0, 0.0, SCIP_VARTYPE_CONTINUOUS) );
    SCIP_CALL( SCIPaddVar(scip, x) );
    SCIP_CALL( SCIPaddVar(scip, y) );
    SCIP_CALL( SCIPaddVar(scip, z) );
@@ -110,4 +111,89 @@ Test(presolve, mergeconss)
    {
       SCIP_CALL( SCIPreleaseCons(scip, &conss[c]) );
    }
+}
+
+/** helper method to create, add, and release an expression constraint */
+static
+SCIP_RETCODE addCons(
+   const char*           exprstr,            /**< string with the expr to parse */
+   SCIP_Real             lhs,                /**< left-hand side */
+   SCIP_Real             rhs                 /**< right-hand side */
+   )
+{
+   SCIP_CONSEXPR_EXPR* expr;
+   SCIP_CONS* cons;
+
+   SCIP_CALL( SCIPparseConsExprExpr(scip, conshdlr, exprstr, NULL, &expr) );
+   SCIP_CALL( SCIPcreateConsExprBasic(scip, &cons, "cons", expr, lhs, rhs) );
+   SCIP_CALL( SCIPaddCons(scip, cons) );
+   SCIP_CALL( SCIPreleaseCons(scip, &cons) );
+   SCIP_CALL( SCIPreleaseConsExprExpr(scip, &expr) );
+
+   return SCIP_OKAY;
+}
+
+/** helper method to check variable types */
+static
+void checkTypes(
+   SCIP_VARTYPE          vartypex,           /**< target variable type of x */
+   SCIP_VARTYPE          vartypey,           /**< target variable type of y */
+   SCIP_VARTYPE          vartypez            /**< target variable type of z */
+   )
+{
+   cr_expect(SCIPvarGetType(SCIPvarGetTransVar(x)) == vartypex);
+   cr_expect(SCIPvarGetType(SCIPvarGetTransVar(y)) == vartypey);
+   cr_expect(SCIPvarGetType(SCIPvarGetTransVar(z)) == vartypez);
+}
+
+/* test for presolSingleLockedVars() */
+Test(presolve, singlelockedvars1)
+{
+   /* -x^2 + y^2 -z^2 >= 0 implies that x and z are at their bounds */
+   SCIP_CALL( addCons("+<x>^2 -<y>^2 +<z>^2 + <x>*<y> + 1/sin(20 + <y>)", 0.5, SCIPinfinity(scip)) );
+
+   /* apply presolving */
+   SCIP_CALL( TESTscipSetStage(scip, SCIP_STAGE_PRESOLVED, FALSE) );
+   cr_assert(SCIPgetNConss(scip) == 1);
+
+   /* check variable types */
+   checkTypes(SCIP_VARTYPE_BINARY, SCIP_VARTYPE_CONTINUOUS, SCIP_VARTYPE_BINARY);
+}
+
+/* test for presolSingleLockedVars() */
+Test(presolve, singlelockedvars2)
+{
+   /* same example as singlelockedvars1 but there is another constraint that contains x and y => only z can be at its bounds */
+   SCIP_CALL( addCons("+<x>^2 -<y>^2 +<z>^2 + <x>*<y> + 1/sin(20 + <y>)", 0.5, SCIPinfinity(scip)) );
+   SCIP_CALL( addCons("<x> * <y> + cos(<x>)", -0.5, 0.5) );
+
+   /* apply presolving */
+   SCIP_CALL( TESTscipSetStage(scip, SCIP_STAGE_PRESOLVED, FALSE) );
+   cr_assert(SCIPgetNConss(scip) == 2);
+
+   /* check variable types */
+   checkTypes(SCIP_VARTYPE_CONTINUOUS, SCIP_VARTYPE_CONTINUOUS, SCIP_VARTYPE_BINARY);
+}
+
+/* test for presolSingleLockedVars() */
+Test(presolve, singlelockedvars3)
+{
+   /* include bound disjunction constraint handler */
+   SCIP_CALL( SCIPincludeConshdlrBounddisjunction(scip) );
+   SCIP_CALL( SCIPincludeReaderCip(scip) );
+
+   /* change bounds of z */
+   SCIP_CALL( SCIPchgVarLbGlobal(scip, z, -1.0) );
+   SCIP_CALL( SCIPchgVarUbGlobal(scip, z, 2.0) );
+
+   /* same example as singlelockedvars1; x should be binary and there should be one disjunction constraint for z */
+   SCIP_CALL( addCons("+<x>^2 -<y>^2 +<z>^2 + <x>*<y> + 1/sin(20 + <y>)", 0.5, SCIPinfinity(scip)) );
+
+   /* apply presolving */
+   SCIP_CALL( TESTscipSetStage(scip, SCIP_STAGE_PRESOLVED, FALSE) );
+   SCIPwriteTransProblem(scip, "trans.cip", NULL, FALSE);
+   cr_expect(SCIPgetNConss(scip) == 2);
+
+   /* check variable types */
+   checkTypes(SCIP_VARTYPE_BINARY, SCIP_VARTYPE_CONTINUOUS, SCIP_VARTYPE_CONTINUOUS);
 }
