5421dd9beef9bd84736ec8a30b52a3ca51845d02
Benjamin Mueller <benjamin.mueller@zib.de>
2020-05-15 13:54:57 +0200

    adapt code in PolySCIP application

--- applications/PolySCIP/src/ReaderMOP.cpp
+++ applications/PolySCIP/src/ReaderMOP.cpp
@@ -43,7 +43,7 @@
 #include "scip/cons_varbound.h"
 #include "scip/cons_sos1.h"
 #include "scip/cons_sos2.h"
-#include "scip/cons_quadratic.h"
+#include "scip/cons_expr.h"
 #include "scip/cons_soc.h"
 #include "scip/cons_bounddisjunction.h"
 #include "scip/pub_misc.h"
@@ -1595,7 +1595,7 @@ SCIP_RETCODE readQMatrix(
          rhs = SCIPinfinity(scip);
       }
 
-      SCIP_CALL( SCIPcreateConsQuadratic(scip, &cons, "qmatrix", 1, &qmatrixvar, &minusone, cnt, quadvars1, quadvars2, quadcoefs, lhs, rhs,
+      SCIP_CALL( SCIPcreateConsExprQuadratic(scip, &cons, "qmatrix", 1, &qmatrixvar, &minusone, cnt, quadvars1, quadvars2, quadcoefs, lhs, rhs,
             initial, separate, enforce, check, propagate, local, modifiable, dynamic, removable) );
 
       SCIP_CALL( SCIPaddCons(scip, cons) );
@@ -1757,7 +1757,7 @@ SCIP_RETCODE readQCMatrix(
    {
       SCIP_CONS* cons = NULL;
 
-      SCIP_CALL( SCIPcreateConsQuadratic(scip, &cons, SCIPconsGetName(lincons), 
+      SCIP_CALL( SCIPcreateConsExprQuadratic(scip, &cons, SCIPconsGetName(lincons),
             SCIPgetNVarsLinear(scip, lincons), SCIPgetVarsLinear(scip, lincons), SCIPgetValsLinear(scip, lincons),
             cnt, quadvars1, quadvars2, quadcoefs, SCIPgetLhsLinear(scip, lincons), SCIPgetRhsLinear(scip, lincons),
             SCIPconsIsInitial(lincons), SCIPconsIsSeparated(lincons), SCIPconsIsEnforced(lincons), SCIPconsIsChecked(lincons),
