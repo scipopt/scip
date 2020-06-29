02c5ea8d9cc20e89d408e73b15d78e26fa68ae5c
Franziska Schloesser <schloesser@zib.de>
2020-06-29 16:44:09 +0200

    add Sudoku doc

---
+++ examples/Sudoku/doc/xternal_sudoku.c
@@ -0,0 +1,38 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the program and library             */
+/*         SCIP --- Solving Constraint Integer Programs                      */
+/*                                                                           */
+/*    Copyright (C) 2002-2020 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SCIP is distributed under the terms of the ZIB Academic License.         */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SCIP; see the file COPYING. If not visit scipopt.org.         */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**@file   examples/Sudoku/doc/xternal_sudoku.c
+ * @brief  main document page
+ * @author Naga V C Gudapati
+ */
+
+/*--+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
+
+/**@page     SUDOKU_MAIN Sudoku Solver
+ * @author Naga V C Gudapati
+ *
+ * This example solves a sudoku puzzles.
+ * The input file is containing the configuration of a sudoku read rowwise.
+ * Missing fields are represented by a dot.
+ *
+ * After compiling you can run it via the command
+ * `./bin/sudoku data/puzzle5.txt`.
+ *
+ * Installation
+ * ============
+ *
+ * See the @ref INSTALL_APPLICATIONS_EXAMPLES "Install file"
+ *
+ */
