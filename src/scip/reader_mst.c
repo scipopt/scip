cfea7e840a921251922da062741ad507a34c25ce
Boro Sofranac <sofranac@zib.de>
2020-08-11 18:09:24 +0200

    fix sto and sol reader file type bug

--- src/scip/reader_mst.c
+++ src/scip/reader_mst.c
@@ -124,7 +124,6 @@ SCIP_DECL_READERREAD(readerReadMst)
 {  /*lint --e{715}*/
    SCIP_FILE* file;
    char buffer[SCIP_MAXSTRLEN];
-   char *s;
 
    assert(reader != NULL);
    assert(strcmp(SCIPreaderGetName(reader), READER_NAME) == 0);
@@ -162,13 +161,7 @@ SCIP_DECL_READERREAD(readerReadMst)
    /* close file */
    SCIPfclose(file);
 
-   /* decide whether it is xml */
-   s = buffer;
-
-   /* skip spaces */
-   while( isspace((unsigned char)*s) )
-      ++s;
-   if( s[0] == '<' && s[1] == '?' && s[2] == 'x' && s[3] == 'm' && s[4] == 'l' )
+   if (strstr(buffer, "<?xml"))
    {
       /* read XML solution and add it to the solution pool */
       SCIP_CALL( readMst(scip, filename, TRUE) );
