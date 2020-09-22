6c716b4782d8cbfb4b69feee8fad894804ae4f1a
Boro Sofranac <sofranac@zib.de>
2020-09-22 16:30:43 +0200

    add scip export

--- src/scip/pub_misc.h
+++ src/scip/pub_misc.h
@@ -2222,6 +2222,7 @@ void SCIPstrCopySection(
    );
 
 /** checks whether a given string t appears at the beginning of the string s (up to spaces at beginning) */
+SCIP_EXPORT
 SCIP_Bool SCIPstrAtStart(
         const char*           s,                  /**< string to search in */
         const char*           t,                  /**< string to search for */
