caf4822813b7e4f30f623c279c5fcbcf8224ae3f
Boro Sofranac <sofranac@zib.de>
2020-09-24 15:16:13 +0200

    tainted string supression

--- src/scip/reader_pip.c
+++ src/scip/reader_pip.c
@@ -2757,6 +2757,7 @@ SCIP_RETCODE printNonlinearCons(
       assert( !SCIPisInfinity(scip, rhs) );
 
       /* equal constraint */
+      /* coverity[tainted_string_warning] */
       printRowNl(scip, file, rowname, "", "=", activevars, activevals, nactivevars,
          exprtrees, exprtreecoefs, nexprtrees,
          rhs - activeconstant);
@@ -2766,6 +2767,7 @@ SCIP_RETCODE printNonlinearCons(
       if( !SCIPisInfinity(scip, -lhs) )
       {
          /* print inequality ">=" */
+         /* coverity[tainted_string_warning] */
          printRowNl(scip, file, rowname, SCIPisInfinity(scip, rhs) ? "" : "_lhs", ">=",
             activevars, activevals, nactivevars,
             exprtrees, exprtreecoefs, nexprtrees,
@@ -2774,6 +2776,7 @@ SCIP_RETCODE printNonlinearCons(
       if( !SCIPisInfinity(scip, rhs) )
       {
          /* print inequality "<=" */
+         /* coverity[tainted_string_warning] */
          printRowNl(scip, file, rowname, SCIPisInfinity(scip, -lhs) ? "" : "_rhs", "<=",
             activevars, activevals, nactivevars,
             exprtrees, exprtreecoefs, nexprtrees,
