d8eaaf29cdd88ba141ea80b15dfd15d79e33f763
Boro Sofranac <sofranac@zib.de>
2020-09-28 15:42:46 +0200

    close file before exiting

--- src/scip/reader_mst.c
+++ src/scip/reader_mst.c
@@ -156,7 +156,7 @@ SCIP_DECL_READERREAD(readerReadMst)
    if( SCIPfgets(buffer, (int) sizeof(buffer), file) == NULL )
    {
       SCIPerrorMessage("cannot parse file.\n");
-      /* coverity[leaked_storage] */
+      SCIPfclose(file);
       return SCIP_READERROR;
    }
    /* close file */
--- src/scip/reader_sol.c
+++ src/scip/reader_sol.c
@@ -181,7 +181,7 @@ SCIP_DECL_READERREAD(readerReadSol)
    if( SCIPfgets(buffer, (int) sizeof(buffer), file) == NULL )
    {
       SCIPerrorMessage("cannot parse file.\n");
-      /* coverity[leaked_storage] */
+      SCIPfclose(file);
       return SCIP_READERROR;
    }
    /* close file */
