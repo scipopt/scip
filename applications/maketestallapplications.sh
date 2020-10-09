3129169e525b28b64274d200ef800c669984c372
Franziska Schloesser <schloesser@zib.de>
2020-10-09 15:32:07 +0200

    adjust indentation to 3 spaces

--- applications/maketestallapplications.sh
+++ applications/maketestallapplications.sh
@@ -20,39 +20,39 @@ LIBTYPE="static"
 LIBEXT="a"
 for i in $@
 do
-  if test "$i" = "-q"
-  then
-    echo "Quiet mode."
-    QUIET=1
-  else
-    # test whether we want to build shared libraries (need bash "testing" w.r.t. regexp)
-    if [[ "$i" =~ SHARED[\s]*=true ]]
-    then
-      LIBTYPE="shared"
-      LIBEXT="so"
-    fi
-  MAKEARGS="$MAKEARGS $i"
-  fi
+   if test "$i" = "-q"
+   then
+      echo "Quiet mode."
+      QUIET=1
+   else
+      # test whether we want to build shared libraries (need bash "testing" w.r.t. regexp)
+      if [[ "$i" =~ SHARED[\s]*=true ]]
+      then
+         LIBTYPE="shared"
+         LIBEXT="so"
+      fi
+      MAKEARGS="$MAKEARGS $i"
+   fi
 done
 
 # determine architecture
 ARCH=`uname -m | \
-    sed \
-    -e 's/sun../sparc/' \
-    -e 's/i.86/x86/' \
-    -e 's/i86pc/x86/' \
-    -e 's/[0-9]86/x86/' \
-    -e 's/amd64/x86_64/' \
-    -e 's/IP../mips/' \
-    -e 's/9000..../hppa/' \
-    -e 's/Power\ Macintosh/ppc/' \
-    -e 's/00........../pwr4/'`
-OSTYPE=`uname -s | tr '[:upper:]' '[:lower:]' | \
-    sed \
-    -e 's/cygwin.*/cygwin/' \
-    -e 's/irix../irix/' \
-    -e 's/windows.*/windows/' \
-    -e 's/mingw.*/mingw/'`
+   sed \
+   -e 's/sun../sparc/' \
+   -e 's/i.86/x86/' \
+   -e 's/i86pc/x86/' \
+   -e 's/[0-9]86/x86/' \
+   -e 's/amd64/x86_64/' \
+   -e 's/IP../mips/' \
+   -e 's/9000..../hppa/' \
+   -e 's/Power\ Macintosh/ppc/' \
+   -e 's/00........../pwr4/'`
+   OSTYPE=`uname -s | tr '[:upper:]' '[:lower:]' | \
+      sed \
+      -e 's/cygwin.*/cygwin/' \
+      -e 's/irix../irix/' \
+      -e 's/windows.*/windows/' \
+      -e 's/mingw.*/mingw/'`
 
 # prepare log file
 echo "" > applicationtestsummary.log
@@ -60,96 +60,96 @@ echo "" > applicationtestsummary.log
 # pretest
 for OPT in ${OPTS[@]}
 do
-  for LPS in ${LPSOLVERS[@]}
-  do
-    LPILIB=../lib/$LIBTYPE/liblpi$LPS.$OSTYPE.$ARCH.gnu.$OPT.$LIBEXT
-    if test ! -e $LPILIB
-    then
-	    echo "Error: "$LPILIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"." >> ../applicationtestsummary.log
-	    echo "Error: "$LPILIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"."
-	    exit 1
-    fi
-    SCIPLIB=../lib/$LIBTYPE/libscip.$OSTYPE.$ARCH.gnu.$OPT.$LIBEXT
-    if test ! -e $SCIPLIB
-    then
-	    echo "Error: "$SCIPLIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"." >> ../applicationtestsummary.log
-	    echo "Error: "$SCIPLIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"."
-	    exit 1
-    fi
-  done
+   for LPS in ${LPSOLVERS[@]}
+   do
+      LPILIB=../lib/$LIBTYPE/liblpi$LPS.$OSTYPE.$ARCH.gnu.$OPT.$LIBEXT
+      if test ! -e $LPILIB
+      then
+         echo "Error: "$LPILIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"." >> ../applicationtestsummary.log
+         echo "Error: "$LPILIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"."
+         exit 1
+      fi
+      SCIPLIB=../lib/$LIBTYPE/libscip.$OSTYPE.$ARCH.gnu.$OPT.$LIBEXT
+      if test ! -e $SCIPLIB
+      then
+         echo "Error: "$SCIPLIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"." >> ../applicationtestsummary.log
+         echo "Error: "$SCIPLIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"."
+         exit 1
+      fi
+   done
 done
 
 # run tests
 for APPLICATION in $APPLICATIONS
 do
-  # See issues #1100 and #1169
-  if test $APPLICATION = "PolySCIP"
-  then
-    continue
-  fi
-  echo
-  echo
-  echo ===== $APPLICATION =====
-  echo ===== $APPLICATION ===== >> applicationtestsummary.log
-  echo
-  cd $APPLICATION
-  for OPT in ${OPTS[@]}
-  do
-    for LPS in ${LPSOLVERS[@]}
-    do
-	    echo make OPT=$OPT LPS=$LPS $MAKEARGS
-	    if (! make OPT=$OPT LPS=$LPS $MAKEARGS )
-	    then
-        echo "Making "$APPLICATION" failed." >> ../applicationtestsummary.log
-        exit 1
-	    else
-        echo "Making "$APPLICATION" successful." >> ../applicationtestsummary.log
-	    fi
-	    echo
-	    if test $QUIET = 1
-	    then
-        echo make OPT=$OPT LPS=$LPS $MAKEARGS test
-        if ( ! make OPT=$OPT LPS=$LPS $MAKEARGS test > /dev/null )
-        then
-          echo "Testing "$APPLICATION" failed."
-          echo "Testing "$APPLICATION" failed." >> ../applicationtestsummary.log
-          exit 1
-        fi
-	    else
-        echo make OPT=$OPT LPS=$LPS $MAKEARGS test
-        if ( ! make OPT=$OPT LPS=$LPS $MAKEARGS test )
-        then
-          echo "Testing "$APPLICATION" failed."
-          echo "Testing "$APPLICATION" failed." >> ../applicationtestsummary.log
-          exit 1
-        fi
-	    fi
-	    echo "Testing "$APPLICATION" successful."
-	    echo "Testing "$APPLICATION" successful." >> ../applicationtestsummary.log
+   # See issues #1100 and #1169
+   if test $APPLICATION = "PolySCIP"
+   then
+      continue
+   fi
+   echo
+   echo
+   echo ===== $APPLICATION =====
+   echo ===== $APPLICATION ===== >> applicationtestsummary.log
+   echo
+   cd $APPLICATION
+   for OPT in ${OPTS[@]}
+   do
+      for LPS in ${LPSOLVERS[@]}
+      do
+         echo make OPT=$OPT LPS=$LPS $MAKEARGS
+         if (! make OPT=$OPT LPS=$LPS $MAKEARGS )
+         then
+            echo "Making "$APPLICATION" failed." >> ../applicationtestsummary.log
+            exit 1
+         else
+            echo "Making "$APPLICATION" successful." >> ../applicationtestsummary.log
+         fi
+         echo
+         if test $QUIET = 1
+         then
+            echo make OPT=$OPT LPS=$LPS $MAKEARGS test
+            if ( ! make OPT=$OPT LPS=$LPS $MAKEARGS test > /dev/null )
+            then
+               echo "Testing "$APPLICATION" failed."
+               echo "Testing "$APPLICATION" failed." >> ../applicationtestsummary.log
+               exit 1
+            fi
+         else
+            echo make OPT=$OPT LPS=$LPS $MAKEARGS test
+            if ( ! make OPT=$OPT LPS=$LPS $MAKEARGS test )
+            then
+               echo "Testing "$APPLICATION" failed."
+               echo "Testing "$APPLICATION" failed." >> ../applicationtestsummary.log
+               exit 1
+            fi
+         fi
+         echo "Testing "$APPLICATION" successful."
+         echo "Testing "$APPLICATION" successful." >> ../applicationtestsummary.log
 
-	    # find most recently changed result file and display it
-	    if test -d check/results
-	    then
-        RESFILE=`find check/results/*.res -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" "`
-        if test -e $RESFILE
-        then
+    # find most recently changed result file and display it
+    if test -d check/results
+    then
+       RESFILE=`find check/results/*.res -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" "`
+       if test -e $RESFILE
+       then
           cat $RESFILE >> ../applicationtestsummary.log
 
-          # exit immediately if there was a fail if STOPONFAIL is yes
-          GREPFAILS=$(grep fail ${RESFILE})
-          if test "${GREPFAILS}" != "" -a "${STOPONFAIL}" = "yes"
-          then
-            echo "Testing "${APPLICATION}" failed:\n${GREPFAILS}\nsee ${RESFILE} in ${APPLICATION} directory for more details."
-            exit 1
-          fi
+       # exit immediately if there was a fail if STOPONFAIL is yes
+       GREPFAILS=$(grep fail ${RESFILE})
+       if test "${GREPFAILS}" != "" -a "${STOPONFAIL}" = "yes"
+       then
+          echo "Testing "${APPLICATION}" failed:\n${GREPFAILS}\nsee ${RESFILE} in ${APPLICATION} directory for more details."
+          exit 1
+       fi
 
-        fi
-	    fi
-	    echo
-	    echo >> ../applicationtestsummary.log
-    done
-  done
-  cd - > /dev/null
+       fi
+    fi
+    echo
+    echo >> ../applicationtestsummary.log
+ done
+done
+cd - > /dev/null
 done
 
 echo
--- examples/maketestallexamples.sh
+++ examples/maketestallexamples.sh
@@ -24,42 +24,42 @@ LIBTYPE="static"
 LIBEXT="a"
 for i in $@
 do
-  if test "$i" = "-q"
-  then
-    echo "Quiet mode."
-    QUIET=1
-  else
-    # test whether we want to build shared libraries (need bash "testing" w.r.t. regexp)
-    if [[ "$i" =~ SHARED[\s]*=true ]]
-    then
-      LIBTYPE="shared"
-      LIBEXT="so"
-    fi
-    MAKEARGS="$MAKEARGS $i"
-  fi
+   if test "$i" = "-q"
+   then
+      echo "Quiet mode."
+      QUIET=1
+   else
+      # test whether we want to build shared libraries (need bash "testing" w.r.t. regexp)
+      if [[ "$i" =~ SHARED[\s]*=true ]]
+      then
+         LIBTYPE="shared"
+         LIBEXT="so"
+      fi
+      MAKEARGS="$MAKEARGS $i"
+   fi
 done
 
 
 # determine architecture
 ARCH=`uname -m | \
-    sed \
-    -e 's/sun../sparc/' \
-    -e 's/i.86/x86/' \
-    -e 's/i86pc/x86/' \
-    -e 's/[0-9]86/x86/' \
-    -e 's/amd64/x86_64/' \
-    -e 's/IP../mips/' \
-    -e 's/9000..../hppa/' \
-    -e 's/Power\ Macintosh/ppc/' \
-    -e 's/00........../pwr4/'`
-OSTYPE=`uname -s | tr '[:upper:]' '[:lower:]' | \
-    sed \
-    -e 's/cygwin.*/cygwin/' \
-    -e 's/irix../irix/' \
-    -e 's/windows.*/windows/' \
-    -e 's/mingw.*/mingw/'`
+   sed \
+   -e 's/sun../sparc/' \
+   -e 's/i.86/x86/' \
+   -e 's/i86pc/x86/' \
+   -e 's/[0-9]86/x86/' \
+   -e 's/amd64/x86_64/' \
+   -e 's/IP../mips/' \
+   -e 's/9000..../hppa/' \
+   -e 's/Power\ Macintosh/ppc/' \
+   -e 's/00........../pwr4/'`
+   OSTYPE=`uname -s | tr '[:upper:]' '[:lower:]' | \
+      sed \
+      -e 's/cygwin.*/cygwin/' \
+      -e 's/irix../irix/' \
+      -e 's/windows.*/windows/' \
+      -e 's/mingw.*/mingw/'`
 
-EXAMPLELOG=${PWD}/exampletestsummary.log
+   EXAMPLELOG=${PWD}/exampletestsummary.log
 
 # prepare log file
 rm -f $EXAMPLELOG
@@ -81,64 +81,64 @@ rm -f $EXAMPLELOG
 # pretest
 for OPT in ${OPTS[@]}
 do
-  for LPS in ${LPSOLVERS[@]}
-  do
-    LPILIB=../lib/$LIBTYPE/liblpi$LPS.$OSTYPE.$ARCH.gnu.$OPT.$LIBEXT
-    if test ! -e $LPILIB
-    then
-       echo "Error: "$LPILIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"." | tee -a ../applicationtestsummary.log
-       exit 1
-    fi
-    SCIPLIB=../lib/$LIBTYPE/libscip.$OSTYPE.$ARCH.gnu.$OPT.$LIBEXT
-    if test ! -e $SCIPLIB
-    then
-       echo "Error: "$SCIPLIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"." | tee -a ../applicationtestsummary.log
-       exit 1
-    fi
-  done
+   for LPS in ${LPSOLVERS[@]}
+   do
+      LPILIB=../lib/$LIBTYPE/liblpi$LPS.$OSTYPE.$ARCH.gnu.$OPT.$LIBEXT
+      if test ! -e $LPILIB
+      then
+         echo "Error: "$LPILIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"." | tee -a ../applicationtestsummary.log
+         exit 1
+      fi
+      SCIPLIB=../lib/$LIBTYPE/libscip.$OSTYPE.$ARCH.gnu.$OPT.$LIBEXT
+      if test ! -e $SCIPLIB
+      then
+         echo "Error: "$SCIPLIB" does not exist, please compile SCIP with OPT="$OPT" and LPS="$LPS"." | tee -a ../applicationtestsummary.log
+         exit 1
+      fi
+   done
 done
 
 # run tests
 for EXAMPLE in $EXAMPLES
 do
-  echo
-  echo
-  echo ===== $EXAMPLE ===== | tee -a $EXAMPLELOG
-  echo
-  pushd $EXAMPLE > /dev/null
-  for OPT in ${OPTS[@]}
-  do
-    for LPS in ${LPSOLVERS[@]}
-    do
-      echo make OPT=$OPT LPS=$LPS $MAKEARGS | tee -a $EXAMPLELOG
-      make OPT=$OPT LPS=$LPS $MAKEARGS
+   echo
+   echo
+   echo ===== $EXAMPLE ===== | tee -a $EXAMPLELOG
+   echo
+   pushd $EXAMPLE > /dev/null
+   for OPT in ${OPTS[@]}
+   do
+      for LPS in ${LPSOLVERS[@]}
+      do
+         echo make OPT=$OPT LPS=$LPS $MAKEARGS | tee -a $EXAMPLELOG
+         make OPT=$OPT LPS=$LPS $MAKEARGS
 
-      echo
-      echo make OPT=$OPT LPS=$LPS $MAKEARGS test | tee -a $EXAMPLELOG
-      if test $QUIET = 1
-      then
-        make OPT=$OPT LPS=$LPS $MAKEARGS test > /dev/null
-      else
-        make OPT=$OPT LPS=$LPS $MAKEARGS test
-      fi
+         echo
+         echo make OPT=$OPT LPS=$LPS $MAKEARGS test | tee -a $EXAMPLELOG
+         if test $QUIET = 1
+         then
+            make OPT=$OPT LPS=$LPS $MAKEARGS test > /dev/null
+         else
+            make OPT=$OPT LPS=$LPS $MAKEARGS test
+         fi
 
       # find most recently changed result file and display it ("|| :" to ignore error)
       RESFILE=`ls -tr check/results/*.res 2>/dev/null | tail -1` || :
       if [ -n "$RESFILE" ] && [ -e "$RESFILE" ]
       then
-        cat $RESFILE >> $EXAMPLELOG
+         cat $RESFILE >> $EXAMPLELOG
 
         # exit immediately if there was a fail if STOPONFAIL is yes
         GREPFAILS=$(grep fail ${RESFILE})
         if test "${GREPFAILS}" != "" -a "${STOPONFAIL}" = "yes"
         then
-          echo "Testing "${EXAMPLE}" failed:\n${GREPFAILS}\nsee ${RESFILE} in ${EXAMPLE} directory for more details."
-          exit 1
+           echo "Testing "${EXAMPLE}" failed:\n${GREPFAILS}\nsee ${RESFILE} in ${EXAMPLE} directory for more details."
+           exit 1
         fi
 
       fi
       echo | tee -a $EXAMPLELOG
-    done
-  done
-  popd > /dev/null
+   done
+done
+popd > /dev/null
 done
