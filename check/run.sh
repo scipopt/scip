6a6900da44e7ece0b4656cbb4803febfbf0482f0
Antonia Chmiela <chmiela@zib.de>
2020-10-15 09:06:06 +0200

    set stacklimit to infinity

--- check/run.sh
+++ check/run.sh
@@ -107,6 +107,7 @@ echo @05 $TIMELIMIT                 >> $OUTFILE
 #if we use a debugger command, we need to replace the errfile place holder by the actual err-file for logging
 #and if we run on the cluster we want to use srun with CPU binding which is defined by the check_cluster script
 EXECNAME=$SRUN${EXECNAME/ERRFILE_PLACEHOLDER/${ERRFILE}}
+ulimit -S -s unlimited
 if test -e $TMPFILE
 then
     eval $EXECNAME                < $TMPFILE 2>>$ERRFILE  | tee -a $OUTFILE
