9d51eb12a9328bc2ecfcbef8ae53700a3f936a4a
Stefan Vigerske <svigerske@gams.com>
2020-10-22 12:53:39 +0200

    increment API version after !2441
    - NPARASCIP define is not SCIP_THREADSAFE

--- src/scip/def.h
+++ src/scip/def.h
@@ -123,7 +123,7 @@ extern "C" {
 
 #define SCIP_VERSION                701 /**< SCIP version number (multiplied by 100 to get integer number) */
 #define SCIP_SUBVERSION               3 /**< SCIP sub version number */
-#define SCIP_APIVERSION              73 /**< SCIP API version number */
+#define SCIP_APIVERSION              74 /**< SCIP API version number */
 #define SCIP_COPYRIGHT   "Copyright (C) 2002-2020 Konrad-Zuse-Zentrum fuer Informationstechnik Berlin (ZIB)"
 
 
