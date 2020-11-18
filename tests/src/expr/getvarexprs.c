5589a58d687811473f31ef1153f950845fc46dad
Stefan Vigerske <svigerske@gams.com>
2020-11-18 16:26:26 +0100

    bring back getvarexprs test

--- tests/src/expr/getvarexprs.c
+++ tests/src/expr/getvarexprs.c
@@ -13,36 +13,30 @@
 /*                                                                           */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-/**@file   get_var_expressions.c
+/**@file   getvarexprs.c
  * @brief  tests for methods to get expressions of variables
  */
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
 
-#include "scip/cons_expr.h"
-#include "scip/cons_expr_sum.h"
-#include "scip/cons_expr_var.h"
+#include "scip/expr.h"
+#include "scip/scipdefplugins.h"
+#include "scip/expr_sum.h"
+#include "scip/expr_var.h"
 #include "include/scip_test.h"
 
 static SCIP* scip;
-static SCIP_CONSHDLR* conshdlr;
+static SCIP_EXPR* expr;
 static SCIP_VAR* x;
 static SCIP_VAR* y;
 static SCIP_VAR* z;
 static SCIP_VAR* w;
-static SCIP_CONSEXPR_EXPR* expr;
 
 static
 void setup(void)
 {
    SCIP_CALL( SCIPcreate(&scip) );
-
-   /* include cons_expr: this adds the operator handlers */
-   SCIP_CALL( SCIPincludeConshdlrExpr(scip) );
-
-   /* get expr conshdlr */
-   conshdlr = SCIPfindConshdlr(scip, "expr");
-   assert(conshdlr != NULL);
+   SCIP_CALL( SCIPincludeDefaultPlugins(scip) );
 
    /* create problem */
    SCIP_CALL( SCIPcreateProbBasic(scip, "test_problem") );
@@ -56,13 +50,13 @@ void setup(void)
    SCIP_CALL( SCIPaddVar(scip, y) );
    SCIP_CALL( SCIPaddVar(scip, z) );
 
-   SCIP_CALL( SCIPparseConsExprExpr(scip, conshdlr, "1.1*<x>*<y>/<z> + 3.2*<x>^2*<y>^(-5)*<z> + 0.5*<z>^3", NULL, &expr) );
+   SCIP_CALL( SCIPparseExpr(scip, &expr, "1.1*<x>*<y>/<z> + 3.2*<x>^2*<y>^(-5)*<z> + 0.5*<z>^3", NULL, NULL, NULL) );
 }
 
 static
 void teardown(void)
 {
-   SCIP_CALL( SCIPreleaseConsExprExpr(scip, &expr) );
+   SCIP_CALL( SCIPreleaseExpr(scip, &expr) );
    SCIP_CALL( SCIPreleaseVar(scip, &w) );
    SCIP_CALL( SCIPreleaseVar(scip, &x) );
    SCIP_CALL( SCIPreleaseVar(scip, &y) );
@@ -76,50 +70,50 @@ TestSuite(getvars, .init = setup, .fini = teardown);
 
 Test(getvars, expression_not_containing_all_vars)
 {
-   SCIP_CONSEXPR_EXPR* varexprs[3];
+   SCIP_EXPR* varexprs[3];
    int nvarexprs;
    int i;
    
    /* note that this captures the variable expressions */
-   SCIP_CALL( SCIPgetConsExprExprVarExprs(scip, conshdlr, expr, varexprs, &nvarexprs) );
+   SCIP_CALL( SCIPgetExprVarExprs(scip, expr, varexprs, &nvarexprs) );
    cr_assert_eq(nvarexprs, 3);
 
    for( i = 0; i < nvarexprs; ++i )
    {
       cr_assert_not_null(varexprs[i]);
-      cr_assert_str_eq(SCIPgetConsExprExprHdlrName(SCIPgetConsExprExprHdlr(varexprs[i])), "var");
+      cr_assert(SCIPisExprVar(scip, varexprs[i]));
 
       /* release variable expression */
-      SCIP_CALL( SCIPreleaseConsExprExpr(scip, &varexprs[i]) );
+      SCIP_CALL( SCIPreleaseExpr(scip, &varexprs[i]) );
    }
 }
 
 Test(getvars, expression_containing_all_vars)
 {
-   SCIP_CONSEXPR_EXPR* varexprs[4];
-   SCIP_CONSEXPR_EXPR* wexpr;
-   SCIP_CONSEXPR_EXPR* sumexpr;
+   SCIP_EXPR* varexprs[4];
+   SCIP_EXPR* wexpr;
+   SCIP_EXPR* sumexpr;
    int nvarexprs;
    int i;
    
-   SCIP_CALL( SCIPcreateConsExprExprSum(scip, conshdlr, &sumexpr, 0, NULL, NULL, 0) );
-   SCIP_CALL( SCIPcreateConsExprExprVar(scip, conshdlr, &wexpr, w) );
-   SCIP_CALL( SCIPappendConsExprExprSumExpr(scip, sumexpr, wexpr, 1.0) );
-   SCIP_CALL( SCIPappendConsExprExprSumExpr(scip, sumexpr, expr, 1.0) );
+   SCIP_CALL( SCIPcreateExprSum(scip, &sumexpr, 0, NULL, NULL, 0, NULL, NULL) );
+   SCIP_CALL( SCIPcreateExprVar(scip, &wexpr, w, NULL, NULL) );
+   SCIP_CALL( SCIPappendExprSumExpr(scip, sumexpr, wexpr, 1.0) );
+   SCIP_CALL( SCIPappendExprSumExpr(scip, sumexpr, expr, 1.0) );
 
    /* note that this captures the variable expressions */
-   SCIP_CALL( SCIPgetConsExprExprVarExprs(scip, conshdlr, sumexpr, varexprs, &nvarexprs) );
+   SCIP_CALL( SCIPgetExprVarExprs(scip, sumexpr, varexprs, &nvarexprs) );
    cr_assert_eq(nvarexprs, 4, "Expecting 4, got %d\n", nvarexprs);
 
    for( i = 0; i < nvarexprs; ++i )
    {
       cr_assert_not_null(varexprs[i]);
-      cr_assert_str_eq(SCIPgetConsExprExprHdlrName(SCIPgetConsExprExprHdlr(varexprs[i])), "var");
+      cr_assert(SCIPisExprVar(scip, varexprs[i]));
 
       /* release variable expression */
-      SCIP_CALL( SCIPreleaseConsExprExpr(scip, &varexprs[i]) );
+      SCIP_CALL( SCIPreleaseExpr(scip, &varexprs[i]) );
    }
 
-   SCIP_CALL( SCIPreleaseConsExprExpr(scip, &sumexpr) );
-   SCIP_CALL( SCIPreleaseConsExprExpr(scip, &wexpr) );
+   SCIP_CALL( SCIPreleaseExpr(scip, &sumexpr) );
+   SCIP_CALL( SCIPreleaseExpr(scip, &wexpr) );
 }
