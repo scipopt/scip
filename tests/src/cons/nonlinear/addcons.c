112a44a0e58ad472db1dd0634e603ab8c3aead4f
Stefan Vigerske <svigerske@gams.com>
2020-12-03 18:44:57 +0100

    fix addcons test and reenable more checks
    - add cons before solve so that var doesn't get fixed to 0 and then we get
      a cutoff from constructLP

--- tests/src/cons/nonlinear/addcons.c
+++ tests/src/cons/nonlinear/addcons.c
@@ -62,12 +62,11 @@ void teardown(void)
 Test(addcons, nonchecked, .init = setup, .fini = teardown)
 {
    SCIP_CONS* cons;
+   SCIP_CONS* t_cons;
    SCIP_EXPR* expr;
-   const char* input = "[nonlinear] <c1>: <t_x> + (<t_x>) == 2";
+   const char* input = "[nonlinear] <c1>: <x> + (<x>) == 2";
    SCIP_Bool success;
    SCIP_Bool cutoff;
-
-   SCIP_CALL( TESTscipSetStage(scip, SCIP_STAGE_SOLVING, FALSE) );
 
    /* create constraint from input string */
    success = FALSE;
@@ -82,12 +81,16 @@ Test(addcons, nonchecked, .init = setup, .fini = teardown)
 
    /* add constraint */
    SCIP_CALL( SCIPaddCons(scip, cons) );
-   expr = SCIPgetExprConsNonlinear(cons);
+
+   SCIP_CALL( TESTscipSetStage(scip, SCIP_STAGE_SOLVING, FALSE) );
+
+   SCIP_CALL( SCIPgetTransformedCons(scip, cons, &t_cons) );
+   expr = SCIPgetExprConsNonlinear(t_cons);
    cr_assert(expr != NULL);
 
    /* check locks */
-//FIXME   cr_expect(SCIPgetExprNLocksNegNonlinear(expr) == 1);
-//FIXME   cr_expect(SCIPgetExprNLocksPosNonlinear(expr) == 1);
+   cr_expect(SCIPgetExprNLocksNegNonlinear(expr) == 1);
+   cr_expect(SCIPgetExprNLocksPosNonlinear(expr) == 1);
 
    /* expression should have been simplified in SCIP_DECL_CONSACTIVE */
    cr_expect(SCIPisExprSum(scip, expr) );
@@ -98,7 +101,7 @@ Test(addcons, nonchecked, .init = setup, .fini = teardown)
    cr_expect(!cutoff);
 
    /* check whether expression has been detected by at least one nonlinear handler (happens now already in addCons during solve */
-//FIXME   cr_expect(SCIPgetExprAuxVarNonlinear(expr) != NULL);
+   cr_expect(SCIPgetExprAuxVarNonlinear(expr) != NULL);
 
    /* release the constraint */
    SCIP_CALL( SCIPreleaseCons(scip, &cons) );
