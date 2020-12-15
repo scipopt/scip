d75e95bebcc4faf8f66f95b9c6f6fef313ec8f0b
Felipe Serrano <serrano@zib.de>
2020-12-15 17:25:02 +0100

    include quad nlhdlr in scipdefplugins

--- src/scip/scipdefplugins.h
+++ src/scip/scipdefplugins.h
@@ -148,6 +148,7 @@
 #include "scip/nlhdlr_bilinear.h"
 #include "scip/nlhdlr_convex.h"
 #include "scip/nlhdlr_default.h"
+#include "scip/nlhdlr_quadratic.h"
 #include "scip/nodesel_bfs.h"
 #include "scip/nodesel_breadthfirst.h"
 #include "scip/nodesel_dfs.h"
