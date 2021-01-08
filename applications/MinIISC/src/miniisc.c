83744ea480124f37082b1c0e609777257b8a9d51
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2021-01-08 19:41:49 +0100

    turn on reoptimization to cover this in CI tests

--- applications/MinIISC/src/miniisc.c
+++ applications/MinIISC/src/miniisc.c
@@ -28,7 +28,7 @@
 /* default parameters */
 #define DEFAULT_SOLVEMASTERAPPROX      FALSE      /**< Solve master problem approximately? */
 #define DEFAULT_MASTERGAPLIMIT         0.1        /**< gap bound for approximately solving the master problem */
-#define DEFAULT_REOPTIMIZATION         FALSE      /**< Use reoptimization to solve master problem? */
+#define DEFAULT_REOPTIMIZATION         TRUE       /**< Use reoptimization to solve master problem? */
 #define DEFAULT_MASTERSTALLNODES       5000L      /**< stall nodes for the master problem */
 
 /** data needed for cut generation */
