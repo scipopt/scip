34745f6463a36341a0b8c408d6b9c66081fabd62
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2021-01-16 17:02:38 +0100

    add assert for testing lowerbound <= primal bound

--- src/scip/primal.c
+++ src/scip/primal.c
@@ -39,6 +39,7 @@
 #include "scip/struct_event.h"
 #include "scip/pub_message.h"
 #include "scip/pub_var.h"
+#include "scip/scip_solvingstats.h"
 
 
 /*
@@ -656,6 +657,9 @@ SCIP_RETCODE primalAddSol(
 
    SCIPsetDebugMsg(set, "insert primal solution %p with obj %g at position %d (replace=%u):\n",
       (void*)sol, obj, insertpos, replace);
+
+   /* make sure that the primal bound is at least the lower bound */
+   assert( SCIPsetIsInfinity(set, obj) || SCIPsetIsInfinity(set, -SCIPgetLowerbound(set->scip)) || SCIPsetIsFeasLE(set, SCIPgetLowerbound(set->scip), obj) );
 
    SCIPdebug( SCIP_CALL( SCIPsolPrint(sol, set, messagehdlr, stat, transprob, NULL, NULL, FALSE, FALSE) ) );
 
