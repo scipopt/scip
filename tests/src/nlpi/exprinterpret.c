b0db40e1d0c4e938f2b9dee7181be5af21dce263
Stefan Vigerske <svigerske@gams.com>
2021-02-02 10:35:06 +0100

    check several points with same expr2 and exprint compile
    - so we can see that we get correct values for the 2nd, 3rd, etc point
      when the tape from the 1st point is reused

--- tests/src/nlpi/exprinterpret.c
+++ tests/src/nlpi/exprinterpret.c
@@ -31,7 +31,7 @@ static SCIP_SOL* sol;
 static SCIP_VAR* vars[nvars];
 static SCIP_EXPR* varexprs[nvars];
 static SCIP_EXPR* varidxexprs[nvars];
-static SCIP_Real varvals[nvars];
+static SCIP_Real varvals[10][nvars];
 static unsigned int soltag = 0L;
 
 #define TOL 1e-12
@@ -57,7 +57,8 @@ void setup(void)
       SCIP_CALL_ABORT( SCIPcreateExprVar(scip, &varexprs[i], vars[i], NULL, NULL) );
       SCIP_CALL_ABORT( SCIPcreateExprVaridx(scip, &varidxexprs[i], i, NULL, NULL) );
    }
-   BMSclearMemoryArray(varvals, nvars);
+   for( i = 0; i < 10; ++i )
+      BMSclearMemoryArray(varvals[i], nvars);
 
    SCIP_CALL( SCIPexprintCreate(scip, &exprint) );
    SCIP_CALL( SCIPcreateSol(scip, &sol, NULL) );
@@ -103,12 +104,13 @@ SCIP_DECL_EXPR_MAPEXPR(mapvar)
  *
  * Evaluates expression, derivative, and Hessian-vector products using expression
  * callbacks and expression interpreter and check that values are the same.
- * Uses varvals as point where to evaluate.
+ * Uses varvals[i], i<=npoints, as points where to evaluate.
  */
 static
 void checkAD(
    SCIP_EXPR*            expr,               /**< expression (in SCIP vars) to be checked */
-   int                   dim                 /**< number of variables used in expression */
+   int                   dim,                /**< number of variables used in expression */
+   int                   npoints             /**< number of points to check */
    )
 {
    SCIP_EXPRINTDATA* exprintdata = NULL;
@@ -123,13 +125,7 @@ void checkAD(
    SCIP_Bool havehess;
    int hesnnz;
    int i, j;
-
-   SCIPinfoMessage(scip, NULL, "checking expression ");
-   SCIPprintExpr(scip, expr, NULL);
-   SCIPinfoMessage(scip, NULL, " at x =");
-   for( i = 0; i < dim; ++i )
-      SCIPinfoMessage(scip, NULL, " %g", varvals[i]);
-   SCIPinfoMessage(scip, NULL, "\n");
+   int p;
 
    /* if exprint cannot eval, then nothing we can check */
    if( !(SCIPexprintGetCapability() & SCIP_EXPRINTCAPABILITY_FUNCVALUE) )
@@ -140,96 +136,104 @@ void checkAD(
    /* let exprint get familiar with expr */
    SCIP_CALL( SCIPexprintCompile(scip, exprint, expr2, &exprintdata) );
 
-   SCIP_CALL( SCIPsetSolVals(scip, sol, nvars, vars, varvals) );
-
-   SCIP_CALL( SCIPevalExpr(scip, expr, sol, ++soltag) );
-   SCIP_CALL( SCIPexprintEval(scip, exprint, expr2, exprintdata, varvals, &val) );
-
-   SCIPinfoMessage(scip, NULL, "  expr value: %g  exprint value: %g\n", SCIPexprGetEvalValue(expr), val);
-   cr_expect(SCIPisFinite(val) == (SCIPexprGetEvalValue(expr) != SCIP_INVALID));
-   if( SCIPisFinite(val) )
-      cr_expect_float_eq(val, SCIPexprGetEvalValue(expr), TOL);
-   else
-      goto TERMINATE;
-
-
-   if( !(SCIPexprintGetCapability() & SCIP_EXPRINTCAPABILITY_GRADIENT) )
-      goto TERMINATE;
-
-   /* evaluate gradient with expr and expr2 and compare */
-   SCIP_CALL( SCIPevalExprGradient(scip, expr, sol, soltag) );
-   SCIP_CALL( SCIPexprintGrad(scip, exprint, expr2, exprintdata, varvals, FALSE, &val, gradient) );
-
-   cr_expect_float_eq(val, SCIPexprGetEvalValue(expr), TOL);
-   for( i = 0; i < dim; ++i )
+   for( p = 0; p < npoints; ++p )
    {
-      SCIPinfoMessage(scip, NULL, "  gradient[%d]: expr: %g  exprint: %g\n", i, SCIPexprGetDerivative(varexprs[i]), gradient[i]);
-      /* this assumes that exprint will set all gradient values to some non-finite number if not differentiable
-       * for this test, this is sufficient at the moment
-       */
-      cr_expect(SCIPisFinite(gradient[i]) == (SCIPexprGetDerivative(expr) != SCIP_INVALID));
-      if( SCIPisFinite(gradient[i]) )
-         cr_expect_float_eq(gradient[i], SCIPexprGetDerivative(varexprs[i]), TOL);
-   }
-
-
-   if( !(SCIPexprintGetCapability() & SCIP_EXPRINTCAPABILITY_HESSIAN) )
-      goto TERMINATE;
-
-   /* evaluate Hessian with expr by dim times Hessian*vector-evaluation */
-   SCIP_CALL( SCIPcreateSol(scip, &hessiandir, NULL) );
-   for( i = 0; i < dim; ++i )
-   {
-      SCIP_CALL( SCIPsetSolVal(scip, hessiandir, vars[i], 1.0) );
-
-      SCIP_CALL( SCIPevalExprHessianDir(scip, expr, sol, soltag, hessiandir) );
-      for( j = 0; j < dim; ++j )
-         hessian[i*dim+j] = SCIPexprGetBardot(expr);
-
-      SCIP_CALL( SCIPsetSolVal(scip, hessiandir, vars[i], 0.0) );
-   }
-   SCIP_CALL( SCIPfreeSol(scip, &hessiandir) );
-
-   havehess = FALSE;
-   SCIPinfoMessage(scip, NULL, "  hessian expr:\n");
-   for( i = 0; i < dim; ++i )
-   {
-      SCIPinfoMessage(scip, NULL, "    ");
-      for( j = 0; j < dim; ++j )
-      {
-         SCIPinfoMessage(scip, NULL, " %10g", hessian[i*dim+j]);
-         if( hessian[i*dim+j] == SCIP_INVALID )
-            havehess = FALSE;
-      }
+      SCIPinfoMessage(scip, NULL, "checking expression ");
+      SCIPprintExpr(scip, expr, NULL);
+      SCIPinfoMessage(scip, NULL, " at x =");
+      for( i = 0; i < dim; ++i )
+         SCIPinfoMessage(scip, NULL, " %g", varvals[p][i]);
       SCIPinfoMessage(scip, NULL, "\n");
+
+      SCIP_CALL( SCIPsetSolVals(scip, sol, nvars, vars, varvals[p]) );
+
+      SCIP_CALL( SCIPevalExpr(scip, expr, sol, ++soltag) );
+      SCIP_CALL( SCIPexprintEval(scip, exprint, expr2, exprintdata, varvals[p], &val) );
+
+      SCIPinfoMessage(scip, NULL, "  expr value: %g  exprint value: %g\n", SCIPexprGetEvalValue(expr), val);
+      cr_expect(SCIPisFinite(val) == (SCIPexprGetEvalValue(expr) != SCIP_INVALID));
+      if( !SCIPisFinite(val) )
+         continue;
+      cr_expect_float_eq(val, SCIPexprGetEvalValue(expr), TOL);
+
+
+      if( !(SCIPexprintGetCapability() & SCIP_EXPRINTCAPABILITY_GRADIENT) )
+         continue;
+
+      /* evaluate gradient with expr and expr2 and compare */
+      SCIP_CALL( SCIPevalExprGradient(scip, expr, sol, soltag) );
+      SCIP_CALL( SCIPexprintGrad(scip, exprint, expr2, exprintdata, varvals[p], FALSE, &val, gradient) );
+
+      cr_expect_float_eq(val, SCIPexprGetEvalValue(expr), TOL);
+      for( i = 0; i < dim; ++i )
+      {
+         SCIPinfoMessage(scip, NULL, "  gradient[%d]: expr: %g  exprint: %g\n", i, SCIPexprGetDerivative(varexprs[i]), gradient[i]);
+         /* this assumes that exprint will set all gradient values to some non-finite number if not differentiable
+          * for this test, this is sufficient at the moment
+          */
+         cr_expect(SCIPisFinite(gradient[i]) == (SCIPexprGetDerivative(expr) != SCIP_INVALID));
+         if( SCIPisFinite(gradient[i]) )
+            cr_expect_float_eq(gradient[i], SCIPexprGetDerivative(varexprs[i]), TOL);
+      }
+
+
+      if( !(SCIPexprintGetCapability() & SCIP_EXPRINTCAPABILITY_HESSIAN) )
+         continue;
+
+      /* evaluate Hessian with expr by dim times Hessian*vector-evaluation */
+      SCIP_CALL( SCIPcreateSol(scip, &hessiandir, NULL) );
+      for( i = 0; i < dim; ++i )
+      {
+         SCIP_CALL( SCIPsetSolVal(scip, hessiandir, vars[i], 1.0) );
+
+         SCIP_CALL( SCIPevalExprHessianDir(scip, expr, sol, soltag, hessiandir) );
+         for( j = 0; j < dim; ++j )
+            hessian[i*dim+j] = SCIPexprGetBardot(expr);
+
+         SCIP_CALL( SCIPsetSolVal(scip, hessiandir, vars[i], 0.0) );
+      }
+      SCIP_CALL( SCIPfreeSol(scip, &hessiandir) );
+
+      havehess = FALSE;
+      SCIPinfoMessage(scip, NULL, "  hessian expr:\n");
+      for( i = 0; i < dim; ++i )
+      {
+         SCIPinfoMessage(scip, NULL, "    ");
+         for( j = 0; j < dim; ++j )
+         {
+            SCIPinfoMessage(scip, NULL, " %10g", hessian[i*dim+j]);
+            if( hessian[i*dim+j] == SCIP_INVALID )
+               havehess = FALSE;
+         }
+         SCIPinfoMessage(scip, NULL, "\n");
+      }
+      /* skip if no Hessian available via expr, could be an unimplemented callback
+       * TODO should have this callback implemented everywhere
+       */
+      if( !havehess )
+         continue;
+
+      /* evaluate Hessian (sparse) with expr2 */
+      SCIP_CALL( SCIPexprintHessian(scip, exprint, expr2, exprintdata, varvals[p], FALSE, &val, &hesrowidx, &hescolidx, &hesvalues, &hesnnz) );
+      SCIPinfoMessage(scip, NULL, "  hessian exprint (%d nz):\n", hesnnz);
+      for( i = 0; i < hesnnz; ++i )
+      {
+         SCIPinfoMessage(scip, NULL, "    (%d,%d) = %g", hesrowidx[i], hescolidx[i], hesvalues[i]);
+         cr_assert(hesrowidx[i] >= 0);
+         cr_assert(hescolidx[i] >= 0);
+         cr_assert(hesrowidx[i] < dim);
+         cr_assert(hescolidx[i] < dim);
+         cr_assert(hescolidx[i] <= hesrowidx[i]);
+
+         /* subtract exprint hessian from expr hessian, so we can check remaining hessian to be 0 */
+         hessian[hesrowidx[i]*dim + hescolidx[i]] -= hesvalues[i];
+         if( hesrowidx[i] != hescolidx[i] )
+            hessian[hescolidx[i]*dim + hesrowidx[i]] -= hesvalues[i];
+      }
+      for( i = 0; i < dim*dim; ++i )
+         cr_expect_float_eq(hessian[i], 0.0, TOL);
    }
-   /* skip if no Hessian available via expr, could be an unimplemented callback
-    * TODO should have this callback implemented everywhere
-    */
-   if( !havehess )
-      goto TERMINATE;
 
-   /* evaluate Hessian (sparse) with expr2 */
-   SCIP_CALL( SCIPexprintHessian(scip, exprint, expr2, exprintdata, varvals, FALSE, &val, &hesrowidx, &hescolidx, &hesvalues, &hesnnz) );
-   SCIPinfoMessage(scip, NULL, "  hessian exprint (%d nz):\n", hesnnz);
-   for( i = 0; i < hesnnz; ++i )
-   {
-      SCIPinfoMessage(scip, NULL, "    (%d,%d) = %g", hesrowidx[i], hescolidx[i], hesvalues[i]);
-      cr_assert(hesrowidx[i] >= 0);
-      cr_assert(hescolidx[i] >= 0);
-      cr_assert(hesrowidx[i] < dim);
-      cr_assert(hescolidx[i] < dim);
-      cr_assert(hescolidx[i] <= hesrowidx[i]);
-
-      /* subtract exprint hessian from expr hessian, so we can check remaining hessian to be 0 */
-      hessian[hesrowidx[i]*dim + hescolidx[i]] -= hesvalues[i];
-      if( hesrowidx[i] != hescolidx[i] )
-         hessian[hescolidx[i]*dim + hesrowidx[i]] -= hesvalues[i];
-   }
-   for( i = 0; i < dim*dim; ++i )
-      cr_expect_float_eq(hessian[i], 0.0, TOL);
-
-TERMINATE:
    SCIP_CALL( SCIPexprintFreeData(scip, exprint, expr2, &exprintdata) );
    SCIP_CALL( SCIPreleaseExpr(scip, &expr2) );
 }
@@ -242,11 +246,9 @@ Test(exprint, abs)
 
    SCIP_CALL( SCIPcreateExprAbs(scip, &expr, varexprs[0], NULL, NULL) );
 
-   varvals[0] = 2.0;
-   checkAD(expr, 1);
-
-   varvals[0] = -3.0;
-   checkAD(expr, 1);
+   varvals[0][0] = 2.0;
+   varvals[1][0] = -3.0;
+   checkAD(expr, 1, 2);
 
    SCIP_CALL( SCIPreleaseExpr(scip, &expr) );
 }
@@ -257,11 +259,9 @@ Test(exprint, cos)
 
    SCIP_CALL( SCIPcreateExprCos(scip, &expr, varexprs[0], NULL, NULL) );
 
-   varvals[0] = 0.0;
-   checkAD(expr, 1);
-
-   varvals[0] = 1.0;
-   checkAD(expr, 1);
+   varvals[0][0] = 0.0;
+   varvals[1][0] = 1.0;
+   checkAD(expr, 1, 2);
 
    SCIP_CALL( SCIPreleaseExpr(scip, &expr) );
 }
@@ -272,14 +272,10 @@ Test(exprint, entropy)
 
    SCIP_CALL( SCIPcreateExprEntropy(scip, &expr, varexprs[0], NULL, NULL) );
 
-   varvals[0] = 0.0;
-   checkAD(expr, 1);
-
-   varvals[0] = 1.0;
-   checkAD(expr, 1);
-
-   varvals[0] = 2.0;
-   checkAD(expr, 1);
+   varvals[0][0] = 1.0;
+   varvals[1][0] = 0.0;
+   varvals[2][0] = 2.0;
+   checkAD(expr, 1, 3);
 
    SCIP_CALL( SCIPreleaseExpr(scip, &expr) );
 }
@@ -290,11 +286,9 @@ Test(exprint, exp)
 
    SCIP_CALL( SCIPcreateExprExp(scip, &expr, varexprs[0], NULL, NULL) );
 
-   varvals[0] = -1.0;
-   checkAD(expr, 1);
-
-   varvals[0] = 1.0;
-   checkAD(expr, 1);
+   varvals[0][0] = -1.0;
+   varvals[1][0] = 1.0;
+   checkAD(expr, 1, 2);
 
    SCIP_CALL( SCIPreleaseExpr(scip, &expr) );
 }
@@ -305,14 +299,10 @@ Test(exprint, log)
 
    SCIP_CALL( SCIPcreateExprLog(scip, &expr, varexprs[0], NULL, NULL) );
 
-   varvals[0] = 0.0;
-   checkAD(expr, 1);
-
-   varvals[0] = 0.1;
-   checkAD(expr, 1);
-
-   varvals[0] = 1.0;
-   checkAD(expr, 1);
+   varvals[0][0] = 0.0;
+   varvals[1][0] = 0.1;
+   varvals[2][0] = 1.0;
+   checkAD(expr, 1, 3);
 
    SCIP_CALL( SCIPreleaseExpr(scip, &expr) );
 }
@@ -327,21 +317,13 @@ Test(exprint, pow)
    {
       SCIP_CALL( SCIPcreateExprPow(scip, &expr, varexprs[0], exponents[i], NULL, NULL) );
 
-      varvals[0] = 1.0;
-      checkAD(expr, 1);
-
-      varvals[0] = 2.0;
-      checkAD(expr, 1);
+      varvals[0][0] = 1.0;
+      varvals[1][0] = 2.0;
+      varvals[2][0] = -3.0;
+      varvals[3][0] = 0.0;
 
       // FIXME CppAD claims that x^1.875 is non-diff at x=0 because it treats this as exp(log(x)*1.875), see https://github.com/coin-or/CppAD/issues/92
-      if( EPSISINT(exponents[i], 0.0) )
-      {
-         varvals[0] = 0.0;
-         checkAD(expr, 1);
-      }
-
-      varvals[0] = -3.0;
-      checkAD(expr, 1);
+      checkAD(expr, 1, EPSISINT(exponents[i], 0.0) ? 4 : 3);
 
       SCIP_CALL( SCIPreleaseExpr(scip, &expr) );
    }
@@ -357,14 +339,10 @@ Test(exprint, signpow)
    {
       SCIP_CALL( SCIPcreateExprSignpower(scip, &expr, varexprs[0], exponents[i], NULL, NULL) );
 
-      varvals[0] = -3.0;
-      checkAD(expr, 1);
-
-      varvals[0] = 0.0;
-      checkAD(expr, 1);
-
-      varvals[0] = 5.0;
-      checkAD(expr, 1);
+      varvals[0][0] = -3.0;
+      varvals[1][0] = 0.0;
+      varvals[2][0] = 5.0;
+      checkAD(expr, 1, 3);
 
       SCIP_CALL( SCIPreleaseExpr(scip, &expr) );
    }
@@ -376,21 +354,19 @@ Test(exprint, product)
 
    SCIP_CALL( SCIPcreateExprProduct(scip, &expr, 2, varexprs, 5.0, NULL, NULL) );
 
-   varvals[0] = 0.0;
-   varvals[1] = 0.0;
-   checkAD(expr, 2);
+   varvals[0][0] = 0.0;
+   varvals[0][1] = 0.0;
 
-   varvals[0] = 0.0;
-   varvals[1] = 1.0;
-   checkAD(expr, 2);
+   varvals[1][0] = 0.0;
+   varvals[1][1] = 1.0;
 
-   varvals[0] = 3.0;
-   varvals[1] = 0.0;
-   checkAD(expr, 2);
+   varvals[2][0] = 3.0;
+   varvals[2][1] = 0.0;
 
-   varvals[0] = 2.0;
-   varvals[1] = -4.0;
-   checkAD(expr, 2);
+   varvals[3][0] = 2.0;
+   varvals[3][1] = -4.0;
+
+   checkAD(expr, 2, 4);
 
    SCIP_CALL( SCIPreleaseExpr(scip, &expr) );
 }
@@ -401,11 +377,9 @@ Test(exprint, sin)
 
    SCIP_CALL( SCIPcreateExprSin(scip, &expr, varexprs[0], NULL, NULL) );
 
-   varvals[0] = 0.0;
-   checkAD(expr, 1);
-
-   varvals[0] = -1.0;
-   checkAD(expr, 1);
+   varvals[0][0] = 0.0;
+   varvals[1][0] = -1.0;
+   checkAD(expr, 1, 2);
 
    SCIP_CALL( SCIPreleaseExpr(scip, &expr) );
 }
@@ -417,11 +391,11 @@ Test(exprint, sum)
 
    SCIP_CALL( SCIPcreateExprSum(scip, &expr, 5, varexprs, coefs, 42.0, NULL, NULL) );
 
-   varvals[0] = 1.0;
-   varvals[1] = -1.0;
-   varvals[2] = 2.0;
-   varvals[3] = -2.0;
-   checkAD(expr, 1);
+   varvals[0][0] = 1.0;
+   varvals[0][1] = -1.0;
+   varvals[0][2] = 2.0;
+   varvals[0][3] = -2.0;
+   checkAD(expr, 1, 1);
 
    SCIP_CALL( SCIPreleaseExpr(scip, &expr) );
 }
@@ -432,7 +406,7 @@ Test(exprint, value)
 
    SCIP_CALL( SCIPcreateExprValue(scip, &expr, 42.0, NULL, NULL) );
 
-   checkAD(expr, 0);
+   checkAD(expr, 0, 1);
 
    SCIP_CALL( SCIPreleaseExpr(scip, &expr) );
 }
