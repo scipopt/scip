4d9dc1169f8e885ff97027f2e2b43ce9d904264f
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2021-02-14 13:52:29 +0100

    fix pclint warnings

--- src/scip/var.c
+++ src/scip/var.c
@@ -12186,7 +12186,7 @@ SCIP_RETCODE SCIPvarGetProbvarBinary(
                break;
             }
          }
-         active = TRUE;
+         active = TRUE;  /*lint !e838*/
          break;
 
       case SCIP_VARSTATUS_AGGREGATED:  /* x = a'*x' + c'  =>  a*x + c == (a*a')*x' + (a*c' + c) */
