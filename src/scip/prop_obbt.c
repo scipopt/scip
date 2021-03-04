e48a331258b2f312fb4ad5d896e82809c54bcd8e
Stefan Vigerske <svigerske@gams.com>
2021-03-04 12:22:40 +0100

    increase tolerance on assert again
    - see comment regarding rsyn0805h.osil in #2953

--- src/scip/prop_obbt.c
+++ src/scip/prop_obbt.c
@@ -693,7 +693,7 @@ SCIP_RETCODE createGenVBound(
                activity += genvboundcoefs[k] * SCIPvarGetLPSol(genvboundvars[k]);
 
             SCIPdebugMsg(scip, "LVB activity = %g lpobj = %g\n", activity, SCIPgetLPObjval(scip));
-            assert(EPSZ(SCIPrelDiff(activity, SCIPgetLPObjval(scip)), 11.0 * SCIPdualfeastol(scip)));
+            assert(EPSZ(SCIPrelDiff(activity, SCIPgetLPObjval(scip)), 18.0 * SCIPdualfeastol(scip)));
 #endif
 
             SCIPdebugMsg(scip, "         adding genvbound\n");
