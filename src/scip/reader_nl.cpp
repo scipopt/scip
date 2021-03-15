09bcb154c94bf3f14ff794c8037fa015bf327556
Stefan Vigerske <svigerske@gams.com>
2021-03-15 16:20:20 +0100

    fix variable types
    - nlvars in cons/obj includes nlvars in both cons and obj

--- src/scip/reader_nl.cpp
+++ src/scip/reader_nl.cpp
@@ -182,15 +182,15 @@ public:
             vartype = SCIP_VARTYPE_CONTINUOUS;
          else if( i < h.num_nl_vars_in_both )
             vartype = SCIP_VARTYPE_INTEGER;
-         // Nonlinear variables in constraints
-         else if( i - h.num_nl_vars_in_both < h.num_nl_vars_in_cons - h.num_nl_integer_vars_in_cons )
+         // Nonlinear variables in constraints (num_nl_vars_in_cons includes num_nl_vars_in_both)
+         else if( i < h.num_nl_vars_in_cons - h.num_nl_integer_vars_in_cons )
             vartype = SCIP_VARTYPE_CONTINUOUS;
-         else if( i - h.num_nl_vars_in_both < h.num_nl_vars_in_cons )
+         else if( i < h.num_nl_vars_in_cons )
             vartype = SCIP_VARTYPE_INTEGER;
-         // Nonlinear variables in objective
-         else if( i - h.num_nl_vars_in_both - h.num_nl_vars_in_cons < h.num_nl_vars_in_objs - h.num_nl_integer_vars_in_objs )
+         // Nonlinear variables in objective (both num_nl_vars_in_cons and num_nl_vars_in_objs includes num_nl_vars_in_both, so subtract once)
+         else if( i - h.num_nl_vars_in_cons < h.num_nl_vars_in_objs - h.num_nl_vars_in_both - h.num_nl_integer_vars_in_objs )
             vartype = SCIP_VARTYPE_CONTINUOUS;
-         else if( i - h.num_nl_vars_in_both - h.num_nl_vars_in_cons < h.num_nl_vars_in_objs )
+         else if( i - h.num_nl_vars_in_cons < h.num_nl_vars_in_objs - h.num_nl_vars_in_both )
             vartype = SCIP_VARTYPE_INTEGER;
          // Linear variables
          else if( i < h.num_vars - h.num_linear_binary_vars - h.num_linear_integer_vars )
