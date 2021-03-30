05661fae57b411e93a312aaca78acec01ff7ceba
Katrin Halbig <katrin.halbig@fau.de>
2021-03-30 17:07:18 +0200

    add new heuristic indicatordiving

---
+++ src/scip/heur_indicatordiving.c
@@ -0,0 +1,188 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the program and library             */
+/*         SCIP --- Solving Constraint Integer Programs                      */
+/*                                                                           */
+/*    Copyright (C) 2002-2020 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SCIP is distributed under the terms of the ZIB Academic License.         */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SCIP; see the file COPYING. If not visit scipopt.org.         */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**@file   heur_indicatordiving.c
+ * @ingroup DEFPLUGINS_HEUR
+ * @brief  indicator diving heuristic
+ * @author Katrin Halbig
+ * @author Alexander Hoen
+ */
+
+/*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
+
+#include <assert.h>
+
+#include "scip/heur_indicatordiving.h"
+#include "scip/heuristics.h"
+#include "scip/pub_heur.h"
+#include "scip/pub_message.h"
+#include "scip/scip_heur.h"
+#include "scip/scip_mem.h"
+#include "scip/scip_numerics.h"
+#include "scip/scip_sol.h"
+#include <string.h>
+
+#define HEUR_NAME             "indicatordiving"
+#define HEUR_DESC             "indicator diving heuristic"
+#define HEUR_DISPCHAR         '?'
+#define HEUR_PRIORITY         0
+#define HEUR_FREQ             1
+#define HEUR_FREQOFS          0
+#define HEUR_MAXDEPTH         -1
+#define HEUR_TIMING           SCIP_HEURTIMING_AFTERNODE
+#define HEUR_USESSUBSCIP      FALSE  /**< does the heuristic use a secondary SCIP instance? */
+
+
+/*
+ * Data structures
+ */
+
+/* TODO: fill in the necessary primal heuristic data */
+
+/** primal heuristic data */
+struct SCIP_HeurData
+{
+};
+
+
+/*
+ * Local methods
+ */
+
+/* put your local methods here, and declare them static */
+
+
+/*
+ * Callback methods
+ */
+
+/** copy method for primal heuristic plugins (called when SCIP copies plugins) */
+static
+SCIP_DECL_HEURCOPY(heurCopyIndicatordiving)
+{  /*lint --e{715}*/
+   assert(scip != NULL);
+   assert(heur != NULL);
+   assert(strcmp(SCIPheurGetName(heur), HEUR_NAME) == 0);
+
+   /* call inclusion method of primal heuristic */
+   SCIP_CALL( SCIPincludeHeurIndicatordiving(scip) );
+
+   return SCIP_OKAY;
+}
+
+
+/** destructor of primal heuristic to free user data (called when SCIP is exiting) */
+static
+SCIP_DECL_HEURFREE(heurFreeIndicatordiving) /*lint --e{715}*/
+{  /*lint --e{715}*/
+   SCIP_HEURDATA* heurdata;
+
+   assert(heur != NULL);
+   assert(strcmp(SCIPheurGetName(heur), HEUR_NAME) == 0);
+   assert(scip != NULL);
+
+   /* free heuristic data */
+   heurdata = SCIPheurGetData(heur);
+   assert(heurdata != NULL);
+   SCIPfreeBlockMemory(scip, &heurdata);
+   SCIPheurSetData(heur, NULL);
+
+   return SCIP_OKAY;
+}
+
+
+/** initialization method of primal heuristic (called after problem was transformed) */
+static
+SCIP_DECL_HEURINIT(heurInitIndicatordiving) /*lint --e{715}*/
+{  /*lint --e{715}*/
+   SCIP_HEURDATA* heurdata;
+
+   assert(heur != NULL);
+   assert(strcmp(SCIPheurGetName(heur), HEUR_NAME) == 0);
+   assert(scip != NULL);
+
+   /* get heuristic data */
+   heurdata = SCIPheurGetData(heur);
+   assert(heurdata != NULL);
+
+   return SCIP_OKAY;
+}
+
+
+/** deinitialization method of primal heuristic (called before transformed problem is freed) */
+static
+SCIP_DECL_HEUREXIT(heurExitIndicatordiving) /*lint --e{715}*/
+{  /*lint --e{715}*/
+   SCIP_HEURDATA* heurdata;
+
+   assert(heur != NULL);
+   assert(strcmp(SCIPheurGetName(heur), HEUR_NAME) == 0);
+   assert(scip != NULL);
+
+   /* get heuristic data */
+   heurdata = SCIPheurGetData(heur);
+   assert(heurdata != NULL);
+
+   return SCIP_OKAY;
+}
+
+
+/** execution method of primal heuristic */
+static
+SCIP_DECL_HEUREXEC(heurExecIndicatordiving)
+{  /*lint --e{715}*/
+   SCIPerrorMessage("method of xyz primal heuristic not implemented yet\n");
+   SCIPABORT(); /*lint --e{527}*/
+
+   return SCIP_OKAY;
+}
+
+
+/*
+ * heuristic specific interface methods
+ */
+
+/** creates the indicatordiving heuristic and includes it in SCIP */
+SCIP_RETCODE SCIPincludeHeurIndicatordiving(
+   SCIP*                 scip                /**< SCIP data structure */
+   )
+{
+   SCIP_HEURDATA* heurdata;
+   SCIP_HEUR* heur;
+
+   /* create indicatordiving primal heuristic data */
+   heurdata = NULL;
+
+   heur = NULL;
+
+
+   /* include primal heuristic */
+   SCIP_CALL( SCIPincludeHeurBasic(scip, &heur,
+         HEUR_NAME, HEUR_DESC, HEUR_DISPCHAR, HEUR_PRIORITY, HEUR_FREQ, HEUR_FREQOFS,
+         HEUR_MAXDEPTH, HEUR_TIMING, HEUR_USESSUBSCIP, heurExecIndicatordiving, heurdata) );
+
+   assert(heur != NULL);
+
+   /* set non fundamental callbacks via setter functions */
+   SCIP_CALL( SCIPsetHeurCopy(scip, heur, heurCopyIndicatordiving) );
+   SCIP_CALL( SCIPsetHeurFree(scip, heur, heurFreeIndicatordiving) );
+   SCIP_CALL( SCIPsetHeurInit(scip, heur, heurInitIndicatordiving) );
+   SCIP_CALL( SCIPsetHeurExit(scip, heur, heurExitIndicatordiving) );
+
+   /* add xyz primal heuristic parameters */
+   /* TODO: (optional) add primal heuristic specific parameters with SCIPaddTypeParam() here */
+
+   return SCIP_OKAY;
+}
