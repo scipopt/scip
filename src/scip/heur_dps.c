56718d9fdd97acbe341007bb9a2be0f201906452
Katrin Halbig <katrin.halbig@fau.de>
2021-09-08 19:53:12 +0200

    add some structure

--- src/scip/heur_dps.c
+++ src/scip/heur_dps.c
@@ -28,8 +28,10 @@
 #include "scip/pub_dcmp.h"
 #include "scip/pub_heur.h"
 #include "scip/pub_misc.h"
+#include "scip/scipdefplugins.h"
 #include "scip/scip_cons.h"
 #include "scip/scip_dcmp.h"
+#include "scip/scip_general.h"
 #include "scip/scip_heur.h"
 #include "scip/scip_mem.h"
 #include "scip/scip_message.h"
@@ -55,9 +57,25 @@
 /** primal heuristic data */
 struct SCIP_HeurData
 {
+   SCIP_CONS**           linkingconss;       /**< linking constraints */
+   int                   nlinking;           /**< number of linking constraints */
    int                   nblocks;            /**< number of blocks */
 };
 
+struct Blockproblem
+{
+   SCIP*                 blockscip;          /**< SCIP data structure */
+   SCIP_VAR**            blockvars;          /**< block variables
+                                              *   blockvars[nblockvars] is first slack variable */
+   SCIP_VAR**            slackvars;          /**< slack variables */
+   SCIP_CONS**           linkingconss;       /**< linking constraints */
+   int*                  linkingindices;     /**< indices of linking constraints in original problem */
+   int                   nlinking;           /**< number of linking constraints */
+   int                   nblockvars;         /**< number of block variables */
+   int                   nslackvars;         /**< number of slack variables */
+   SCIP_Real*            origobj;            /**< original objective coefficients */
+};
+typedef struct Blockproblem BLOCKPROBLEM;
 
 /*
  * Local methods
@@ -151,6 +169,53 @@ SCIP_RETCODE assignLinking(
    return SCIP_OKAY;
 }
 
+/** creates a sub-SCIP and sets parameters */
+static
+SCIP_RETCODE createSubscip(
+    SCIP*                scip,               /**< main SCIP data structure */
+    SCIP**               subscip             /**< pointer to store created sub-SCIP */
+   )
+{
+   assert(scip != NULL);
+   assert(subscip != NULL);
+
+   /* create a new SCIP instance */
+   SCIP_CALL( SCIPcreate(subscip) );
+   SCIP_CALL( SCIPincludeDefaultPlugins(*subscip) );
+
+   SCIP_CALL( SCIPcopyLimits(scip, *subscip) );
+
+   /* avoid recursive calls */
+   SCIP_CALL( SCIPsetSubscipsOff(*subscip, TRUE) );
+
+   /* disable cutting plane separation */
+   SCIP_CALL( SCIPsetSeparating(*subscip, SCIP_PARAMSETTING_OFF, TRUE) );
+
+   /* disable expensive presolving */
+   SCIP_CALL( SCIPsetPresolving(*subscip, SCIP_PARAMSETTING_FAST, TRUE) );
+
+   /* disable expensive techniques */
+   SCIP_CALL( SCIPsetIntParam(*subscip, "misc/usesymmetry", 0) );
+
+   /* do not abort subproblem on CTRL-C */
+   SCIP_CALL( SCIPsetBoolParam(*subscip, "misc/catchctrlc", FALSE) );
+
+#ifdef SCIP_DEBUG
+   /* for debugging, enable full output */
+   SCIP_CALL( SCIPsetIntParam(*subscip, "display/verblevel", 5) );
+   SCIP_CALL( SCIPsetIntParam(*subscip, "display/freq", 100000000) );
+#else
+   /* disable statistic timing inside sub SCIP and output to console */
+   SCIP_CALL( SCIPsetIntParam(*subscip, "display/verblevel", 0) );
+   SCIP_CALL( SCIPsetBoolParam(*subscip, "timing/statistictiming", FALSE) );
+#endif
+
+   /* speed up sub-SCIP by not checking dual LP feasibility */
+   SCIP_CALL( SCIPsetBoolParam(*subscip, "lp/checkdualfeas", FALSE) );
+
+   return SCIP_OKAY;
+}
+
 
 /*
  * Callback methods of primal heuristic
@@ -203,12 +268,15 @@ SCIP_DECL_HEUREXEC(heurExecDps)
    SCIP_VAR** sortedvars;
    SCIP_CONS** sortedconss;
    SCIP_HEURDATA* heurdata;
+   BLOCKPROBLEM** blockproblem;
    int* sortedvarlabels;
    int* sortedconslabels;
    SCIP_Real memory; /* in MB */
    int ndecomps;
    int nvars;
    int nconss;
+   int nblocks;
+   int b;
 
    assert( heur != NULL );
    assert( scip != NULL );
@@ -218,6 +286,7 @@ SCIP_DECL_HEUREXEC(heurExecDps)
    assert(heurdata != NULL);
 
    assigneddecomp = NULL;
+   blockproblem = NULL;
 
    *result = SCIP_DIDNOTRUN;
 
@@ -233,12 +302,12 @@ SCIP_DECL_HEUREXEC(heurExecDps)
    assert(decomp != NULL);
    SCIPdebugMsg(scip, "First transformed decomposition is selected\n");
 
-   heurdata->nblocks = SCIPdecompGetNBlocks(decomp);
+   nblocks = SCIPdecompGetNBlocks(decomp);
    nconss = SCIPgetNConss(scip);
    nvars = SCIPgetNVars(scip);
 
    /* if problem has no constraints, no variables or less than two blocks, return */
-   if( nconss == 0 || nvars == 0 || heurdata->nblocks <= 1 )
+   if( nconss == 0 || nvars == 0 || nblocks <= 1 )
    {
       SCIPdebugMsg(scip, "problem has no constraints, no variables or less than two blocks\n");
       return SCIP_OKAY;
@@ -246,9 +315,9 @@ SCIP_DECL_HEUREXEC(heurExecDps)
 
    /* estimate required memory for all blocks and terminate if not enough memory is available */
    SCIP_CALL( SCIPgetRealParam(scip, "limits/memory", &memory) );
-   if( ((SCIPgetMemUsed(scip) + SCIPgetMemExternEstim(scip))/1048576.0) * (heurdata->nblocks/4.0 + 2) >= memory )
+   if( ((SCIPgetMemUsed(scip) + SCIPgetMemExternEstim(scip))/1048576.0) * (nblocks/4.0 + 2) >= memory )
    {
-      SCIPdebugMsg(scip, "The estimated memory usage for %d blocks is too large.\n", heurdata->nblocks);
+      SCIPdebugMsg(scip, "The estimated memory usage for %d blocks is too large.\n", nblocks);
       return SCIP_OKAY;
    }
 
@@ -268,14 +337,14 @@ SCIP_DECL_HEUREXEC(heurExecDps)
    if( sortedvarlabels[0] == SCIP_DECOMP_LINKVAR )
    {
       /* create new decomposition; don't change the decompositions in the decompstore */
-      SCIP_CALL( SCIPdecompCreate(&assigneddecomp, SCIPblkmem(scip), heurdata->nblocks, SCIPdecompIsOriginal(decomp), SCIPdecompUseBendersLabels(decomp)) );
+      SCIP_CALL( SCIPdecompCreate(&assigneddecomp, SCIPblkmem(scip), nblocks, SCIPdecompIsOriginal(decomp), SCIPdecompUseBendersLabels(decomp)) );
 
       SCIP_CALL( assignLinking(scip, assigneddecomp, sortedvars, sortedconss, sortedvarlabels, sortedconslabels, nvars, nconss, SCIPdecompGetNBorderVars(decomp)) );
       assert(SCIPdecompGetNBlocks(decomp) >= SCIPdecompGetNBlocks(assigneddecomp));
       decomp = assigneddecomp;
 
       /* number of blocks can get smaller */
-      heurdata->nblocks = SCIPdecompGetNBlocks(decomp);
+      nblocks = SCIPdecompGetNBlocks(decomp);
    }
 
 #ifdef SCIP_DEBUG
@@ -285,15 +354,51 @@ SCIP_DECL_HEUREXEC(heurExecDps)
 
    if( sortedvarlabels[0] == SCIP_DECOMP_LINKVAR ||
          sortedconslabels[0] != SCIP_DECOMP_LINKCONS ||
-         heurdata->nblocks <= 1 )
+         nblocks <= 1 )
    {
       SCIPdebugMsg(scip, "Problem has linking variables or no linking constraints or less than two blocks\n");
       goto TERMINATE;
    }
 
+   /* initialize heurdata */
+   heurdata->linkingconss = sortedconss;
+   heurdata->nlinking = SCIPdecompGetNBorderConss(decomp);
+   heurdata->nblocks = nblocks;
+
+   /* allocate memory for blockproblems and initialize partially */
+   SCIP_CALL( SCIPallocBufferArray(scip, &blockproblem, nblocks) );
+   for( b = 0; b < nblocks; b++ )
+   {
+      SCIP_CALL( SCIPallocBlockMemory(scip, &blockproblem[b]) );
+      createSubscip(scip, &blockproblem[b]->blockscip);
+
+      SCIP_CALL( SCIPallocBufferArray(scip, &blockproblem[b]->linkingconss, heurdata->nlinking) );
+      SCIP_CALL( SCIPallocBufferArray(scip, &blockproblem[b]->linkingindices, heurdata->nlinking) );
+      SCIP_CALL( SCIPallocBufferArray(scip, &blockproblem[b]->slackvars, heurdata->nlinking * 2) ); /* maximum two slacks per linking constraint */
+      SCIP_CALL( SCIPallocBufferArray(scip, &blockproblem[b]->origobj, nvars) );
+      blockproblem[b]->blockvars = NULL;
+      blockproblem[b]->nblockvars = 0;
+      blockproblem[b]->nlinking = 0;
+      blockproblem[b]->nslackvars = 0;
+   }
+
    /** ------------------------------------------------------------------------ */
    /** free memory */
 TERMINATE:
+   if( blockproblem != NULL )
+   {
+      for( b = nblocks - 1; b >= 0; b-- )
+      {
+         SCIPfreeBufferArray(scip, &(blockproblem[b])->origobj);
+         SCIPfreeBufferArray(scip, &(blockproblem[b])->slackvars);
+         SCIPfreeBufferArray(scip, &(blockproblem[b])->linkingindices);
+         SCIPfreeBufferArray(scip, &(blockproblem[b])->linkingconss);
+         SCIP_CALL( SCIPfree(&blockproblem[b]->blockscip) );
+         SCIPfreeBlockMemory(scip, &blockproblem[b]);
+      }
+      SCIPfreeBufferArray(scip, &blockproblem);
+   }
+
    if( assigneddecomp != NULL )
    {
       SCIPdecompFree(&assigneddecomp, SCIPblkmem(scip));
