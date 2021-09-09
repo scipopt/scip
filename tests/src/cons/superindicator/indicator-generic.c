48df0082e05fa50f1c0034662cbb8f2678548237
Mathieu Besançon <besancon@zib.de>
2021-09-09 18:21:59 +0200

    Bugfix: Generic indicator not registered in the model

--- tests/src/cons/superindicator/indicator-generic.c
+++ tests/src/cons/superindicator/indicator-generic.c
@@ -23,7 +23,38 @@
 #include "scip/scipdefplugins.h"
 #include "include/scip_test.h"
 
+#include <math.h>
+#include <limits.h>
+#include <float.h>
+
+#undef SCIP_CALL
+#define SCIP_CALL(x)   do                                                                 \
+                       {                                                                  \
+                          SCIP_RETCODE _restat_;                                          \
+                          if( (_restat_ = (x)) != SCIP_OKAY )                             \
+                          {                                                               \
+                             cr_assert(FALSE, "Error <%d> in function call\n", _restat_); \
+                          }                                                               \
+                       }                                                                  \
+                       while( FALSE )
+
+/* hardcoded eps equality */
+#undef EPSEQ
+#define EPSEQ(x, y) (fabs((x) - (y)) <= 0.000005)
+
 static SCIP* scip;
+
+static SCIP_VAR* x1;
+static SCIP_VAR* x2;
+static SCIP_VAR* z1;
+static SCIP_VAR* z2;
+static SCIP_CONS* lc1;
+static SCIP_CONS* lc2;
+static SCIP_CONS* ic1;
+static SCIP_CONS* ic2;
+static SCIP_SOL* sol;
+static SCIP_Real xval;
+static SCIP_Real zval;
 
 /* creates scip, problem */
 static
@@ -33,12 +64,13 @@ void setup(void)
    SCIP_CALL( SCIPincludeDefaultPlugins(scip) );
 
    /* create a problem */
-   SCIP_CALL( SCIPcreateProbBasic(scip, "problem") );
+   SCIP_CALL( SCIPcreateProbBasic(scip, "indicatorProblem") );
 }
 
 static
 void teardown(void)
 {
+
    /* free SCIP */
    SCIP_CALL( SCIPfree(&scip) );
 
@@ -46,35 +78,31 @@ void teardown(void)
    cr_assert_eq(BMSgetMemoryUsed(), 0, "There are memory leaks!");
 }
 
-TestSuite(indicatorzero, .init = setup, .fini = teardown);
+TestSuite(indicator, .init = setup, .fini = teardown);
 
-Test(indicatorzero, feasiblezero, .init = setup, .fini = teardown,
-   .description = "check upgrade from quadratic constraint with linear binary variables to SOC"
+Test(indicator, feasiblezero1, .init = setup, .fini = teardown,
+   .description = "MIP with two indicator constraints"
    )
 {
-   SCIP_VAR* x1;
-   SCIP_VAR* x2;
-   SCIP_VAR* z1;
-   SCIP_VAR* z2;
-   SCIP_CONS* lc1;
-   SCIP_CONS* lc2;
-   SCIP_CONS* ic1;
-   SCIP_CONS* ic2;
    /*
-      Unit test based on MathOptInterface.jl
+      Unit test inspired by MathOptInterface.jl
       minimize
         obj: -2 * x1 - 3 * x2
       Subject to
         lc1: x1 + x2 <= 10
-        ic2: z1 == 0 => x2 <= 8
-        ic3: z2 == 1 => x2 + x1/5 <= 9
-        lc2: z1 - z2 <= 0
+        lc2: z1 + z2 >= 1
+        ic1: z1 == 1 => x2 <= 8
+        ic2: z2 == 1 => x2 + x1/5 <= 9
       Binary
         z1 z2
-      End
+      Solution:
+        x: 1.25, 8.75
+        z: 0, 1
+        value: -28.75
    */
-   SCIP_CALL( SCIPcreateVarBasic(scip, &x1, "x1", 0.0, 10.0, -2.0, SCIP_VARTYPE_CONTINUOUS) );
-   SCIP_CALL( SCIPcreateVarBasic(scip, &x2, "x2", 0.0, 10.0, -3.0, SCIP_VARTYPE_CONTINUOUS) );
+
+   SCIP_CALL( SCIPcreateVarBasic(scip, &x1, "x1", -SCIPinfinity(scip), SCIPinfinity(scip), -2.0, SCIP_VARTYPE_CONTINUOUS) );
+   SCIP_CALL( SCIPcreateVarBasic(scip, &x2, "x2", -SCIPinfinity(scip), SCIPinfinity(scip), -3.0, SCIP_VARTYPE_CONTINUOUS) );
    SCIP_CALL( SCIPcreateVarBasic(scip, &z1, "z1", 0.0, 1.0, 0.0, SCIP_VARTYPE_BINARY) );
    SCIP_CALL( SCIPcreateVarBasic(scip, &z2, "z2", 0.0, 1.0, 0.0, SCIP_VARTYPE_BINARY) );
 
@@ -87,51 +115,218 @@ Test(indicatorzero, feasiblezero, .init = setup, .fini = teardown,
    SCIP_CALL( SCIPaddCoefLinear(scip, lc1, x1, 1.0) );
    SCIP_CALL( SCIPaddCoefLinear(scip, lc1, x2, 1.0) );
 
-   SCIP_CALL( SCIPcreateConsBasicLinear(scip, &lc2, "lc2", 0, NULL, NULL, -SCIPinfinity(scip), 0.0) );
+   SCIP_CALL( SCIPaddCons(scip, lc1) );
+
+   SCIP_CALL( SCIPcreateConsBasicLinear(scip, &lc2, "lc2", 0, NULL, NULL, 1.0, SCIPinfinity(scip)) );
    SCIP_CALL( SCIPaddCoefLinear(scip, lc2, z1, 1.0) );
-   SCIP_CALL( SCIPaddCoefLinear(scip, lc2, z2, 2.0) );
+   SCIP_CALL( SCIPaddCoefLinear(scip, lc2, z2, 1.0) );
 
-   SCIP_CALL( SCIPcreateConsIndicatorGeneric(
-       scip,
-       &ic1,
-       "ic1",
-       z1,
-       0, NULL, NULL,
-       8.0,
-       FALSE,
-       TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE) );
-    SCIP_CALL( SCIPaddVarIndicator(
-       scip,
-       ic1,
-       x2,
-       1.0) );
+   SCIP_CALL( SCIPaddCons(scip, lc2) );
 
-   SCIP_CALL( SCIPcreateConsIndicatorGeneric(
-       scip,
-       &ic2,
-       "ic2",
-       z2,
-       0, NULL, NULL,
-       9.0,
-       TRUE,
-       TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE) );
-    SCIP_CALL( SCIPaddVarIndicator(
-       scip,
-       ic2,
-       x2,
-       1.0) );
-    SCIP_CALL( SCIPaddVarIndicator(
-       scip,
-       ic2,
-       x1,
-       1.0 / 5.0) );
+   SCIP_CALL( SCIPcreateConsIndicatorGeneric(scip, &ic1, "ic1", z1, 0, NULL, NULL, 8.0,
+       TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE) );
+    SCIP_CALL( SCIPaddVarIndicator(scip, ic1, x2, 1.0) );
+
+   SCIP_CALL( SCIPcreateConsIndicatorGeneric(scip, &ic2, "ic2", z2, 0, NULL, NULL, 9.0,
+       TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE) );
+    SCIP_CALL( SCIPaddVarIndicator(scip, ic2, x2, 1.0) );
+    SCIP_CALL( SCIPaddVarIndicator(scip, ic2, x1, 1.0 / 5.0) );
+
+   SCIP_CALL( SCIPaddCons(scip, ic1) );
+   SCIP_CALL( SCIPaddCons(scip, ic2) );
+
+   SCIP_CALL( SCIPsetIntParam(scip, "display/verblevel", 0) );
 
    SCIP_CALL( SCIPsolve(scip) );
-   cr_assert_not_null(SCIPgetBestSol(scip));
 
+   sol = SCIPgetBestSol( scip );
+   cr_assert_not_null(sol);
+
+   cr_assert_eq(SCIPgetStatus(scip), SCIP_STATUS_OPTIMAL);
+
+   xval = SCIPgetSolVal(scip, sol, x1);
+   cr_assert_eq(xval, 1.25);
+   xval = SCIPgetSolVal(scip, sol, x2);
+   cr_assert_eq(xval, 8.75);
+
+   zval = SCIPgetSolVal(scip, sol, z1);
+   cr_assert_eq(zval, 0.0);
+   zval = SCIPgetSolVal(scip, sol, z2);
+   cr_assert_eq(zval, 1.0);
+
+   SCIP_CALL( SCIPreleaseCons(scip, &ic1) );
+   SCIP_CALL( SCIPreleaseCons(scip, &ic2) );
+   SCIP_CALL( SCIPreleaseCons(scip, &lc1) );
+   SCIP_CALL( SCIPreleaseCons(scip, &lc2) );
    SCIP_CALL( SCIPreleaseVar(scip, &x1) );
    SCIP_CALL( SCIPreleaseVar(scip, &x2) );
    SCIP_CALL( SCIPreleaseVar(scip, &z1) );
    SCIP_CALL( SCIPreleaseVar(scip, &z2) );
+}
 
+Test(indicator, feasibleone1, .init = setup, .fini = teardown,
+   .description = "MIP with two indicator constraints, one negated variable"
+   )
+{
+   /*
+      Unit test inspired by MathOptInterface.jl
+      minimize
+        obj: -2 * x1 - 3 * x2
+      Subject to
+        lc1: x1 + x2 <= 10
+        lc2: z1 + z2 >= 1
+        ic1: z1 == 1 => x2 <= 8
+        ic2: z2 == 1 => x2 + x1/5 <= 9
+      Binary
+        z1 z2 z1neg
+      Solution:
+        x: 1.25, 8.75
+        z: 0, 1, 0
+        value: -28.75
+   */
+
+   SCIP_VAR* z1neg;
+   SCIP_CALL( SCIPcreateVarBasic(scip, &x1, "x1", -SCIPinfinity(scip), SCIPinfinity(scip), -2.0, SCIP_VARTYPE_CONTINUOUS) );
+   SCIP_CALL( SCIPcreateVarBasic(scip, &x2, "x2", -SCIPinfinity(scip), SCIPinfinity(scip), -3.0, SCIP_VARTYPE_CONTINUOUS) );
+   SCIP_CALL( SCIPcreateVarBasic(scip, &z1neg, "z1neg", 0.0, 1.0, 0.0, SCIP_VARTYPE_BINARY) );
+   SCIP_CALL( SCIPcreateVarBasic(scip, &z2, "z2", 0.0, 1.0, 0.0, SCIP_VARTYPE_BINARY) );
+
+   SCIP_CALL( SCIPaddVar(scip, x1) );
+   SCIP_CALL( SCIPaddVar(scip, x2) );
+   SCIP_CALL( SCIPaddVar(scip, z1neg) );
+   SCIP_CALL( SCIPaddVar(scip, z2) );
+
+   SCIP_CALL( SCIPgetNegatedVar(scip, z1neg, &z1) );
+
+   SCIP_CALL( SCIPcreateConsBasicLinear(scip, &lc1, "lc1", 0, NULL, NULL, -SCIPinfinity(scip), 10.0) );
+   SCIP_CALL( SCIPaddCoefLinear(scip, lc1, x1, 1.0) );
+   SCIP_CALL( SCIPaddCoefLinear(scip, lc1, x2, 1.0) );
+
+   SCIP_CALL( SCIPaddCons(scip, lc1) );
+
+   SCIP_CALL( SCIPcreateConsBasicLinear(scip, &lc2, "lc2", 0, NULL, NULL, 1.0, SCIPinfinity(scip)) );
+   SCIP_CALL( SCIPaddCoefLinear(scip, lc2, z1, 1.0) );
+   SCIP_CALL( SCIPaddCoefLinear(scip, lc2, z2, 1.0) );
+
+   SCIP_CALL( SCIPaddCons(scip, lc2) );
+
+   SCIP_CALL( SCIPcreateConsIndicatorGeneric(scip, &ic1, "ic1", z1, 0, NULL, NULL, 8.0,
+         TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE) );
+   SCIP_CALL( SCIPaddVarIndicator(scip, ic1, x2, 1.0) );
+
+   SCIP_CALL( SCIPcreateConsIndicatorGeneric(scip, &ic2, "ic2", z2, 0, NULL, NULL, 9.0,
+         TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE) );
+   SCIP_CALL( SCIPaddVarIndicator(scip, ic2, x2, 1.0) );
+   SCIP_CALL( SCIPaddVarIndicator(scip, ic2, x1, 1.0 / 5.0) );
+
+   SCIP_CALL( SCIPaddCons(scip, ic1) );
+   SCIP_CALL( SCIPaddCons(scip, ic2) );
+
+   SCIP_CALL( SCIPsetIntParam(scip, "display/verblevel", 0) );
+
+   SCIP_CALL( SCIPsolve(scip) );
+
+   sol = SCIPgetBestSol( scip );
+   cr_assert_not_null(sol);
+
+   cr_assert_eq(SCIPgetStatus(scip), SCIP_STATUS_OPTIMAL);
+
+   xval = SCIPgetSolVal(scip, sol, x1);
+   cr_assert(EPSEQ(xval, 1.249999));
+   xval = SCIPgetSolVal(scip, sol, x2);
+   cr_assert(EPSEQ(xval, 8.75));
+
+   zval = SCIPgetSolVal(scip, sol, z1);
+   cr_assert(EPSEQ(zval, 0.0));
+   zval = SCIPgetSolVal(scip, sol, z2);
+   cr_assert(EPSEQ(zval, 1.0));
+
+   SCIP_CALL( SCIPreleaseCons(scip, &ic1) );
+   SCIP_CALL( SCIPreleaseCons(scip, &ic2) );
+   SCIP_CALL( SCIPreleaseCons(scip, &lc1) );
+   SCIP_CALL( SCIPreleaseCons(scip, &lc2) );
+   SCIP_CALL( SCIPreleaseVar(scip, &x1) );
+   SCIP_CALL( SCIPreleaseVar(scip, &x2) );
+   SCIP_CALL( SCIPreleaseVar(scip, &z1neg) );
+   SCIP_CALL( SCIPreleaseVar(scip, &z2) );
+}
+
+Test(indicator, feasibleone2, .init = setup, .fini = teardown,
+   .description = "MIP with two indicator constraints, one negated variable, actual var activated on 0"
+   )
+{
+   /*
+      Unit test inspired by MathOptInterface.jl
+      minimize
+        obj: -2 * x1 - 3 * x2
+      Subject to
+        lc1: x1 + x2 <= 10
+        lc2: z1 - z2 <= 0
+        ic1: z1 == 0 => x2 <= 8
+        ic2: z2 == 1 => x2 + x1 / 5 <= 9
+      Binary
+        z1 z2
+      Solution:
+        x: 1.25, 8.75
+        z: 1, 1
+        value: -28.75
+   */
+
+   SCIP_CALL( SCIPcreateVarBasic(scip, &x1, "x1", -SCIPinfinity(scip), SCIPinfinity(scip), -2.0, SCIP_VARTYPE_CONTINUOUS) );
+   SCIP_CALL( SCIPcreateVarBasic(scip, &x2, "x2", -SCIPinfinity(scip), SCIPinfinity(scip), -3.0, SCIP_VARTYPE_CONTINUOUS) );
+   SCIP_CALL( SCIPcreateVarBasic(scip, &z1, "z1", 0.0, 1.0, 0.0, SCIP_VARTYPE_BINARY) );
+   SCIP_CALL( SCIPcreateVarBasic(scip, &z2, "z2", 0.0, 1.0, 0.0, SCIP_VARTYPE_BINARY) );
+
+   SCIP_CALL( SCIPaddVar(scip, x1) );
+   SCIP_CALL( SCIPaddVar(scip, x2) );
+   SCIP_CALL( SCIPaddVar(scip, z1) );
+   SCIP_CALL( SCIPaddVar(scip, z2) );
+
+   SCIP_CALL( SCIPcreateConsBasicLinear(scip, &lc1, "lc1", 0, NULL, NULL, -SCIPinfinity(scip), 10.0) );
+   SCIP_CALL( SCIPaddCoefLinear(scip, lc1, x1, 1.0) );
+   SCIP_CALL( SCIPaddCoefLinear(scip, lc1, x2, 1.0) );
+
+   SCIP_CALL( SCIPaddCons(scip, lc1) );
+
+   SCIP_CALL( SCIPcreateConsBasicLinear(scip, &lc2, "lc2", 0, NULL, NULL, -SCIPinfinity(scip), 0.0) );
+   SCIP_CALL( SCIPaddCoefLinear(scip, lc2, z1, 1.0) );
+   SCIP_CALL( SCIPaddCoefLinear(scip, lc2, z2, -1.0) );
+
+   SCIP_CALL( SCIPaddCons(scip, lc2) );
+
+   SCIP_CALL( SCIPcreateConsIndicatorGeneric(scip, &ic1, "ic1", z1, 0, NULL, NULL, 8.0, FALSE,
+         TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE) );
+   SCIP_CALL( SCIPaddVarIndicator(scip, ic1, x2, 1.0) );
+
+   SCIP_CALL( SCIPcreateConsIndicatorGeneric(scip, &ic2, "ic2", z2, 0, NULL, NULL, 9.0, TRUE,
+         TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE) );
+    SCIP_CALL( SCIPaddVarIndicator(scip, ic2, x2, 1.0) );
+    SCIP_CALL( SCIPaddVarIndicator(scip, ic2, x1, 1.0 / 5.0) );
+
+   SCIP_CALL( SCIPaddCons(scip, ic1) );
+   SCIP_CALL( SCIPaddCons(scip, ic2) );
+
+   SCIP_CALL( SCIPsolve(scip) );
+
+   sol = SCIPgetBestSol( scip );
+   cr_assert_not_null(sol);
+
+   cr_assert_eq(SCIPgetStatus(scip), SCIP_STATUS_OPTIMAL);
+
+   xval = SCIPgetSolVal(scip, sol, x1);
+   cr_assert(EPSEQ(xval, 1.25));
+   xval = SCIPgetSolVal(scip, sol, x2);
+   cr_assert(EPSEQ(xval, 8.75));
+
+   zval = SCIPgetSolVal(scip, sol, z1);
+   cr_assert(EPSEQ(zval, 1.0));
+
+   SCIP_CALL( SCIPreleaseCons(scip, &ic1) );
+   SCIP_CALL( SCIPreleaseCons(scip, &ic2) );
+   SCIP_CALL( SCIPreleaseCons(scip, &lc1) );
+   SCIP_CALL( SCIPreleaseCons(scip, &lc2) );
+   SCIP_CALL( SCIPreleaseVar(scip, &x1) );
+   SCIP_CALL( SCIPreleaseVar(scip, &x2) );
+   SCIP_CALL( SCIPreleaseVar(scip, &z1) );
+   SCIP_CALL( SCIPreleaseVar(scip, &z2) );
 }
