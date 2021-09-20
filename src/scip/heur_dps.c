5dd411aef22f18348386817183429e27984b10b7
Katrin Halbig <katrin.halbig@fau.de>
2021-09-20 19:04:14 +0200

    set HEUR_DISPCHAR

--- src/scip/heur_dps.c
+++ src/scip/heur_dps.c
@@ -49,7 +49,7 @@
 
 #define HEUR_NAME             "dps"
 #define HEUR_DESC             "primal heuristic for decomposable MIPs"
-#define HEUR_DISPCHAR         '?' /** todo */
+#define HEUR_DISPCHAR         SCIP_HEURDISPCHAR_LNS
 #define HEUR_PRIORITY         75000
 #define HEUR_FREQ             0
 #define HEUR_FREQOFS          0
