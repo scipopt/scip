scipopt/scip

commit 5a318489ee37e26cb116d1ab27ab4a48f88be0ef

fix generateCut of sepa_gauge
- use expr value instead of nlrow activity to avoid having to substract
  value of linear part again

src/scip/sepa_gauge.c
@@ -553,7 +553,7 @@ SCIP_RETCODE generateCut(
    )
 {
    SCIP_EXPR* expr;
-   SCIP_Real activity;
+   SCIP_Real exprval;
    SCIP_Real gradx0; /* <grad f(x_0), x_0> */
    int i;
 
@@ -569,7 +569,6 @@ SCIP_RETCODE generateCut(
    /* linear part */
    for( i = 0; i < SCIPnlrowGetNLinearVars(nlrow); i++ )
    {
-      /** @todo: don't we also need the gradx0 += part here? */
       SCIP_CALL( SCIPaddVarToRow(scip, row, SCIPnlrowGetLinearVars(nlrow)[i], SCIPnlrowGetLinearCoefs(nlrow)[i]) );
    }
 
@@ -614,18 +613,19 @@ SCIP_RETCODE generateCut(
    SCIPdebugMsg(scip, "gradient dot x_0: %g\n", gradx0);
 #endif
 
-   /* gradient cut is f(x_0) - <grad f(x_0), x_0> + <grad f(x_0), x> <= rhs or >= lhs */
-   SCIP_CALL( SCIPgetNlRowSolActivity(scip, nlrow, sol, &activity) );
+   /* gradient cut is linear part + f(x_0) - <grad f(x_0), x_0> + <grad f(x_0), x> <= rhs or >= lhs */
+   exprval = SCIPexprGetEvalValue(SCIPnlrowGetExpr(nlrow));
+   assert(exprval != SCIP_INVALID);  /* we should have noticed a domain error above */  /*lint !e777*/
    if( convexside == RHS )
    {
       assert(!SCIPisInfinity(scip, SCIPnlrowGetRhs(nlrow)));
-      SCIP_CALL( SCIPchgRowRhs(scip, row, SCIPnlrowGetRhs(nlrow) - activity + gradx0) );
+      SCIP_CALL( SCIPchgRowRhs(scip, row, SCIPnlrowGetRhs(nlrow) - exprval + gradx0) );
    }
    else
    {
       assert(convexside == LHS);
       assert(!SCIPisInfinity(scip, -SCIPnlrowGetLhs(nlrow)));
-      SCIP_CALL( SCIPchgRowLhs(scip, row, SCIPnlrowGetLhs(nlrow) - activity + gradx0) );
+      SCIP_CALL( SCIPchgRowLhs(scip, row, SCIPnlrowGetLhs(nlrow) - exprval + gradx0) );
    }
 
 #ifdef CUT_DEBUG

commit 11c3e6be33a57c7986034c7697417ca2f45d70ce

regard nlrow constant when constructing gauge cut
- fixes bug introduced with 5a318489ee37e26cb116d1ab27ab4a48f88be0ef

src/scip/sepa_gauge.c
@@ -39,6 +39,7 @@
 #include "scip/pub_misc.h"
 #include "scip/pub_nlp.h"
 #include "scip/pub_sepa.h"
+#include "scip/pub_var.h"
 #include "scip/scip_cut.h"
 #include "scip/scip_lp.h"
 #include "scip/scip_mem.h"
@@ -539,6 +540,10 @@ SCIP_RETCODE generateCut(
 
    SCIP_CALL( SCIPcacheRowExtensions(scip, row) );
 
+#ifdef CUT_DEBUG
+   SCIPdebug( SCIP_CALL( SCIPprintNlRow(scip, nlrow, NULL) ) );
+#endif
+
    /* linear part */
    for( i = 0; i < SCIPnlrowGetNLinearVars(nlrow); i++ )
    {
@@ -569,6 +574,7 @@ SCIP_RETCODE generateCut(
          *success = FALSE;
          break;
       }
+      /* SCIPdebugMsg(scip, "grad w.r.t. <%s> (%g) = %g, gradx0 += %g\n", SCIPvarGetName(var), SCIPgetSolVal(scip, sol, var), grad, grad * SCIPgetSolVal(scip, sol, var)); */
 
       gradx0 += grad * SCIPgetSolVal(scip, sol, var);
       SCIP_CALL( SCIPaddVarToRow(scip, row, var, grad) );
@@ -592,13 +598,13 @@ SCIP_RETCODE generateCut(
    if( convexside == RHS )
    {
       assert(!SCIPisInfinity(scip, SCIPnlrowGetRhs(nlrow)));
-      SCIP_CALL( SCIPchgRowRhs(scip, row, SCIPnlrowGetRhs(nlrow) - exprval + gradx0) );
+      SCIP_CALL( SCIPchgRowRhs(scip, row, SCIPnlrowGetRhs(nlrow) - SCIPnlrowGetConstant(nlrow) - exprval + gradx0) );
    }
    else
    {
       assert(convexside == LHS);
       assert(!SCIPisInfinity(scip, -SCIPnlrowGetLhs(nlrow)));
-      SCIP_CALL( SCIPchgRowLhs(scip, row, SCIPnlrowGetLhs(nlrow) - exprval + gradx0) );
+      SCIP_CALL( SCIPchgRowLhs(scip, row, SCIPnlrowGetLhs(nlrow) - SCIPnlrowGetConstant(nlrow) - exprval + gradx0) );
    }
 
 #ifdef CUT_DEBUG