521035084dfe4ab6718bebd21cd596fb5604316e
Franziska Schloesser <schloesser@zib.de>
2021-12-15 12:58:23 +0100

    Revert "update findipopt"
    This reverts commit 48240c16d2b721004617375ba57f771707831fd3.

--- cmake/Modules/FindIPOPT.cmake
+++ cmake/Modules/FindIPOPT.cmake
@@ -105,106 +105,93 @@ else()
 
   set(IPOPT_DIR $ENV{IPOPT_DIR} CACHE PATH "Path to IPOPT build directory")
 
-  find_path(IPOPT_INCLUDE_DIRS NAMES IpIpoptApplication.hpp PATH_SUFFIXES coin coin-or PATHS ${IPOPT_DIR}/include/coin)
+  # version ipopt >= 3.13
+  set(IPOPT_INCLUDE_DIRS ${IPOPT_DIR}/include/coin-or)
 
-  # See https://github.com/coin-or/Ipopt/blob/releases/3.13.3/src/Interfaces/Ipopt.java#L167 for a possible library names
-  find_library(IPOPT_IPOPT_LIBRARY_RELEASE NAMES libipopt ipopt ipopt-3 ipopt-0 libipopt-3 libipopt-0
-                                           HINTS ${IPOPT_DIR}/lib ${IPOPT_DIR}/lib/coin ${IPOPT_DIR}/lib/coin-or)
-  find_library(IPOPT_IPOPT_LIBRARY_DEBUG NAMES libipoptD ipoptD ipoptD-3 ipoptD-0 libipoptD-3 libipoptD-0
-                                         HINTS ${IPOPT_DIR}/lib ${IPOPT_DIR}/lib/coin ${IPOPT_DIR}/lib/coin-or)
+  if(NOT EXISTS "${IPOPT_INCLUDE_DIRS}")
+      # version ipopt <= 3.12
+      set(IPOPT_INCLUDE_DIRS ${IPOPT_DIR}/include/coin)
+  endif()
+
+  find_library(IPOPT_IPOPT_LIBRARY_RELEASE libipopt ${IPOPT_DIR}/lib
+                                                    ${IPOPT_DIR}/lib/coin
+                                                    ${IPOPT_DIR}/lib/coin-or
+                                                    NO_DEFAULT_PATH)
+  find_library(IPOPT_IPOPT_LIBRARY_DEBUG   libipoptD ${IPOPT_DIR}/lib
+                                                     ${IPOPT_DIR}/lib/coin
+                                                     ${IPOPT_DIR}/lib/coin-or
+                                                     NO_DEFAULT_PATH)
 
   select_library_configurations(IPOPT_IPOPT)
   set(IPOPT_LIBRARIES ${IPOPT_IPOPT_LIBRARY})
 
-  # Some old version of binary releases of IPOPT have Intel fortran
-  # libraries embedded in the library, newer releases require them to
-  # be explicitly linked.
-  if(IPOPT_IPOPT_LIBRARY)
-    get_filename_component(_MSVC_DIR "${CMAKE_LINKER}" DIRECTORY)
+  if(IPOPT_LIBRARIES)
+    find_file(IPOPT_DEP_FILE ipopt_addlibs_cpp.txt ${IPOPT_DIR}/share/doc/coin-or/Ipopt
+                                                   ${IPOPT_DIR}/share/coin-or/doc/Ipopt
+                                                   ${IPOPT_DIR}/share/doc/coin/Ipopt
+                                                   ${IPOPT_DIR}/share/coin/doc/Ipopt
+                                                   NO_DEFAULT_PATH)
+    mark_as_advanced(IPOPT_DEP_FILE)
 
-    # Find the lib.exe executable
-    find_program(LIB_EXECUTABLE
-                 NAMES lib.exe
-                 HINTS "${_MSVC_BINDIR}"
-                       "C:/Program Files/Microsoft Visual Studio 10.0/VC/bin"
-                       "C:/Program Files (x86)/Microsoft Visual Studio 10.0/VC/bin"
-                       "C:/Program Files/Microsoft Visual Studio 11.0/VC/bin"
-                       "C:/Program Files (x86)/Microsoft Visual Studio 11.0/VC/bin"
-                       "C:/Program Files/Microsoft Visual Studio 12.0/VC/bin"
-                       "C:/Program Files (x86)/Microsoft Visual Studio 12.0/VC/bin"
-                       "C:/Program Files/Microsoft Visual Studio 14.0/VC/bin"
-                       "C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin"
-                 DOC "Path to the lib.exe executable")
-    mark_as_advanced(LIB_EXECUTABLE)
+    if(IPOPT_DEP_FILE)
+      # parse the file and acquire the dependencies
+      file(READ ${IPOPT_DEP_FILE} IPOPT_DEP)
 
-    # backup PATH environment variable
-    set(_path $ENV{PATH})
+      string(REGEX REPLACE "-[^l][^ ]* " "" IPOPT_DEP ${IPOPT_DEP})
+      string(REPLACE "\n"                "" IPOPT_DEP ${IPOPT_DEP})
+      string(REPLACE "\n"                "" IPOPT_DEP ${IPOPT_DEP})
+      string(REPLACE "ipopt"             "" IPOPT_DEP ${IPOPT_DEP})       # remove any possible auto-dependency
+      separate_arguments(IPOPT_DEP)
 
-    # Add th MSVC "Common7/IDE" dir containing the dlls in the PATH when needed.
-    get_filename_component(_MSVC_LIBDIR "${_MSVC_BINDIR}/../../Common7/IDE" ABSOLUTE)
-    if(NOT EXISTS "${_MSVC_LIBDIR}")
-      get_filename_component(_MSVC_LIBDIR "${_MSVC_BINDIR}/../../../Common7/IDE" ABSOLUTE)
-    endif()
+      # use the find_library command in order to prepare rpath correctly
+      foreach(LIB ${IPOPT_DEP})
 
-    if(EXISTS "${_MSVC_LIBDIR}")
-      set(_MSVC_LIBDIR_FOUND 0)
-      file(TO_CMAKE_PATH "$ENV{PATH}" _env_path)
-      foreach(_dir ${_env_path})
-        if("${_dir}" STREQUAL ${_MSVC_LIBDIR})
-          set(_MSVC_LIBDIR_FOUND 1)
+        # skip LD library flags (this can be either -libflags or -l)
+        if(${LIB} MATCHES "-l*")
+          continue()
         endif()
-      endforeach()
-      if(NOT _MSVC_LIBDIR_FOUND)
-        file(TO_NATIVE_PATH "${_MSVC_LIBDIR}" _MSVC_LIBDIR)
-        set(ENV{PATH} "$ENV{PATH};${_MSVC_LIBDIR}")
-      endif()
-    endif()
 
-    if(IPOPT_IPOPT_LIBRARY_RELEASE)
-      set(_IPOPT_LIB ${IPOPT_IPOPT_LIBRARY_RELEASE})
-    else()
-      set(_IPOPT_LIB ${IPOPT_IPOPT_LIBRARY_DEBUG})
-    endif()
-
-    execute_process(COMMAND ${LIB_EXECUTABLE} /list "${_IPOPT_LIB}"
-                    OUTPUT_VARIABLE _lib_output)
-
-    set(ENV{PATH} "${_path}")
-    unset(_path)
-
-    if(NOT "${_lib_output}" MATCHES "libifcoremd.dll")
-      get_filename_component(_IPOPT_IPOPT_LIBRARY_DIR "${_IPOPT_LIB}" DIRECTORY)
-
-      foreach(_lib ifconsol
-                   libifcoremd
-                   libifportmd
-                   libmmd
-                   libirc
-                   svml_dispmd)
-        string(TOUPPER "${_lib}" _LIB)
-        find_library(IPOPT_${_LIB}_LIBRARY_RELEASE ${_lib} ${_IPOPT_IPOPT_LIBRARY_DIR})
-        find_library(IPOPT_${_LIB}_LIBRARY_DEBUG ${_lib}d ${_IPOPT_IPOPT_LIBRARY_DIR})
-        select_library_configurations(IPOPT_${_LIB})
-        if(NOT "${IPOPT_${_LIB}_LIBRARY}" MATCHES "NOTFOUND$")
-          list(APPEND IPOPT_LIBRARIES ${IPOPT_${_LIB}_LIBRARY})
+        # check whether we compile for x86 or x64
+        if(${CMAKE_SIZEOF_VOID_P} EQUAL 8)
+          set(MKL_ARCH_DIR "intel64")
+        else()
+          set(MKL_ARCH_DIR "ia32")
         endif()
+
+        find_library(IPOPT_SEARCH_FOR_${LIB} ${LIB} $ENV{MKLROOT}/lib/${MKL_ARCH_DIR}
+                                                    ${IPOPT_DIR}/lib
+                                                    ${IPOPT_DIR}/lib/coin-or
+                                                    ${IPOPT_DIR}/lib/coin-or/ThirdParty
+                                                    ${IPOPT_DIR}/lib/coin
+                                                    ${IPOPT_DIR}/lib/coin/ThirdParty
+                                                    NO_DEFAULT_PATH)
+
+        if(IPOPT_SEARCH_FOR_${LIB})
+          set(IPOPT_LIBRARIES ${IPOPT_LIBRARIES} ${IPOPT_SEARCH_FOR_${LIB}})
+        endif()
+        mark_as_advanced(IPOPT_SEARCH_FOR_${LIB})
       endforeach()
     endif()
-  endif()
-
-  set(IPOPT_DEFINITIONS "")
-  if(MSVC)
-    set(IPOPT_LINK_FLAGS "/NODEFAULTLIB:libcmt.lib;libcmtd.lib")
-  else()
-    set(IPOPT_LINK_FLAGS "")
   endif()
 
 endif()
 
+# parse the version number
+foreach( INCLUDE_DIR ${IPOPT_INCLUDE_DIRS} )
+  if( EXISTS ${INCLUDE_DIR}/IpoptConfig.h )
+  file(STRINGS ${INCLUDE_DIR}/IpoptConfig.h CONFIGFILE)
+    foreach(STR ${CONFIGFILE})
+      if("${STR}" MATCHES "^#define IPOPT_VERSION ")
+        string(REGEX REPLACE "#define IPOPT_VERSION " "" IPOPT_VERSION ${STR})
+        string(REGEX REPLACE "\"" "" IPOPT_VERSION ${IPOPT_VERSION})
+      endif()
+    endforeach()
+  endif()
+endforeach()
+
 mark_as_advanced(IPOPT_INCLUDE_DIRS
                  IPOPT_LIBRARIES
-                 IPOPT_DEFINITIONS
-                 IPOPT_LINK_FLAGS)
+                 IPOPT_MODERN)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(IPOPT
