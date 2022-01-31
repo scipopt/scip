445bf1b0864bb7e5e2568aafbfd99b512e9ea26b
Stefan Vigerske <svigerske@gams.com>
2022-01-31 11:00:52 +0100

    declare SCIPsetFeastol() and SCIPsetDualfeastol() as pure, too
    - should sqaush some pclint warnings about SCIP(dual)feastol() calling
      non-pure function (if NDEBUG not defined)

--- src/scip/set.h
+++ src/scip/set.h
@@ -1094,6 +1094,9 @@ SCIP_Real SCIPsetSumepsilon(
    );
 
 /** returns feasibility tolerance for constraints */
+#ifdef __GNUC__
+__attribute__ ((pure))
+#endif
 SCIP_Real SCIPsetFeastol(
    SCIP_SET*             set                 /**< global SCIP settings */
    );
@@ -1104,6 +1107,9 @@ SCIP_Real SCIPsetLPFeastolFactor(
    );
 
 /** returns feasibility tolerance for reduced costs */
+#ifdef __GNUC__
+__attribute__ ((pure))
+#endif
 SCIP_Real SCIPsetDualfeastol(
    SCIP_SET*             set                 /**< global SCIP settings */
    );
