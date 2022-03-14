a186717d46c57c52a748be243bd4da000a6430cc
Jasper van Doornmalen <m.j.v.doornmalen@tue.nl>
2022-03-14 16:01:34 +0100

    Unit test SST remove leaderrule 3

--- src/scip/prop_symmetry.c
+++ src/scip/prop_symmetry.c
@@ -7669,7 +7669,7 @@ SCIP_RETCODE SCIPincludePropSymmetry(
 
    SCIP_CALL( SCIPaddIntParam(scip,
          "propagating/" PROP_NAME "/sstleaderrule",
-         "rule to select the leader in an orbit (0: first var; 1: last var; 2: var having most conflicting vars in orbit; 3: var having most conflicting vars in problem)",
+         "rule to select the leader in an orbit (0: first var; 1: last var; 2: var having most conflicting vars in orbit)",
          &propdata->sstleaderrule, TRUE, DEFAULT_SSTLEADERRULE, 0, 2, NULL, NULL) );
 
    SCIP_CALL( SCIPaddIntParam(scip,
