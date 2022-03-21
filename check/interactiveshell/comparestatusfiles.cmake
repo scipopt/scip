96b3da46b8a91d6ec35ee7c4b715312e5f637467
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2022-03-21 16:42:43 +0100

    replace "\." by "[.]"

--- check/interactiveshell/comparestatusfiles.cmake
+++ check/interactiveshell/comparestatusfiles.cmake
@@ -8,13 +8,13 @@
 # read FILE1 into variable R1
 file(STRINGS ${FILE1} R1)
 # remove all occurence of floating point numbers
-string(REGEX REPLACE "[0-9]+\.[0-9]+" "" R1OUT "${R1}")
+string(REGEX REPLACE "[0-9]+[.][0-9]+" "" R1OUT "${R1}")
 # write result into file
 file(WRITE ${FILE1}.s ${R1OUT})
 #
 # similarly for FILE2
 file(STRINGS ${FILE2} R2)
-string(REGEX REPLACE "[0-9]+\.[0-9]+" "" R2OUT "${R2}")
+string(REGEX REPLACE "[0-9]+[.][0-9]+" "" R2OUT "${R2}")
 file(WRITE ${FILE2}.s ${R2OUT})
 #
 # finally compare the two resulting files
