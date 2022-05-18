322cd41d283efe6e23069e137f65604a3852de9a
Stefan Vigerske <svigerske@gams.com>
2022-05-18 15:36:08 +0200

    remove NO_CONFIG_HEADER define
    - scip/scipbuildflags.c is not build with cmake system anyway

--- src/scip/scipbuildflags.c
+++ src/scip/scipbuildflags.c
@@ -22,9 +22,7 @@
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
 
 #include "scip/scipbuildflags.h"
-#ifdef NO_CONFIG_HEADER
 #include "buildflags.c"
-#endif
 
 /** returns the flags that were used to build SCIP */
 const char* SCIPgetBuildFlags(
