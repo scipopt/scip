c9739e480a6426b2aa33bb6ced7b64948c3503b4
Ksenia Bestuzheva <bestuzheva@zib.de>
2022-10-04 23:27:49 +0200

    fix issues on indices

--- src/scip/nlhdlr_soc.c
+++ src/scip/nlhdlr_soc.c
@@ -2696,7 +2696,7 @@ SCIP_DECL_NLHDLRINITSEPA(nlhdlrInitSepaSoc)
       int pos;
 
       i = nlhdlrexprdata->transcoefsidx[nlhdlrexprdata->termbegins[0]];
-      v1i = nlhdlrexprdata->transcoefs[0];
+      v1i = nlhdlrexprdata->transcoefs[nlhdlrexprdata->termbegins[0]];
       assert(v1i != 0.0);
 
       v2zero = nlhdlrexprdata->termbegins[1] == nlhdlrexprdata->termbegins[2];
@@ -2705,7 +2705,7 @@ SCIP_DECL_NLHDLRINITSEPA(nlhdlrInitSepaSoc)
       v2i = 0.0;
       if( !v2zero && SCIPsortedvecFindInt(nlhdlrexprdata->transcoefsidx + nlhdlrexprdata->termbegins[1], i, nlhdlrexprdata->termbegins[2] - nlhdlrexprdata->termbegins[1], &pos) )
       {
-         assert(nlhdlrexprdata->transcoefsidx[nlhdlrexprdata->termbegins[1] + pos] == j);
+         assert(nlhdlrexprdata->transcoefsidx[nlhdlrexprdata->termbegins[1] + pos] == i);
          v2i = nlhdlrexprdata->transcoefs[nlhdlrexprdata->termbegins[1] + pos];
       }
 
@@ -2719,9 +2719,9 @@ SCIP_DECL_NLHDLRINITSEPA(nlhdlrInitSepaSoc)
 
          /* get (v_1)_j; as it is a sparse vector, we need to search for j in transcoefsidx */
          v1j = 0.0;
-         if( SCIPsortedvecFindInt(nlhdlrexprdata->transcoefsidx + nlhdlrexprdata->termbegins[0], j, nlhdlrexprdata->termbegins[1] - nlhdlrexprdata->termbegins[0], &pos) )
+         if( SCIPsortedvecFindInt(nlhdlrexprdata->transcoefsidx + nlhdlrexprdata->termbegins[0], nlhdlrexprdata->transcoefsidx[k], nlhdlrexprdata->termbegins[1] - nlhdlrexprdata->termbegins[0], &pos) )
          {
-            assert(nlhdlrexprdata->transcoefsidx[nlhdlrexprdata->termbegins[0] + pos] == j);
+            assert(nlhdlrexprdata->transcoefsidx[nlhdlrexprdata->termbegins[0] + pos] == nlhdlrexprdata->transcoefsidx[k]);
             v1j = nlhdlrexprdata->transcoefs[nlhdlrexprdata->termbegins[0] + pos];
          }
 
