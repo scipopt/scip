0ea620e2c12006ebb9eaf0d527591be4c46c930d
Jasper van Doornmalen <m.j.v.doornmalen@tue.nl>
2023-03-22 11:41:21 +0100

    Fix incorrect rule for first last and centre

--- src/scip/symmetry_orbitopal.c
+++ src/scip/symmetry_orbitopal.c
@@ -334,18 +334,18 @@ SCIP_RETCODE updateColumnOrderWhenBranchingOnColumn(
          {
             case SCIP_COLUMNORDERING_FIRST:
                /* only swap with c if c is earlier in column order than swaporigcolid */
-               if ( colorder[c] >= colorder[swaporigcolid] )
+               if ( colorderinv[c] >= colorderinv[swaporigcolid] )
                   goto CONDITIONFAIL;
                break;
             case SCIP_COLUMNORDERING_LAST:
                /* only swap with c if c is later in column order than swaporigcolid */
-               if ( colorder[c] <= colorder[swaporigcolid] )
+               if ( colorderinv[c] <= colorderinv[swaporigcolid] )
                   goto CONDITIONFAIL;
                break;
             case SCIP_COLUMNORDERING_CENTRE:
                /* if the column is not more central than swaporigcolid, ignore */
-               if ( ABS(colorder[c] - middlecolumn) >=
-                     ABS(colorder[swaporigcolid] - middlecolumn) )
+               if ( ABS(colorderinv[c] - middlecolumn) >=
+                     ABS(colorderinv[swaporigcolid] - middlecolumn) )
                   goto CONDITIONFAIL;
                break;
             default:
