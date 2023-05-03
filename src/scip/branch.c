29077b46548c37497fb65acad00c899387f5689d
Stefan Vigerske <svigerske@gams.com>
2023-05-03 17:36:08 +0200

    allow primal > SCIP_INVALID in branch.c assert
    - for #1487

--- src/scip/branch.c
+++ src/scip/branch.c
@@ -262,7 +262,7 @@ SCIP_RETCODE branchcandCalcLPCands(
          assert(col->lpipos >= 0);
 
          primsol = SCIPcolGetPrimsol(col);
-         assert(primsol < SCIP_INVALID);
+         assert(primsol != SCIP_INVALID);
          assert(SCIPsetIsInfinity(set, -col->lb) || SCIPsetIsFeasGE(set, primsol, col->lb));
          assert(SCIPsetIsInfinity(set, col->ub) || SCIPsetIsFeasLE(set, primsol, col->ub));
 
