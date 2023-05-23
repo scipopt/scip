47913980a7d3bc5492bfbf335aba0c802f602d6a
Dominik Kamp <dominik.kamp@uni-bayreuth.de>
2023-05-24 00:27:24 +0200

    Separate counter declarations

--- src/scip/reopt.c
+++ src/scip/reopt.c
@@ -5220,7 +5220,9 @@ SCIP_RETCODE SCIPreoptFree(
    /* free solutions and variable histories */
    if( set->stage >= SCIP_STAGE_PROBLEM )
    {
-      int p, v;
+      int p;
+      int v;
+
       for( p = (*reopt)->run-1; p >= 0; p-- )
       {
          if( (*reopt)->soltree->sols[p] != NULL )
