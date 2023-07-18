0f1465436758b2b83580d5dceff8e23f0b445aa7
Gioni Mexi <mexi@zib.de>
2023-07-18 11:38:10 +0200

    lint

--- src/scip/conflict_graphanalysis.c
+++ src/scip/conflict_graphanalysis.c
@@ -197,7 +197,7 @@ void confgraphWriteNode(
    assert(confgraphfile != NULL);
 
 #ifdef SCIP_CONFGRAPH_DOT
-   SCIPdotWriteNode(confgraphfile, (int)(size_t) idptr, label, nodetype, fillcolor, bordercolor);
+   SCIPdotWriteNode(confgraphfile, (unsigned int)(size_t) idptr, label, nodetype, fillcolor, bordercolor);
 #else
    SCIPgmlWriteNode(confgraphfile, (unsigned int)(size_t)idptr, label, nodetype, fillcolor, bordercolor);
 #endif
@@ -214,7 +214,7 @@ void confgraphWriteEdge(
    assert(confgraphfile != NULL);
 
 #ifdef SCIP_CONFGRAPH_DOT
-   SCIPdotWriteArc(confgraphfile, (int)(size_t)source, (int)(size_t)target, color);
+   SCIPdotWriteArc(confgraphfile, (unsigned int)(size_t)source, (unsigned int)(size_t)target, color);
 #else
 #ifndef SCIP_CONFGRAPH_EDGE
    SCIPgmlWriteArc(confgraphfile, (unsigned int)(size_t)source, (unsigned int)(size_t)target, NULL, color);
