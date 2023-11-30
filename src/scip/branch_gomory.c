cc329c1020a8dbfb7e6241e2af3c10062e7cb184
Mark Turner <turner@zib.de>
2023-11-30 15:39:22 +0100

    Update comments at top of file

--- src/scip/branch_gomory.c
+++ src/scip/branch_gomory.c
@@ -26,6 +26,26 @@
  * @ingroup DEFPLUGINS_BRANCH
  * @brief  Gomory cut branching rule
  * @author Mark Turner
+ *
+ * The approach is based on the following papers.
+ *
+ * M. Turner, T. Berthold, M. Besancon, T. Koch@n
+ * Branching via Cutting Plane Selection: Improving Hybrid Branching,@n
+ * arXiv preprint arXiv:2306.06050
+ *
+ * The Gomory cut branching rule selects a candidate integer variable $j$ with a fractional solution value.
+ * Each candidate variable must be a basic variable in the LP Tableau (if not then it would have to be at its bound
+ * that is integer-valued)
+ * This branching rule calculates the GMI cut for the aggregated row of the LP tableau associated with the
+ * candidate variable.
+ * The generated cut is then scored using a weighted sum rule.
+ * The branching candidate whose cut is highest scoring is then selected.
+ * For more details on the method, see:
+ *
+ * @par
+ * Mark Turner, Timo Berthold, Mathieu Besançon, Thorsten Koch@n
+ * Branching via Cutting Plane Selection: Improving Hybrid Branching@n
+ * 2023@n
  */
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
@@ -56,9 +76,9 @@
 #define BRANCHRULE_MAXBOUNDDIST    1.0
 
 #define DEFAULT_MAXNCANDS          -1    /**< maximum number of branching candidates to produce a cut for */
-#define DEFAULT_EFFICACYWEIGHT     1.0   /**< weight of efficacy in the cut scoring rule */
-#define DEFAULT_OBJPARALLELWEIGHT  0.0   /**< weight of objective parallelism in the cut scoring rule */
-#define DEFAULT_INTSUPPORTWEIGHT   0.0   /**< weight of objective parallelism in the cut scoring rule */
+#define DEFAULT_EFFICACYWEIGHT     1.0   /**< the weight of efficacy in weighted sum cut scoring rule */
+#define DEFAULT_OBJPARALLELWEIGHT  0.0   /**< the weight of objective parallelism in weighted sum scoring rule */
+#define DEFAULT_INTSUPPORTWEIGHT   0.0   /**< the weight of integer support in weighted sum cut scoring rule */
 #define DEFAULT_PERFORMRELPSCOST   FALSE /**< if default branching rule without execution should be called */
 #define DEFAULT_USEWEAKERCUTS      TRUE  /**< use weaker cuts derived from the exact branching split */
 
@@ -71,11 +91,11 @@
 struct SCIP_BranchruleData
 {
    int                   maxncands;             /**< maximum number of variable candidates to produce cut for */
-   SCIP_Real             efficacyweight;        /**< the weight of efficacy in weighted sum scoring rule */
+   SCIP_Real             efficacyweight;        /**< the weight of efficacy in weighted sum cut scoring rule */
    SCIP_Real             objparallelweight;     /**< the weight of objective parallelism in weighted sum scoring rule */
-   SCIP_Real             intsupportweight;      /**< the weight of integer support in weighted sum scoring rule */
+   SCIP_Real             intsupportweight;      /**< the weight of integer support in weighted sum cut scoring rule */
    SCIP_Bool             performrelpscost;      /**< if default branching rule without execution should be called */
-   SCIP_Bool             useweakercuts;         /**< use weakener cuts derived from the exact branching split */
+   SCIP_Bool             useweakercuts;         /**< use weaker cuts derived from the exact branching split */
 };
 
 
