701d3c13e1d815f9a073b90781e18cec25c346c2
Stefan Vigerske <svigerske@gams.com>
2024-01-04 14:46:28 +0700

    remove redundant checks for _WIN64
    - if _WIN64 then also _WIN32, so sufficient to check for latter

--- src/scip/def.h
+++ src/scip/def.h
@@ -102,8 +102,7 @@
 /*
  * Add some macros for differing functions on Windows
  */
-#if defined(_WIN32) || defined(_WIN64)
-
+#ifdef _WIN32
 #define strcasecmp _stricmp
 #define strncasecmp _strnicmp
 #define getcwd _getcwd
@@ -160,7 +159,7 @@
 #define SCIP_LONGINT_MAX          LLONG_MAX
 #define SCIP_LONGINT_MIN          LLONG_MIN
 #ifndef SCIP_LONGINT_FORMAT
-#if defined(_WIN32) || defined(_WIN64)
+#ifdef _WIN32
 #define SCIP_LONGINT_FORMAT           "I64d"
 #else
 #define SCIP_LONGINT_FORMAT           "lld"
@@ -218,7 +217,7 @@
 
 /* platform-dependent specification of the log1p, which is numerically more stable around x = 0.0 */
 #ifndef LOG1P
-#if defined(_WIN32) || defined(_WIN64)
+#ifdef _WIN32
 #define LOG1P(x) (log(1.0+x))
 #else
 #define LOG1P(x) (log1p(x))
