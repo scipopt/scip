c9c9e5eaa5104a63a5627772438d6d209f9ed19d
Christopher Hojny <c.hojny@tue.nl>
2024-02-01 09:18:24 +0100

    removed unused function

--- src/scip/symmetry.h
+++ src/scip/symmetry.h
@@ -284,22 +284,6 @@ SCIP_RETCODE SCIPdetectSingleOrDoubleLexMatrices(
    int*                  ncolmatrices        /**< pointer to store number of single lex column matrices in rows */
    );
 
-/** tries to handle variable matrices with lex ordered rows and columns */
-SCIP_EXPORT
-SCIP_RETCODE tryHandleDoubleLexMatrices(
-   SCIP*                 scip,               /**< SCIP pointer */
-   SCIP_VAR**            vars,               /**< variables on which permutations act */
-   int**                 perms,              /**< array of permutations */
-   int                   nperms,             /**< number of permutations in perms */
-   int                   permlen,            /**< number of original (non-negated) variables in a permutation */
-   SCIP_Bool             issignedperm,       /**< whether permutations are encoded as signed */
-   SCIP_Bool*            success,            /**< pointer to store whether symmetries are handled */
-   int                   cidx,               /**< identifier of component to be handled by double lex matrices */
-   SCIP_CONS***          genorbconss,        /**< pointer to store generated orbitope constraints */
-   int*                  ngenorbconss,       /**< pointer to store number of generated orbitope constraints */
-   int*                  genorbconsssize     /**< pointer to store size genorbconss */
-   );
-
 /** helper function to test if val1 = val2 while permitting infinity-values */
 SCIP_Bool SCIPsymEQ(
    SCIP*                 scip,               /**< SCIP data structure */
