260f26c420aeb761e8875aa4e8b7841f79b85dce
Dominik Kamp <dominik.kamp@uni-bayreuth.de>
2024-03-14 13:04:24 +0100

    Advance internal maxrounds

--- src/scip/presol_milp.cpp
+++ src/scip/presol_milp.cpp
@@ -944,7 +944,7 @@ SCIP_RETCODE SCIPincludePresolMILP(
 #if PAPILO_VERSION_MAJOR > 2 || (PAPILO_VERSION_MAJOR == 2 && PAPILO_VERSION_MINOR >= 3)
    SCIP_CALL(SCIPaddIntParam(scip, "presolving/" PRESOL_NAME "/internalmaxrounds",
          "internal maxrounds for each milp presolving (-1: no limit, 0: model cleanup)",
-         &presoldata->internalmaxrounds, FALSE, DEFAULT_INTERNAL_MAXROUNDS, -1, INT_MAX, NULL, NULL));
+         &presoldata->internalmaxrounds, TRUE, DEFAULT_INTERNAL_MAXROUNDS, -1, INT_MAX, NULL, NULL));
 #else
    presoldata->internalmaxrounds = DEFAULT_INTERNAL_MAXROUNDS;
 #endif
