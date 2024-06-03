85012685ea5a68ddcdb767be74d114ca31dd5dac
Stefan Vigerske <svigerske@gams.com>
2024-06-03 15:20:37 +0200

    fix unused variables

--- src/nauty/naurng.c
+++ src/nauty/naurng.c
@@ -64,6 +64,7 @@ ran_init_2(unsigned long long seed1, unsigned long long seed2)
     kissz = KISSZ;
 
     for (i = 0; i < 1000; ++i) ul = KISS;
+    (void)ul;  //SV avoid unused variable
 }
 
 unsigned long long
@@ -73,7 +74,7 @@ ran_init_time(unsigned long long extra)
    the same initialization. */
 {
     double t;
-    unsigned long long ul,seed;
+    unsigned long long seed;
     REALTIMEDEFS
 
     t = NAUTYREALTIME;
