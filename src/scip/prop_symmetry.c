d7ca8a2f03c644f9bb4d87f36145dcc545f535ab
Christopher Hojny <c.hojny@tue.nl>
2024-11-23 00:06:54 +0100

    remove assert and replace cmp-function

--- src/scip/prop_symmetry.c
+++ src/scip/prop_symmetry.c
@@ -6969,9 +6969,7 @@ SCIP_RETCODE SCIPincludePropSymmetry(
          &propdata->symtiming, TRUE, DEFAULT_SYMCOMPTIMING, 0, 2, NULL, NULL) );
 
    /* for symmetry detection tool Nauty, we add further parameters to terminate it early */
-   assert( (strlen(SYMsymmetryGetName()) == 4 && memcmp(SYMsymmetryGetName(), "none", 4) == 0)
-      || strlen(SYMsymmetryGetName()) >= 5 );
-   if ( strlen(SYMsymmetryGetName()) >= 5 && memcmp(SYMsymmetryGetName(), "Nauty", 5) == 0 ) /*lint !e747*/
+   if ( strlen(SYMsymmetryGetName()) >= 5 && strncmp(SYMsymmetryGetName(), "Nauty", 5) == 0 ) /*lint !e747*/
    {
       SCIP_CALL( SCIPaddIntParam(scip,
             "propagating/" PROP_NAME "/nautymaxncells",
