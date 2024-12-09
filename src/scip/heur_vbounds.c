b914dfd3af62a59b023ae48819eb7a4f132a32fd
rolfvdhulst <rolfvdhulst@gmail.com>
2024-12-09 12:52:27 +0100

    Fix infinite loop in heur_vbounds

--- src/scip/heur_vbounds.c
+++ src/scip/heur_vbounds.c
@@ -398,7 +398,7 @@ SCIP_RETCODE dfs(
       /* the current node was completely handled, remove it from the stack */
       stacksize--;
 
-      if( (maxstacksize > 1) && SCIPvarGetType(startvar) != SCIP_VARTYPE_CONTINUOUS )
+      if( (maxstacksize > 1) && SCIPvarIsIntegral(startvar))
       {
          /* store node in the sorted nodes array */
          dfsnodes[(*ndfsnodes)] = curridx;
@@ -584,7 +584,7 @@ SCIP_RETCODE applyVboundsFixings(
          SCIPvarGetLbLocal(var), SCIPvarGetUbLocal(var), SCIPvarGetObj(var));*/
 
       /* only check integer or binary variables */
-      if( SCIPvarGetType(var) == SCIP_VARTYPE_CONTINUOUS )
+      if( !SCIPvarIsIntegral(var) )
          continue;
 
       /* skip variables which are already fixed */
