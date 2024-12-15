be5aad9a0b113b36062dff17e015a0f9a499471e
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2024-12-15 18:58:34 +0100

    add test for sensible weights

--- src/scip/cons_sos1.c
+++ src/scip/cons_sos1.c
@@ -10624,6 +10624,15 @@ SCIP_RETCODE SCIPcreateConsSOS1(
    /* are we in the transformed problem? */
    transformed = SCIPgetStage(scip) >= SCIP_STAGE_TRANSFORMED;
 
+#ifndef NDEBUG
+   /* Check that the weights are sensible (not nan or inf); although not strictly needed, such values are likely a mistake. */
+   if ( nvars > 0 && weights != NULL )
+   {
+      for (v = 0; v < nvars; ++v)
+         assert( SCIPisFinite(weights[v]) );
+   }
+#endif
+
    /* create constraint data */
    SCIP_CALL( SCIPallocBlockMemory(scip, &consdata) );
    consdata->vars = NULL;
@@ -10726,6 +10735,8 @@ SCIP_RETCODE SCIPaddVarSOS1(
    assert( scip != NULL );
    assert( var != NULL );
    assert( cons != NULL );
+   /* Check weight is sensible (not nan or inf); although not strictly needed, such values are likely a mistake. */
+   assert( SCIPisFinite(weight) );
 
    SCIPdebugMsg(scip, "adding variable <%s> to constraint <%s> with weight %g\n", SCIPvarGetName(var), SCIPconsGetName(cons), weight);
 
