8fa65e67431c91f69a38513f7944435364ccc2db
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2025-01-12 11:53:23 +0100

    add comment

--- src/lpiexact/lpiexact_spx.cpp
+++ src/lpiexact/lpiexact_spx.cpp
@@ -124,6 +124,7 @@
 /*----------------------------- C++ --------------------------------*/
 /********************************************************************/
 
+/* MP@LE I suggest to remove the following and replace 0/NULL by nullptr */
 /* in C++ we have to use "0" instead of "(void*)0" */
 #undef NULL
 #define NULL 0
