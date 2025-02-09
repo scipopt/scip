7f0dd0609e77f9fbff4e6e6d26d57c20ad4d9579
Stefan Vigerske <svigerske@gams.com>
2025-02-09 17:44:20 +0100

    SCIP_DEPRECATED in enum only with gcc-like compiler

--- src/scip/type_var.h
+++ src/scip/type_var.h
@@ -63,7 +63,11 @@ enum SCIP_Vartype
 {
    SCIP_VARTYPE_BINARY     = 0,              /**< binary variable: \f$ x \in \{0,1\} \f$ */
    SCIP_VARTYPE_INTEGER    = 1,              /**< integer variable: \f$ x in \{lb, \dots, ub\} \f$ */
-   SCIP_VARTYPE_IMPLINT SCIP_DEPRECATED = 2, /**< Deprecated, use `SCIPcreateVarImpl` or `SCIPvarChgImplType` to control implied integrality. */
+#if defined(GCC_VERSION) && GCC_VERSION >= 600 && !defined(__INTEL_COMPILER) /* _attribute__ ((deprecated)) within enums only supported by selected compilers  */
+   SCIP_VARTYPE_IMPLINT SCIP_DEPRECATED = 2, /**< @deprecated use `SCIPcreateVarImpl` or `SCIPvarChgImplType` to control implied integrality */
+#else
+   SCIP_VARTYPE_IMPLINT    = 2,              /**< @deprecated use `SCIPcreateVarImpl` or `SCIPvarChgImplType` to control implied integrality. */
+#endif
    SCIP_VARTYPE_CONTINUOUS = 3               /**< continuous variable: \f$ lb \leq x \leq ub \f$ */
 };
 typedef enum SCIP_Vartype SCIP_VARTYPE;
