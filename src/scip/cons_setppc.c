c4dd16966ec15bde4c313a1754eb015919992b15
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2025-07-23 11:59:53 +0200

    fix count of deleted constraints

--- src/scip/cons_setppc.c
+++ src/scip/cons_setppc.c
@@ -5062,6 +5062,7 @@ SCIP_RETCODE preprocessCliques(
    SCIP_Bool* undoneaggrtypes;               /* storage for not yet performed aggregation type (x = y or x + y = 1) */
    int saggregations;
    int naggregations;
+   int startndelconss;
 
    assert(scip != NULL);
    assert(conshdlrdata != NULL);
@@ -5109,6 +5110,7 @@ SCIP_RETCODE preprocessCliques(
    nusefulvars = 0;
    nusefulconss = 0;
    maxnvars = 0;
+   startndelconss = *ndelconss;
 
    /* @todo: check for round limit for adding extra clique constraints */
    /* adding clique constraints which arises from global clique information */
@@ -5453,8 +5455,8 @@ SCIP_RETCODE preprocessCliques(
    /* free hashmap */
    SCIPhashmapFree(&vartoindex);
 
-   if( *ndelconss < 0 )
-      *ndelconss = 0;
+   if( *ndelconss < startndelconss )
+      *ndelconss = startndelconss;
 
    return SCIP_OKAY;
 }
