8fcae93de6d730a30bfb74c69c444c7235dd9c6e
Stefan Vigerske <svigerske@gams.com>
2025-08-25 12:24:24 +0200

    include string.h as system header

--- src/scip/sepa_zerohalf.c
+++ src/scip/sepa_zerohalf.c
@@ -54,7 +54,7 @@
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
 
-#include "string.h"
+#include <string.h>
 #include "scip/sepa_zerohalf.h"
 #include "scip/scipdefplugins.h"
 #include "scip/cutsel_hybrid.h"
