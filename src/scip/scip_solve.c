13f9cc53f4903c701db785a69711c86d5c3c490d
Dominik Kamp <kamp@zib.de>
2025-09-18 14:55:44 +0200

    Stop when known

--- src/scip/scip_solve.c
+++ src/scip/scip_solve.c
@@ -2897,7 +2897,7 @@ SCIP_RETCODE SCIPsolveConcurrent(
       {
          /* if yes, then presolve the problem */
          SCIP_CALL( SCIPpresolve(scip) );
-         if( SCIPgetStatus(scip) >= SCIP_STATUS_OPTIMAL )
+         if( SCIPgetStatus(scip) != SCIP_STATUS_UNKNOWN )
             return SCIP_OKAY;
       }
       else
