018f642e9e82edc55a7d3e8695427c176820232a
Stefan Vigerske <svigerske@gams.com>
2025-09-25 10:16:29 +0200

    add multiplier for randomseedshift
    - new parameter randomization/randomseedshiftmultiplier to define a
      multiplier for randomseedshift, with current default of 1
    - adjust SCIPsetInitializeRandomSeed() to multiply randomseedshift by
      randomseedshiftmultiplier
    - the purpose is to have a single parameter that can be used to change
      the random seeds obtained from setting randomseedshift to different
      values, except when the randomseedshift is set to 0 (the default)
    - the randomseedshiftmultiplier is set to 1 now to avoid a change in
      current behavior, but should be updated regularly, e.g., around major
      releases

--- src/scip/set.c
+++ src/scip/set.c
@@ -342,6 +342,7 @@
 
 /* Randomization */
 #define SCIP_DEFAULT_RANDOM_RANDSEEDSHIFT     0 /**< global shift of all random seeds in the plugins, this will have no impact on the permutation and LP seeds */
+#define SCIP_DEFAULT_RANDOM_RANDSEEDSHIFTMULT 1 /**< multiplier for global shift of all random seeds in the plugins */
 #define SCIP_DEFAULT_RANDOM_PERMUTATIONSEED   0 /**< seed value for permuting the problem after reading/transformation (0: no permutation) */
 #define SCIP_DEFAULT_RANDOM_LPSEED            0 /**< random seed for LP solver, e.g. for perturbations in the simplex (0: LP default) */
 #define SCIP_DEFAULT_RANDOM_PERMUTECONSS   TRUE /**< should order of constraints be permuted (depends on permutationseed)? */
@@ -2233,6 +2234,12 @@ SCIP_RETCODE SCIPsetCreate(
          "randomization/randomseedshift",
          "global shift of all random seeds in the plugins and the LP random seed",
          &(*set)->random_randomseedshift, FALSE, SCIP_DEFAULT_RANDOM_RANDSEEDSHIFT, 0, INT_MAX,
+         NULL, NULL) );
+
+   SCIP_CALL( SCIPsetAddIntParam(*set, messagehdlr, blkmem,
+         "randomization/randomseedshiftmultiplier",
+         "multiplier for global shift of all random seeds in the plugins and the LP random seed; this multiplier will be changed with every SCIP major release",
+         &(*set)->random_randomseedshiftmultiplier, FALSE, SCIP_DEFAULT_RANDOM_RANDSEEDSHIFTMULT, 1, INT_MAX,
          NULL, NULL) );
 
    SCIP_CALL( SCIPsetAddIntParam(*set, messagehdlr, blkmem,
@@ -7800,5 +7807,5 @@ unsigned int SCIPsetInitializeRandomSeed(
 {
    assert(set != NULL);
 
-   return (unsigned int)(initialseedvalue + (unsigned) set->random_randomseedshift);
+   return (unsigned int)(initialseedvalue + (unsigned) set->random_randomseedshiftmultiplier * (unsigned) set->random_randomseedshift);
 }
