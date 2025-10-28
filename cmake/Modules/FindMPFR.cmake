0c84837f5cc1051692fe414499e815dc2de22990
Stefan Vigerske <svigerske@gams.com>
2025-10-28 09:38:55 +0100

    fix parsing of MPFR version from mpfr.h
    - surprisingly, MPFR is not using the GMP macro names
    - could also just look at MPFR_VERSION_STRING

--- cmake/Modules/FindMPFR.cmake
+++ cmake/Modules/FindMPFR.cmake
@@ -43,18 +43,18 @@ if(NOT MPFR_LIBRARIES)
    SET(MPFR_LIBRARIES ${MPFR_LIBRARY})
 endif()
 
-file(GLOB MPFR_HEADERS "${MPFR_INCLUDE_DIRS}/mpfr.h" "${MPFR_INCLUDE_DIRS}/mpfr-*.h")
+file(GLOB MPFR_HEADERS "${MPFR_INCLUDE_DIRS}/mpfr.h")
 foreach (mpfr_header_filename ${MPFR_HEADERS})
     file(READ "${mpfr_header_filename}" _mpfr_version_header)
     string(REGEX MATCH
-            "define[ \t]+__GNU_MP_VERSION[ \t]+([0-9]+)" _mpfr_major_version_match
+            "define[ \t]+MPFR_VERSION_MAJOR[ \t]+([0-9]+)" _mpfr_major_version_match
             "${_mpfr_version_header}")
     if (_mpfr_major_version_match)
         set(MPFR_MAJOR_VERSION "${CMAKE_MATCH_1}")
-        string(REGEX MATCH "define[ \t]+__GNU_MP_VERSION_MINOR[ \t]+([0-9]+)"
+        string(REGEX MATCH "define[ \t]+MPFR_VERSION_MINOR[ \t]+([0-9]+)"
                 _mpfr_minor_version_match "${_mpfr_version_header}")
         set(MPFR_MINOR_VERSION "${CMAKE_MATCH_1}")
-        string(REGEX MATCH "define[ \t]+__GNU_MP_VERSION_PATCHLEVEL[ \t]+([0-9]+)"
+        string(REGEX MATCH "define[ \t]+MPFR_VERSION_PATCHLEVEL[ \t]+([0-9]+)"
                 _mpfr_patchlevel_version_match "${_mpfr_version_header}")
         set(MPFR_PATCHLEVEL_VERSION "${CMAKE_MATCH_1}")
         set(MPFR_VERSION
