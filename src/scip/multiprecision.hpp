45c30100c55256b66df79c1cfdab53809f6a0454
Ambros Gleixner <gleixner@zib.de>
2025-11-12 16:04:29 +0100

    add operator= for double to ratinal dummy class

--- src/scip/multiprecision.hpp
+++ src/scip/multiprecision.hpp
@@ -81,6 +81,7 @@ namespace scip
       inline int sign() const { return 0; }
       inline bool is_zero() const { return this->val == 0; }
       inline std::string str() const { return ""; }
+      Rational& operator=(const double& r){val = r; return *this;};
       Rational& operator=(const int& i){val = i; return *this;};
       Rational& operator+(const Rational& r){return *this;};
       Rational& operator+(const double& r){return *this;};
