d22c17ecfccb6fc141cce21216bf4d11af733895
Christopher Hojny <c.hojny@tue.nl>
2025-11-16 18:42:58 +0100

    removed unused section

--- src/scip/struct_set.h
+++ src/scip/struct_set.h
@@ -675,9 +675,6 @@ struct SCIP_Set
    char*                 certificate_filename;/**< name of the certificate file, or "-" if no output should be created */
    SCIP_Real             certificate_maxfilesize;/**< maximum size of the certificate file in MB (stop printing when reached) */
 
-   /* symmetry settings */
-   
-
    /* Reading */
    SCIP_Bool             read_initialconss;  /**< should model constraints be marked as initial? */
    SCIP_Bool             read_dynamicconss;  /**< should model constraints be subject to aging? */
