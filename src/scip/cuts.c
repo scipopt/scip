add24da2b0476c1a4e88705a8391109122b6d211
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2025-12-15 08:41:21 +0100

    Remove the SCIP_ prefix from default cutgen params

--- src/scip/cuts.c
+++ src/scip/cuts.c
@@ -67,15 +67,15 @@
 /*
  * Default values for cut generation parameters
  */
-#define SCIP_DEFAULT_CUTGEN_POSTPROCESS      TRUE   /**< default for applying post-processing */
-#define SCIP_DEFAULT_CUTGEN_BOUNDSWITCH      0.5    /**< default fraction of domain for bound switching */
-#define SCIP_DEFAULT_CUTGEN_ALLOWLOCAL       TRUE   /**< default for allowing local cuts */
-#define SCIP_DEFAULT_CUTGEN_VARTYPEUSEVBDS   2      /**< default variable types for varbound substitution */
-#define SCIP_DEFAULT_CUTGEN_MINFRAC          0.05   /**< default minimal fractionality of rhs */
-#define SCIP_DEFAULT_CUTGEN_MAXFRAC          0.999  /**< default maximal fractionality of rhs */
-#define SCIP_DEFAULT_CUTGEN_SCALE            1.0    /**< default scaling factor */
-#define SCIP_DEFAULT_CUTGEN_MAXTESTDELTA     (-1)   /**< default max deltas to test (-1 = unlimited) */
-#define SCIP_DEFAULT_CUTGEN_FIXINTEGRALRHS   TRUE   /**< default for fixing integral rhs */
+#define DEFAULT_CUTGEN_POSTPROCESS      TRUE   /**< default for applying post-processing */
+#define DEFAULT_CUTGEN_BOUNDSWITCH      0.5    /**< default fraction of domain for bound switching */
+#define DEFAULT_CUTGEN_ALLOWLOCAL       TRUE   /**< default for allowing local cuts */
+#define DEFAULT_CUTGEN_VARTYPEUSEVBDS   2      /**< default variable types for varbound substitution */
+#define DEFAULT_CUTGEN_MINFRAC          0.05   /**< default minimal fractionality of rhs */
+#define DEFAULT_CUTGEN_MAXFRAC          0.999  /**< default maximal fractionality of rhs */
+#define DEFAULT_CUTGEN_SCALE            1.0    /**< default scaling factor */
+#define DEFAULT_CUTGEN_MAXTESTDELTA     (-1)   /**< default max deltas to test (-1 = unlimited) */
+#define DEFAULT_CUTGEN_FIXINTEGRALRHS   TRUE   /**< default for fixing integral rhs */
 
 /* =========================================== general static functions =========================================== */
 #ifdef SCIP_DEBUG
@@ -13639,17 +13639,17 @@ void SCIPinitCutGenParams(
 {
    assert(params != NULL);
 
-   params->postprocess = SCIP_DEFAULT_CUTGEN_POSTPROCESS;
-   params->boundswitch = SCIP_DEFAULT_CUTGEN_BOUNDSWITCH;
-   params->allowlocal = SCIP_DEFAULT_CUTGEN_ALLOWLOCAL;
-   params->vartypeusevbds = SCIP_DEFAULT_CUTGEN_VARTYPEUSEVBDS;
-   params->minfrac = SCIP_DEFAULT_CUTGEN_MINFRAC;
-   params->maxfrac = SCIP_DEFAULT_CUTGEN_MAXFRAC;
-   params->scale = SCIP_DEFAULT_CUTGEN_SCALE;
-   params->maxtestdelta = SCIP_DEFAULT_CUTGEN_MAXTESTDELTA;
+   params->postprocess = DEFAULT_CUTGEN_POSTPROCESS;
+   params->boundswitch = DEFAULT_CUTGEN_BOUNDSWITCH;
+   params->allowlocal = DEFAULT_CUTGEN_ALLOWLOCAL;
+   params->vartypeusevbds = DEFAULT_CUTGEN_VARTYPEUSEVBDS;
+   params->minfrac = DEFAULT_CUTGEN_MINFRAC;
+   params->maxfrac = DEFAULT_CUTGEN_MAXFRAC;
+   params->scale = DEFAULT_CUTGEN_SCALE;
+   params->maxtestdelta = DEFAULT_CUTGEN_MAXTESTDELTA;
    params->boundsfortrans = NULL;
    params->boundtypesfortrans = NULL;
-   params->fixintegralrhs = SCIP_DEFAULT_CUTGEN_FIXINTEGRALRHS;
+   params->fixintegralrhs = DEFAULT_CUTGEN_FIXINTEGRALRHS;
 }
 
 /** tries multiple cut generation methods on an aggregation row and returns the best cut by efficacy */
