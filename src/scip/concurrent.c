65549725d44109f1a2510fda1edf1ea9cd60ad2f
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2025-12-27 12:39:28 +0100

    fix typo in documentation

--- src/scip/concurrent.c
+++ src/scip/concurrent.c
@@ -378,8 +378,7 @@ SCIP_Longint SCIPgetConcurrentNTightenedIntBnds(
    return scip->concurrent->propsync != NULL ? SCIPpropSyncGetNTightenedIntBnds(scip->concurrent->propsync) : 0;
 }
 
-/** pass a solution to the given SCIP instance using that was received via synchronization by using
- * the sync heuristic */
+/** pass a solution to the given SCIP instance that was received via synchronization by using the sync heuristic */
 SCIP_RETCODE SCIPaddConcurrentSol(
    SCIP*                 scip,               /**< SCIP datastructure */
    SCIP_SOL*             sol                 /**< solution */
