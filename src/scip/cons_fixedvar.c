05a22101f32f4a6f731e08921b17fa55e0a82b7d
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2025-12-31 13:16:01 +0100

    fix info message

--- src/scip/cons_fixedvar.c
+++ src/scip/cons_fixedvar.c
@@ -512,7 +512,7 @@ SCIP_DECL_CONSCHECK(consCheckFixedvar)
 
          if( printreason )
          {
-            SCIPinfoMessage(scip, NULL, "solution violates lower bound of fixed variable <%s> [%g,%g], solution value <%g>\n",
+            SCIPinfoMessage(scip, NULL, "solution violates lower bound of non-active variable <%s> [%g,%g], solution value <%g>\n",
                SCIPvarGetName(vars[i]), lb, ub, val);
          }
 
@@ -532,7 +532,7 @@ SCIP_DECL_CONSCHECK(consCheckFixedvar)
 
          if( printreason )
          {
-            SCIPinfoMessage(scip, NULL, "solution violates upper bound of fixed variable <%s> [%g,%g], solution value <%g>\n",
+            SCIPinfoMessage(scip, NULL, "solution violates upper bound of non-active variable <%s> [%g,%g], solution value <%g>\n",
                SCIPvarGetName(vars[i]), lb, ub, val);
          }
 
