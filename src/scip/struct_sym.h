e8b75806049ddeaac43e3877fc90f5f466c17efa
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2026-02-17 14:35:00 +0100

    remove const to avoid warnings

--- src/scip/struct_sym.h
+++ src/scip/struct_sym.h
@@ -47,7 +47,7 @@ struct SCIP_SymComp
 {
    SCIP_SYMHDLR*         symhdlr;            /**< symmetry handler for this symmetry component */
    SCIP_SYMCOMPDATA*     symcompdata;        /**< data of symmetry component */
-   const char*           name;               /**< name of symmetry component */
+   char*                 name;               /**< name of symmetry component */
 };
 
 /** symmetry handler */
