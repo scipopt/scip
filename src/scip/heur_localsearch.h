9c426b6d93d25873c9035b5e9609568c66ff4805
Dominik Kamp <kamp@zib.de>
2026-08-03 17:19:10 +0200

    Complete base reference

--- src/scip/heur_localsearch.h
+++ src/scip/heur_localsearch.h
@@ -50,6 +50,10 @@
  *  Peng Lin, Shaowei Cai, Mengchuan Zou, Jinkun Lin.
  *  "Local-MIP: Efficient Local Search for Mixed Integer Programming"
  *  Artificial Intelligence, Volume 348, 2025, 104405.
+ *
+ *  Peng Lin, Mengchuan Zou, Shaowei Cai.
+ *  "An Efficient Local Search Solver for Mixed Integer Programming"
+ *  Proceedings of the 30th International Conference on Principles and Practice of Constraint Programming (CP 2024).
  */
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
