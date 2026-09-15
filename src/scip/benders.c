4d491d0e96f889d883cce885951d860916dedbbd
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2026-09-15 15:44:48 +0200

    Revert "streamline some initializations; add asserts"
    This reverts commit c873466804320d26aae2b14c4ef7f6e6616033ec.

--- src/scip/benders.c
+++ src/scip/benders.c
@@ -3468,9 +3468,6 @@ SCIP_RETCODE solveBendersSubproblems(
 
    assert(benders != NULL);
    assert(set != NULL);
-   assert(infeasible != NULL);
-   assert(optimal != NULL);
-   assert(stopped != NULL);
 
    /* in the case of an LNS check, only the convex relaxations of the subproblems will be solved. This is a performance
     * feature, since solving the convex relaxation is typically much faster than solving the corresponding CIP. While
@@ -4435,9 +4432,8 @@ SCIP_RETCODE executeUserDefinedSolvesub(
    assert(benders != NULL);
    assert(probnumber >= 0 && probnumber < benders->nsubproblems);
    assert(benders->benderssolvesubconvex != NULL || benders->benderssolvesub != NULL);
+
    assert(solveloop == SCIP_BENDERSSOLVELOOP_USERCONVEX || solveloop == SCIP_BENDERSSOLVELOOP_USERCIP);
-   assert(objective != NULL);
-   assert(result != NULL);
 
    (*objective) = -SCIPsetInfinity(set);
 
@@ -4512,7 +4508,6 @@ SCIP_RETCODE SCIPbendersExecSubproblemSolve(
 
    assert(benders != NULL);
    assert(probnumber >= 0 && probnumber < benders->nsubproblems);
-   assert(solved != NULL);
 
    SCIPsetDebugMsg(set, "Benders' decomposition: solving subproblem %d\n", probnumber);
 
@@ -5406,6 +5401,8 @@ SCIP_Bool SCIPbendersSubproblemIsOptimal(
    assert(benders != NULL);
    assert(set != NULL);
    assert(probnumber >= 0 && probnumber < benders->nsubproblems);
+
+   optimal = FALSE;
 
    optimal = FALSE;
 
